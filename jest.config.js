d1955778a3557691e83b6f3b9d5f936efc25a4f6
Jim Cresswell <jim.cresswell@gmail.com>
2022-02-24 11:27:10 +0000

    chore: convert jest config back to js so it works in CI

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 // jest.config.js
-import nextJest from "next/jest";
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -11,11 +11,11 @@ const customJestConfig = {
   collectCoverage: true,
   collectCoverageFrom: ["./src/**", "!**/__snapshots__/**"],
   // Add more setup options before each test is run
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(customJestConfig);
+module.exports = createJestConfig(customJestConfig);
