c1c6f56fc61d819ab298a51f75835ed0eb8b2d82
Jim Cresswell <jim.cresswell@gmail.com>
2022-02-24 12:10:01 +0000

    test: increase test coverage

---
+++ src/pages/api/hello.test.ts
@@ -0,0 +1,21 @@
+import handler from "./hello";
+
+describe("hello api", () => {
+  it("has a handler", () => {
+    const spy = jest.fn();
+    const req = {};
+    const res = {
+      status: () => {
+        return {
+          json: spy,
+        };
+      },
+    };
+
+    /*eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+    // @ts-ignore
+    handler(req, res);
+
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
