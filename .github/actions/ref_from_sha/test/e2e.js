e7d25d7e4d7d5e67236d0968aa19fa33a3421411
Jim Cresswell <jim.cresswell@gmail.com>
2022-02-25 16:48:28 +0000

    chore: tidy up

--- .github/actions/ref_from_sha/test/e2e.js
+++ .github/actions/ref_from_sha/test/e2e.js
@@ -1,6 +1,6 @@
 const { Octokit } = require("octokit");
 
-const prFromSha = require("./pr_from_sha");
+const prFromSha = require("../pr_from_sha");
 
 if (!process.env.GITHUB_TOKEN) {
   throw new Error("Need Github token");
@@ -12,7 +12,7 @@ const repoInfo = {
   repo: "samara",
 };
 
-// Set this to the head commit on a branch in a PR
+// Set this to the head commit on a branch in a PR, short SHAs are fine.
 const sha = "c6458f3";
 
 prFromSha(octokit, repoInfo, sha).then((pr) => {
