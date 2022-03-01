ce2998b8fa579e571274e5c9703d4dd2ab6a4f17
mantagen <contact.mantagen@gmail.com>
2022-03-01 10:46:45 +0000

    chore: tightens up import/order rules #14

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
+import styles from "../styles/Home.module.css";
 import Main from "../components/Main";
 
 const Home: NextPage = () => {
