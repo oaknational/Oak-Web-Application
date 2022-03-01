b588e78c391c6f95a684b17ffa7939799fe9d9e9
mantagen <contact.mantagen@gmail.com>
2022-03-01 11:10:19 +0000

    build: check types on commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -5,6 +5,12 @@ const buildEslintCommand = (filenames) =>
     .map((f) => path.relative(process.cwd(), f))
     .join(" --file ")}`;
 
+// https://github.com/okonet/lint-staged/issues/968#issuecomment-1011403980
+const checkTypes = [
+  "sh -c 'git stash push --message pre-tsc --keep-index --include-untracked'",
+  "sh -c 'yarn tsc --pretty; STATUS=$?; git stash pop --quiet; exit $STATUS'",
+];
+
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": [buildEslintCommand, ...checkTypes],
 };
