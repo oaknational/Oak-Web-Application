690ac5c516637e4aa34a2947fcec19705cb5bb62
mantagen <contact.mantagen@gmail.com>
2022-03-01 10:16:21 +0000

    test: adds node-mocks-http

--- src/__tests__/pages/api/hello.test.ts
+++ src/__tests__/pages/api/hello.test.ts
@@ -1,19 +1,18 @@
+import { createMocks } from "node-mocks-http";
 import handler from "../../../pages/api/hello";
 
 describe("hello api", () => {
   it("has a handler", () => {
     const spy = jest.fn();
-    const req = {};
-    const res = {
-      status: () => {
-        return {
-          json: spy,
-        };
-      },
+
+    const { req, res } = createMocks();
+
+    res.status = () => {
+      return {
+        json: spy,
+      };
     };
 
-    /*eslint-disable-next-line @typescript-eslint/ban-ts-comment */
-    // @ts-ignore
     handler(req, res);
 
     expect(spy).toHaveBeenCalledTimes(1);
