9e969de8a5c20e2bbec4a3de869e1ca35b223e1a
Jim Cresswell <jim.cresswell@gmail.com>
2022-03-02 11:58:13 +0000

    test: update code coverage config

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,11 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   collectCoverage: true,
-  collectCoverageFrom: ["./src/**", "!**/__snapshots__/**"],
+  collectCoverageFrom: [
+    "./src/**",
+    "!**/__snapshots__/**",
+    "!src/pages/_document.tsx",
+  ],
   // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
