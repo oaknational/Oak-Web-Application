e1d4e1299fd0176be63ad0fd7c9769cf85e62d77
Jim Cresswell <jim.cresswell@gmail.com>
2022-03-04 15:53:09 +0000

    chore(pa11y): support object version of URL definition

--- pa11yci.config.js
+++ pa11yci.config.js
@@ -15,6 +15,14 @@ const baseUrl = process.env.BASE_URL || "http://localhost:3000";
 // URLs should end with a `/` to avoid redirects from
 // e.g. `/unit` to `/unit/index.html` during tests.
 const relativeUrls = ["/"];
-config.urls = relativeUrls.map((relUrl) => new URL(relUrl, baseUrl).href);
+// Add the base URL to the relative URLs.
+config.urls = relativeUrls.map((relUrl) => {
+  if (typeof relUrl === "string") {
+    return new URL(relUrl, baseUrl).href;
+  } else {
+    relUrl.url = new URL(relUrl.url, baseUrl).href;
+    return relUrl;
+  }
+});
 
 module.exports = config;
