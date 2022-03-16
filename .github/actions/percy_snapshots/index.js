3d5ea73d30dfeb520d801b3d2a4160355dcecda3
Jim Cresswell <jim.cresswell@gmail.com>
2022-03-16 17:15:10 +0000

    chore: start adding Percy plumbing

---
+++ .github/actions/percy_snapshots/index.js
@@ -0,0 +1,54 @@
+/**
+ * Given a base URL, run some Percy snapshots against it using
+ * the Percy CLI.
+ */
+
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+const setEnvVariables = (envVars) => {
+  core.debug(`
+  Percy envVars:
+  ${JSON.stringify(envVars, null, 2)}
+  `);
+
+  for (const [key, value] of Object.entries(envVars)) {
+    core.exportVariable(key, value);
+  }
+};
+
+async function run() {
+  try {
+    const baseUrl = core.getInput("base_url");
+    const branchName = core.getInput("branch_name");
+    const prNumber = core.getInput("pr_number");
+
+    // Presumably the Percy CLI picks up the SHA from the GITHUB_SHA variable.
+    setEnvVariables({
+      PERCY_BASE_URL: baseUrl,
+      // Need to figure this out because the Vercel deployment events lack a proper `ref`,
+      // and Percy needs to know what branch we are comparing to the baseline.
+      PERCY_BRANCH: branchName,
+      // We should either be in a PR or on the `main` branch.
+      // We need the PR info to report the findings back to
+      // the right PR UI.
+      PERCY_PULL_REQUEST: prNumber || undefined,
+      // Should always be main, no need to override.
+      // PERCY_TARGET_BRANCH: 'main',
+      // The percy token should already be set as a secret and available to the CLI.
+      // PERCY_TOKEN: percyTokenForWorkspace,
+    });
+
+    await exec.exec(
+      "npx percy snapshot percy.snapshot.list.js -c percy.config.js "
+    );
+
+    // The script ran to completion, a success state for this action
+    // will be set. The actual snapshot states will be set by Percy itself.
+  } catch (error) {
+    core.error(error);
+    core.setFailed(error.message);
+  }
+}
+
+run();
---
+++ percy.config.js
@@ -0,0 +1,18 @@
+/**
+ * Configure Percy
+ * https://docs.percy.io/docs/cli-configuration#a-complete-config
+ *
+ * Note: Percy is triggered in Github workflows, the logic is here .github/actions/percy_snapshots/index.js
+ */
+module.exports = {
+  version: 2,
+  snapshot: {
+    widths: [375, 1280],
+    minHeight: 1024,
+    percyCss: "",
+  },
+  discovery: {
+    allowedHostnames: [],
+    userAgent: "Percy",
+  },
+};
