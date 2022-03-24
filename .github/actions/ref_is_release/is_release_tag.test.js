52bc71abba3db14f2b0f8873538dc9c5131fe2ec
Jim Cresswell <jim.cresswell@gmail.com>
2022-03-24 15:58:57 +0000

    chore(semantic release): replace Vercel auto-deploys with Github action driven Vercel CLI deploys
    so that we can drive production commits off release tags rather than commits to the 'main' branch

---
+++ .github/actions/ref_is_release/is_release_tag.test.js
@@ -0,0 +1,17 @@
+const isReleaseTag = require("./is_release_tag");
+
+const branchRef = "refs/heads/some_branch";
+const nonReleaseTagRef = "refs/tags/some_tag";
+const prodReleaseTag = "refs/tags/v12.3.456";
+
+describe("isReleaseTag is", () => {
+  it("false for branches", () => {
+    expect(isReleaseTag(branchRef)).toBe(false);
+  });
+  it("false for non-release tags", () => {
+    expect(isReleaseTag(nonReleaseTagRef)).toBe(false);
+  });
+  it("true for release tags", () => {
+    expect(isReleaseTag(prodReleaseTag)).toBe(true);
+  });
+});
