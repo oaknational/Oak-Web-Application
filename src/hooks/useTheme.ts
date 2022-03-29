16e97277436b95cf0710601d06796b2bce5d4b88
mantagen <contact.mantagen@gmail.com>
2022-03-29 12:27:00 +0100

    test: add tests for hooks useEventListener useLocalStorage useTheme

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -4,36 +4,46 @@ import { Theme, defaultTheme, ausTheme } from "../styles/themes";
 
 import useLocalStorage, { dispatchLocalStorageEvent } from "./useLocalStorage";
 
+export type WindowOakThemes = {
+  setTheme: (themeName: ThemeName) => void;
+  availableThemes: ThemeNames;
+};
+declare global {
+  interface Window {
+    oakThemes?: WindowOakThemes;
+  }
+}
+
 // @TODO move to centralised local storage keys list
 const LS_KEY_THEME = "theme";
 
 const themeNames = ["default", "aus"] as const;
-type ThemeName = typeof themeNames[number];
+type ThemeNames = typeof themeNames;
+type ThemeName = ThemeNames[number];
 
 const themes: Record<ThemeName, Theme> = {
   default: defaultTheme,
   aus: ausTheme,
 };
 
-if (typeof window !== "undefined") {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  window.oakThemes = {
-    setTheme: (themeName: ThemeName) => {
-      if (!themeNames.includes(themeName)) {
-        return console.error(
-          `Theme name must be one of: ${themeNames.join(", ")}`
-        );
-      }
-      window.localStorage.setItem(LS_KEY_THEME, JSON.stringify(themeName));
-      dispatchLocalStorageEvent();
-    },
-    availableThemes: themeNames,
-  };
-}
 const useTheme = () => {
   const [selectedTheme] = useLocalStorage<ThemeName>(LS_KEY_THEME, "default");
   const theme = themes[selectedTheme];
+
+  useEffect(() => {
+    window.oakThemes = {
+      setTheme: (themeName: ThemeName) => {
+        if (!themeNames.includes(themeName)) {
+          return console.error(
+            `Theme name must be one of: ${themeNames.join(", ")}`
+          );
+        }
+        window.localStorage.setItem(LS_KEY_THEME, JSON.stringify(themeName));
+        dispatchLocalStorageEvent();
+      },
+      availableThemes: themeNames,
+    };
+  }, []);
 
   useEffect(() => {
     if (!theme) {
