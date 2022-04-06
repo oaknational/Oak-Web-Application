3969e276b43ca31ac91d3031207b9adea45885b6
Jim Cresswell <jim.cresswell@gmail.com>
2022-04-06 16:34:09 +0100

    feat(app versioning): determine app version and release stage at build time

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en-GB">
-      <Head />
+      <Head>
+        <meta
+          name="release-stage"
+          content={process.env.NEXT_PUBLIC_RELEASE_STAGE}
+        />
+        <meta name="revised" content={new Date().toUTCString()} />
+        <meta name="version" content={process.env.NEXT_PUBLIC_APP_VERSION} />
+      </Head>
       <body>
         <Main />
         <NextScript />
