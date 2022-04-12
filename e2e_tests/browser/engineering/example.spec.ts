846593923999347c8bcc0fc67afebef1b616a870
Jim Cresswell <jim.cresswell@gmail.com>
2022-04-12 12:13:54 +0100

    test(playwright): working test against local server

---
+++ e2e_tests/browser/engineering/example.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+// Because of config in "playwright.config.ts",
+// `PLAYWRIGHT_TEST_BASE_URL` will be `localhost:3000` in local test environments.
+// The string default is to keep Typescript happy.
+const frontPageUrl =
+  process.env.BASE_URL ||
+  process.env.PLAYWRIGHT_TEST_BASE_URL ||
+  "localhost:3000";
+
+const locators = {
+  lessonTitle: "[data-test-id='lesson-title']",
+};
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(frontPageUrl);
+});
+
+test.describe("Does Playwright work", () => {
+  test("we can do things on the front page", async ({ page }) => {
+    await expect(page.locator(locators.lessonTitle)).toHaveText([
+      "Physics only review",
+    ]);
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -19,7 +19,10 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  testPathIgnorePatterns: ["src/__tests__/__helpers__/*"]
+  testPathIgnorePatterns: [
+    "src/__tests__/__helpers__/*",
+    "e2e_tests/browser/engineering/*",
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
