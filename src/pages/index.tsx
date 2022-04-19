09e8302932d5f0067cde5bb36d5757fa83c12a23
Joe Baker <blakeage77@googlemail.com>
2022-04-19 11:06:23 +0100

    chore: remove example from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import api from "../data-layer/graphql/api";
 import Layout from "../components/Layout";
 import { useLessonsBySlugQuery } from "../data-layer/graphql/generated/apollo";
-import Text from "../components/Typography/Typography";
 
 const Home: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (
   props
@@ -22,13 +21,6 @@ const Home: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (
     <Layout>
       <h1 data-test-id="lesson-title">{title}</h1>
       <p>Status: {loading ? "loading" : "idle"}</p>
-
-      <Text semanticVariant="h2">h2 - Heading</Text>
-      <Text semanticVariant="h3">h3 - Heading</Text>
-      <Text semanticVariant="h4">h4 - Heading</Text>
-      <Text semanticVariant="body1">body 1</Text>
-      <Text semanticVariant="body2">body 2</Text>
-      <Text semanticVariant="body3">body 3</Text>
     </Layout>
   );
 };
