04cc67a302cb45c230bcc67802a5ffd1e9101fbb
mantagen <contact.mantagen@gmail.com>
2022-04-20 17:59:30 +0100

    test(bookmarks): add bookmark tests

---
+++ src/__tests__/__helpers__/LocalStorageMock.ts
@@ -0,0 +1,26 @@
+class LocalStorageMock {
+  store: Record<string, unknown> = {};
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key: string) {
+    return this.store[key] || null;
+  }
+
+  setItem(key: string, value: unknown) {
+    this.store[key] = value + "";
+  }
+
+  removeItem(key: string) {
+    delete this.store[key];
+  }
+}
+
+Object.defineProperty(window, "localStorage", {
+  value: new LocalStorageMock(),
+});
+
+// To ensure file is a module
+export {}
--- src/__tests__/__helpers__/apolloMocks.ts
+++ src/__tests__/__helpers__/apolloMocks.ts
@@ -4,15 +4,27 @@ import {
   BookmarkedLessonRemoveDocument,
 } from "../../browser-lib/graphql/generated/apollo";
 
-export const testLesson = {
-  id: 1,
-  slug: "physics-only-review-chj3cd",
-  title: "Physics only review",
-};
-export const testBookmark = {
-  lesson: testLesson,
+export const testLessons = [
+  {
+    id: 1,
+    slug: "physics-only-review-chj3cd",
+    title: "Physics only review",
+  },
+  {
+    id: 2,
+    slug: "macbeth-chj3cd",
+    title: "Macbeth",
+  },
+];
+
+export const testBookmarks = testLessons.map((lesson) => ({
+  lesson,
   created_at: new Date().toISOString(),
-};
+}));
+
+export const bookmarkToAdd = testBookmarks[0];
+export const bookmarkToRemove = testBookmarks[1];
+
 const apolloMocks = [
   {
     request: {
@@ -20,29 +32,32 @@ const apolloMocks = [
     },
     result: {
       data: {
-        bookmarked_lesson: [testBookmark],
+        bookmarked_lesson: testBookmarks,
       },
     },
   },
   {
     request: {
       query: BookmarkedLessonAddDocument,
-      variables: { lessonId: 1 },
+      variables: { lessonId: bookmarkToAdd?.lesson.id },
     },
     result: {
       data: {
-        insert_bookmarked_lesson_one: testBookmark,
+        insert_bookmarked_lesson_one: testBookmarks[0],
       },
     },
   },
   {
     request: {
       query: BookmarkedLessonRemoveDocument,
-      variables: { lessonId: 1, userId: 123 },
+      variables: { lessonId: bookmarkToAdd?.lesson.id, userId: 123 },
     },
     result: {
       data: {
-        delete_bookmarked_lesson_by_pk: {},
+        delete_bookmarked_lesson_by_pk: {
+          lesson_id: bookmarkToAdd?.lesson.id,
+          user_id: 123,
+        },
       },
     },
   },
