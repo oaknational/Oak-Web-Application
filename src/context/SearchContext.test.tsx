ac8dd5cf687b79d1f959b9ee3255de6f242f2737
mantagen <contact.mantagen@gmail.com>
2022-04-20 16:16:06 +0100

    test(search-context): add search-context tests and fix search form tets

---
+++ src/context/SearchContext.test.tsx
@@ -0,0 +1,36 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+
+import { KeyStage, SearchProvider, useSearchQuery } from "./SearchContext";
+
+describe("context/SearchContext.tsx", () => {
+  it("should default searchQuery.text to empty string", () => {
+    const { result } = renderHook(useSearchQuery, { wrapper: SearchProvider });
+    const { text } = result.current;
+    expect(text).toEqual(text);
+  });
+  it("should update text with setText", async () => {
+    const { result } = renderHook(useSearchQuery, { wrapper: SearchProvider });
+
+    const searchText = "macb";
+    await act(async () => {
+      await result.current.setText(searchText);
+    });
+
+    expect(result.current.text).toEqual(searchText);
+  });
+  it("should default searchQuery.keyStages to empty set", () => {
+    const { result } = renderHook(useSearchQuery, { wrapper: SearchProvider });
+    const { keyStages } = result.current;
+    expect(keyStages).toEqual(new Set());
+  });
+  it("should update keyStages with setKeyStages", async () => {
+    const { result } = renderHook(useSearchQuery, { wrapper: SearchProvider });
+
+    const keyStages = new Set<KeyStage>(["foundation"]);
+    await act(async () => {
+      await result.current.setKeyStages(keyStages);
+    });
+
+    expect(result.current.keyStages).toEqual(keyStages);
+  });
+});
