d3e171ab07714db8e02d05208d04ab63e4fbb008
Joe Baker <blakeage77@googlemail.com>
2022-04-21 15:15:11 +0100

    fix: remove axe a11y accessibility tests

--- .storybook/storybook_a11y.test.js
+++
@@ -1,6 +0,0 @@
-// From https://www.npmjs.com/package/@storybook/addon-storyshots-puppeteer
-
-import initStoryshots from "@storybook/addon-storyshots";
-import { axeTest } from "@storybook/addon-storyshots-puppeteer";
-jest.retryTimes(3);
-initStoryshots({ suite: "A11y checks", test: axeTest() });
