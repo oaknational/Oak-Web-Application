c9afd3cf1c715435be10f73238e9c92f68a977c5
Joe Baker <blakeage77@googlemail.com>
2022-05-17 12:36:59 +0100

    feat: add card component to storybook

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import cardImage from "../../../public/Image.png";
-
 import Card from "./Card";
 import CardText from "./CardText";
 import CardButton from "./CardButton";
 import CardContainer from "./CardContainer";
-import CardImage from "./CardImage";
+import CardImageButtonVariant from "./CardImageButtonVariant";
 
 export default {
   title: "Components/Card",
@@ -22,42 +20,51 @@ const Template: ComponentStory<typeof Card> = () => (
       <h2>Card title</h2>
       <p>You can put anything you like in here.</p>
     </Card>
-    <div style={{ width: "308px" }}>
-      <div>
-        <CardContainer>
-          <CardImage imageUrl={cardImage} />
-          <Card radius={false}>
-            <CardText>Title</CardText>
-            <CardText variant={"body3"}>
-              Short snappy description of what this card is about.
-            </CardText>
-            <CardButton href="/" label="Label" />
-          </Card>
-        </CardContainer>
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr 1fr ",
+        gap: "12px",
+        paddingTop: "12px",
+      }}
+    >
+      <CardImageButtonVariant
+        title={"Title"}
+        text={"CardImageButtonVariant component."}
+        buttonHref={"/"}
+        buttonLabel={"Label"}
+      />
 
-        <p>-----</p>
-
-        <Card>
-          <CardText alignCenter icon="Star" iconPosition="aboveTitle">
-            Title
-          </CardText>
-          <CardText alignCenter variant={"body3"}>
+      <CardContainer>
+        {/* <CardImage imageUrl={cardImage} width={308} height={74} /> */}
+        <Card radius={false}>
+          <CardText>Title</CardText>
+          <CardText variant={"body3"}>
             Short snappy description of what this card is about.
           </CardText>
           <CardButton href="/" label="Label" />
         </Card>
-        <p>---</p>
-        <Card>
-          <CardText icon="Download" iconPosition="before">
-            Need some help?
-          </CardText>
-          <CardText variant={"body3"}>
-            Preview, plan and customise each element of our lessons to meet your
-            needs - whether inside and outside the classroom.
-          </CardText>
-          <CardButton href="/" label="Label" />
-        </Card>
-      </div>
+      </CardContainer>
+      <Card>
+        <CardText icon="Download" iconPosition="before">
+          Need some help?
+        </CardText>
+        <CardText variant={"body3"}>
+          Preview, plan and customise each element of our lessons to meet your
+          needs - whether inside and outside the classroom.
+        </CardText>
+        <CardButton href="/" label="Label" />
+      </Card>
+
+      <Card>
+        <CardText alignCenter icon="Star" iconPosition="aboveTitle">
+          Title
+        </CardText>
+        <CardText alignCenter variant={"body3"}>
+          Short snappy description of what this card is about.
+        </CardText>
+        <CardButton href="/" label="Label" />
+      </Card>
     </div>
   </div>
 );
--- src/components/Card/CardImage.tsx
+++ src/components/Card/CardImage.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import Image, { StaticImageData } from "next/image";
-// import imageUrl from "../../../public/Image.png";
 
 import styles from "./CardImage.module.css";
 
@@ -8,15 +7,19 @@ export type ImageUrlType = string | StaticImageData;
 
 interface CardImageProps {
   imageUrl: ImageUrlType;
+  width?: number;
+  height?: number;
 }
 
-const CardImage: FC<CardImageProps> = ({ imageUrl }) => {
+const CardImage: FC<CardImageProps> = ({ imageUrl, width, height }) => {
   return (
     <div className={styles.imageWrapper}>
       <Image
-        className={styles.image}
+        // className={styles.image}
         layout={"responsive"}
         src={imageUrl}
+        width={width}
+        height={height}
       ></Image>
     </div>
   );
