57474e1cb187143fbc8867bd0c0faaca046d70a2
mantagen <contact.mantagen@gmail.com>
2022-05-25 12:32:56 +0100

    test: input component

---
+++ src/components/Input/Input.test.tsx
@@ -0,0 +1,15 @@
+import { screen } from "@testing-library/react";
+
+import renderWithProviders from "../../__tests__/__helpers__/renderWithProviders";
+
+import Input from "./Input";
+
+describe("Input", () => {
+  it("renders an input", () => {
+    renderWithProviders(<Input value="Hello world" />);
+
+    const input = screen.getByRole("textbox");
+
+    expect(input).toBeInTheDocument();
+  });
+});
