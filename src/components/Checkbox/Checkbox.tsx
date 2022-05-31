ea5fe4b3f6b5c76a71546a5d950d3a4b4fd48f44
craigmccahill <craig.mccahill@gmail.com>
2022-05-31 11:52:23 +0100

    feat: create simple unstyled html checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,5 +1,18 @@
-const Checkbox = () => {
-  return <span>foo</span>;
+import { FC } from "react";
+// import styled from "styled-components";
+
+interface CheckboxProps {
+  labelText: string;
+}
+
+const Checkbox: FC<CheckboxProps> = (props: CheckboxProps) => {
+  const { labelText } = props;
+  return (
+    <label htmlFor="oak-checkbox">
+      <input type="checkbox" id="oak-checkbox"></input>
+      <span>{labelText}</span>
+    </label>
+  );
 };
 
 export default Checkbox;
