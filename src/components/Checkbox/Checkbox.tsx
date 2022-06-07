82344ea0adcbd90f0c2385e36aca020535c25a56
craigmccahill <craig.mccahill@gmail.com>
2022-06-07 14:18:30 +0100

    feat: add high contrast mode media queries

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -28,15 +28,27 @@ const ScreenReaderCheckbox = styled.input.attrs({ type: "checkbox" })<{
   position: absolute;
   width: 1.5rem;
   height: 1.5rem;
-  opacity: 0.0001;
+  opacity: 0;
 `;
 
-// left this in here for convenience - if we wish to style the whole checkbox
 const VisualCheckbox = styled.svg``;
 
 const Checkmark = styled.path<{ checked: boolean }>`
   opacity: ${(props) => (props.checked ? "1" : "0")};
   transition: all 0.1s linear;
+  fill: currentcolor;
+
+  @media screen and (-ms-high-contrast: active) {
+    fill: ${(props) => (props.checked ? "highlight" : "windowText")};
+  }
+`;
+
+const CheckmarkBox = styled.rect`
+  stroke: currentcolor;
+
+  @media screen and (-ms-high-contrast: active) {
+    stroke: windowText;
+  }
 `;
 
 const CheckboxLabelText = styled.span`
@@ -85,18 +97,10 @@ const Checkbox: FC<CheckboxProps> = (props) => {
         {/* the checkmark */}
         <Checkmark
           d="M10.0664 16.8233C10.3008 17.0589 10.6992 17.0589 10.9336 16.8233L17.8242 9.8966C18.0586 9.66099 18.0586 9.26047 17.8242 9.02487L16.9805 8.1767C16.7461 7.9411 16.3711 7.9411 16.1367 8.1767L10.5117 13.8312L7.86328 11.1924C7.62891 10.9568 7.25391 10.9568 7.01953 11.1924L6.17578 12.0406C5.94141 12.2762 5.94141 12.6767 6.17578 12.9123L10.0664 16.8233Z"
-          fill="currentColor"
           checked={checked}
         />
         {/*  the border */}
-        <rect
-          x="0.5"
-          y="0.5"
-          width="23"
-          height="23"
-          rx="3.5"
-          stroke="currentColor"
-        />
+        <CheckmarkBox x="0.5" y="0.5" width="23" height="23" rx="3.5" />
       </VisualCheckbox>
       {labelText && <CheckboxLabelText>{labelText}</CheckboxLabelText>}
     </CheckboxLabel>
