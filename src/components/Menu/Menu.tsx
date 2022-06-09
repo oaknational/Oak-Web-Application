7261a69c8ed9483d4424883da8dd6931ac5c1b80
craigmccahill <craig.mccahill@gmail.com>
2022-06-09 21:45:03 +0100

    feat: create storybook for menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,21 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+import styled from "styled-components";
 
 import IconButton from "../Button/IconButton";
 
+interface FlyOutMenuProps {
+  visible: boolean;
+}
+
+const FlyOutMenu = styled.div<FlyOutMenuProps>`
+  // TODO: replace with sliding transition etc
+  display: ${(visible) => (visible ? "flex" : "none")};
+`;
+
 const Menu: FC = (props) => {
+  const [expanded, setExpanded] = useState(false);
   const { children } = props;
 
   return (
     <nav>
       <IconButton
         icon={"Hamburger"}
+        background-color={"transparent"}
         aria-label={"menu"}
         onClick={() => {
-          console.log("Open menu");
+          setExpanded(!expanded);
         }}
-        iconColorOverride={"transparent"}
       />
-      <div>{children}</div>
+      <FlyOutMenu visible={expanded}>{children}</FlyOutMenu>
     </nav>
   );
 };
