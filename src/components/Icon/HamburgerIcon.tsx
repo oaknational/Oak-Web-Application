bc92efde280b3e5f896c555c2dd89f5f451857d6
craigmccahill <craig.mccahill@gmail.com>
2022-06-09 20:46:25 +0100

    feat: make hamburger icon responsive

--- src/components/Icon/HamburgerIcon.tsx
+++ src/components/Icon/HamburgerIcon.tsx
@@ -3,8 +3,8 @@ import { FC } from "react";
 const Hamburger: FC = () => {
   return (
     <svg
-      width="18"
-      height="17"
+      width="100%"
+      height="100%"
       viewBox="0 0 18 17"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
