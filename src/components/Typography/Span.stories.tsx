9ce3d96d416894835399469d1c9186e87d3eb28a
mantagen <contact.mantagen@gmail.com>
2022-06-14 18:24:12 +0100

    chore: rename Body -> Typography

--- src/components/Typography/Span.stories.tsx
+++ src/components/Typography/Span.stories.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import Component from "./Span";
-import Body from "./Body";
+
+import Typography, { P } from ".";
 
 export default {
   title: "Typography/Span",
@@ -10,15 +11,17 @@ export default {
 } as ComponentMeta<typeof Component>;
 
 const Template: ComponentStory<typeof Component> = (args) => (
-  <Body>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Id semper risus in
-    hendrerit gravida rutrum quisque non tellus. Nibh praesent{" "}
-    <Component {...args}>tristique magna sit.</Component> A arcu cursus vitae
-    congue mauris rhoncus aenean. Turpis egestas maecenas pharetra convallis
-    posuere morbi leo. Faucibus pulvinar elementum integer enim neque volutpat
-    ac tincidunt. Vitae suscipit tellus mauris a diam maecenas.
-  </Body>
+  <Typography>
+    <P>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Id semper risus in
+      hendrerit gravida rutrum quisque non tellus. Nibh praesent{" "}
+      <Component {...args}>tristique magna sit.</Component> A arcu cursus vitae
+      congue mauris rhoncus aenean. Turpis egestas maecenas pharetra convallis
+      posuere morbi leo. Faucibus pulvinar elementum integer enim neque volutpat
+      ac tincidunt. Vitae suscipit tellus mauris a diam maecenas.
+    </P>
+  </Typography>
 );
 
 export const Span = Template.bind({});
--- src/components/Typography/index.ts
+++ src/components/Typography/index.ts
@@ -1,6 +1,6 @@
 export { default as Heading } from "./Heading";
 export type { HeadingTag } from "./Heading";
-export { default as Body } from "./Body";
+export { default } from "./Typography";
 export { default as Span } from "./Span";
 export { default as P } from "./P";
 export { default as Hr } from "./Hr";
