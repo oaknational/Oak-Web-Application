fd870756e2ea7158665d107def5b75194fee29f1
craigmccahill <craig.mccahill@gmail.com>
2022-07-13 11:31:55 +0100

    feat: tweek site header layout for tablets

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -28,7 +28,7 @@ const SiteHeader: FC = () => {
           </Span>
         </HomeLink>
       </Link>
-      <Flex alignItems={"center"} mt={[16, 16, 0]}>
+      <Flex alignItems={"center"} mt={[16, 0, 0]}>
         <P>
           <Link href={"/beta/onboarding"}>Join the Beta</Link>
         </P>
