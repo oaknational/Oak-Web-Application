49bb0451671a27aa508629da3aad90168e7ee08a
Joe Baker <blakeage77@googlemail.com>
2022-07-14 12:42:51 +0100

    feat: policy page

---
+++ src/browser-lib/fixtures/policyPageBody.ts
@@ -0,0 +1,252 @@
+const policyPageBody = `
+<h2>2. The personal data we hold</h2>
+<p>
+  The personal information that we will collect about you falls into
+  the following categories:
+</p>
+<h3>Information collected automatically by our website</h3>
+<p>
+  Our website collects some limited information about you
+  automatically when you visit the site using your electronic device
+  (this could be a computer desktop, laptop, phone, tablet,
+  television). We do not seek to identify you from this information
+  or do anything with it that will affect you individually. To the
+  extent practicable, we aggregate and/or remove anything that could
+  be used to identify you from this information before we use it,
+  including (where possible) anonymising IP (Internet Protocol)
+  addresses.
+</p>
+<p>This information includes, or may in the future include:</p>
+<ul>
+  <li>the IP address from which you access our website;</li>
+  <li>
+    information about the device used to access the site (including
+    the number of pupils using it);
+  </li>
+  <li>your approximate location;</li>
+  <li>your school;</li>
+  <li>
+    the pages that you click on our site, including the lessons that
+    you view (and therefore your likely year group);
+  </li>
+  <li>
+    the time that you spend on the site and the time that you spend
+    viewing videos on the site;
+  </li>
+  <li>
+    the website uses cookies and web beacons. Please see the{" "}
+    <a href="https://www.thenational.academy/cookie-policy">
+      Oak Cookie Policy
+    </a>{" "}
+    for full details.
+  </li>
+</ul>
+<h3>Information that you input to our website</h3>
+<p>
+  Some pages on our website invite you to input information about
+  yourself – for example, where we invite you to sign up to receive
+  an e-newsletter from us, or to provide feedback through a survey.
+  In those cases the information sought is clear on the relevant
+  page.
+</p>
+<p>
+  We also ask teachers to input their school name on some pages,
+  and, in particular, in order to download our resources.
+</p>
+<h3>
+  Information that you send to us by email or other electronic means
+</h3>
+<p>
+  You may also send us information about yourself by email or other
+  electronic means (for example, through Facebook or Twitter). In
+  that case we will record your name and basic contact details and,
+  of course, the information that you include in your communication.
+</p>
+<p>
+  When we invite you to provide information about yourself, it is
+  entirely up to you whether you give us the information or not.
+  Often, however, we will not be able to do something that you are
+  asking for (for example, send you an e-newsletter, or respond to
+  an enquiry) if you do not give us the information.
+</p>
+<h3>Information collected through the Teacher Hub</h3>
+<p>
+  If you are using the Teacher Hub (
+  <a href="https://teachers.thenational.academy/">
+    teachers.thenational.academy
+  </a>
+  ), you will be asked to provide us with the following information:
+</p>
+<ul>
+  <li>Full name</li>
+  <li>Email address</li>
+  <li>School details (school name and URN)</li>
+  <li>School contact email address</li>
+  <li>Survey</li>
+</ul>
+<h3>Information collected through the Classroom</h3>
+<p>
+  Users of the{" "}
+  <a href="https://classroom.thenational.academy/">
+    classroom.thenational.academy
+  </a>{" "}
+  site are invited to take quizzes at the start and end of a lesson.
+  The Classroom quizzes typically collect and store users’ answers,
+  marks and total scores.
+</p>
+<h3>Information collected through the Feedback Tool</h3>
+<p>
+  We use a third party (Hubspot and Gleap) feedback tool (widget)
+  hosted on the Homesite (
+  <a href="https://www.thenational.academy/">
+    www.thenational.academy
+  </a>
+  ) and Teacher Hub (
+  <a href="https://teachers.thenational.academy/">
+    teachers.thenational.academy
+  </a>
+  ) to enable users to submit a feedback form to report a technical
+  bug or error in a lesson, make a general enquiry or to share
+  product improvement ideas with us.
+</p>
+<p>
+  Gleap and Hubspot will process the following information about
+  you:
+</p>
+<ul>
+  <li>Name (if you provided it)</li>
+  <li>Email address</li>
+  <li>Approximate geo-location</li>
+  <li>Operating system</li>
+  <li>Browser information (type and version)</li>
+  <li>Screen size</li>
+  <li>URL visited</li>
+  <li>Screenshot data</li>
+  <li>
+    Session replay data (bug issue and lesson concern types only)
+  </li>
+</ul>
+<h3>Information collected through the Recruitment Portal</h3>
+<p>
+  A third-party tool hosted within the website for managing our job
+  adverts and applications:{" "}
+  <a href="https://jobs.thenational.academy/">
+    jobs.thenational.academy
+  </a>
+</p>
+<p>
+  In order to make an application for an Oak advertised position we
+  typically request the following personal information from you:
+</p>
+<ul>
+  <li>Full name</li>
+  <li>
+    Contact details including: phone number, email and postal
+    address
+  </li>
+  <li>Job history and CV</li>
+  <li>
+    Sometimes we may request video or written evidence from your job
+    history to support your job application.
+  </li>
+</ul>
+<h2>3. Special category data</h2>
+<p>
+  The information we hold is not normally classed as special
+  category data. This includes any information relating to racial or
+  ethnic origin, political opinions, religious or philosophical
+  beliefs, health, genetic or biometric data. If we do hold any such
+  data about you, we will inform you.
+</p>
+<h2>4. Why we use this data</h2>
+<p>We use your personal information for the following purposes:</p>
+<h3>Information collected automatically by our website</h3>
+<p>
+  We use this information to improve our website and the lessons and
+  other materials that we provide through it. We do not use it to do
+  anything that will affect you individually.
+</p>
+<h3>Information that you input to our website</h3>
+<p>
+  The purposes for which we use this information (for example, to
+  send you an e-newsletter) are explained on the web pages where we
+  ask you to submit it.
+</p>
+<h3>
+  Information that you send to us by email or other electronic means
+</h3>
+<p>
+  We use this information to communicate with you, to respond to any
+  enquiries that you may raise, and to manage our relationship with
+  you.
+</p>
+<h3>Information collected through the Teacher Hub</h3>
+<p>We use this information to:</p>
+<ul>
+  <li>
+    provide you with subscription services, such as email updates
+  </li>
+  <li>
+    understand how our services are being used and help us improve
+    the resources we create
+  </li>
+  <li>
+    ensure that our educational content is reaching the intended
+    audience
+  </li>
+  <li>to allow us to know where our materials are being used</li>
+  <li>
+    to allow you to opt into updates relating to the Teacher Hub
+    site
+  </li>
+  <li>
+    to allow us to invite you to share your views on how to improve
+    Oak and the services it provides.
+  </li>
+</ul>
+<h3>Information collected through the Classroom</h3>
+<p>
+  We use this information to help drive motivation in the users and
+  to inform our continuous improvement of the content of our
+  lessons.
+</p>
+<h3>Information collected through the Feedback Tool</h3>
+<p>We use this information to:</p>
+<ul>
+  <li>
+    Allow us to contact you to seek clarity on the issue you report
+  </li>
+  <li>Enable us to update you when an issue is resolved</li>
+  <li>Add contextual detail to manually submitted feedback</li>
+  <li>Enable analysis of feedback items</li>
+</ul>
+<h3>Information collected through the Recruitment Portal</h3>
+<p>
+  We use this information to allow us to select candidates to take
+  forward for interview.
+</p>
+<p>
+  Your personal information also forms part of a record of the
+  conduct of our programme, the delivery of our lessons and our
+  relationships with visitors to our website. We use these records
+  to oversee and assess our programme and derive insights for the
+  education sector. The records may also in some circumstances be
+  used as evidence of these various matters, should a disagreement
+  arise.
+</p>
+<p>
+  We may share in anonymised form only, aggregated records of usage
+  with the Department for Education, and third party education
+  research organisations.
+</p>
+<p>
+  We will not sell or rent your personal information or use or share
+  your personal information for marketing purposes (other than to
+  send you our e-newsletter, with your consent).
+</p>
+<p>
+  We may need to use or disclose your personal information in order
+  to comply with our legal obligations.
+</p>`;
+
+export default policyPageBody;
