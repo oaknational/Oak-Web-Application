3e6e40e582d5c5d3c7d26c0e37b55cd3caee2c42
mantagen <contact.mantagen@gmail.com>
2022-07-27 10:37:13 +0100

    test(gleap): add describe blocks back in

--- src/browser-lib/gleap/useGleap.test.ts
+++ src/browser-lib/gleap/useGleap.test.ts
@@ -14,24 +14,24 @@ describe("useGleap", () => {
     jest.clearAllMocks();
     jest.resetModules();
   });
+  describe("with enabled true", () => {
+    test("should call startGleap() with the correct gleap config", () => {
+      renderHook(() => useGleap({ enabled: true }));
 
-  test("[enabled: true] should call startGleap() with the correct gleap config", () => {
-    renderHook(() => useGleap({ enabled: true }));
-
-    expect(startGleap).toHaveBeenCalledWith({
-      widgetUrl: "NEXT_PUBLIC_GLEAP_WIDGET_URL",
-      apiUrl: "NEXT_PUBLIC_GLEAP_API_URL",
-      apiKey: "NEXT_PUBLIC_GLEAP_API_KEY",
+      expect(startGleap).toHaveBeenCalledWith({
+        widgetUrl: "NEXT_PUBLIC_GLEAP_WIDGET_URL",
+        apiUrl: "NEXT_PUBLIC_GLEAP_API_URL",
+        apiKey: "NEXT_PUBLIC_GLEAP_API_KEY",
+      });
     });
+
+    test.todo("re-rendering should not cause startGleap() to be called again");
   });
+  describe("with enabled false", () => {
+    test("should not call startGleap()", () => {
+      renderHook(() => useGleap({ enabled: false }));
 
-  test.todo(
-    "[enabled: true] re-rendering should not cause startGleap() to be called again"
-  );
-
-  test("[enabled: false] should not call startGleap()", () => {
-    renderHook(() => useGleap({ enabled: false }));
-
-    expect(startGleap).not.toHaveBeenCalled();
+      expect(startGleap).not.toHaveBeenCalled();
+    });
   });
 });
