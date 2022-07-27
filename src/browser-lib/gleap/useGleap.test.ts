e78f9ea0b74918ac20414fc1cc6b5f15aefaf7e9
mantagen <contact.mantagen@gmail.com>
2022-07-27 10:30:41 +0100

    test(gleap): fix failing test

--- src/browser-lib/gleap/useGleap.test.ts
+++ src/browser-lib/gleap/useGleap.test.ts
@@ -2,49 +2,36 @@ import { renderHook } from "@testing-library/react-hooks";
 
 import useGleap from ".";
 
-const configGetSpy = jest.fn((key: string) => {
-  return {
-    gleapWidgetUrl: "widget-url",
-    gleapApiUrl: "api-url",
-    gleapApiKey: "api-key",
-  }[key];
-});
 const startGleap = jest.fn();
 
+jest.mock("./startGleap", () => ({
+  __esModule: true,
+  default: (...args: unknown[]) => startGleap(...args),
+}));
+
 describe("useGleap", () => {
-  describe("with enabled true", () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-      jest.resetModules();
-      jest.mock("../../config", () => ({
-        get: configGetSpy,
-      }));
-      jest.mock("./startGleap", () => ({
-        __esModule: true,
-        default: startGleap,
-      }));
-    });
-
-    test.skip("should call startGleap() with the correct gleap config", () => {
-      /**
-       * @todo: fix this test, not sure why it's failing
-       */
-      renderHook(() => useGleap({ enabled: true }));
-
-      expect(startGleap).toHaveBeenCalledWith({
-        widgetUrl: "widget-url",
-        apiUrl: "api-url",
-        apiKey: "api-key",
-      });
-    });
-
-    test.todo("re-rendering should not cause startGleap() to be called again");
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
   });
-  describe("with enabled false", () => {
-    test("should not call startGleap()", () => {
-      renderHook(() => useGleap({ enabled: false }));
 
-      expect(startGleap).not.toHaveBeenCalled();
+  test("[enabled: true] should call startGleap() with the correct gleap config", () => {
+    renderHook(() => useGleap({ enabled: true }));
+
+    expect(startGleap).toHaveBeenCalledWith({
+      widgetUrl: "NEXT_PUBLIC_GLEAP_WIDGET_URL",
+      apiUrl: "NEXT_PUBLIC_GLEAP_API_URL",
+      apiKey: "NEXT_PUBLIC_GLEAP_API_KEY",
     });
+  });
+
+  test.todo(
+    "[enabled: true] re-rendering should not cause startGleap() to be called again"
+  );
+
+  test("[enabled: false] should not call startGleap()", () => {
+    renderHook(() => useGleap({ enabled: false }));
+
+    expect(startGleap).not.toHaveBeenCalled();
   });
 });
