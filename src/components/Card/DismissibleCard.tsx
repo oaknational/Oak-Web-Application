16a138d3e97006562c7437152e8d266a599b63db
mantagen <contact.mantagen@gmail.com>
2022-07-27 14:06:39 +0100

    fix(dissmissable-card): title

--- src/components/Card/DismissibleCard.tsx
+++ src/components/Card/DismissibleCard.tsx
@@ -1,5 +1,3 @@
-import { title } from "process";
-
 import { FC, useState } from "react";
 import styled from "styled-components";
 
@@ -14,10 +12,11 @@ const CloseButtonWrapper = styled.div`
 `;
 
 type DismissibleCardProps = {
+  // 'title' used for the close button's aria-label
   title: string;
 };
 
-const DismissibleCard: FC<DismissibleCardProps> = ({ children }) => {
+const DismissibleCard: FC<DismissibleCardProps> = ({ children, title }) => {
   const [dismissed, setDismissed] = useState(false);
 
   return (
@@ -27,7 +26,7 @@ const DismissibleCard: FC<DismissibleCardProps> = ({ children }) => {
           {children}
           <CloseButtonWrapper>
             <IconButton
-              aria-label={`Close ${title} Banner`}
+              aria-label={`Dismiss ${title}`}
               icon={"Close"}
               onClick={() => {
                 setDismissed(true);
