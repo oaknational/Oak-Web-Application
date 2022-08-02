390d5b36ed0cd2d05667434fe6e8edc9ca2b6e92
Ross Mackay <ross@mcky.me>
2022-08-02 12:32:57 +0100

    feat(cms): add CMSImage component

---
+++ src/__tests__/__helpers__/cms.ts
@@ -0,0 +1,10 @@
+import { SanityImage } from "../../node-lib/cms";
+
+export const mockImageAsset = (id = "img-id"): SanityImage => {
+  return {
+    asset: {
+      _id: `image-${id}-300x300-png`,
+      url: `https://cdn.sanity.io/images/p/d/${id}-300x300.png`,
+    },
+  };
+};
