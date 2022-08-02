ad442795d6419fbfd9255a3853a27ff1f9dd51d5
Ross Mackay <ross@mcky.me>
2022-08-02 11:40:19 +0100

    chore(cms): extract cms types from index file

---
+++ src/node-lib/cms/types/documents.ts
@@ -0,0 +1,20 @@
+import { Document, PortableTextJSON } from "./base";
+
+export type TeamMember = Document & {
+  name: string;
+};
+
+export type TeamMemberPreview = Pick<TeamMember, "name">;
+
+export type Webinar = Document & {
+  title: string;
+  slug: string;
+  date: Date;
+  hosts: TeamMember[];
+  summaryPortableText: PortableTextJSON;
+};
+
+export type WebinarPreview = Pick<
+  Webinar,
+  "id" | "title" | "slug" | "summaryPortableText"
+>;
