b7b32332d621ce2417d49dafdf70cd755df00740
Ross Mackay <ross@mcky.me>
2022-08-03 11:08:19 +0100

    fix(cms): add previewSecret to config

--- next.config.js
+++ next.config.js
@@ -121,6 +121,8 @@ module.exports = async (phase) => {
       SANITY_USE_CDN: process.env.SANITY_USE_CDN || oakConfig.sanity?.useCDN,
       SANITY_AUTH_SECRET:
         process.env.SANITY_AUTH_SECRET || secretsFromNetwork.SANITY_AUTH_SECRET,
+      SANITY_PREVIEW_SECRET:
+        process.env.SANITY_PREVIEW_SECRET || oakConfig.sanity?.previewSecret,
     },
   };
 
