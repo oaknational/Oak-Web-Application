6b6449d44288aa48bdc6565abd23b0b45f556963
John Roberts <john.roberts@thenational.academy>
2022-08-03 17:56:46 +0100

    Testing netlify subdomains
    Adding edge function and updating netlify toml file

---
+++ netlify/edge-functions/subdomain-redirect.js
@@ -0,0 +1,17 @@
+export default async (request, context) => {
+    const subdomain = request.url.match(/https:\/\/(.*?)\-\-/);
+    const redirected = request.headers.get("x-cloudflare-redirect");
+  
+    console.log("redirect", redirected);
+    if (subdomain && !redirected) {
+      console.log(
+        "Redirected to Cloudflare - ",
+        `https://${subdomain[1]}.netlify.thenational.academy.com/`
+      );
+      return Response.redirect(`https://${subdomain[1]}.netlify.thenational.academy/`);
+    } else {
+      console.log("Request allowed through");
+      let res = await context.next();
+      return res;
+    }
+  };
