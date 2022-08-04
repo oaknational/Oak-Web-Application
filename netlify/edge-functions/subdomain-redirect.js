7678f9964c41ff12b53c83eac8b0e7a108fbb94e
Jim Cresswell <jim.cresswell@gmail.com>
2022-08-04 10:13:43 +0100

    chore: test edge functions

--- netlify/edge-functions/subdomain-redirect.js
+++ netlify/edge-functions/subdomain-redirect.js
@@ -1,17 +1,22 @@
 export default async (request, context) => {
-    const subdomain = request.url.match(/https:\/\/(.*?)\-\-/);
-    const redirected = request.headers.get("x-cloudflare-redirect");
-  
-    console.log("redirect", redirected);
-    if (subdomain && !redirected) {
-      console.log(
-        "Redirected to Cloudflare - ",
-        `https://${subdomain[1]}.netlify.thenational.academy/`
-      );
-      return Response.redirect(`https://${subdomain[1]}.netlify.thenational.academy/`);
-    } else {
-      console.log("Request allowed through");
-      let res = await context.next();
-      return res;
-    }
-  };
+  // Don't need to match the thenational.academy domain here, just for speed of testing.
+  const subdomain = request.url.match(
+    /(?:https:\/\/)?([^.]+)?(?:\.netlify)?\.(?:thenational\.academy|netlify\.app)/
+  )[1];
+  const redirected = request.headers.get("x-cloudflare-redirect");
+
+  console.log("redirect", redirected);
+  if (subdomain && !redirected) {
+    console.log(
+      "Redirected to Cloudflare - ",
+      `https://${subdomain}.netlify.thenational.academy/`
+    );
+    return Response.redirect(
+      `https://${subdomain}.netlify.thenational.academy/`
+    );
+  } else {
+    console.log("Request allowed through");
+    let res = await context.next();
+    return res;
+  }
+};
