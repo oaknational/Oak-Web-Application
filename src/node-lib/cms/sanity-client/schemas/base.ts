e9144cf089418200f8343a96220aa08e4a569d93
Ross Mackay <ross@mcky.me>
2022-08-05 11:45:46 +0100

    feat: add textBlock schema/fragment/types, add missing fields to T&M

--- src/node-lib/cms/sanity-client/schemas/base.ts
+++ src/node-lib/cms/sanity-client/schemas/base.ts
@@ -21,32 +21,41 @@ export const dateSchema = z.preprocess((arg) => {
   if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
 }, z.date());
 
-export const cardSchema = z.object({
-  title: z.string().nonempty(),
-  image: z.any(),
-  bodyPortableText: portableTextSchema,
+const CTASchemaBase = z.object({
+  label: z.string(),
 });
 
-export const CTASchema = z.object({
-  label: z.string().nonempty(),
-  linkType: z.enum(["internal", "external"]),
-  external: z.string().nonempty(),
-  internal: z.object({
-    /**
-     * If you've ended up here because of a validation error
-     * about internal.type not being defined, the fragments
-     * in cta.fragment.gql may be out of date.
-     * It'll need a separate inline fragment for each possible
-     * document type in the generated union, e.g.
-     * ```
-     *   ...on DocumentType {
-     *     type: _type
-     *   }
-     * ```
-     */
-    type: z.string(),
-    slug: slugSchema.optional(),
+export const CTASchema = z.discriminatedUnion("linkType", [
+  CTASchemaBase.extend({
+    linkType: z.literal("internal"),
+    internal: z.object({
+      /**
+       * If you've ended up here because of a validation error
+       * about internal.type not being defined, the fragments
+       * in cta.fragment.gql may be out of date.
+       * It'll need a separate inline fragment for each possible
+       * document type in the generated union, e.g.
+       * ```
+       *   ...on DocumentType {
+       *     type: _type
+       *   }
+       * ```
+       */
+      contentType: z.string(),
+      slug: slugSchema.optional(),
+    }),
   }),
+  CTASchemaBase.extend({
+    linkType: z.literal("external"),
+    external: z.string().url(),
+  }),
+]);
+
+export const cardSchema = z.object({
+  title: z.string().nonempty(),
+  bodyPortableText: portableTextSchema,
+  image: z.any(),
+  cta: CTASchema.optional(),
 });
 
 export const imageSchema = z.object({
@@ -56,10 +65,26 @@ export const imageSchema = z.object({
   }),
 });
 
-export const textAndMediaSchema = z.object({
+export const textBlockSchema = z.object({
   title: z.string().nonempty(),
   bodyPortableText: portableTextSchema,
-  image: imageSchema,
-  alignMedia: z.enum(["left", "right"]),
-  mediaType: z.enum(["image", "video"]),
+  cta: CTASchema.optional()
 });
+
+export const textAndMediaSchemaBase = z.object({
+  title: z.string().nonempty(),
+  bodyPortableText: portableTextSchema,
+  cta: CTASchema.nullable().optional(),
+  alignMedia: z.enum(["left", "right"]),
+});
+
+export const textAndMediaSchema = z.discriminatedUnion("mediaType", [
+  textAndMediaSchemaBase.extend({
+    mediaType: z.literal("image"),
+    image: imageSchema,
+  }),
+  textAndMediaSchemaBase.extend({
+    mediaType: z.literal("video"),
+    video: z.object({}),
+  }),
+]);
--- src/node-lib/cms/types/base.ts
+++ src/node-lib/cms/types/base.ts
@@ -15,21 +15,40 @@ export type SanityImage = {
   };
 };
 
-export type Card = {
-  title: string;
-  image?: string;
-  bodyPortableText?: PortableTextJSON;
-};
-
 export type CTA = {
   label: string;
+} & (
+  | {
+      linkType: "internal";
+      internal: { contentType: string; slug?: string };
+    }
+  | { linkType: "external"; external: string }
+);
+
+export type Card = {
+  title: string;
+  image?: SanityImage | null;
+  bodyPortableText?: PortableTextJSON;
+  cta?: CTA | null;
 };
 
+export type TextBlock = {
+  title: string;
+  bodyPortableText: PortableTextJSON;
+  cta?: CTA | null;
+};
 
 export type TextAndMedia = {
   title: string;
   bodyPortableText: PortableTextJSON;
   alignMedia: "left" | "right";
-  mediaType: "image" | "video";
-  image: SanityImage;
-};
+  cta?: CTA | null;
+} & (
+  | {
+      mediaType: "image";
+      image: SanityImage;
+    }
+  | {
+      mediaType: "video";
+    }
+);
