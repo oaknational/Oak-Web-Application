54709cc047b484a754c4ec4beb7b3dfc1fb25c56
craigmccahill <craig.mccahill@gmail.com>
2022-08-09 16:00:03 +0100

    feat: add menu contents

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -1,15 +1,24 @@
 import { FC } from "react";
 import Link from "next/link";
-import { useTheme } from "styled-components";
+import styled, { useTheme } from "styled-components";
 
 import Flex from "../Flex";
-import P, { Span } from "../Typography";
+import P, { Heading } from "../Typography";
 import FixedHeader from "../FixedHeader";
 import { Menu } from "../Menu";
 import Logo from "../Logo";
 import { useMenuContext } from "../../context/Menu";
 import IconButton from "../Button/IconButton";
-// import HamburgerButton from "../Menu/HamburgerButton";
+import Icon from "../Icon";
+
+const CurrentLocation = styled(Heading)`
+  opacity: 0.6;
+  text-decoration: underline;
+`;
+
+const LocationIcon = styled(Icon)`
+  opacity: 0.6;
+`;
 
 const SiteHeader: FC = () => {
   const theme = useTheme();
@@ -42,36 +51,66 @@ const SiteHeader: FC = () => {
         }}
       />
       <Menu>
-        <Link href={"/"}>Home</Link>
+        <Flex $alignItems={"center"}>
+          <LocationIcon variant="minimal" name="ArrowRight" size={[48]} />
+          <CurrentLocation tag={"h2"} $fontSize={[32]} $color={"black"}>
+            Home
+          </CurrentLocation>
+        </Flex>
         <ul role="list">
           <li>
-            <Span $fontFamily={"heading"} $fontSize={[32]}>
+            <P $fontFamily={"heading"} $fontSize={[32]} $mt={[32]}>
               <Link href={"https://teachers.thenational.academy/"}>
                 Teacher Hub
               </Link>
-            </Span>
+            </P>
           </li>
           <li>
-            <Span $fontFamily={"heading"} $fontSize={[32]}>
+            <P $fontFamily={"heading"} $fontSize={[32]} $mt={[16]}>
               <Link href={"https://classroom.thenational.academy/"}>
                 Classroom
               </Link>
-            </Span>
+            </P>
           </li>
           <li>
-            <Span $fontFamily={"heading"} $fontSize={[24]}>
+            <P $fontFamily={"heading"} $fontSize={[24]} $mt={[32]}>
               <Link href={"/"}>Develop Your Curriculum</Link>
-            </Span>
+            </P>
           </li>
           <li>
-            <Span $fontFamily={"heading"} $fontSize={[24]}>
+            <P $fontFamily={"heading"} $fontSize={[24]} $mt={[12]}>
               <Link href={"/"}>Support Your Team</Link>
-            </Span>
+            </P>
           </li>
           <li>
-            <Span $fontFamily={"heading"} $fontSize={[24]}>
+            <P $fontFamily={"heading"} $fontSize={[24]} $mt={[12]}>
               <Link href={"/"}>Plan a lesson</Link>
-            </Span>
+            </P>
+          </li>
+          <li>
+            <P $fontFamily={"ui"} $fontSize={[16]} $mt={[32]}>
+              <Link href={"/"}>Blogs</Link>
+            </P>
+          </li>
+          <li>
+            <P $fontFamily={"ui"} $fontSize={[16]} $mt={[8]}>
+              <Link href={"/"}>Webinars</Link>
+            </P>
+          </li>
+          <li>
+            <P $fontFamily={"ui"} $fontSize={[16]} $mt={[8]}>
+              <Link href={"/"}>About us</Link>
+            </P>
+          </li>
+          <li>
+            <P $fontFamily={"ui"} $fontSize={[16]} $mt={[8]}>
+              <Link href={"/"}>Contact us</Link>
+            </P>
+          </li>
+          <li>
+            <P $fontFamily={"ui"} $fontSize={[16]} $mt={[8]}>
+              <Link href={"/"}>Help</Link>
+            </P>
           </li>
         </ul>
       </Menu>
