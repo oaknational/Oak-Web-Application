c356510faf7b0a7047a03d59dce79ed907a0c523
Joe Baker <blakeage77@googlemail.com>
2022-08-09 22:19:32 +0100

    chore: add maxwidth

--- src/components/Layout/LandingPageLayout.tsx
+++ src/components/Layout/LandingPageLayout.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { FC } from "react";
 import styled from "styled-components";
-import Link from "next/link";
 
 import SiteHeader from "../SiteHeader";
 import SiteFooter from "../SiteFooter";
@@ -10,9 +9,6 @@ import Seo, { SeoProps } from "../../browser-lib/seo/Seo";
 import background, { BackgroundProps } from "../../styles/utils/background";
 import { OakColorName } from "../../styles/theme";
 import footerSections from "../../browser-lib/fixtures/footerSectionLinks";
-import Flex from "../Flex";
-import IconButtonAsLink from "../Button/IconButtonAsLink";
-import { Heading } from "../Typography";
 
 const Container = styled.div<BackgroundProps>`
   display: flex;
@@ -47,24 +43,7 @@ const LandingPageLayout: FC<LayoutProps> = (props) => {
       <Container $background={$background}>
         <SiteHeader />
         <StyledLayout>{children}</StyledLayout>
-        <SiteFooter
-          footerSections={footerSections}
-          footerNotification={
-            <Flex $justifyContent={"center"} $alignItems={"center"}>
-              <IconButtonAsLink
-                icon={"Rocket"}
-                aria-label={"Join new beta oak"}
-                href={"/beta/onboarding"}
-                size={"tiny"}
-                $mr={12}
-                variant={"minimal"}
-              />
-              <Heading tag="h5" $fontSize={16}>
-                <Link href={"/beta/onboarding"}>Join Beta</Link>
-              </Heading>
-            </Flex>
-          }
-        />
+        <SiteFooter footerSections={footerSections} />
       </Container>
     </>
   );
--- src/components/SiteFooter/SiteFooter.tsx
+++ src/components/SiteFooter/SiteFooter.tsx
@@ -12,35 +12,13 @@ import {
 } from "../../browser-lib/fixtures/footerSectionLinks";
 import IconButtonAsLink from "../Button/IconButtonAsLink";
 import LogoText from "../../../public/images/oak-logo-text.svg";
-import { getBreakpoint } from "../../styles/utils/responsive";
+import MaxWidth from "../MaxWidth/MaxWidth";
 
 const StyledSiteFooter = styled.footer`
   background: ${getColorByName("white")};
   width: 100%;
   margin-top: 80px;
   z-index: 0;
-`;
-
-const BackgroundImageContainer = styled.div`
-  padding-bottom: 80px;
-
-  ::before {
-    z-index: -1;
-    content: " ";
-    position: absolute;
-    width: 110%;
-    height: 100%;
-    background-image: url("images/pen/loop-down.svg");
-    background-size: 50%;
-    background-repeat: no-repeat;
-    background-position: bottom right 20px;
-
-    @media (max-width: ${getBreakpoint("small")}px) {
-      width: 97%;
-      background-size: 160%;
-      background-position: bottom right -56px;
-    }
-  }
 `;
 
 type SiteFooterProps = {
@@ -74,105 +52,99 @@ const SiteFooter: FC<SiteFooterProps> = ({
   return (
     <StyledSiteFooter>
       <nav>
-        <Flex
+        <MaxWidth
           $position={"relative"}
           $justifyContent={"center"}
           $flexDirection={"column"}
           $ph={12}
-          $maxWidth={1200}
           $ma={"auto"}
+          $width={"100%"}
         >
-          <BackgroundImageContainer>
-            <Flex $flexWrap={["wrap"]}>
-              <Flex
-                $mb={32}
-                $flexGrow={[1, 0]}
-                $mr={48}
-                $flexDirection="column"
-              >
-                <Flex $mb={16} $flexDirection={"column"}>
-                  <FooterSectionLinks
-                    title={footerSections[0]?.title}
-                    links={footerSections[0]?.links}
-                  />
-                </Flex>
-                <Flex $mb={16} $flexDirection={"column"}>
-                  <FooterSectionLinks
-                    title={footerSections[1]?.title}
-                    links={footerSections[1]?.links}
-                  />
-                </Flex>
-              </Flex>
-
-              <Flex
-                $flexGrow={[1, 0]}
-                $flexDirection="column"
-                $mb={[24, 0]}
-                $mr={48}
-              >
+          <Flex $width={"100%"} $flexWrap={["wrap"]}>
+            <Flex $mb={32} $flexGrow={[1, 0]} $mr={48} $flexDirection="column">
+              <Flex $mb={16} $flexDirection={"column"}>
                 <FooterSectionLinks
-                  title={footerSections[2]?.title}
-                  links={footerSections[2]?.links}
+                  title={footerSections[0]?.title}
+                  links={footerSections[0]?.links}
                 />
               </Flex>
-
-              <Flex $flexDirection="column">
+              <Flex $mb={16} $flexDirection={"column"}>
                 <FooterSectionLinks
-                  title={footerSections[3]?.title}
-                  links={footerSections[3]?.links}
+                  title={footerSections[1]?.title}
+                  links={footerSections[1]?.links}
                 />
-              </Flex>
-
-              <Flex
-                $flexDirection={"column"}
-                $justifyContent={"space-between"}
-                $alignItems={"flex-end"}
-                $flexGrow={[0, 1]}
-                $pl={[32, 0]}
-              >
-                <Flex $position="relative">
-                  <Image
-                    alt={"Oak National Academy Logo"}
-                    height={63}
-                    width={150}
-                    src={LogoText}
-                  ></Image>
-                </Flex>
-
-                {footerNotification}
               </Flex>
             </Flex>
-            <Flex $mt={32}>
-              <Flex $justifyContent={"center"}>
-                <IconButtonAsLink
-                  aria-label={"instagram"}
-                  icon={"Instagram"}
-                  href={"/instagram"} // update link
-                  variant={"minimal"}
-                  size={"tiny"}
-                  $mr={24}
-                />
-                <IconButtonAsLink
-                  aria-label={"facebook"}
-                  icon={"Facebook"}
-                  href={"/facebook"} // update link
-                  variant={"minimal"}
-                  size={"tiny"}
-                  $mr={24}
-                />
-                <IconButtonAsLink
-                  aria-label={"twitter"}
-                  icon={"Twitter"}
-                  href={"/twitter"} // update link
-                  variant={"minimal"}
-                  size={"tiny"}
-                  $mr={24}
-                />
-              </Flex>
-              <P fontSize={12}>© Oak National Academy</P>
+
+            <Flex
+              $flexGrow={[1, 0]}
+              $flexDirection="column"
+              $mb={[24, 0]}
+              $mr={48}
+            >
+              <FooterSectionLinks
+                title={footerSections[2]?.title}
+                links={footerSections[2]?.links}
+              />
             </Flex>
-          </BackgroundImageContainer>
-        </Flex>
+
+            <Flex $flexDirection="column">
+              <FooterSectionLinks
+                title={footerSections[3]?.title}
+                links={footerSections[3]?.links}
+              />
+            </Flex>
+
+            <Flex
+              $flexDirection={"column"}
+              $justifyContent={"space-between"}
+              $alignItems={"flex-end"}
+              $flexGrow={[0, 1]}
+              // $pl={[32, 0]}
+            >
+              <Flex $position="relative">
+                <Image
+                  alt={"Oak National Academy Logo"}
+                  height={66}
+                  width={150}
+                  src={LogoText}
+                ></Image>
+              </Flex>
+
+              {footerNotification}
+            </Flex>
+          </Flex>
+          <Flex $mb={80} $width={"100%"} $mt={32}>
+            <Flex $alignItems={"center"} $justifyContent={"center"}>
+              <IconButtonAsLink
+                aria-label={"instagram"}
+                icon={"Instagram"}
+                href={"/instagram"} // update link
+                variant={"minimal"}
+                $mr={12}
+              />
+              <IconButtonAsLink
+                aria-label={"facebook"}
+                icon={"Facebook"}
+                href={"/facebook"} // update link
+                variant={"minimal"}
+                $mr={12}
+              />
+              <IconButtonAsLink
+                aria-label={"twitter"}
+                icon={"Twitter"}
+                href={"/twitter"} // update link
+                variant={"minimal"}
+                $mr={12}
+              />
+            </Flex>
+            <Flex $alignItems={"center"}>
+              <P $textAlign="center" fontSize={12}>
+                © Oak National Academy
+              </P>
+            </Flex>
+          </Flex>
+        </MaxWidth>
       </nav>
     </StyledSiteFooter>
   );
