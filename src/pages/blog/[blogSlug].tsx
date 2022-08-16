43acf8bdea7bf75db78ea972ea324bd46b99225b
craigmccahill <craig.mccahill@gmail.com>
2022-08-16 11:36:09 +0100

    feat: add timeout on pulling blog data through

--- src/pages/blog/[blogSlug].tsx
+++ src/pages/blog/[blogSlug].tsx
@@ -192,6 +192,10 @@ export const getStaticProps: GetStaticProps<BlogPageProps, URLParams> = async (
       blog,
       isPreviewMode,
     },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
   };
 };
 
