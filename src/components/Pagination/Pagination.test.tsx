87cb34cb8f72e4386ebecf969ace428b518c1640
Mitch Lloyd <mitchelllloyd154@gmail.com>
2022-08-23 13:02:53 +0100

    test: check basic functionality of Pagination component

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,22 +1,128 @@
-import { render } from "@testing-library/react";
+import renderWithProviders from "../../__tests__/__helpers__/renderWithProviders";
 
 import Pagination from "./Pagination";
 
 describe("Pagination", () => {
-  test("it displays the correct total pages", () => {
-    render(
+  test("displays the correct total pages", () => {
+    const totalCount = 100;
+    const currentPage = 1;
+    const pageSize = 6;
+    const onPageChange = jest.fn();
+
+    const { getByText } = renderWithProviders(
       <Pagination
-        totalCount={13}
-        currentPage={1}
-        pageSize={4}
-        onPageChange={jest.fn()}
+        totalCount={totalCount}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
       />
     );
 
-    expect(true).toBe(true);
+    getByText("page 1/17");
   });
-  //   test("it displays the correct currenct page", () => {});
-  //   test("clicking next arrow takes you to the next page", () => {});
-  //   test("clicking previous arrow takes you to the next page", () => {});
-  //   test("the next arrow is disabled when there are no more pages", () => {});
+  test("displays the correct current page", () => {
+    const totalCount = 100;
+    const currentPage = 3;
+    const pageSize = 4;
+    const onPageChange = jest.fn();
+
+    const { getByText } = renderWithProviders(
+      <Pagination
+        totalCount={totalCount}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    );
+
+    getByText("page 3/25");
+  });
+  test("clicking next arrow takes you to the next page", () => {
+    const totalCount = 100;
+    const currentPage = 6;
+    const pageSize = 4;
+    const onPageChange = jest.fn();
+    const { getByText, rerender } = renderWithProviders(
+      <Pagination
+        totalCount={totalCount}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    );
+
+    getByText("page 6/25");
+    rerender(
+      <Pagination
+        totalCount={totalCount}
+        currentPage={currentPage + 1}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    );
+    getByText("page 7/25");
+  });
+  test("clicking previous arrow takes you to the previous page", () => {
+    const totalCount = 100;
+    const currentPage = 6;
+    const pageSize = 4;
+    const onPageChange = jest.fn();
+    const { getByText, rerender } = renderWithProviders(
+      <Pagination
+        totalCount={totalCount}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    );
+
+    getByText("page 6/25");
+    rerender(
+      <Pagination
+        totalCount={totalCount}
+        currentPage={currentPage - 1}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    );
+    getByText("page 5/25");
+  });
+  test("the next arrow is disabled when there are no more pages", () => {
+    const totalCount = 100;
+    const currentPage = 25;
+    const pageSize = 4;
+    const onPageChange = jest.fn();
+    const { getByText, getByLabelText } = renderWithProviders(
+      <Pagination
+        totalCount={totalCount}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    );
+    const nextButton = getByLabelText("next");
+
+    getByText("page 25/25");
+
+    expect(nextButton).toBeDisabled();
+  });
+  test("previous button is disabled on page 1", () => {
+    const totalCount = 100;
+    const currentPage = 1;
+    const pageSize = 4;
+    const onPageChange = jest.fn();
+    const { getByText, getByLabelText } = renderWithProviders(
+      <Pagination
+        totalCount={totalCount}
+        currentPage={currentPage}
+        pageSize={pageSize}
+        onPageChange={onPageChange}
+      />
+    );
+    const previousButton = getByLabelText("previous");
+
+    getByText("page 1/25");
+
+    expect(previousButton).toBeDisabled();
+  });
 });
