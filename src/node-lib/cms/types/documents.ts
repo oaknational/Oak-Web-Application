b65e89fcb20aaf068c694fb61396a4a9fcddc62c
Ross Mackay <ross@mcky.me>
2022-08-25 09:38:55 +0100

    fix: correct mocks and ts type for blog category slugs

--- src/node-lib/cms/types/documents.ts
+++ src/node-lib/cms/types/documents.ts
@@ -31,9 +31,7 @@ export type BlogPost = Document & {
   contentPortableText: PortableTextJSON;
   category: {
     title: string;
-    slug: {
-      current: string;
-    };
+    slug: string;
   };
 };
 
