1727b8ea057dbe5d2db83b8542bb7147cac9eac5
craigmccahill <craig.mccahill@gmail.com>
2022-09-05 07:32:49 +0100

    test: add tests to ordered list

--- src/components/Typography/OL.test.tsx
+++ src/components/Typography/OL.test.tsx
@@ -1,21 +1,22 @@
+import { screen, within } from "@testing-library/react";
+
 import renderWithProviders from "../../__tests__/__helpers__/renderWithProviders";
 
 import OL from "./OL";
 
 describe("P", () => {
-  test("should render numbers in bold", () => {
-    const { getByTestId, debug } = renderWithProviders(
+  test("it renders a list", () => {
+    renderWithProviders(
       <OL>
-        <li data-testid="ordered-list-item">One</li>
+        <li>One</li>
         <li>Two</li>
         <li>Three</li>
       </OL>
     );
 
-    debug();
-    //     expect(getByTestId("ordered-list-item")).toHaveStyle(`&::before {
-    // font-weight: 600;
-    //       }
-    //     `);
+    const list = screen.getByRole("list");
+    const { getAllByRole } = within(list);
+    const items = getAllByRole("listitem");
+    expect(items.length).toBe(3);
   });
 });
