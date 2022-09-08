8861aba93f19916c4324839e7a9ccd8993e9d212
craigmccahill <craig.mccahill@gmail.com>
2022-09-08 14:57:32 +0100

    fix: push borders in one by pixel for browser pixel rounding errors

--- src/components/Button/ButtonAsLink.tsx
+++ src/components/Button/ButtonAsLink.tsx
@@ -16,7 +16,6 @@ import {
 
 const StyledA = styled.a<ButtonStylesProps>`
   ${buttonStyles}
-  outline: ${(props) => `solid ${props.background} 2px`};
   ${({ disabled }) =>
     disabled === true &&
     `
