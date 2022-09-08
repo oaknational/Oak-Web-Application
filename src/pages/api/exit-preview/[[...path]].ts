4dc116e87f8559e9d07692cfa20993db0b5d2ddb
Ross Mackay <ross@mcky.me>
2022-09-08 18:46:38 +0100

    feat: tests for exit-preview, bring back in line with enter preview api

--- src/pages/api/exit-preview/[[...path]].ts
+++ src/pages/api/exit-preview/[[...path]].ts
@@ -1,20 +1,38 @@
 import type { NextApiHandler } from "next";
 import { z } from "zod";
 
+import errorReporter from "../../../common-lib/error-reporter";
+import OakError from "../../../errors/OakError";
 import { slugStringSchema } from "../preview/[[...path]]";
 
+const reportError = errorReporter("/api/exit-preview/[[...path]]");
+
 const preview: NextApiHandler = async (req, res) => {
-  const redirectLocation = z
-    .array(slugStringSchema)
-    .transform((segments) => {
-      return `/${segments.join("/")}`;
-    })
-    .parse(req.query.path || []);
+  try {
+    const redirectLocation = z
+      .array(slugStringSchema)
+      .transform((segments) => {
+        return `/${segments.join("/")}`;
+      })
+      .parse(req.query.path || []);
 
-  res.clearPreviewData();
+    res.clearPreviewData();
 
-  res.writeHead(307, { Location: redirectLocation });
-  res.end();
+    res.writeHead(307, { Location: redirectLocation });
+    res.end();
+  } catch (error) {
+    reportError(error);
+
+    if (error instanceof OakError) {
+      return res.status(error.config.responseStatusCode || 500).json({
+        code: error.code,
+      });
+    }
+
+    return res.status(500).json({
+      code: "misc/unknown",
+    });
+  }
 };
 
 export default preview;
