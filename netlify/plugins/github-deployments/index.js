6e12d201d8c42862022024c5c9d394d3e197044a
Jim Cresswell <jim.cresswell@gmail.com>
2022-09-15 16:50:02 +0100

    fix(Netlify): use CJS because Netlify

--- netlify/plugins/github-deployments/index.js
+++ netlify/plugins/github-deployments/index.js
@@ -3,21 +3,21 @@
  * @todo are there published types for these functions?
  */
 
-export const onPreBuild = function ({ packageJson, netlifyConfig }) {
+module.exports.onPreBuild = function ({ packageJson, netlifyConfig }) {
   console.log("*** onPreBuild ***");
   console.log("packageJson", packageJson);
   console.log("netlifyConfig", netlifyConfig);
   console.log("ENV:", process.env);
 };
 
-export const onError = function ({ packageJson, netlifyConfig }) {
+module.exports.onError = function ({ packageJson, netlifyConfig }) {
   console.log("*** onError ***");
   console.log("packageJson", packageJson);
   console.log("netlifyConfig", netlifyConfig);
   console.log("ENV:", process.env);
 };
 
-export const onSuccess = function ({ packageJson, netlifyConfig }) {
+module.exports.onSuccess = function ({ packageJson, netlifyConfig }) {
   console.log("*** onSuccess ***");
   console.log("packageJson", packageJson);
   console.log("netlifyConfig", netlifyConfig);
