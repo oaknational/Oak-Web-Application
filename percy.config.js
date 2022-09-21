e1c47ccbad0554fb46a13da2d87b351f3e04d565
Jim Cresswell <jim.cresswell@gmail.com>
2022-09-21 16:30:04 +0100

    chore(CI): have Percy set the CF Access headers

--- percy.config.js
+++ percy.config.js
@@ -4,6 +4,16 @@
  *
  * Note: Percy is triggered in Github workflows, the logic is here .github/actions/percy_snapshots/index.js
  */
+
+// Cloudflare Access token
+const CfAccessClientId = process.env.CF_ACCESS_CLIENT_ID;
+const CfAccessClientSecret = process.env.CF_ACCESS_CLIENT_SECRET;
+if (!CfAccessClientId || !CfAccessClientSecret) {
+  throw new TypeError(
+    "Please specify Cloudflare Access token headers in envs\nfor background info see https://developers.cloudflare.com/cloudflare-one/identity/service-tokens/"
+  );
+}
+
 module.exports = {
   version: 2,
   snapshot: {
@@ -16,5 +26,9 @@ module.exports = {
   discovery: {
     allowedHostnames: [],
     userAgent: "Percy",
+    requestHeaders: {
+      "CF-Access-Client-Id": CfAccessClientId,
+      "CF-Access-Client-Secret": CfAccessClientSecret,
+    },
   },
 };
