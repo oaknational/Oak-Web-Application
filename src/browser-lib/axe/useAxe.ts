ff57db09ba644085767b0ed465313628bd9f5264
mantagen <contact.mantagen@gmail.com>
2022-09-23 17:07:45 +0100

    test: useAxe

--- src/browser-lib/axe/useAxe.ts
+++ src/browser-lib/axe/useAxe.ts
@@ -1,30 +1,6 @@
-import React, { useEffect } from "react";
-import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
-import isBrowser from "../../utils/isBrowser";
-
-const axeConfig = {
-  rules: [
-    {
-      id: "skip-link",
-      enabled: true,
-    },
-  ],
-  disableDeduplicate: true,
-};
-const axeContext = {
-  exclude: [
-    // Gleap
-    [".bb-logo-logo"],
-  ],
-};
-
-const startAxe = async () => {
-  if (isBrowser) {
-    const axe = (await import("@axe-core/react")).default;
-    axe(React, ReactDOM, 1000, axeConfig, axeContext);
-  }
-};
+import startAxe from "./startAxe";
 
 type UseAxeProps = {
   enabled: boolean;
