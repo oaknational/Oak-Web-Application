e339a544b3133df057507c12211675e637131bf0
mantagen <contact.mantagen@gmail.com>
2022-09-27 17:39:45 +0100

    fix(blog-list-item): remove image at tablet breakpoint

--- src/components/BlogList/BlogListItem/BlogListItem.tsx
+++ src/components/BlogList/BlogListItem/BlogListItem.tsx
@@ -61,7 +61,13 @@ const BlogListItem: FC<BlogListItemProps> = (props) => {
       $pa={0}
     >
       {withImage && mainImage && (
-        <Box $position={"relative"} $minWidth={240} $mr={[0, 32]} $mb={[32, 0]}>
+        <Box
+          $display={["block", "none", "block"]}
+          $position={"relative"}
+          $minWidth={240}
+          $mr={[0, 32]}
+          $mb={[32, 0]}
+        >
           <BoxBorders zIndex={"inFront"} gapPosition="bottomRight" />
           <Box $ma={1}>
             <AspectRatio ratio={"3:2"}>
