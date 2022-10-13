18da1e4b337f990a95652dc7825d1d2b87bd2fb4
craigmccahill <craig.mccahill@gmail.com>
2022-10-13 11:20:20 +0100

    test: menu close button updates context

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -1,6 +1,8 @@
+import userEvent from "@testing-library/user-event";
 import mockRouter from "next-router-mock";
 
 import { MenuProvider } from "../../context/Menu";
+import { menuContext } from "../../context/Menu/MenuProvider";
 import renderWithTheme from "../../__tests__/__helpers__/renderWithTheme";
 
 import Menu from "./Menu";
@@ -20,5 +22,25 @@ describe("Menu", () => {
     );
 
     expect(getByLabelText("Close Menu")).toBeInTheDocument();
+  });
+
+  test("clicking the close button runs the closeMenu callback", async () => {
+    const menuValue = {
+      open: true,
+      toggleMenu: jest.fn(),
+      closeMenu: jest.fn(),
+    };
+
+    const { getByLabelText } = renderWithTheme(
+      <menuContext.Provider value={menuValue}>
+        <Menu />
+      </menuContext.Provider>
+    );
+    const user = userEvent.setup();
+    const closeButton = getByLabelText("Close Menu");
+
+    await user.click(closeButton);
+
+    expect(menuValue.closeMenu).toHaveBeenCalled();
   });
 });
