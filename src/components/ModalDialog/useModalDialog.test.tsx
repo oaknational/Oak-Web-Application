34e104fd3d3b4b5cb6b876dc58a4120c3d9eb10f
mantagen <contact.mantagen@gmail.com>
2022-10-19 09:36:36 +0100

    test(modal-dialog): test escape key to close modal

--- src/components/ModalDialog/useModalDialog.test.tsx
+++ src/components/ModalDialog/useModalDialog.test.tsx
@@ -1,5 +1,6 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { OverlayProvider } from "react-aria";
+import userEvent from "@testing-library/user-event";
 
 import noop from "../../__tests__/__helpers__/noop";
 import renderWithTheme from "../../__tests__/__helpers__/renderWithTheme";
@@ -25,5 +26,25 @@ describe("useModalDialog", () => {
       // @todo this id should not be null
       id: null,
     });
+  });
+  test("escape calls closeModal()", async () => {
+    const closeModal = jest.fn();
+    const { result } = renderHook(
+      () =>
+        useModalDialog({
+          size: "fullscreen",
+          closeModal,
+          isOpen: true,
+          isDismissable: true,
+          isKeyboardDismissDisabled: true,
+        }),
+      { wrapper: OverlayProvider }
+    );
+    renderWithTheme(<ModalDialog {...result.current}>Contents</ModalDialog>);
+
+    const user = userEvent.setup();
+    await user.keyboard("{Escape}");
+
+    expect(result.current.closeModal).toHaveBeenCalled();
   });
 });
