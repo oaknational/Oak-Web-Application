60e4d01c197d222a248a633cc9690031fb4b6c0b
Joe Baker <blakeage77@googlemail.com>
2022-10-20 08:19:32 +0100

    fix: remove nested a tag from oak link

--- src/components/Forms/NewsletterForm/NewsletterFormWrap.tsx
+++ src/components/Forms/NewsletterForm/NewsletterFormWrap.tsx
@@ -40,10 +40,7 @@ const NewsletterFormWrap: FC<NewsletterFormWrapProps> = (props) => {
       <P $mb={24} color={"black"} id={descriptionId}>
         Join 80,000 teachers and get free resources and other helpful content by
         email. Unsubscribe at any time. Read our{" "}
-        <OakLink page="privacy-policy">
-          <a>privacy policy</a>
-        </OakLink>
-        .
+        <OakLink page="privacy-policy">privacy policy</OakLink>
       </P>
       <NewsletterForm descriptionId={descriptionId} id={id} {...props} />
     </Card>
