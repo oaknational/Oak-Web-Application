d767287930c6efcd3c2efa309e6049c44de97684
Ross Mackay <ross@mcky.me>
2022-10-21 18:14:03 +0100

    refactor(hsforms): don't wrap/unwrap fields in arrays when transforming
    The use of flatMap with dependent fields was necessitating extra
    wrapping and unwrapping of array values to appease the type system.
    Split the function 2 to remove the need, and make responsibilities
    clearer

--- src/node-lib/hubspot-forms/transformHubspotForm.ts
+++ src/node-lib/hubspot-forms/transformHubspotForm.ts
@@ -41,7 +41,7 @@ const addConditions = (
   return { ...field, renderWhen: conditions };
 };
 
-const transformField = (field: HubspotFormField): FormField[] => {
+const transformField = (field: HubspotFormField): FormField => {
   const baseFields = {
     name: field.name,
     description: field.description || null,
@@ -52,62 +52,27 @@ const transformField = (field: HubspotFormField): FormField[] => {
     required: field.required ?? true,
   };
 
-  if (field.dependentFieldFilters?.length > 0) {
-    const { dependentFieldFilters, ...restOfField } = field;
-
-    const mainField = transformField({
-      ...restOfField,
-      dependentFieldFilters: [],
-    });
-    const wrappedMainField = Array.isArray(mainField) ? mainField : [mainField];
-
-    const dependentFields = dependentFieldFilters.map((dependentFieldDef) => {
-      const transformedDependentField = transformField(
-        dependentFieldDef.dependentFormField
-      );
-
-      // Since our transform can return an array of fields, unbox the value which
-      // should always be of length 1, and add a guard to appease typescript
-      const unboxedDependentField = Array.isArray(transformedDependentField)
-        ? transformedDependentField[0]
-        : transformedDependentField;
-
-      if (!unboxedDependentField) {
-        throw new Error("Nope");
-      }
-      return addConditions(
-        unboxedDependentField,
-        dependentFieldDef.filters,
-        restOfField.name
-      );
-    });
-
-    return [...wrappedMainField, ...dependentFields];
-  }
-
   if (field.name === "email") {
     /**
      * Our main hubspot form has `{ type:"string", fieldType:"text"}` but hubspot treats it
      * as an email with appropriate validation, so hack around it here so we handle
      * it correctly internally
      */
-    return [{ ...baseFields, type: "email" }];
+    return { ...baseFields, type: "email" };
   }
 
   switch (field.type) {
     case "string":
-      return [{ ...baseFields, type: "string" }];
+      return { ...baseFields, type: "string" };
     case "enumeration":
-      return [
-        {
-          ...baseFields,
-          type: "select",
-          options: field.options.map((opt) => ({
-            label: opt.label,
-            value: opt.value,
-          })),
-        },
-      ];
+      return {
+        ...baseFields,
+        type: "select",
+        options: field.options.map((opt) => ({
+          label: opt.label,
+          value: opt.value,
+        })),
+      };
     default:
       assertUnreachable(
         field,
@@ -116,11 +81,34 @@ const transformField = (field: HubspotFormField): FormField[] => {
   }
 };
 
+const transformFieldAndDependents = (field: HubspotFormField): FormField[] => {
+  if (field.dependentFieldFilters?.length > 0) {
+    const { dependentFieldFilters, ...restOfField } = field;
+
+    const mainField = transformField({
+      ...restOfField,
+      dependentFieldFilters: [],
+    });
+
+    const dependentFields = dependentFieldFilters.map((dependentFieldDef) =>
+      addConditions(
+        transformField(dependentFieldDef.dependentFormField),
+        dependentFieldDef.filters,
+        restOfField.name
+      )
+    );
+
+    return [mainField, ...dependentFields];
+  }
+
+  return [transformField(field)];
+};
+
 export const transformHubspotForm = (
   form: HubspotFormDefinition
 ): FormDefinition => {
   const flattenedFields = form.formFieldGroups.flatMap((fieldGroup) =>
-    fieldGroup.fields.flatMap(transformField)
+    fieldGroup.fields.flatMap(transformFieldAndDependents)
   );
 
   return {
