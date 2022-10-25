f33775af0b53f71df961e0eb67d5d8c49fd812f9
Ross Mackay <ross@mcky.me>
2022-10-25 19:38:27 +0100

    wip

---
+++ src/pages/api/og/blog/[blogSlug].tsx
@@ -0,0 +1,23 @@
+import { ImageResponse } from '@vercel/og';
+
+export const config = {
+  runtime: 'experimental-edge',
+};
+
+export default function () {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          fontSize: 128,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        Hello, World!
+      </div>
+    ),
+  );
+}
