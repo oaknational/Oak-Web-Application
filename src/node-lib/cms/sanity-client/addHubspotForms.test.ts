c0e0088630830762d075ab8b463b240a69e46dcb
Ross Mackay <ross@mcky.me>
2022-10-26 18:32:11 +0100

    feat(hsforms): resolve references to hubspot forms in documents

---
+++ src/node-lib/cms/sanity-client/addHubspotForms.test.ts
@@ -0,0 +1,106 @@
+import OakError from "../../../errors/OakError";
+import { getHubspotFormById } from "../../hubspot-forms";
+
+import { addHubspotForms } from "./addHubspotForms";
+
+jest.mock("../../hubspot-forms");
+
+const mockedGetHubspotFormById = getHubspotFormById as jest.MockedFn<
+  typeof getHubspotFormById
+>;
+
+describe("addHubspotForms", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+
+    mockedGetHubspotFormById
+      .mockResolvedValueOnce({
+        portalId: 12345,
+        formId: "abcd-efgh-ijkl",
+        fields: [{ name: "name", label: "Name", type: "string" }],
+      })
+      .mockResolvedValueOnce({
+        portalId: 6789,
+        formId: "mnop-qrst-uvwx",
+        fields: [{ name: "email", label: "Email", type: "email" }],
+      });
+  });
+
+  const mockObjWithReferences = {
+    foo: {
+      bar: {
+        hubspotForm: { id: "abcd-efgh-ijkl" },
+      },
+    },
+    baz: [
+      {
+        hubspotForm: { id: "mnop-qrst-uvwx" },
+      },
+    ],
+  };
+
+  it("replaces each hubspotForm object with the result of the api call", async () => {
+    const resolved = await addHubspotForms(mockObjWithReferences);
+
+    expect(resolved.foo.bar.hubspotForm).toMatchObject({
+      portalId: 12345,
+      formId: "abcd-efgh-ijkl",
+      fields: [{ name: "name", label: "Name", type: "string" }],
+    });
+
+    expect(resolved?.baz?.[0]?.hubspotForm).toMatchObject({
+      portalId: 6789,
+      formId: "mnop-qrst-uvwx",
+      fields: [{ name: "email", label: "Email", type: "email" }],
+    });
+  });
+
+  it("calls getHubspotFormById with each referenced form ID", async () => {
+    mockedGetHubspotFormById
+      .mockResolvedValueOnce({
+        portalId: 12345,
+        formId: "abcd-efgh-ijkl",
+        fields: [],
+      })
+      .mockResolvedValueOnce({
+        portalId: 6789,
+        formId: "mnop-qrst-uvwx",
+        fields: [],
+      });
+
+    await addHubspotForms(mockObjWithReferences);
+
+    expect(getHubspotFormById).toHaveBeenNthCalledWith(1, "abcd-efgh-ijkl");
+    expect(getHubspotFormById).toHaveBeenNthCalledWith(2, "mnop-qrst-uvwx");
+  });
+
+  it.skip("throws an OakError with metadata when it can't match refs to responses", async () => {
+    // @TODO: getById fail response
+    const mockErrorCausingResponse = [
+      {
+        contentType: "aboutCorePage",
+        _type: "aboutCorePage",
+        id: "wont-be-found",
+      },
+    ];
+
+    mockedGetHubspotFormById.mockResolvedValue(mockErrorCausingResponse);
+
+    const capturedError = await addHubspotForms(mockObjWithReferences).catch(
+      (err) => err
+    );
+
+    await expect(
+      async () => await addHubspotForms(mockObjWithReferences)
+    ).rejects.toThrowError(
+      new OakError({ code: "cms/invalid-reference-data" })
+    );
+
+    expect(capturedError.meta).toEqual({
+      portableTextPath: ["foo", "bar", "post"],
+      portableTextRefId: "ref1",
+      queryResults: JSON.stringify(mockErrorCausingResponse),
+    });
+  });
+});
---
+++ src/node-lib/cms/sanity-client/addHubspotForms.ts
@@ -0,0 +1,112 @@
+import { get, update } from "lodash/fp";
+
+import OakError from "../../../errors/OakError";
+import { getHubspotFormById } from "../../hubspot-forms";
+
+type ObjectPath = string[];
+
+/**
+ * Given a portable text JSON blob, search for all objects that have
+ * `{_type: "reference"}` and fetch and replace them with actual content
+ */
+export const addHubspotForms = async <
+  T extends Record<string, unknown> | Record<string, unknown>[]
+>(
+  document: T
+): Promise<T> => {
+  /**
+   * Find all paths to embedded references within the document, e.g.
+   * [[0, 'sidebar', 'sidebarForm'], [5, 'formBlock']]
+   * We hold onto this paths array so we can update them later
+   */
+  const pathsToUpdate = getAllPaths(document, isHubspotFormObject);
+
+  /**
+   * Grab the actual _ref for each of the paths and store in a tuple with the path
+   */
+  const pathsAndRefs: [ObjectPath, { id: string }][] = pathsToUpdate.map(
+    (path) => [path, get([...path, "hubspotForm"], document)]
+  );
+
+  // doc is tuple of [id, resolved form]
+  const queryResults = (
+    await Promise.allSettled(
+      pathsAndRefs.map(async ([, formRef]) => {
+        const actualForm = await getHubspotFormById(formRef.id);
+        // TODO: loudly log error
+        return [formRef.id, actualForm];
+      })
+    )
+  ).map((result) => (result.status === "fulfilled" ? result.value : null));
+
+  /**
+   * For each of the paths we found earlier, replace the _ref object at that
+   * location with the result of the graphql query for it's data
+   */
+  const updated = pathsAndRefs.reduce((acc, [path, formRef]) => {
+    const queryPart = queryResults.find(([foundId]) => {
+      return foundId === formRef.id;
+    })?.[1];
+
+    if (!queryPart) {
+      /**
+       * If you're getting errors here make sure:
+       * - You've checked you're fetching the correct data in blogPortableTextReferences.gql
+       * - You've ran gql-codegen:sanity
+       * - Checked they're covered in portableTextReferencedEntrySchema
+       */
+      throw new OakError({
+        code: "cms/invalid-form-reference",
+        meta: {
+          portableTextPath: path,
+          portableTextRefId: formRef,
+          queryResults: JSON.stringify(queryResults.allDocument),
+        },
+      });
+    }
+
+    return update(path, (data) => ({ ...data, hubspotForm: queryPart }), acc);
+  }, document);
+
+  return updated;
+};
+
+const isHubspotFormObject = (entry: unknown): boolean => {
+  if (entry?.hubspotForm) {
+    return true;
+  }
+  return false;
+};
+
+function isRecord(value: unknown): value is Record<string, unknown> {
+  return !Array.isArray(value) && typeof value === "object" && value !== null;
+}
+
+/**
+ * Deeply search an object for each value that matches
+ * the provided predicate, returning the path for each
+ * match as an array
+ *
+ * @example
+ * getAllPaths({foo: [{bar: 'baz'}]}, x => x.bar === 'baz')
+ * // => [['foo', '0', 'bar']]
+ */
+export const getAllPaths = (
+  obj: Record<string, unknown> | Record<string, unknown>[],
+  pred: (v: unknown) => boolean,
+  prev: ObjectPath = []
+): ObjectPath[] => {
+  const result = [];
+
+  for (const [key, value] of Object.entries(obj)) {
+    const path = [...prev, key];
+
+    if (pred(value)) {
+      result.push(path);
+    } else if (value && (Array.isArray(value) || isRecord(value))) {
+      result.push(...getAllPaths(value, pred, path));
+    }
+  }
+
+  return result;
+};
