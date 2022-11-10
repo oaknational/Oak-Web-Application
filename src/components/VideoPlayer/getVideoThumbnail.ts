f1767fb2fd56876cf94d67ed9d0fb6ecae256dfc
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-10 09:54:31 +0000

    refactor: video thumbnail default values

--- src/components/VideoPlayer/getVideoThumbnail.ts
+++ src/components/VideoPlayer/getVideoThumbnail.ts
@@ -1,17 +1,10 @@
 import { SizeValues } from "../../styles/utils/size";
 
-export const getVideoThumbnail = ({
-  mainImage,
-  thumbTime,
-  width = 400,
-  height = 200,
-}: {
-  mainImage: string;
-  thumbTime?: number | null;
-  width?: SizeValues;
-  height?: SizeValues;
-}) => {
-  return `https://image.mux.com/${mainImage}/thumbnail.png?width=${width}&height=${height}&fit_mode=smartcrop&time=${
-    thumbTime ? thumbTime : 20
-  }`;
+export const getVideoThumbnail = (
+  mainImage: string,
+  thumbTime: number | null = 20,
+  width: SizeValues = 400,
+  height: SizeValues = 200
+) => {
+  return `https://image.mux.com/${mainImage}/thumbnail.png?width=${width}&height=${height}&fit_mode=smartcrop&time=${thumbTime}`;
 };
