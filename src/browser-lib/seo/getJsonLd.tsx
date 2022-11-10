8bb370e474f3f724009e0f3b6884fa2105d9fbd7
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-10 09:39:58 +0000

    refactor: json ld naming and resolving url

--- src/browser-lib/seo/getJsonLd.tsx
+++ src/browser-lib/seo/getJsonLd.tsx
@@ -15,6 +15,7 @@ import config from "../../config/browser";
 import { SerializedBlogPostPreview } from "../../components/pages/BlogIndex.page";
 import { SerializedWebinarPreview } from "../../components/pages/WebinarsIndex.page";
 import { getVideoThumbnail } from "../../components/VideoPlayer/getVideoThumbnail";
+import { resolveOakHref } from "../../common-lib/urls";
 
 const courseProvider = {
   name: config.get("appName"),
@@ -65,10 +66,13 @@ export const BreadcrumbJsonLd: FC<BreadcrumbProps> = (props) => {
 
 // Blogs
 
-const blogObject = (blog: SerializedBlogPostPreview): ArticleJsonLdProps => {
+const blogToArticle = (blog: SerializedBlogPostPreview): ArticleJsonLdProps => {
   return {
     type: "Article",
-    url: `${config.get("appUrl")}/blog/${blog.slug}`,
+    url: `${config.get("appUrl")}${resolveOakHref({
+      page: "blog",
+      slug: blog.slug,
+    })}`,
     title: blog.seo?.title || blog.title,
     images: [blog.mainImage.asset?.url || config.get("appLogo")],
     datePublished: blog.date,
@@ -78,21 +82,26 @@ const blogObject = (blog: SerializedBlogPostPreview): ArticleJsonLdProps => {
   };
 };
 
-const webinarObject = (blog: SerializedWebinarPreview): ArticleJsonLdProps => {
+const webinarToArticle = (
+  webinar: SerializedWebinarPreview
+): ArticleJsonLdProps => {
   return {
     type: "Article",
-    url: `${config.get("appUrl")}/beta/webinars/${blog.slug}`, // @todo remove beta on merge
-    title: blog.seo?.title || blog.title,
+    url: `${config.get("appUrl")}${resolveOakHref({
+      page: "webinars",
+      slug: webinar.slug,
+    })}`,
+    title: webinar.seo?.title || webinar.title,
     images: [
       getVideoThumbnail(
-        blog.video.video.asset.playbackId,
-        blog.video.video.asset.thumbTime
+        webinar.video.video.asset.playbackId,
+        webinar.video.video.asset.thumbTime
       ) || config.get("appLogo"),
     ],
-    datePublished: blog.date,
-    dateModified: blog.date,
-    authorName: blog.hosts ? blog.hosts[0]?.name : "",
-    description: blog.seo?.description || blog.title,
+    datePublished: webinar.date,
+    dateModified: webinar.date,
+    authorName: webinar.hosts ? webinar.hosts[0]?.name : "",
+    description: webinar.seo?.description || webinar.title,
   };
 };
 
@@ -103,7 +112,7 @@ type BlogListJsonLdProps = {
 export const BlogListJsonLd: FC<BlogListJsonLdProps> = ({ blogs }) => {
   const blogListForJsonLd = blogs.map((blog, index) => {
     const blogObjects =
-      "video" in blog ? webinarObject(blog) : blogObject(blog);
+      "video" in blog ? webinarToArticle(blog) : blogToArticle(blog);
     return {
       position: index + 1,
       courseName: blog.title,
@@ -126,7 +135,10 @@ export const BlogJsonLd: FC<BlogJsonLdProps> = ({ blog }) => {
   return "video" in blog
     ? ArticleJsonLdNextSeo({
         isAccessibleForFree: true,
-        ...webinarObject(blog),
+        ...webinarToArticle(blog),
       })
-    : ArticleJsonLdNextSeo({ isAccessibleForFree: true, ...blogObject(blog) });
+    : ArticleJsonLdNextSeo({
+        isAccessibleForFree: true,
+        ...blogToArticle(blog),
+      });
 };
