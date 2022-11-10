abb2d82e26214d1c7cda9e9c2c0c5015e69d8542
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-10 09:41:22 +0000

    refactor: video thumbnail width and height props and defaults

--- src/components/VideoPlayer/getVideoThumbnail.ts
+++ src/components/VideoPlayer/getVideoThumbnail.ts
@@ -1,8 +1,17 @@
-export const getVideoThumbnail = (
-  mainImage: string,
-  thumbTime?: number | null
-) => {
-  return `https://image.mux.com/${mainImage}/thumbnail.png?width=400&height=200&fit_mode=smartcrop&time=${
+import { SizeValues } from "../../styles/utils/size";
+
+export const getVideoThumbnail = ({
+  mainImage,
+  thumbTime,
+  width = 400,
+  height = 200,
+}: {
+  mainImage: string;
+  thumbTime?: number | null;
+  width?: SizeValues;
+  height?: SizeValues;
+}) => {
+  return `https://image.mux.com/${mainImage}/thumbnail.png?width=${width}&height=${height}&fit_mode=smartcrop&time=${
     thumbTime ? thumbTime : 20
   }`;
 };
