584c3d46b10e082ac90d3dd8b6551f8171a9e92f
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-15 12:40:32 +0000

    style: remove notch in border for webinars

--- src/components/Blog/BlogList/BlogListItem/BlogListItem.tsx
+++ src/components/Blog/BlogList/BlogListItem/BlogListItem.tsx
@@ -114,7 +114,10 @@ const BlogListItem: FC<BlogListItemProps> = (props) => {
           $mr={[0, 32]}
           $mb={[32, 0]}
         >
-          <BoxBorders $zIndex={"inFront"} gapPosition="bottomRight" />
+          <BoxBorders
+            $zIndex={"inFront"}
+            gapPosition={contentType === "webinar" ? undefined : "bottomRight"}
+          />
           <Box $ma={1}>
             <AspectRatio ratio={"3:2"}>
               {contentType === "blog-post" ? (
