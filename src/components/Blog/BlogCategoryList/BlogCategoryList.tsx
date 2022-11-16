37bf01a10fd3927039562cfb5df0eef896f00a37
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-16 15:26:41 +0000

    style: change focus size

--- src/components/Blog/BlogCategoryList/BlogCategoryList.tsx
+++ src/components/Blog/BlogCategoryList/BlogCategoryList.tsx
@@ -4,6 +4,7 @@ import { BoxProps } from "../../Box";
 import { LI, UL } from "../../Typography";
 import OakLink from "../../OakLink";
 import Icon from "../../Icon";
+import Flex from "../../Flex";
 
 import BlogCategoryListItem from "./BlogCategoryListItem";
 
@@ -45,21 +46,24 @@ const BlogCategoryList: FC<BlogCategoryListProps> = (props) => {
         })}
 
         <LI $mt={32} $height={30} $font={"heading-7"}>
-          <OakLink
-            $display="flex"
-            $height="100%"
-            $alignItems="center"
-            page={page === "webinars-index" ? "blog-index" : "webinars-index"}
-          >
-            {`Switch to ${page === "blog-index" ? "webinars" : "blogs"}`}
-            <Icon
-              $ml={12}
-              variant="brush"
-              size={30}
-              $background={"teachersHighlight"}
-              name="ArrowRight"
-            />
-          </OakLink>
+          <Flex>
+            <OakLink
+              $display="flex"
+              $width={"auto"}
+              $height="100%"
+              $alignItems="center"
+              page={page === "webinars-index" ? "blog-index" : "webinars-index"}
+            >
+              {`Switch to ${page === "blog-index" ? "webinars" : "blogs"}`}
+              <Icon
+                $ml={12}
+                variant="brush"
+                size={30}
+                $background={"teachersHighlight"}
+                name="ArrowRight"
+              />
+            </OakLink>
+          </Flex>
         </LI>
       </UL>
     </nav>
