970721a353eb76970788f674c4afbb2a6c4f635f
craigmccahill <craig.mccahill@gmail.com>
2022-11-22 16:47:05 +0000

    feat: add teachers page with new header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 
-import SearchForm from "../SearchForm";
 import Flex from "../Flex";
 import FixedHeader from "../FixedHeader";
 import Logo from "../Logo";
 import { HeaderProps } from "../Layout/Layout";
 import OakLink from "../OakLink";
+import Svg from "../Svg";
+import Box from "../Box";
 
 /**
  * Header for logging in and using search -
@@ -14,13 +15,21 @@ import OakLink from "../OakLink";
  */
 const AppHeader: FC<HeaderProps> = () => {
   return (
-    <FixedHeader $background="grey3">
+    <FixedHeader $background="pastelTurqoise">
       <Flex $mr={40} $justifyContent={"space-between"}>
         <OakLink page={"home"}>
           <Logo title={"Oak National Academy"} height={48} width={104} />
         </OakLink>
-        <SearchForm />
       </Flex>
+      <Box
+        $position="absolute"
+        $height={[8, 12]}
+        $bottom={[4, -4]}
+        $right={0}
+        $left={0}
+      >
+        <Svg name="HeaderUnderline" $color="teachersHighlight" />
+      </Box>
     </FixedHeader>
   );
 };
