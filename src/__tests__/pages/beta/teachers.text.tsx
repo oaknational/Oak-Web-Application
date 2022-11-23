8d93d8881c149897814e367171bb087c804716e6
craigmccahill <craig.mccahill@gmail.com>
2022-11-23 15:39:52 +0000

    feat: add hamburger menu and tests to app header

--- src/__tests__/pages/beta/teachers.text.tsx
+++ src/__tests__/pages/beta/teachers.text.tsx
@@ -4,7 +4,7 @@ import Teachers from "../../../pages/beta/teachers";
 import renderWithProviders from "../../__helpers__/renderWithProviders";
 
 describe("pages/beta/teachers.tsx", () => {
-  test("it renders correct title ", () => {
+  test("it renders correct title", () => {
     renderWithProviders(<Teachers />);
     expect(screen.getByRole("heading", { level: 1 }).textContent).toBe(
       "Teachers homepage"
