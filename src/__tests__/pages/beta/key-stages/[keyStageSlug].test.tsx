31bd71dc5c0ca082905a5b2f87276b427aa9cb44
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-23 19:48:09 +0000

    test: add seo test

--- src/__tests__/pages/beta/key-stages/[keyStageSlug].test.tsx
+++ src/__tests__/pages/beta/key-stages/[keyStageSlug].test.tsx
@@ -3,13 +3,30 @@ import { screen, waitFor } from "@testing-library/react";
 import KeyStageListPage, {
   KeyStageProps,
 } from "../../../../pages/beta/key-stages/[keyStageSlug]";
+import { mockSeoResult } from "../../../__helpers__/cms";
 import renderWithProviders from "../../../__helpers__/renderWithProviders";
 import renderWithSeo from "../../../__helpers__/renderWithSeo";
 
 describe("pages/key-stages/[keyStageSlug].tsx", () => {
-  describe("BlogDetailPage", () => {
-    it("Renders title from props ", async () => {
-      renderWithProviders(
+  it("Renders title from props ", async () => {
+    renderWithProviders(
+      <KeyStageListPage
+        keyStageData={{
+          data: "key-stage-1",
+        }}
+      />
+    );
+
+    await waitFor(() => {
+      expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+        "Key stage key-stage-1"
+      );
+    });
+  });
+
+  describe("SEO", () => {
+    it("renders the correct SEO details", async () => {
+      const { seo } = renderWithSeo(
         <KeyStageListPage
           keyStageData={{
             data: "key-stage-1",
@@ -17,24 +34,15 @@ describe("pages/key-stages/[keyStageSlug].tsx", () => {
         />
       );
 
-      await waitFor(() => {
-        expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
-          "Key stage key-stage-1"
-        );
-      });
-    });
-
-    describe.skip("SEO", () => {
-      it("renders the correct SEO details", async () => {
-        const { seo } = renderWithSeo(
-          <KeyStageListPage
-            keyStageData={{
-              data: "key-stage-1",
-            }}
-          />
-        );
-
-        expect(seo).toEqual({});
+      expect(seo).toEqual({
+        ...mockSeoResult,
+        ogSiteName: "NEXT_PUBLIC_SEO_APP_NAME",
+        title: "Key stage | NEXT_PUBLIC_SEO_APP_NAME",
+        description: "Key stage by subject",
+        ogTitle: "Key stage | NEXT_PUBLIC_SEO_APP_NAME",
+        ogDescription: "Key stage by subject",
+        ogUrl: "NEXT_PUBLIC_SEO_APP_URL",
+        canonical: "NEXT_PUBLIC_SEO_APP_URL",
       });
     });
   });
