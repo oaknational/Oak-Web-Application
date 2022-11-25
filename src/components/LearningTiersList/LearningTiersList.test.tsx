6e2e85da181231780ace08ae52d060b38401f3b0
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-25 10:52:32 +0000

    test: add test to check it renders the correct titles

---
+++ src/components/LearningTiersList/LearningTiersList.test.tsx
@@ -0,0 +1,32 @@
+import { screen, waitFor } from "@testing-library/react";
+
+import renderWithTheme from "../../__tests__/__helpers__/renderWithTheme";
+
+import LearningTiersList from "./LearningTiersList";
+
+describe("LearningTiersList", () => {
+  it("Renders correct titles ", async () => {
+    renderWithTheme(
+      <LearningTiersList
+        tiers={[
+          { title: "Foundation", numberUnits: 3, numberLessons: 4 },
+          { title: "Core", numberUnits: 3, numberLessons: 4 },
+          { title: "Higher", numberUnits: 3, numberLessons: 4 },
+        ]}
+        background="teachersYellow"
+      />
+    );
+
+    await waitFor(() => {
+      expect(screen.getAllByRole("heading", { level: 3 })[0]?.textContent).toBe(
+        "Foundation"
+      );
+      expect(screen.getAllByRole("heading", { level: 3 })[1]?.textContent).toBe(
+        "Core"
+      );
+      expect(screen.getAllByRole("heading", { level: 3 })[2]?.textContent).toBe(
+        "Higher"
+      );
+    });
+  });
+});
