9f3d905c1eb08a04216ff9075829366987a0074b
benprotheroe <bprotheroe28@gmail.com>
2022-11-28 14:02:24 +0000

    fix(subject card story): unit test prettier

--- src/components/Card/SubjectCardLink.test.tsx
+++ src/components/Card/SubjectCardLink.test.tsx
@@ -4,8 +4,7 @@ import renderWithTheme from "../../__tests__/__helpers__/renderWithTheme";
 
 import SubjectCardLink from "./SubjectCardLink";
 
-
-describe("SubjectCardLink", () => {   
+describe("SubjectCardLink", () => {
   test("render a Card with the Name of the Subject", () => {
     renderWithTheme(
       <SubjectCardLink
@@ -26,29 +25,27 @@ describe("SubjectCardLink", () => {
     );
     expect(screen.getByText("Art and Design")).toBeInTheDocument();
   });
-  test(
-    "if available has a link to take you to the corresponding subject page",()=>{
-        const {getByRole} = renderWithTheme(
-            <SubjectCardLink
-              imageProps={{
-                src: "/images/illustrations/subject-art-and-design.svg",
-                alt: "planning",
-              }}
-              titleTag={"h3"}
-              subjectTitle={"Art and Design"}
-              imageBackground={"teachersPastelYellow"}
-              background={"white"}
-              lessons={130}
-              units={14}
-              available={true}
-              keyStageSlug={"keyStage"}
-             subjectSlug={"subject"}
-            />
-          )
-        const cardClickTarget = getByRole("link", {
-            name: "Art and Design",
-          });
-          expect(cardClickTarget).toBeInTheDocument(); 
-    }
-  );
+  test("if available has a link to take you to the corresponding subject page", () => {
+    const { getByRole } = renderWithTheme(
+      <SubjectCardLink
+        imageProps={{
+          src: "/images/illustrations/subject-art-and-design.svg",
+          alt: "planning",
+        }}
+        titleTag={"h3"}
+        subjectTitle={"Art and Design"}
+        imageBackground={"teachersPastelYellow"}
+        background={"white"}
+        lessons={130}
+        units={14}
+        available={true}
+        keyStageSlug={"keyStage"}
+        subjectSlug={"subject"}
+      />
+    );
+    const cardClickTarget = getByRole("link", {
+      name: "Art and Design",
+    });
+    expect(cardClickTarget).toBeInTheDocument();
+  });
 });
