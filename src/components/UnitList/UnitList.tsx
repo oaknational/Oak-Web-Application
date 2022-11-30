34d4efe6cfb61274a534f73f3bc142f76609a26e
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-11-30 12:16:49 +0000

    style: unit and unit list item

--- src/components/UnitList/UnitList.tsx
+++ src/components/UnitList/UnitList.tsx
@@ -1,18 +1,30 @@
 import { FC } from "react";
-import Box from "../Box";
+import { useRouter } from "next/router";
 
+import Box from "../Box";
 import Flex from "../Flex";
 import Pagination, { PaginationProps } from "../Pagination";
-import { Hr, LI, UL } from "../Typography";
+import { Heading, LI, Span, UL } from "../Typography";
+import { HeadingTag } from "../Typography/Heading";
+import OakLink from "../OakLink";
+
 import UnitListItem from "./UnitListItem";
 import { UnitListItemProps } from "./UnitListItem/UnitListItem";
+
+export type Tier = {
+  title: "Foundation" | "Core" | "Higher";
+  slug: "foundation" | "core" | "higher";
+  unitCount: number;
+};
 
 export type UnitListProps = {
   units: UnitListItemProps[];
   paginationProps: PaginationProps;
+  headingTag: HeadingTag;
+  tiers?: Tier[];
 };
 /**
- * Contains a list of UnitListItem with dividers between them. Optionally
+ * Contains a list of Units
  * displays an upcoming webinar at the top.
  *
  * ## Usage
@@ -20,20 +32,36 @@ export type UnitListProps = {
  * Use `useUnitList()` to get props to pass to `<UnitList />`
  */
 const UnitList: FC<UnitListProps> = (props) => {
-  const { units, paginationProps } = props;
+  const { units, paginationProps, headingTag, tiers } = props;
+  const router = useRouter();
 
   return (
-    <Flex
-      $flexDirection="column"
-      $alignItems="flex-start"
-      $minHeight={[0, 840]}
-    >
+    <Flex $flexDirection="column">
+      <Heading $mb={24} tag={headingTag}>
+        Units
+      </Heading>
+      <Flex $mb={32}>
+        {tiers &&
+          tiers.map(({ title, slug, unitCount }) => (
+            // @todo change page to unit when slug page when it exists
+            <OakLink
+              path={router.pathname}
+              selectedTier={{ tier: slug }}
+              page={"unit-tiers"}
+            >
+              <Span
+                $font={"heading-7"}
+                $mr={32}
+              >{`${title} (${unitCount})`}</Span>
+            </OakLink>
+          ))}
+      </Flex>
+
       {units.length ? (
         <>
           <UL $reset>
             {units.map((item, i) => (
               <LI key={`UnitList-UnitListItem-${i}`}>
-                {i !== 0 && <Hr thickness={4} $mv={32} />}
                 <UnitListItem {...item} />
               </LI>
             ))}
--- src/components/UnitList/UnitListItem/UnitListItem.tsx
+++ src/components/UnitList/UnitListItem/UnitListItem.tsx
@@ -1,18 +1,20 @@
 import { FC } from "react";
-import { useHover } from "react-aria";
 
 import useClickableCard from "../../../hooks/useClickableCard";
-
 import Flex from "../../Flex";
 import Icon, { IconName } from "../../Icon";
-import { Heading } from "../../Typography";
+import { Heading, Span } from "../../Typography";
 import { OakColorName } from "../../../styles/theme/types";
+import BoxBorders from "../../SpriteSheet/BrushSvgs/BoxBorders";
+import Card from "../../Card";
+import OakLink from "../../OakLink";
 
 export type UnitListItemProps = {
   title: string;
-  learningTheme?: string;
-  totalLessons: number;
-  unitQuiz?: string;
+  slug: string;
+  learningThemeTitle?: string;
+  lessonCount: number;
+  hasUnitQuiz: boolean;
   subjectIcon: IconName;
   background: OakColorName;
 };
@@ -24,57 +26,70 @@ export type UnitListItemProps = {
  * The title tag (h1, h2, ...) is passed as a prop.
  */
 const UnitListItem: FC<UnitListItemProps> = (props) => {
-  const { title, learningTheme, totalLessons, unitQuiz, subjectIcon } = props;
-
   const {
-    containerProps,
-    primaryTargetProps,
-    isHovered: cardIsHovered,
-  } = useClickableCard<HTMLAnchorElement>();
-  const { hoverProps: categoryHoverProps, isHovered: categoryIsHovered } =
-    useHover({});
+    title,
+    learningThemeTitle,
+    lessonCount,
+    hasUnitQuiz,
+    subjectIcon,
+    slug,
+  } = props;
+
+  const { containerProps, isHovered, primaryTargetProps } =
+    useClickableCard<HTMLAnchorElement>();
 
   return (
-    <Flex
-      {...containerProps}
-      $position={"relative"}
-      $flexDirection={["column", "row"]}
-      $justifyContent={"space-between"}
-      $alignItems={"center"}
-      $pa={0}
-    >
-      <Flex $flexDirection={"column"}>
-        <Heading $font={"heading-6"} tag={"h3"}>
-          {title}
-        </Heading>
-        <Flex $flexDirection={["column", "row"]}>
-          {learningTheme && (
-            <Heading $font={"heading-light-7"} tag={"h4"}>
-              {learningTheme}
+    <Card $mb={16} $overflow={"hidden"} {...containerProps} $pa={0}>
+      <Flex
+        $transform={isHovered ? "translateY(-4px)" : null}
+        $transition={"all 0.4s ease-out"}
+        $width={"100%"}
+        $position={"relative"}
+        $flexDirection={"row"}
+        $justifyContent={"space-between"}
+        $alignItems={"center"}
+        $pa={0}
+        $dropShadow={isHovered ? "subjectCardHover" : "subjectCard"}
+      >
+        <Flex $mh={[16, 24]} $flexDirection={"column"}>
+          <OakLink slug={slug} page={"unit"} {...primaryTargetProps}>
+            <Heading $mb={12} $font={["heading-7", "heading-6"]} tag={"h3"}>
+              {title}
             </Heading>
-          )}
-          <Heading $font={"heading-light-7"} tag={"h4"}>
-            {totalLessons}
-          </Heading>
-          {unitQuiz && (
-            <Heading $font={"heading-light-7"} tag={"h4"}>
-              {unitQuiz}
-            </Heading>
-          )}
+          </OakLink>
+          <Flex $flexDirection={["column", "row"]}>
+            {learningThemeTitle && (
+              <Span $mr={6} $font={["body-3", "heading-light-7"]}>
+                {learningThemeTitle}
+              </Span>
+            )}
+            <Flex>
+              <Span $mr={6} $font={["body-3", "heading-light-7"]}>
+                {`${lessonCount} lessons`}
+              </Span>
+              {hasUnitQuiz && (
+                <Span $mr={6} $font={["body-3", "heading-light-7"]}>
+                  and unit quiz
+                </Span>
+              )}
+            </Flex>
+          </Flex>
+        </Flex>
+        <Flex
+          $justifyContent={"center"}
+          $alignItems={"center"}
+          $minHeight={110}
+          $minWidth={[72, 130]}
+          $background={"pupilsPink"}
+          $position={"relative"}
+        >
+          <Icon size={[50, 92]} name={subjectIcon}>
+            {title}
+          </Icon>
         </Flex>
       </Flex>
-      <Flex
-        $justifyContent={"center"}
-        $alignItems={"center"}
-        $minHeight={110}
-        $width={[72, 130]}
-        $background={"pupilsPink"}
-      >
-        <Icon size={92} name={subjectIcon}>
-          {title}
-        </Icon>
-      </Flex>
-    </Flex>
+      <BoxBorders gapPosition="bottomRight" />
+    </Card>
   );
 };
 
