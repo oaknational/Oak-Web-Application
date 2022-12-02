ed2ffb3a1969562c0a8f43328f4a85baa00e6d42
Joe Baker <7987787+JBR90@users.noreply.github.com>
2022-12-02 18:05:30 +0000

    chore: update import for key stage props

--- src/components/pages/SubjectListing/index.tsx
+++ src/components/pages/SubjectListing/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 
+import { KeyStageProps } from "../../../pages/beta/teachers/key-stages/[keyStageSlug]";
 import SubjectCardLink, {
   SubjectCardLinkProps,
 } from "../../Card/SubjectCardLink";
