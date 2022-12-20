602339f8ca3a23554eed29cdb3f375a7b2e6dab4
mantagen <contact.mantagen@gmail.com>
2022-12-20 13:49:58 +0000

    feat: add zod parsing to currcilum data api

--- src/node-lib/curriculum-api/index.ts
+++ src/node-lib/curriculum-api/index.ts
@@ -1,4 +1,5 @@
 import { GraphQLClient } from "graphql-request";
+import { z } from "zod";
 
 import config from "../../config/server";
 
@@ -15,91 +16,157 @@ const graphqlClient = new GraphQLClient(curriculumApiUrl, {
   },
 });
 
-export type TeachersHomePageData = {
-  keyStages: {
-    slug: string;
-    title: string;
-    shortCode: string;
-  }[];
+/**
+ * Rather than using a lib, or build a function to deeply transform any keys
+ * from snake_case to camelCase, we prefer to make sucha transform in the gql
+ * query. However, we are unable (using codegen) to transform the names of
+ * the tables from which data is returned. So for simplicity, this function
+ * transforms just the upper most level (the table/MV names) of the responses
+ * from the gql queries.
+ */
+const transformMVCase = <K, S, T, U, L>(res: {
+  mv_key_stages?: K;
+  mv_subjects?: S;
+  mv_tiers?: T;
+  mv_units?: U;
+  mv_lessons?: L;
+}) => {
+  return {
+    keyStages: res.mv_key_stages,
+    subjects: res.mv_subjects,
+    tiers: res.mv_tiers,
+    units: res.mv_units,
+    lessons: res.mv_lessons,
+  };
 };
 
-export type TeachersKeyStageSubjectsData = {
-  keyStageSlug: string;
-  keyStageTitle: string;
-  subjects: {
-    slug: string;
-    title: string;
-    keyStageSlug: string;
-    keyStageTitle: string;
-    unitCount: number | null;
-    lessonCount: number | null;
-    tierCount: number | null;
-  }[];
-};
+const teachersHomePageData = z.object({
+  keyStages: z.array(
+    z.object({
+      slug: z.string(),
+      title: z.string(),
+      shortCode: z.string(),
+    })
+  ),
+});
+const teachersKeyStageSubjectsData = z.object({
+  keyStageSlug: z.string(),
+  keyStageTitle: z.string(),
+  subjects: z.array(
+    z.object({
+      slug: z.string(),
+      title: z.string(),
+      keyStageSlug: z.string(),
+      keyStageTitle: z.string(),
+      unitCount: z.number().nullable(),
+      lessonCount: z.number().nullable(),
+      tierCount: z.number().nullable(),
+    })
+  ),
+});
 
-export type TeachersKeyStageSubjectTiersData = {
-  keyStageSlug: string;
-  keyStageTitle: string;
-  subjectSlug: string;
-  subjectTitle: string;
-  tiers: {
-    slug: string;
-    title: string;
-    unitCount: number | null;
-    lessonCount: number | null;
-  }[];
-};
+const teachersKeyStageSubjectTiersData = z.object({
+  keyStageSlug: z.string(),
+  keyStageTitle: z.string(),
+  subjectSlug: z.string(),
+  subjectTitle: z.string(),
+  tiers: z.array(
+    z.object({
+      slug: z.string(),
+      title: z.string(),
+      unitCount: z.number().nullable(),
+      lessonCount: z.number().nullable(),
+    })
+  ),
+});
+const teachersKeyStageSubjectTiersPathsSchema = z.object({
+  tiers: z.array(
+    z.object({
+      subjectSlug: z.string(),
+      keyStageSlug: z.string(),
+    })
+  ),
+});
+const teachersKeyStageSubjectUnitsData = z.object({
+  keyStageSlug: z.string(),
+  keyStageTitle: z.string(),
+  subjectSlug: z.string(),
+  subjectTitle: z.string(),
+  tierSlug: z.string().nullable(),
+  tiers: z.array(
+    z.object({
+      slug: z.string(),
+      title: z.string(),
+      unitCount: z.number().nullable(),
+    })
+  ),
+  units: z.array(
+    z.object({
+      slug: z.string(),
+      title: z.string(),
+      keyStageSlug: z.string(),
+      keyStageTitle: z.string(),
+      subjectSlug: z.string(),
+      subjectTitle: z.string(),
+      themeSlug: z.string(),
+      themeTitle: z.string(),
+      lessonCount: z.number().nullable(),
+      quizCount: z.number().nullable(),
+    })
+  ),
+});
+const teachersKeyStageSubjectUnitsPathsSchema = z.object({
+  subjects: z.array(
+    z.object({
+      subjectSlug: z.string(),
+      keyStageSlug: z.string(),
+    })
+  ),
+});
+const teachersKeyStageSubjectUnitsLessonsData = z.object({
+  keyStageSlug: z.string(),
+  keyStageTitle: z.string(),
+  subjectSlug: z.string(),
+  subjectTitle: z.string(),
+  tierSlug: z.string().nullable(),
+  unitSlug: z.string(),
+  unitTitle: z.string(),
+  lessons: z.array(
+    z.object({
+      slug: z.string(),
+      title: z.string(),
+      description: z.string(),
+      keyStageSlug: z.string(),
+      keyStageTitle: z.string(),
+      subjectSlug: z.string(),
+      subjectTitle: z.string(),
+      unitSlug: z.string(),
+      themeSlug: z.string(),
+      themeTitle: z.string(),
+      quizCount: z.number().nullable(),
+      videoCount: z.number().nullable(),
+      presentationCount: z.number().nullable(),
+      worksheetCount: z.number().nullable(),
+    })
+  ),
+});
 
-export type TeachersKeyStageSubjectUnitsData = {
-  keyStageSlug: string;
-  keyStageTitle: string;
-  subjectSlug: string;
-  subjectTitle: string;
-  tierSlug: string | null;
-  tiers: {
-    slug: string;
-    title: string;
-    unitCount: number | null;
-  }[];
-  units: {
-    slug: string;
-    title: string;
-    keyStageSlug: string;
-    keyStageTitle: string;
-    subjectSlug: string;
-    subjectTitle: string;
-    themeSlug: string;
-    themeTitle: string;
-    lessonCount: number | null;
-    quizCount: number | null;
-  }[];
-};
+export type TeachersHomePageData = z.infer<typeof teachersHomePageData>;
+export type TeachersKeyStageSubjectsData = z.infer<
+  typeof teachersKeyStageSubjectsData
+>;
 
-export type TeachersKeyStageSubjectUnitsLessonsData = {
-  keyStageSlug: string;
-  keyStageTitle: string;
-  subjectSlug: string;
-  subjectTitle: string;
-  tierSlug: string | null;
-  unitSlug: string;
-  unitTitle: string;
-  lessons: {
-    slug: string;
-    title: string;
-    description: string;
-    keyStageSlug: string;
-    keyStageTitle: string;
-    subjectSlug: string;
-    subjectTitle: string;
-    unitSlug: string;
-    themeSlug: string;
-    themeTitle: string;
-    quizCount: number | null;
-    videoCount: number | null;
-    presentationCount: number | null;
-    worksheetCount: number | null;
-  }[];
-};
+export type TeachersKeyStageSubjectTiersData = z.infer<
+  typeof teachersKeyStageSubjectTiersData
+>;
+
+export type TeachersKeyStageSubjectUnitsData = z.infer<
+  typeof teachersKeyStageSubjectUnitsData
+>;
+
+export type TeachersKeyStageSubjectUnitsLessonsData = z.infer<
+  typeof teachersKeyStageSubjectUnitsLessonsData
+>;
 
 const sdk = getSdk(graphqlClient);
 
@@ -107,73 +174,63 @@ const curriculumApi = {
   teachersHomePage: async () => {
     const res = await sdk.teachersHomePage();
 
-    return {
-      keyStages: res.mv_key_stages.filter(
-        (ks) => Boolean(ks.title) && Boolean(ks.slug) && Boolean(ks.shortCode)
-      ) as { slug: string; title: string; shortCode: string }[],
-    } as TeachersHomePageData;
+    return teachersHomePageData.parse(transformMVCase(res));
   },
   teachersKeyStageSubjects: async (
     ...args: Parameters<typeof sdk.teachersKeyStageSubjects>
   ) => {
     const res = await sdk.teachersKeyStageSubjects(...args);
+    const { keyStages, subjects } = transformMVCase(res);
 
-    return {
-      keyStageSlug: args[0].keyStageSlug,
-      keyStageTitle: (res.mv_subjects[0]?.keyStageTitle || null) as string,
-      subjects: res.mv_subjects.filter(
-        (ks) =>
-          Boolean(ks.title) &&
-          Boolean(ks.slug) &&
-          Boolean(ks.keyStageTitle) &&
-          Boolean(ks.keyStageSlug)
-      ),
-    } as TeachersKeyStageSubjectsData;
+    return teachersKeyStageSubjectsData.parse({
+      keyStageSlug: keyStages?.[0]?.slug,
+      keyStageTitle: keyStages?.[0]?.title,
+      subjects,
+    });
   },
   teachersKeyStageSubjectTiers: async (
     ...args: Parameters<typeof sdk.teachersKeyStageSubjectTiers>
   ) => {
     const res = await sdk.teachersKeyStageSubjectTiers(...args);
+    const { tiers, subjects, keyStages } = transformMVCase(res);
 
-    return {
-      keyStageSlug: res.mv_key_stages[0]?.slug,
-      keyStageTitle: res.mv_key_stages[0]?.title,
-      subjectSlug: res.mv_subjects[0]?.slug,
-      subjectTitle: res.mv_subjects[0]?.title,
-      tiers: res.mv_tiers,
-    } as TeachersKeyStageSubjectTiersData;
+    return teachersKeyStageSubjectTiersData.parse({
+      keyStageSlug: keyStages?.[0]?.slug,
+      keyStageTitle: keyStages?.[0]?.title,
+      subjectSlug: subjects?.[0]?.slug,
+      subjectTitle: subjects?.[0]?.title,
+      tiers,
+    });
   },
   teachersKeyStageSubjectTiersPaths: async () => {
-    const pairs = (await sdk.teachersKeyStageSubjectTiersPaths()).mv_tiers as {
-      subjectSlug: string;
-      keyStageSlug: string;
-    }[];
+    const res = await sdk.teachersKeyStageSubjectTiersPaths();
+    const { tiers } = transformMVCase(res);
 
-    return pairs;
+    return teachersKeyStageSubjectTiersPathsSchema.parse({
+      tiers,
+    });
   },
   teachersKeyStageSubjectUnits: async (
     ...args: Parameters<typeof sdk.teachersKeyStageSubjectUnits>
   ) => {
     const res = await sdk.teachersKeyStageSubjectUnits(...args);
-
-    return {
-      keyStageSlug: res.mv_key_stages[0]?.slug,
-      keyStageTitle: res.mv_key_stages[0]?.title,
-      subjectSlug: res.mv_subjects[0]?.slug,
-      subjectTitle: res.mv_subjects[0]?.title,
-      tierSlug: args[0].tierSlug || null,
-      tiers: res.mv_tiers,
-      units: res.mv_units,
-    } as TeachersKeyStageSubjectUnitsData;
+    const { tiers, units, keyStages, subjects } = transformMVCase(res);
+    return teachersKeyStageSubjectUnitsData.parse({
+      keyStageSlug: keyStages?.[0]?.slug,
+      keyStageTitle: keyStages?.[0]?.title,
+      subjectSlug: subjects?.[0]?.slug,
+      subjectTitle: subjects?.[0]?.title,
+      tierSlug: args[0].tierSlug,
+      tiers,
+      units,
+    });
   },
   teachersKeyStageSubjectUnitsPaths: async () => {
-    const pairs = (await sdk.teachersKeyStageSubjectUnitsPaths())
-      .mv_subjects as {
-      subjectSlug: string;
-      keyStageSlug: string;
-    }[];
-
-    return pairs;
+    const res = await sdk.teachersKeyStageSubjectUnitsPaths();
+    const { subjects } = transformMVCase(res);
+    return teachersKeyStageSubjectUnitsPathsSchema.parse({
+      subjects,
+    });
   },
 };
 
