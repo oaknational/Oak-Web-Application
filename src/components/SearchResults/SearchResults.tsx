325720ed7982f998949a28d0f2c381aee7610cef
Weronika Szalas <weronika@vitl.com>
2023-01-10 17:25:12 +0000

    feat: 1078 add pagination to search results

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,25 +1,50 @@
 import { SearchHit } from "../../pages/beta/teachers/search";
 import OakLink from "../OakLink";
+import Box from "../Box";
+import Pagination from "../Pagination";
+import usePagination from "../Pagination/usePagination";
 
 interface SearchResultsProps {
   hits: Array<SearchHit>;
 }
 
+const RESULTS_PER_PAGE = 8;
+
 const SearchResults = (props: SearchResultsProps) => {
   const { hits } = props;
-  const resultElements: Array<JSX.Element> = hits.map((hit: SearchHit) => {
-    const { _source } = hit;
-    const { title, id, key_stage_title, slug } = _source;
-    return (
-      <li key={id}>
-        <OakLink page={null} href={`/beta/lessons/${slug}`}>
-          {title} - <i>{key_stage_title}</i>
-        </OakLink>
-      </li>
-    );
+
+  const paginationProps = usePagination({
+    totalResults: hits.length,
+    pageSize: RESULTS_PER_PAGE,
+    items: hits,
   });
 
-  return <ul data-testid="search-results">{resultElements}</ul>;
+  const { currentPageItems } = paginationProps;
+
+  const resultElements: Array<JSX.Element> = currentPageItems.map(
+    (hit: SearchHit) => {
+      const { _source } = hit;
+      const { title, id, key_stage_title, slug } = _source;
+      return (
+        <li key={id}>
+          <OakLink page={null} href={`/beta/lessons/${slug}`}>
+            {title} - <i>{key_stage_title}</i>
+          </OakLink>
+        </li>
+      );
+    }
+  );
+
+  return (
+    <div>
+      <ul data-testid="search-results">{resultElements}</ul>
+      {hits.length > RESULTS_PER_PAGE && (
+        <Box $width="100%" $mt={[0, "auto"]} $pt={48}>
+          <Pagination {...paginationProps} />
+        </Box>
+      )}
+    </div>
+  );
 };
 
 export default SearchResults;
