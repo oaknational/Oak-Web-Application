4105bf19e62fcd1111ec862856d5a1b839641e31
craigmccahill <craig.mccahill@gmail.com>
2023-01-11 12:45:31 +0000

    feat: add links for search

--- src/pages/beta/teachers/index.tsx
+++ src/pages/beta/teachers/index.tsx
@@ -12,13 +12,14 @@ import Flex from "../../../components/Flex";
 import Grid, { GridArea } from "../../../components/Grid";
 import KeyStageKeypad from "../../../components/KeyStageKeypad";
 import MaxWidth from "../../../components/MaxWidth/MaxWidth";
+import OakLink from "../../../components/OakLink";
 import {
   HomeSiteCards,
   SharedHomeContent,
 } from "../../../components/pages/Home";
 import usePostList from "../../../components/Posts/PostList/usePostList";
 import SearchForm from "../../../components/SearchForm";
-import { Heading, P } from "../../../components/Typography";
+import { Heading, P, Span } from "../../../components/Typography";
 import UnderlinedHeading from "../../../components/Typography/UnderlinedHeading";
 import CMSClient from "../../../node-lib/cms";
 import curriculumApi, {
@@ -63,6 +64,35 @@ const Teachers: NextPage<TeachersHomePageProps> = (props) => {
                 <Box $mt={16}>
                   <SearchForm />
                 </Box>
+                <P $mt={16} $font={"body-2"}>
+                  Search suggestions:
+                </P>
+                <Span>
+                  <OakLink
+                    page={"beta-search"}
+                    term={"algebra"}
+                    $color={"black"}
+                    $font="heading-7"
+                  >
+                    Algebra,&nbsp;
+                  </OakLink>
+                  <OakLink
+                    page={"beta-search"}
+                    term={"computing"}
+                    $color={"black"}
+                    $font="heading-7"
+                  >
+                    Computing,&nbsp;
+                  </OakLink>
+
+                  <OakLink
+                    page={"beta-search"}
+                    term={"a+midsummer+nights+dream"}
+                    $font="heading-7"
+                  >
+                    A Midsummer Night's Dream
+                  </OakLink>
+                </Span>
               </GridArea>
               <GridArea $colSpan={[12, 6, 4]}>
                 <UnderlinedHeading $font={"heading-6"} tag={"h2"} $mt={8}>
