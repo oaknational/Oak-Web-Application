904b1cf1f41b65b9415914504c2d6fc0f3cdef06
Weronika Szalas <weronika@vitl.com>
2023-01-25 13:42:26 +0000

    feat: 1074 Amend filter checkbox styles

--- src/components/CheckboxButton/CheckboxButton.tsx
+++ src/components/CheckboxButton/CheckboxButton.tsx
@@ -3,18 +3,53 @@ import styled, { css } from "styled-components";
 
 import { OakColorName } from "../../styles/theme";
 import getColorByLocation from "../../styles/themeHelpers/getColorByLocation";
+import getColorByName from "../../styles/themeHelpers/getColorByName";
 import getFontFamily from "../../styles/themeHelpers/getFontFamily";
-import BrushBorders from "../SpriteSheet/BrushSvgs/BrushBorders";
+import Icon from "../../components/Icon";
+import FocusUnderline from "../../components/OakLink/FocusUnderline";
 
-export type CheckboxButtonConfig = {
+export type CustomCheckboxConfig = {
   default: {
     color: OakColorName;
-    background: OakColorName;
   };
   disabled: {
     color: OakColorName;
   };
 };
+
+const checkboxFocusStyles = css`
+  ${FocusUnderline} {
+    display: none;
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: -10px;
+    height: 7px;
+    filter: drop-shadow(1px 5px 0 rgb(0 0 0));
+    width: calc(100% - 10px);
+  }
+
+  input[type="checkbox"]:focus ~ ${FocusUnderline} {
+    display: block;
+  }
+`;
+
+const checkboxHoverStyles = css`
+  input[type="checkbox"]:hover + span {
+    background-color: ${getColorByName("white")};
+    border-color: ${getColorByName("oakGrey3")};
+
+    &::after {
+      content: "";
+      position: absolute;
+      display: block;
+      width: 16px;
+      height: 16px;
+      border-radius: 2px;
+      background-color: ${getColorByName("teachersHighlight")};
+    }
+  }
+`;
 
 const CheckboxLabel = styled.label<{ disabled: boolean; checked: boolean }>`
   position: relative;
@@ -22,41 +57,17 @@ const CheckboxLabel = styled.label<{ disabled: boolean; checked: boolean }>`
   display: flex;
   align-items: center;
   font-family: ${getFontFamily("ui")};
-  font-weight: 600;
-  margin: 8px;
-  color: ${getColorByLocation(
-    ({ theme }) => theme.checkboxButton.default.color
-  )};
-  background-color: ${getColorByLocation(
-    ({ theme }) => theme.checkboxButton.default.background
-  )};
+  color: ${getColorByLocation(({ theme }) => theme.checkbox.default.color)};
   ${(props) =>
     props.disabled &&
     css`
       color: ${getColorByLocation(
-        ({ theme }) => theme.checkboxButton.disabled.color
+        ({ theme }) => theme.checkbox.disabled.color
       )};
     `}
-  ${(props) =>
-    props.checked &&
-    css`
-      color: ${getColorByLocation(
-        ({ theme }) => theme.checkboxButton.disabled.color
-      )};
-      background-color: transparent;
-      text-decoration: underline;
-    `}
 
-  input[type="checkbox"]:hover + span,
-  input[type="checkbox"]:focus + span {
-    text-decoration: underline;
-  }
-
-  input[type="checkbox"]:focus + svg {
-    // TODO: add focus ring component to replace this
-    outline: 4px auto -webkit-focus-ring-color;
-    outline-offset: 4px;
-  }
+  ${checkboxFocusStyles}
+  ${checkboxHoverStyles}
 `;
 
 const ScreenReaderCheckbox = styled.input.attrs({ type: "checkbox" })<{
@@ -69,9 +80,29 @@ const ScreenReaderCheckbox = styled.input.attrs({ type: "checkbox" })<{
   opacity: 0;
 `;
 
+const VisualCheckbox = styled.span<{ checked: boolean }>`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 28px;
+  height: 28px;
+  border-style: solid;
+  border-width: 2px;
+  border-radius: 2px;
+  border-color: ${(props) =>
+    props.checked
+      ? getColorByName("teachersHighlight")
+      : getColorByName("oakGrey3")};
+  background-color: ${(props) =>
+    props.checked
+      ? getColorByName("teachersHighlight")
+      : getColorByName("white")};
+`;
+
 const CheckboxLabelText = styled.span`
   margin-left: 8px;
-  margin-right: 8px;
+  margin-right: 16px;
 `;
 
 type CheckboxProps = {
@@ -82,7 +113,7 @@ type CheckboxProps = {
   onChange: () => void;
 };
 
-const CheckboxButton: FC<CheckboxProps> = (props) => {
+const CustomCheckbox: FC<CheckboxProps> = (props) => {
   const { labelText, checked, disabled = false, onChange, id } = props;
 
   const select = () => {
@@ -90,26 +121,26 @@ const CheckboxButton: FC<CheckboxProps> = (props) => {
   };
 
   return (
-    <div>
-      <CheckboxLabel
-        htmlFor={id}
-        onClick={() => select}
-        disabled={disabled}
+    <CheckboxLabel
+      htmlFor={id}
+      onClick={() => select}
+      checked={checked}
+      disabled={disabled}
+    >
+      <ScreenReaderCheckbox
+        type="checkbox"
+        id={id}
+        onChange={select}
         checked={checked}
-      >
-        {!checked && <BrushBorders color={"pastelTurquoise"} />}
-        <ScreenReaderCheckbox
-          type="checkbox"
-          id={id}
-          onChange={select}
-          checked={checked}
-          disabled={disabled}
-        />
-
-        {labelText && <CheckboxLabelText>{labelText}</CheckboxLabelText>}
-      </CheckboxLabel>
-    </div>
+        disabled={disabled}
+      />
+      <VisualCheckbox checked={checked}>
+        {checked && <Icon name={"Tick"} $color={"white"} size={20} />}
+      </VisualCheckbox>
+      {labelText && <CheckboxLabelText>{labelText}</CheckboxLabelText>}
+      <FocusUnderline $color={"teachersYellow"} />
+    </CheckboxLabel>
   );
 };
 
-export default CheckboxButton;
+export default CustomCheckbox;
