ef6a854ddb537b33f29099edc9b36998214c215b
benprotheroe <bprotheroe28@gmail.com>
2023-02-14 13:29:46 +0000

    fix(downloads): test coverage increase

--- src/node-lib/curriculum-api/index.test.ts
+++ src/node-lib/curriculum-api/index.test.ts
@@ -189,13 +189,13 @@ describe("curriculum-api", () => {
       keyStageSlug: "ks123",
       subjectSlug: "english-9",
       unitSlug: "macbeth-1",
-      lessonSlug: "islamic-geometry",
+      lessonSlug: "islamic-geometry1",
     });
     expect(teachersKeyStageSubjectUnitLessonsDownloads).toHaveBeenCalledWith({
       keyStageSlug: "ks123",
       subjectSlug: "english-9",
       unitSlug: "macbeth-1",
-      lessonSlug: "islamic-geometry",
+      lessonSlug: "islamic-geometry1",
     });
   });
   test("teachersLessonOverview", async () => {
