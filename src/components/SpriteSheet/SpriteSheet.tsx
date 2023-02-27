34a4add3ea7f2ac79a71e4125998061f9dae2ccc
mantagen <contact.mantagen@gmail.com>
2023-02-27 19:13:34 +0000

    chore: add role presentation to sprite object

--- src/components/SpriteSheet/SpriteSheet.tsx
+++ src/components/SpriteSheet/SpriteSheet.tsx
@@ -1,8 +1,12 @@
+/**
+ * See scripts/build/image_data/generate_sprite.js
+ */
 import { FC } from "react";
 
 const SpriteSheet: FC = () => {
   return (
     <object
+      role="presentation"
       type="image/svg+xml"
       data="/images/sprite/sprite.svg"
       width="0"
