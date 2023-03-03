ef5f40a9ae72489290538b4e3a085ffeaccc69ed
Joe Baker <7987787+JBR90@users.noreply.github.com>
2023-03-03 14:24:20 +0000

    chore: update unit count in tests and storybook

--- src/components/SubjectCardList/SubjectCardListItem.stories.tsx
+++ src/components/SubjectCardList/SubjectCardListItem.stories.tsx
@@ -19,7 +19,7 @@ SubjectCardListItem.args = {
   title: "Art and Design",
   slug: "art",
   lessonCount: 130,
-  unitCountActive: 14,
+  activeUnitCount: 14,
 };
 
 export const SubjectCardListItemUnavailable = Template.bind({});
@@ -29,5 +29,5 @@ SubjectCardListItemUnavailable.args = {
   title: "Art and Design",
   slug: "art",
   lessonCount: 0,
-  unitCountActive: 14,
+  activeUnitCount: 14,
 };
