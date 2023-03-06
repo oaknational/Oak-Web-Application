ed885291cb729135ec088198f85dbc64de28960a
Ben Protheroe <benprotheroe@Bens-MacBook-Pro.local>
2023-03-06 10:46:23 +0000

    fix(questions): increase test coverage

--- src/components/QuizContainer/QuestionListItem/QuestionListItem.test.tsx
+++ src/components/QuizContainer/QuestionListItem/QuestionListItem.test.tsx
@@ -111,4 +111,19 @@ describe("QuestionListItem", () => {
 
     expect(questionItemTitle).toHaveTextContent("what is a question");
   });
+  it("renders correctly where correct answer doesnt have an image and incorrect choice does have image", () => {
+    testProps.choices =  [
+      {
+        choice: "this one",
+        image:null,
+      },
+      { choice: "that one", image: "https://lh6.googleusercontent.com/OjgbTYtK-NU8_lzFznF36BYjENk_zmTmfitGHQvwt4xZNqTGPX9D6lsyCcvv_JV2dCCxKKqSgffHuamqaOvg8t7K-8I5GnkFSY1EO3QboKWeFXJkAB76pnTXU9xH9okF=w287" },
+    ];
+    const { getByTestId } = renderWithTheme(
+      <QuestionListItem {...testProps} />
+    );
+    const questionItemTitle = getByTestId("title-div");
+
+    expect(questionItemTitle).toHaveTextContent("what is a question");
+  });
 });
