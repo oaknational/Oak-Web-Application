722a25cd61c840ed45e77c11b64f4c5e57564644
Joe Baker <7987787+JBR90@users.noreply.github.com>
2023-03-07 17:24:34 +0000

    chore: add story

---
+++ src/components/DownloadComponents/TermsAndConditionsCheckbox/TermsAndConditionsCheckbox.stories.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Component from ".";
+
+export default {
+  title: "Form Fields/Checkbox/Terms and Conditions Checkbox",
+  component: Component,
+  argTypes: {
+    labelText: {
+      defaultValue: "Click me",
+    },
+    id: {
+      defaultValue: "1",
+    },
+    name: {
+      defaultValue: "checkbox-group",
+    },
+    checked: {
+      defaultValue: false,
+    },
+    onChange: {
+      action: "changed",
+    },
+  },
+} as ComponentMeta<typeof Component>;
+
+const Template: ComponentStory<typeof Component> = (args) => {
+  const [value, setValue] = useState(false);
+  return (
+    <Component {...args} checked={value} onChange={() => setValue(!value)} />
+  );
+};
+
+export const Checkbox = Template.bind({});
