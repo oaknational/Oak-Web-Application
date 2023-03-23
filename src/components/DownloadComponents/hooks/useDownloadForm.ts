ec3430710a7ed20b3dfae20d875eb9ae844251fd
Weronika Szalas <weronika.szalas@thenational.academy>
2023-03-23 12:48:25 +0000

    feat: 1152 Convert terms boolean to string

--- src/components/DownloadComponents/hooks/useDownloadForm.ts
+++ src/components/DownloadComponents/hooks/useDownloadForm.ts
@@ -39,7 +39,7 @@ const useDownloadForm = (props: UseDownloadFormProps = {}) => {
     }
 
     if (terms) {
-      setTermsInLocalStorage(terms);
+      setTermsInLocalStorage(terms.toString());
     }
 
     await downloadLessonResources(slug, selectedResources);
