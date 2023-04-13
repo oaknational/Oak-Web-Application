334162233b5041c6c4e5b039817a1f56aed08f91
mantagen <contact.mantagen@gmail.com>
2023-04-13 15:18:53 +0100

    test(sharing-image): add new sharing image to mockSeoResult

--- src/__tests__/__helpers__/cms.ts
+++ src/__tests__/__helpers__/cms.ts
@@ -65,7 +65,9 @@ export const mockSeoResult = {
   ogTitle: "Mock Page Title | NEXT_PUBLIC_SEO_APP_NAME",
   ogDescription: "Mock page description",
   ogUrl: "NEXT_PUBLIC_SEO_APP_URL",
-  ogImage: "NEXT_PUBLIC_SEO_APP_URLNEXT_PUBLIC_SEO_APP_SOCIAL_SHARING_IMG?2022",
+  ogImage:
+    "public/images/sharing/default-social-sharing-2022.png?" +
+    new Date().getFullYear(),
   ogSiteName: "NEXT_PUBLIC_SEO_APP_NAME",
   robots: "index,follow",
 };
