a733e132f6bea4660a91c49d83d118ab77955190
Kyle <kyle@Kyles-MacBook-Pro.local>
2023-04-19 16:31:13 +0100

    chore(programmes-page): creation of programmes page

---
+++ src/pages/beta/teachers/key-stages/[keyStageSlug]/subjects/[subjectSlug]/programmes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import { GetStaticProps } from "next";
+
+// import { URLParams } from "./units/[unitSlug]";
+
+const ProgrammesListingPage = () => {
+  return <div>programmes</div>;
+};
+
+// export const getStaticProps: GetStaticProps<
+//   ProgrammeListingPageProps,
+//   URLParams
+// > = async (context) => {
+//   console.log(context.params, "context.params");
+//   if (!context.params?.keyStageSlug) {
+//     throw new Error("No keyStageSlug");
+//   }
+//   return {}
+// };
+
+export default ProgrammesListingPage;
