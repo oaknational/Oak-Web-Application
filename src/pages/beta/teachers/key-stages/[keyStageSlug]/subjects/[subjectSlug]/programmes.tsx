b421d78ebe8525004a290e42d66bb37dd347acdb
Kyle <kyle@Kyles-MacBook-Pro.local>
2023-04-20 11:06:00 +0100

    chore(programmes-page): programmes page and test file

--- src/pages/beta/teachers/key-stages/[keyStageSlug]/subjects/[subjectSlug]/programmes.tsx
+++ src/pages/beta/teachers/key-stages/[keyStageSlug]/subjects/[subjectSlug]/programmes.tsx
@@ -1,21 +1,105 @@
 import React from "react";
-// import { GetStaticProps } from "next";
+import { GetServerSideProps, NextPage } from "next";
 
-// import { URLParams } from "./units/[unitSlug]";
+import curriculumApi, {
+  TierListingData,
+} from "../../../../../../../node-lib/curriculum-api";
+import { getSeoProps } from "../../../../../../../browser-lib/seo/getSeoProps";
+import AppLayout from "../../../../../../../components/AppLayout/AppLayout";
+import MaxWidth from "../../../../../../../components/MaxWidth/MaxWidth";
+import Breadcrumbs from "../../../../../../../components/Breadcrumbs/Breadcrumbs";
+import Box from "../../../../../../../components/Box";
+import SubjectTierListing from "../../../../../../../components/SubjectTierListing/SubjectTierListing";
+import useTrackPageView from "../../../../../../../hooks/useTrackPageView";
 
-const ProgrammesListingPage = () => {
-  return <div>programmes</div>;
+export type ProgrammeListingPageProps = TierListingData;
+
+const ProgrammesListingPage: NextPage<ProgrammeListingPageProps> = (props) => {
+  const { programmes } = props;
+  if (!programmes[0]) {
+    throw new Error("No programmes");
+  }
+
+  const keyStageSlug = programmes[0]?.keyStageSlug;
+  const keyStageTitle = programmes[0]?.keyStageTitle;
+  const slug = programmes[0]?.slug;
+  const title = programmes[0]?.title;
+
+  const programmeDetails = {
+    keyStageSlug,
+    keyStageTitle,
+    slug,
+    title,
+  };
+
+  // Manually input in Avo.ts, confirm the avo process to ensure this is correct
+  useTrackPageView({ pageName: "Programmes Listing" });
+
+  const tiersSEO = {
+    ...getSeoProps({
+      title: `${keyStageTitle} ${title} tiers`,
+      description: `We have resources for tiers: ${programmes
+        .map((programme) => programme.tierTitle)
+        .join(", ")}`,
+    }),
+    ...{ noFollow: true, noIndex: true },
+  };
+  return (
+    <AppLayout seoProps={tiersSEO}>
+      <MaxWidth $ph={16}>
+        <Box $mv={[24, 48]}>
+          {" "}
+          <Breadcrumbs
+            breadcrumbs={[
+              { oakLinkProps: { page: "beta-teachers-home" }, label: "Home" },
+              {
+                oakLinkProps: { page: "subject-index", slug: keyStageSlug },
+                label: keyStageTitle,
+              },
+              {
+                oakLinkProps: {
+                  page: "key-stage-subject-programmes",
+                  subject: slug,
+                  keyStage: keyStageSlug,
+                },
+                label: title,
+              },
+            ]}
+          />
+        </Box>
+        <SubjectTierListing
+          programmes={programmes}
+          programmeDetails={programmeDetails}
+        />
+      </MaxWidth>
+    </AppLayout>
+  );
 };
 
-// export const getStaticProps: GetStaticProps<
-//   ProgrammeListingPageProps,
-//   URLParams
-// > = async (context) => {
-//   console.log(context.params, "context.params");
-//   if (!context.params?.keyStageSlug) {
-//     throw new Error("No keyStageSlug");
-//   }
-//   return {}
-// };
+export type URLParams = {
+  keyStageSlug: string;
+  subjectSlug: string;
+};
+
+export const getServerSideProps: GetServerSideProps<
+  ProgrammeListingPageProps,
+  URLParams
+> = async (context) => {
+  if (!context.params) {
+    throw new Error("No context params");
+  }
+  const curriculumData = await curriculumApi.tierListing({
+    keyStageSlug: context.params?.keyStageSlug,
+    subjectSlug: context.params?.subjectSlug,
+  });
+
+  const results = {
+    props: {
+      programmes: curriculumData.programmes,
+    },
+  };
+
+  return results;
+};
 
 export default ProgrammesListingPage;
