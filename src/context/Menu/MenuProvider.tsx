4fb1d278c20e584e1f1acc711870c3e46068a751
Kyle <kyle@Kyles-MacBook-Pro.local>
2023-04-24 13:01:12 +0100

    chore(menu): conditionally render different home page options

--- src/context/Menu/MenuProvider.tsx
+++ src/context/Menu/MenuProvider.tsx
@@ -1,4 +1,7 @@
 import { createContext, FC, useCallback, useState } from "react";
+import { useRouter } from "next/router";
+
+import { menuSections } from "../../browser-lib/fixtures/menuSections";
 
 type MenuContext = {
   open: boolean;
@@ -12,6 +15,8 @@ export const MenuProvider: FC<{
   children?: React.ReactNode;
 }> = ({ children }) => {
   const [open, setOpen] = useState(false);
+  const { pathname } = useRouter();
+  const displayMenu = pathname.startsWith("/beta/teachers");
 
   const openMenu = useCallback(() => {
     setOpen(true);
@@ -20,6 +25,26 @@ export const MenuProvider: FC<{
   const closeMenu = useCallback(() => {
     setOpen(false);
   }, [setOpen]);
+
+  if (displayMenu) {
+    if (!menuSections.large[0]) {
+      return null;
+    }
+    menuSections.large[0].linkText = "Home (early access)";
+    menuSections.large[0].resolveOakHrefProps.page = "home";
+    if ("viewType" in menuSections.large[0].resolveOakHrefProps) {
+      menuSections.large[0].resolveOakHrefProps.viewType = "teachers";
+    }
+  } else {
+    if (!menuSections.large[0]) {
+      return null;
+    }
+    menuSections.large[0].linkText = "Home";
+    menuSections.large[0].resolveOakHrefProps.page = "home";
+    if ("viewType" in menuSections.large[0].resolveOakHrefProps) {
+      menuSections.large[0].resolveOakHrefProps.viewType = null;
+    }
+  }
 
   const menuValue: MenuContext = {
     open,
