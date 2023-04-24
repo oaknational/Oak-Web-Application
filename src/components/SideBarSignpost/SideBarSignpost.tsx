a29f5dd19f077816af9b4099229ccc3d2e3afb0d
Kyle <kyle@Kyles-MacBook-Pro.local>
2023-04-24 17:03:49 +0100

    chore(removes-mb): mb coded into SideBarSignpost component

--- src/components/SideBarSignpost/SideBarSignpost.tsx
+++ src/components/SideBarSignpost/SideBarSignpost.tsx
@@ -1,17 +1,15 @@
 import React, { FC } from "react";
 
-import { MarginProps } from "../../styles/utils/spacing";
 import Box from "../Box/Box";
 import { P } from "../Typography";
 
 type SideBarSignpostProps = {
-  mb?: MarginProps["$mb"];
   display: string | string[];
 };
 
-const SideBarSignpost: FC<SideBarSignpostProps> = ({ mb, display }) => {
+const SideBarSignpost: FC<SideBarSignpostProps> = ({ display }) => {
   return (
-    <Box $mb={mb} $display={display} $width={[320, 480]}>
+    <Box $mb={[48, 64]} $display={display} $width={[320, 480]}>
       <P $font={["body-2", "body-1"]}>
         You're using our new area for teachers, currently under development and
         testing.
