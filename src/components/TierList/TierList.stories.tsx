62867f48d478432ba2f4e4e359f4d3e929ff5eff
Weronika Szalas <weronika.szalas@thenational.academy>
2023-04-25 14:22:20 +0100

    feat: 1446 fix duplication warnings

--- src/components/TierList/TierList.stories.tsx
+++ src/components/TierList/TierList.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
+import tierListingFixture from "../../node-lib/curriculum-api/fixtures/tierListing.fixture";
+
 import Component from ".";
 
 export default {
@@ -14,39 +16,5 @@ const Template: ComponentStory<typeof Component> = (args) => (
 export const TierList = Template.bind({});
 
 TierList.args = {
-  programmes: [
-    {
-      programmeSlug: "maths-secondary-ks4-core",
-      keyStageSlug: "ks4",
-      keyStageTitle: "Key stage 4",
-      slug: "maths",
-      title: "Maths",
-      tierSlug: "core",
-      tierTitle: "Core",
-      totalUnitCount: 61,
-      activeLessonCount: 284,
-    },
-    {
-      programmeSlug: "maths-secondary-ks4-foundation",
-      keyStageSlug: "ks4",
-      keyStageTitle: "Key stage 4",
-      slug: "maths",
-      title: "Maths",
-      tierSlug: "foundation",
-      tierTitle: "Foundation",
-      totalUnitCount: 63,
-      activeLessonCount: 256,
-    },
-    {
-      programmeSlug: "maths-secondary-ks4-higher",
-      keyStageSlug: "ks4",
-      keyStageTitle: "Key stage 4",
-      slug: "maths",
-      title: "Maths",
-      tierSlug: "higher",
-      tierTitle: "Higher",
-      totalUnitCount: 63,
-      activeLessonCount: 275,
-    },
-  ],
+  ...tierListingFixture(),
 };
