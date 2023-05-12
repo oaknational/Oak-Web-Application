25b6de187ec83a1888b0c781f2c6ad67b3e0f8c1
Kyle <kyle@Kyles-MacBook-Pro.local>
2023-05-12 20:43:47 +0100

    chore(underline-component): adds underline component and formatter function

---
+++ src/components/Underline/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const UnderlineSpan = styled.span`
+  display: inline-block;
+  border-bottom: 1px solid black;
+  padding-bottom: 2px;
+  min-width: 48px;
+  position: relative;
+`;
+
+const Underline = () => {
+  return <UnderlineSpan data-testid={"underline"} />;
+};
+
+export default Underline;
