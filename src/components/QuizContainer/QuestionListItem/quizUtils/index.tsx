19a5eed667062c733c46bc132bf0f6bca0e287ea
Joe Baker <7987787+JBR90@users.noreply.github.com>
2023-05-30 14:44:21 +0100

    fix: short answer curly braces questions

--- src/components/QuizContainer/QuestionListItem/quizUtils/index.tsx
+++ src/components/QuizContainer/QuestionListItem/quizUtils/index.tsx
@@ -5,7 +5,7 @@ import Underline from "../../../Underline";
 export const shortAnswerTitleFormatter = (
   title: string | null | undefined
 ): string | React.ReactNode => {
-  const shortAnswerRegex = /\{\{(.+?)\}\}|(\{\{\}\})/g;
+  const shortAnswerRegex = /\{\{(?:[^{}]|(?!\{\{|\}\})\w)*\}\}/g;
   if (!title) return "";
   if (shortAnswerRegex.test(title)) {
     return reactStringReplace(title, shortAnswerRegex, (match, i) => (
