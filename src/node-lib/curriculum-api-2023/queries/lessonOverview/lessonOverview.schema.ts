f919b31457b0db3f4200242d6778dc13a0e8be1c
Kyle <kyle@Kyles-MacBook-Pro.local>
2023-06-29 10:00:04 +0100

    chore(remove-unused): remove unused / commented out code

--- src/node-lib/curriculum-api-2023/queries/lessonOverview/lessonOverview.schema.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonOverview/lessonOverview.schema.ts
@@ -1,9 +1,5 @@
-// import { z } from "zod";
-
 import { lessonOverviewData } from "../../../curriculum-api";
 
 // OLD SCHEMA
-
-// export type LessonOverviewPageData = z.infer<typeof lessonOverviewSchema>;
 
 export default lessonOverviewData;
