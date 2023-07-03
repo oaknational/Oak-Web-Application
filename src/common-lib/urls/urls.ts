ccbbb68ec916b409eb19d1ab4f7524ab1c91f58e
Jim Cresswell <jim.cresswell@gmail.com>
2023-07-03 14:46:09 +0100

    chore: remove stray log

--- src/common-lib/urls/urls.ts
+++ src/common-lib/urls/urls.ts
@@ -564,7 +564,7 @@ export type ResolveOakHrefProps = Exclude<
  * viewType from window.location.path is "teachers".
  */
 function replaceViewType2023(path: string): string {
-  console.log("replaceViewType2023", path);
+  // console.log("replaceViewType2023", path);
   if (typeof window === "undefined") {
     return path;
   }
