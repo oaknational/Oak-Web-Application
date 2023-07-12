b63e8eaed2d6156b08c81a3a71d7ad8c5e450af3
Ben Protheroe <benprotheroe@iphone.ddns.warwick.ac.uk>
2023-07-12 10:33:40 +0100

    feat(unit-listing): sitemap isnt called

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,7 @@ const customJestConfig = {
     "/node_modules/",
     "node-lib/sanity-graphql/generated/*",
     "src/storybook-decorators/*",
-    "src/pages/beta/key-stages/sitemap.xml.tsx",
+    "src/pages/beta/key-stages/sitemap.xml/index.tsx",
   ],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
