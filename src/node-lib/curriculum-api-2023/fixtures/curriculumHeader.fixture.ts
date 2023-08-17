15f5002daada05f4b62abfdef2034313d66e2c48
Sonali Singh <sonali.singh@live.co.uk>
2023-08-17 15:55:58 +0100

    feat(curric): added fixtures

---
+++ src/node-lib/curriculum-api-2023/fixtures/curriculumHeader.fixture.ts
@@ -0,0 +1,13 @@
+import { CurriculumHeaderData } from "..";
+
+const curriculumHeaderFixture = (
+  partial?: Partial<CurriculumHeaderData>
+): CurriculumHeaderData => {
+  return {
+    subject: { title: "Maths", slug: "maths" },
+    phase: { title: "Secondary", slug: "secondary" },
+    examBoard: { title: "", slug: "" },
+    ...partial,
+  };
+};
+export default curriculumHeaderFixture;
