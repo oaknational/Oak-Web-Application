345bde4b23fb7a0cdd5c143f6e634f26fa90d34e
Ben Protheroe <benprotheroe@bens-mbp.home>
2023-08-21 12:46:15 +0100

    feat(programmes): fix failing tests

--- src/node-lib/curriculum-api/fixtures/tierAndExamboardListing.fixture.ts
+++ src/node-lib/curriculum-api/fixtures/tierAndExamboardListing.fixture.ts
@@ -1,4 +1,3 @@
-import { TierListingData } from "..";
 import { ProgrammeListingPageData } from "../../curriculum-api-2023/queries/programmeListing/programmeListing.schema";
 
 export const programmeListingFixture = (
@@ -111,51 +110,3 @@ export const programmeListingFixture = (
     ...partial,
   };
 };
-
-const tierListingFixture = (
-  partial?: Partial<TierListingData>
-): TierListingData => {
-  return {
-    programmes: [
-      {
-        programmeSlug: "maths-secondary-ks4-core",
-        keyStageSlug: "ks3",
-        keyStageTitle: "Key stage 3",
-        subjectSlug: "maths",
-        subjectTitle: "Maths",
-        tierSlug: "core",
-        tierTitle: "Core",
-        totalUnitCount: 61,
-        activeLessonCount: 284,
-        activeUnitCount: 61,
-      },
-      {
-        programmeSlug: "maths-secondary-ks4-foundation",
-        keyStageSlug: "ks1",
-        keyStageTitle: "Key stage 1",
-        subjectSlug: "maths",
-        subjectTitle: "Maths",
-        tierSlug: "foundation",
-        tierTitle: "Foundation",
-        totalUnitCount: 63,
-        activeLessonCount: 256,
-        activeUnitCount: 63,
-      },
-      {
-        programmeSlug: "maths-secondary-ks4-higher",
-        keyStageSlug: "ks4",
-        keyStageTitle: "Key stage 4",
-        subjectSlug: "maths",
-        subjectTitle: "Maths",
-        tierSlug: "higher",
-        tierTitle: "Higher",
-        totalUnitCount: 63,
-        activeLessonCount: 275,
-        activeUnitCount: 63,
-      },
-    ],
-    ...partial,
-  };
-};
-
-export default tierListingFixture;
