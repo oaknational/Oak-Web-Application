e91d685230d8aab287267ee917c55a6e2ad2ce00
Sonali Singh <sonali.singh@live.co.uk>
2023-08-30 22:21:47 +0100

    fix(curric): updated fixture and removed old tests

--- src/__tests__/pages/beta/[viewType]/curriculum/[subjectPhaseSlug]/overview.test.tsx
+++ src/__tests__/pages/beta/[viewType]/curriculum/[subjectPhaseSlug]/overview.test.tsx
@@ -1,29 +1,9 @@
-import OverviewPage, {
+import {
   getStaticProps,
   getStaticPaths,
 } from "@/pages/beta/[viewType]/curriculum/[subjectPhaseSlug]/overview";
-import curriculumOverviewTabFixture from "@/node-lib/curriculum-api-2023/fixtures/curriculumOverview.fixture";
-import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
-import subjectPhaseOptions from "@/browser-lib/fixtures/subjectPhaseOptions";
-import curriculumHeaderFixture from "@/node-lib/curriculum-api-2023/fixtures/curriculumHeader.fixture";
-const render = renderWithProviders();
 
 describe("curriculum overview page", () => {
-  describe("components rendering on page", () => {
-    it("renders overview headings", () => {
-      const { queryByTestId } = render(
-        <OverviewPage
-          overviewData={curriculumOverviewTabFixture()}
-          curriculumHeaderData={curriculumHeaderFixture()}
-          slug="maths-secondary"
-          subjectPhaseOptions={subjectPhaseOptions}
-        />
-      );
-
-      expect(queryByTestId("intent-heading")).toBeInTheDocument();
-    });
-  });
-
   describe("getStaticProps", () => {
     it("should fail if no props specified", async () => {
       await expect(async () => {
