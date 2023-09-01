df0546a503e9616cedd33e491c483ad8163b8e03
Joe Baker <7987787+JBR90@users.noreply.github.com>
2023-09-01 15:18:36 +0100

    feat: exemplar units component

---
+++ src/components/ExemplarUnits/ExemplarUnits.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { UnitListItemProps } from "../UnitAndLessonLists/UnitList/UnitListItem/UnitListItem";
+
+import { ExemplarUnitsProps } from "./ExemplarUnits";
+
+import Component from ".";
+
+import unitListingFixture from "@/node-lib/curriculum-api/fixtures/unitListing.fixture";
+import AnalyticsDecorator from "@/storybook-decorators/AnalyticsDecorator";
+
+const meta: Meta<typeof Component> = {
+  component: Component,
+  decorators: [AnalyticsDecorator, (Story) => <Story />],
+};
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+const units = unitListingFixture()
+  .units.slice(0, 10)
+  .map((unit) => unit[0]) as unknown as UnitListItemProps[];
+
+const exemplarUnitsProps: ExemplarUnitsProps = {
+  heading: "Primary Units",
+  subHeading: "View and download our early-release units.",
+  color: "lavender50",
+  quote: {
+    text: "To find resources that tick all the boxes is quite unusual",
+    author: "— Sophie Baker",
+    occupation: "Class Teacher, St Agnes Academy",
+  },
+  units: units,
+  viewType: "teachers-2023",
+};
+
+export const ExemplarUnits: Story = {
+  args: exemplarUnitsProps,
+  render: (args) => <Component {...args} />,
+};
