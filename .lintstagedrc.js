922c82e5fbced7640d66f1a96842768983e43115
mantagen <contact.mantagen@gmail.com>
2023-09-07 11:50:40 +0100

    chore: remove unused function

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,14 +1,9 @@
 const path = require("path");
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --base-dir . --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --file ")}`;
-
 module.exports = {
   "*.{js,jsx,ts,tsx}": [
     "npm run format:precommit",
-    "npx eslint --fix",
+    "eslint --fix",
     "npm run lint:styles",
   ],
 };
