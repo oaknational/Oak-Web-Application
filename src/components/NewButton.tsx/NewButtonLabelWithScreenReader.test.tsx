9f8c5d94d21da9f5583fd4eb5423db87f7acac88
Ben Protheroe <benprotheroe@Bens-MacBook-Pro.local>
2023-09-11 11:24:02 +0100

    feat(tab-image-button): new button label test

---
+++ src/components/NewButton.tsx/NewButtonLabelWithScreenReader.test.tsx
@@ -0,0 +1,12 @@
+import renderWithTheme from "../../__tests__/__helpers__/renderWithTheme";
+
+import ButtonLabel from './NewButtonLabelWithScreenReaderTitle'
+
+describe("Span", () => {
+  test("should have correct default styles", () => {
+    const { getByText } = renderWithTheme(<ButtonLabel>Test</ButtonLabel>);
+    const span = getByText("Test");
+    expect(span).toHaveStyle("font-family: Lexend,sans-serif");
+    expect(span).toHaveStyle(`font-size: 1rem`);
+  });
+});
