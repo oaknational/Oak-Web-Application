401bd0b10b3f85ccbc44f234d2a27eff2c03d285
Kyle Huggins <kyle.huggins7@gmail.com>
2023-09-12 18:15:31 +0100

    chore: fixes test

--- src/components/SubjectPhasePicker/SubjectPhasePicker.test.tsx
+++ src/components/SubjectPhasePicker/SubjectPhasePicker.test.tsx
@@ -108,9 +108,9 @@ describe("Component - subject phase picker", () => {
       <SubjectPhasePicker {...subjectPhaseOptions} />,
     );
     await userEvent.click(getByTitle("Subject"));
-    expect(queryByText("Latest resources for 2023/24")).toBeTruthy();
+    expect(queryByText("Latest resources")).toBeTruthy();
     await userEvent.keyboard("{Escape}");
-    expect(queryByText("Latest resources for 2023/24")).toBeNull();
+    expect(queryByText("Latest resources")).toBeNull();
     await userEvent.click(getByTitle("Phase"));
     expect(queryByText("Choose a school phase:")).toBeTruthy();
     await userEvent.keyboard("{Escape}");
