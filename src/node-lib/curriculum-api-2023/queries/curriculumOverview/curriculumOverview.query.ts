4f8c340b88b122d75ff582dbe7e8323ca947b560
Sonali Singh <sonali.singh@live.co.uk>
2023-09-14 14:06:20 +0100

    refactor: removed comments

--- src/node-lib/curriculum-api-2023/queries/curriculumOverview/curriculumOverview.query.ts
+++ src/node-lib/curriculum-api-2023/queries/curriculumOverview/curriculumOverview.query.ts
@@ -15,19 +15,7 @@ const curriculumOverviewQuery =
     if (!args.phaseSlug || !args.subjectSlug) {
       throw new OakError({ code: "curriculum-api/not-found" });
     }
-    // const res = await sdk.curriculumOverview({
-    //   subjectSlug: args.subjectSlug,
-    //   examboardSlug: args.examboardSlug,
-    //   phaseSlug: args.phaseSlug,
-    // });
 
-    // const [programmes] = res.programmes;
-
-    // if (!programmes) {
-    //   throw new OakError({ code: "curriculum-api/not-found" });
-    // }
-
-    // return curriculumOverviewMVFixture.parse();
     return curriculumOverviewMVFixture();
   };
 
