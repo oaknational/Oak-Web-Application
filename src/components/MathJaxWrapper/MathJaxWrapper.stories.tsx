20ce3670d128cd9b528b2865b9ac5ff1c9a09baf
Natassja Blore <natassja.blore@thenational.academy>
2023-09-20 11:24:12 +0100

    feat: add mathjax component to storybook

---
+++ src/components/MathJaxWrapper/MathJaxWrapper.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Component from "./MathJaxWrapper";
+
+export default {
+  title: "MathJax/Wrapper ",
+  component: Component,
+} as ComponentMeta<typeof Component>;
+
+const data =
+  "When \\(a \\ne 0\\), there exists two solutions for\\(ax^2 + bx + c = 0\\) as \\[x = {-b \\pm \\sqrt{b^2-4ac} \\over 2a}.\\]";
+
+const Template: ComponentStory<typeof Component> = () => (
+  <Component>{data}</Component>
+);
+
+export const MathJaxWrapper = Template.bind({});
