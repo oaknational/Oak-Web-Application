09d5749440b71dcc17a3d59ca99bb13f43cd4dc4
mantagen <contact.mantagen@gmail.com>
2023-09-26 16:47:57 +0100

    fix(generate-sprite): filter only icons which have both slug and image url

--- scripts/build/image_data/generate_sprite.js
+++ scripts/build/image_data/generate_sprite.js
@@ -26,7 +26,12 @@ async function main() {
   }`);
 
   function getSlugs({ assets }) {
-    return assets.map((asset) => asset.slug.current);
+    return (
+      assets
+        // filter to only have assets with an image url and a slug
+        .filter((asset) => asset.image?.asset?.url && asset.slug?.current)
+        .map((asset) => asset.slug.current)
+    );
   }
 
   /**
