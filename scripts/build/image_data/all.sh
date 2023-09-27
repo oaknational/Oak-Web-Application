02e88ee5843cf6f794bda4eb52eee6bd3c307d67
mantagen <contact.mantagen@gmail.com>
2023-09-27 09:59:11 +0100

    fix(generate sprites): exit on image data fail

--- scripts/build/image_data/all.sh
+++ scripts/build/image_data/all.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 node ./scripts/build/image_data/generate_favicons.js
 node ./scripts/build/image_data/generate_inline_sprite.js
 node ./scripts/build/image_data/generate_social_sharing_image.js
--- scripts/build/image_data/generate_inline_sprite.js
+++ scripts/build/image_data/generate_inline_sprite.js
@@ -8,9 +8,11 @@ const {
 } = require("./helpers");
 
 async function main() {
-  const client = getSanityClient();
+  try {
+    const client = getSanityClient();
 
-  const [inlineSpriteAssetRes] = await client.fetch(`*[_type == "brandAsset"] {
+    const [inlineSpriteAssetRes] =
+      await client.fetch(`*[_type == "brandAsset"] {
     logoWithText {
       image {
         asset->
@@ -18,38 +20,47 @@ async function main() {
     },
   }`);
 
-  const { logoWithText } = inlineSpriteAssetRes;
+    const { logoWithText } = inlineSpriteAssetRes;
 
-  const assetsByName = {
-    "logo-with-text": logoWithText,
-  };
+    const assetsByName = {
+      "logo-with-text": logoWithText,
+    };
 
-  /**
-   * assetsByName -> image-data/inline-sprite.json
-   * This will enable static types for InlineSpriteSvg.
-   */
-  const jsonPath = await writeJsonForTypes({
-    names: Object.keys(assetsByName),
-    fileName: "inline-sprite.json",
-  });
-  console.log(`✅ Inline sprite asset names written to ${jsonPath}`);
+    /**
+     * assetsByName -> image-data/inline-sprite.json
+     * This will enable static types for InlineSpriteSvg.
+     */
+    const jsonPath = await writeJsonForTypes({
+      names: Object.keys(assetsByName),
+      fileName: "inline-sprite.json",
+    });
+    console.log(`✅ Inline sprite asset names written to ${jsonPath}`);
 
-  const spriter = getSpriterInstance({ mode: "defs" });
+    const spriter = getSpriterInstance({ mode: "defs" });
 
-  for (const [name, asset] of Object.entries(assetsByName)) {
-    if (!asset?.image?.asset?.url) {
-      throw new Error(`Expected asset "${name}" to exist in sanity`);
+    for (const [name, asset] of Object.entries(assetsByName)) {
+      if (!asset?.image?.asset?.url) {
+        throw new Error(`Expected asset "${name}" to exist in sanity`);
+      }
+      await fetchSvgAndAddToSprite({
+        url: asset.image.asset.url,
+        name,
+        spriter,
+      });
     }
-    await fetchSvgAndAddToSprite({ url: asset.image.asset.url, name, spriter });
+
+    const spritePath = getGeneratedImageDataPath({
+      fileName: "inline-sprite.svg",
+    });
+
+    compileAndWriteSpriteToFile({ path: spritePath, spriter });
+
+    console.log(`✅ SVG inline sprite sheet written to ${spritePath}`);
+  } catch (error) {
+    console.error(error);
+    console.error("An exception occurred, see above for details.");
+    process.exit(1);
   }
-
-  const spritePath = getGeneratedImageDataPath({
-    fileName: "inline-sprite.svg",
-  });
-
-  compileAndWriteSpriteToFile({ path: spritePath, spriter });
-
-  console.log(`✅ SVG inline sprite sheet written to ${spritePath}`);
 }
 
 main();
