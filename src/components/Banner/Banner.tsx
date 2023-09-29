f3c0322700d8bd0137cdf9736d486b482d39a3e5
Ben Protheroe <benprotheroe@bens-mbp.home>
2023-09-29 09:12:14 +0100

    feat(homepage): style fixes

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 
 import Box from "../Box/Box";
-import MaxWidth from "../MaxWidth/MaxWidth";
 import Flex from "../Flex/Flex";
 import Typography from "../Typography/Typography";
 import ButtonAsLink from "../Button/ButtonAsLink";
@@ -11,27 +10,30 @@ import Tag from "@/components/TagPromotional";
 
 const HomePageBanner: FC = () => {
   return (
-    <Box>
-      <Hr $color={"oakGreen"} $height={4} $mt={0} />
-      <MaxWidth>
-        <Flex $justifyContent={"space-between"} $alignItems={"center"}>
-          <Flex $alignItems={"center"} $gap={16}>
-            <Tag />
-            <Typography $font={"heading-7"}>
-              Interactive curriculum plans
-            </Typography>
-          </Flex>
-          <ButtonAsLink
-            label={"View curriculum plans"}
-            variant={"brushNav"}
-            page={"curriculum-landing-page"}
-            icon={"arrow-right"}
-            $iconPosition={"trailing"}
-            iconBackground="black"
-          />
+    <Box $ph={16}>
+      <Hr $color={"oakGreen"} thickness={4} $mt={0} $mb={0} />
+      <Flex
+        $justifyContent={["center", "space-between"]}
+        $alignItems={"center"}
+        $pv={24}
+      >
+        <Flex $alignItems={"center"} $gap={16} $display={["none", "flex"]}>
+          <Tag />
+          <Typography $font={"heading-7"}>
+            Interactive curriculum plans
+          </Typography>
         </Flex>
-      </MaxWidth>
-      <Hr $color={"mint"} $height={4} $mb={0} />
+        <ButtonAsLink
+          label={"View curriculum plans"}
+          variant={"brushNav"}
+          page={"curriculum-landing-page"}
+          icon={"arrow-right"}
+          $iconPosition={"trailing"}
+          iconBackground="black"
+          size={"small"}
+        />
+      </Flex>
+      <Hr $color={"mint"} thickness={4} $mb={0} $mt={0} />
     </Box>
   );
 };
