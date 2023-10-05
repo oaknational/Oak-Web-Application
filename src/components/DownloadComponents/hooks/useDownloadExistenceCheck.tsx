37246b3e9382b128a4550d0055c957981fc132f8
Simon Katan <simon.katan@thenational.academy>
2023-10-05 11:43:14 +0100

    fix: resourcesExistenceAsArray assumed incorrect type

--- src/components/DownloadComponents/hooks/useDownloadExistenceCheck.tsx
+++ src/components/DownloadComponents/hooks/useDownloadExistenceCheck.tsx
@@ -36,21 +36,24 @@ const useDownloadExistenceCheck = (props: UseDownloadExistenceCheckProps) => {
           await getDownloadResourcesExistence(
             lessonSlug,
             resourceTypesAsString,
-
             isLegacyDownload,
           );
 
-        const resourcesExistenceAsArray = resourceExistence
-          ? Object.entries(
-              resourceExistence as Partial<
-                Record<DownloadResourceType, boolean>
-              >,
-            )
-          : [];
+        const resourcesExistenceAsArray: {
+          item: DownloadResourceType;
+          exists: boolean;
+        }[] = resourceExistence.map((r: unknown[]) => {
+          const k = r[0] as DownloadResourceType;
+          const v = r[1] as Record<string, unknown>;
+          return {
+            item: k,
+            exists: v.exists as boolean,
+          };
+        });
 
         const filteredResourcesExistenceAsArray = resourcesExistenceAsArray
-          .map(([key, value]) => {
-            if (value === true) return key;
+          .map((r) => {
+            if (r.exists) return r.item;
           })
           .filter((resource) => resource !== undefined);
 
