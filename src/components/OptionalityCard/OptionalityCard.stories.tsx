16fdb09ad01e0def1edfb07aa9cf84060227559d
Ben Protheroe <benprotheroe@bens-mbp.home>
2023-10-10 10:27:42 +0100

    fix(listing card): maintanability storybook fix

--- src/components/OptionalityCard/OptionalityCard.stories.tsx
+++ src/components/OptionalityCard/OptionalityCard.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 
 import AnalyticsDecorator from "../../storybook-decorators/AnalyticsDecorator";
 
@@ -6,24 +6,26 @@ import Component, { UnitOption } from "./OptionalityCard";
 
 import unitListingFixture from "@/node-lib/curriculum-api/fixtures/unitListing.fixture";
 
-export default {
-  title: "Lists/Optionality Card",
-  decorators: [AnalyticsDecorator],
-  component: Component,
-  argTypes: {},
-} as ComponentMeta<typeof Component>;
-
-const Template: ComponentStory<typeof Component> = (args) => (
-  <Component {...args} />
-);
-
-export const OptionalityCard = Template.bind({});
 const units = unitListingFixture().units;
 const option1 = units[0] ? units[0][0] : null;
 const option2 = units[1] ? units[1][0] : null;
 const unitOptions = [option1, option2] as UnitOption[];
 
-OptionalityCard.args = {
-  unitOptions: unitOptions,
-  index: 1,
+const meta: Meta<typeof Component> = {
+  title: "Lists/Optionality Card",
+  decorators: [AnalyticsDecorator],
+  component: Component,
+};
+
+export default meta;
+type Story = StoryObj<typeof Component>;
+
+export const OptionalityCard: Story = {
+  args: {
+    unitOptions: unitOptions,
+    index: 1,
+  },
+  render: (args) => {
+    return <Component {...args} />;
+  },
 };
