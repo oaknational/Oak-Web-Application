40356828c96e3e9c856a873616008537ba77ced3
lioneljingles <mrjingles@gmail.com>
2023-10-11 11:08:11 +0100

    fix: show line breaks in curriculum explainer

--- src/components/pages/CurriculumInfo/tabs/OverviewTab/OverviewTab.tsx
+++ src/components/pages/CurriculumInfo/tabs/OverviewTab/OverviewTab.tsx
@@ -110,6 +110,7 @@ const OverviewTab: FC<OverviewTabProps> = (props: OverviewTabProps) => {
             style={{ fontWeight: "light" }}
             $mt={10}
             $mr={12}
+            $whiteSpace={"break-spaces"}
           >
             {curriculaDesc}
           </Typography>
