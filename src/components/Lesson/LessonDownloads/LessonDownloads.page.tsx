9df9e2365b32bb153d446a59512c7bc1d8fd6cbb
Natassja Blore <natassja.blore@thenational.academy>
2023-10-30 15:14:55 +0000

    feat: require valid form before allowing download

--- src/components/Lesson/LessonDownloads/LessonDownloads.page.tsx
+++ src/components/Lesson/LessonDownloads/LessonDownloads.page.tsx
@@ -463,7 +463,7 @@ export function LessonDownloads(props: LessonDownloadsProps) {
                       text="Download .zip"
                       icon="download"
                       isLoading={isAttemptingDownload}
-                      disabled={hasFormErrors}
+                      disabled={hasFormErrors || !formState.isValid}
                       loadingText="Downloading..."
                     />
                   </Flex>
