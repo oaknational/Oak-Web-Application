0e84490e6f1a76fce51ea8cde98d5e7ac4621fa7
Kyle Huggins <kyle.huggins7@gmail.com>
2023-10-30 19:04:33 +0000

    chore: update form

--- src/components/DownloadComponents/helpers/getDownloadFormErrorMessage.test.tsx
+++ src/components/DownloadComponents/helpers/getDownloadFormErrorMessage.test.tsx
@@ -6,32 +6,37 @@ describe("getDownloadFormErrorMessage", () => {
   it("should return correct error message for one error", () => {
     const errorKeysArray: ErrorKeysType[] = ["terms"];
 
-    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual(
+    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual([
       "Accept terms and conditions to download",
-    );
+    ]);
   });
 
   it("should return correct error message for two error messages", () => {
     const errorKeysArray: ErrorKeysType[] = ["terms", "email"];
 
-    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual(
-      "Enter a valid email address and Accept terms and conditions to download",
-    );
+    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual([
+      "Enter a valid email address",
+      "Accept terms and conditions to download",
+    ]);
   });
 
-  it("should return correct error message for more than two error messages", () => {
+  it.only("should return correct error message for more than two error messages", () => {
     const errorKeysArray: ErrorKeysType[] = ["terms", "email", "downloads"];
 
-    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual(
-      "Enter a valid email address, Accept terms and conditions to download and Select at least one resource to download",
-    );
+    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual([
+      "Enter a valid email address",
+      "Accept terms and conditions to download",
+      "Select at least one resource to download",
+    ]);
   });
 
   it("should return error message in correct order", () => {
     const errorKeysArray: ErrorKeysType[] = ["email", "downloads", "terms"];
 
-    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual(
-      "Enter a valid email address, Accept terms and conditions to download and Select at least one resource to download",
-    );
+    expect(getDownloadFormErrorMessage(errorKeysArray)).toEqual([
+      "Enter a valid email address",
+      "Accept terms and conditions to download",
+      "Select at least one resource to download",
+    ]);
   });
 });
--- src/components/DownloadComponents/helpers/getDownloadFormErrorMessage.tsx
+++ src/components/DownloadComponents/helpers/getDownloadFormErrorMessage.tsx
@@ -6,8 +6,6 @@ type ErrorMessagesAndOrderType = {
 };
 
 const getDownloadFormErrorMessage = (errorsArray: ErrorKeysType[]) => {
-  let formErrorMessage;
-
   const errorMessagesAndOrder: Record<
     ErrorKeysType,
     ErrorMessagesAndOrderType
@@ -38,27 +36,7 @@ const getDownloadFormErrorMessage = (errorsArray: ErrorKeysType[]) => {
     )
     .filter(Boolean);
 
-  const errorCount = errorMessagesArray?.length;
-
-  if (errorCount === 1) {
-    formErrorMessage = `${errorMessagesArray[0]}`;
-  } else if (errorCount === 2) {
-    formErrorMessage = `${errorMessagesArray[0]} and ${errorMessagesArray[1]}`;
-  } else if (errorCount > 2) {
-    const allErrors = errorMessagesArray.map((errorMessage, i) => {
-      if (i < errorCount - 2) {
-        return `${errorMessage}, `;
-      } else if (i === errorCount - 2) {
-        return `${errorMessage} and `;
-      } else {
-        return errorMessage;
-      }
-    });
-
-    formErrorMessage = `${allErrors.join("")}`;
-  }
-
-  return formErrorMessage;
+  return errorMessagesArray;
 };
 
 export default getDownloadFormErrorMessage;
