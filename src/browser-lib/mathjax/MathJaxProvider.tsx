250f4e4d8e18ac263aa202caa27f37244f09d663
Joe Baker <joe.baker@thenational.academy>
2023-10-30 17:34:42 +0000

    fix: only allow mathjax on client

--- src/browser-lib/mathjax/MathJaxProvider.tsx
+++ src/browser-lib/mathjax/MathJaxProvider.tsx
@@ -2,8 +2,14 @@
 import React, { PropsWithChildren } from "react";
 import { MathJaxContext } from "better-react-mathjax";
 
-const MathJaxProvider = ({ children }: PropsWithChildren) => (
-  <MathJaxContext src={"/mathjax/tex-chtml.js"}>{children}</MathJaxContext>
-);
+const MathJaxProvider = ({ children }: PropsWithChildren) => {
+  if (typeof window === "undefined") {
+    return children;
+  } else {
+    return (
+      <MathJaxContext src={"/mathjax/tex-chtml.js"}>{children}</MathJaxContext>
+    );
+  }
+};
 
 export { MathJaxProvider };
--- src/browser-lib/mathjax/MathJaxWrap.tsx
+++ src/browser-lib/mathjax/MathJaxWrap.tsx
@@ -3,11 +3,15 @@ import { MathJax } from "better-react-mathjax";
 import React from "react";
 
 const MathJaxWrap = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <MathJax hideUntilTypeset="every" dynamic>
-      {children}
-    </MathJax>
-  );
+  if (typeof window === "undefined") {
+    return children;
+  } else {
+    return (
+      <MathJax hideUntilTypeset="every" dynamic>
+        {children}
+      </MathJax>
+    );
+  }
 };
 
 export { MathJaxWrap };
