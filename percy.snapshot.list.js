c52771e33f6c3b3bcc4a0db83512e4fad2e22a70
Jim Cresswell <jim.cresswell@gmail.com>
2023-11-07 12:22:44 +0000

    chore(Percy): wait for app to load

--- percy.snapshot.list.js
+++ percy.snapshot.list.js
@@ -12,8 +12,13 @@ console.log("Percy base url:", baseUrl);
 
 // Support single string relative URLs, or objects with a `url` key.
 // https://docs.percy.io/docs/percy-snapshot#configuration
-
-const snapshotRelativeUrls = getDeploymentTestUrls();
+// Make Percy wait for the Next app to load.
+const snapshotRelativeUrls = getDeploymentTestUrls().map((url) => {
+  return {
+    url,
+    waitForSelector: "#__next",
+  };
+});
 
 const urls = snapshotRelativeUrls.map((relUrl) => {
   if (typeof relUrl === "string") {
