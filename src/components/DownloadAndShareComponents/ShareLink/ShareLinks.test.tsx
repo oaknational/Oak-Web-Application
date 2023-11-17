c441ae9514a0bf7d0a8e06150a320e8916be4de0
Kyle Huggins <kyle.huggins7@gmail.com>
2023-11-17 10:39:55 +0000

    chore: add test

--- src/components/DownloadAndShareComponents/ShareLink/ShareLinks.test.tsx
+++ src/components/DownloadAndShareComponents/ShareLink/ShareLinks.test.tsx
@@ -5,6 +5,16 @@ import ShareLinks from "./ShareLinks";
 
 import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
 
+const x = jest.fn();
+
+jest.mock("@/context/Analytics/useAnalytics", () => ({
+  useAnalytics: jest.fn(() => ({
+    track: {
+      lessonShared: x,
+    },
+  })),
+}));
+
 describe("ShareLinks", () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -38,5 +48,24 @@ describe("ShareLinks", () => {
     const user = userEvent.setup();
     await user.click(copyLinkButton);
     expect(copyLinkButton).toHaveTextContent("Link copied to clipboard");
+  });
+
+  it("should call onSubmit with copy-link", async () => {
+    const onSubmit = jest.fn();
+    renderWithTheme(
+      <ShareLinks
+        disabled={false}
+        lessonSlug="test-slug"
+        selectedActivities={["exit-quiz-questions"]}
+        onSubmit={onSubmit}
+      />,
+    );
+    const copyLinkButton = screen.getByRole("button", {
+      name: "Copy link to clipboard",
+    });
+    expect(copyLinkButton).toBeInTheDocument();
+    const user = userEvent.setup();
+    await user.click(copyLinkButton);
+    expect(onSubmit).toHaveBeenCalledWith("copy-link");
   });
 });
