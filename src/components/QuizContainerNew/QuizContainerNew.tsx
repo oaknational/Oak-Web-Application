308b955051b8f1bd9f63b420f2c736cf4270418a
Joe Baker <joe.baker@thenational.academy>
2023-11-21 14:06:47 +0000

    feat: image attribution below quiz

--- src/components/QuizContainerNew/QuizContainerNew.tsx
+++ src/components/QuizContainerNew/QuizContainerNew.tsx
@@ -1,5 +1,8 @@
 import { FC } from "react";
 
+import { Span } from "../Typography";
+import Box from "../Box";
+
 import QuestionsListNew from "./QuestionsListNew";
 
 import { LessonOverviewQuizData } from "@/node-lib/curriculum-api-2023/shared.schema";
@@ -11,16 +14,30 @@ export type QuizProps = {
 };
 
 const QuizContainerNew: FC<QuizProps> = (props) => {
+  const imageAttribution: { attribution: string; questionNumber: string } | [] =
+    [];
   return props.questions && props.questions.length > 0 ? (
-    <Flex
-      $flexDirection={"column"}
-      $justifyContent={"center"}
-      $width={"100%"}
-      $position={"relative"}
-    >
-      <QuestionsListNew {...props} />
-      <BoxBorders />
-    </Flex>
+    <>
+      <Flex
+        $flexDirection={"column"}
+        $justifyContent={"center"}
+        $width={"100%"}
+        $position={"relative"}
+      >
+        <QuestionsListNew {...props} />
+        <BoxBorders />
+      </Flex>
+      {imageAttribution.length > 0 && (
+        <Box $mt={24}>
+          {imageAttribution.map(({ attribution, questionNumber }) => (
+            <>
+              <Span $font={"body-3-bold"}>{`Q${questionNumber} `}</Span>
+              <Span $font={"body-3"}>{`${attribution} `}</Span>
+            </>
+          ))}
+        </Box>
+      )}
+    </>
   ) : null;
 };
 
