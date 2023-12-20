e84b81a450589ca255fc3587be3b8fd10925f4b2
Simon Katan <simon.katan@thenational.academy>
2023-12-20 10:16:20 +0000

    feat: wip QuizMCQMultiAnswer

--- src/components/PupilJourneyComponents/QuizMCQMultiAnswer/QuizMCQMultiAnswer.stories.tsx
+++ src/components/PupilJourneyComponents/QuizMCQMultiAnswer/QuizMCQMultiAnswer.stories.tsx
@@ -1,18 +1,41 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import {
+  OakBox,
+  OakThemeProvider,
+  oakDefaultTheme,
+} from "@oak-academy/oak-components";
 
 import { QuizMCQMultiAnswer } from "./QuizMCQMultiAnswer";
 
+import { mcqTextAnswers } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.fixture";
+
 const meta = {
   component: QuizMCQMultiAnswer,
+  tags: ["autodocs"],
 } satisfies Meta<typeof QuizMCQMultiAnswer>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const multiTextAnswers = [...mcqTextAnswers];
+if (multiTextAnswers[0]) {
+  multiTextAnswers[0].answer_is_correct = true;
+}
+
 /*
  * This component is not yet implemented.
  */
 export const Primary: Story = {
-  render: () => <QuizMCQMultiAnswer />,
+  render: (args) => (
+    <OakThemeProvider theme={oakDefaultTheme}>
+      <OakBox
+        $background={"bg-decorative1-very-subdued"}
+        $pa={"inner-padding-m"}
+      >
+        <QuizMCQMultiAnswer {...args} />
+      </OakBox>
+    </OakThemeProvider>
+  ),
+  args: { questionUid: "123", answers: multiTextAnswers },
 };
--- src/components/PupilJourneyComponents/QuizMCQMultiAnswer/QuizMCQMultiAnswer.tsx
+++ src/components/PupilJourneyComponents/QuizMCQMultiAnswer/QuizMCQMultiAnswer.tsx
@@ -1,5 +1,35 @@
-export type QuizMCQMultiAnswerProps = {};
+import { OakFlex, OakQuizCheckBox } from "@oak-academy/oak-components";
+
+import { MCAnswer } from "@/node-lib/curriculum-api-2023/shared.schema";
+
+export type QuizMCQMultiAnswerProps = {
+  questionUid: string;
+  answers: MCAnswer[];
+  currentQuestionIndex: number;
+  isFeedbackMode: boolean;
+  // selectedAnswer?: { answer?: MCAnswer | null; index: number };
+  // setSelectedAnswer: (val: { answer?: MCAnswer | null; index: number }) => void;
+};
 
 export const QuizMCQMultiAnswer = (props: QuizMCQMultiAnswerProps) => {
-  return <div>QuizMCQMultiAnswer</div>;
+  const {
+    questionUid,
+    answers,
+    // currentQuestionIndex,
+    isFeedbackMode,
+    // selectedAnswer,
+    // setSelectedAnswer,
+  } = props;
+
+  return (
+    <OakFlex $flexDirection={"column"} $gap={"space-between-s"}>
+      {answers.map((answer, index) => (
+        <OakQuizCheckBox
+          id={`${questionUid}-answer${index}`}
+          value={answer.text}
+          isFeedback={isFeedbackMode}
+        />
+      ))}
+    </OakFlex>
+  );
 };
