fabc3c6d41917ce259c24fd8b4056991c3990665
Simon Katan <simon.katan@thenational.academy>
2024-01-05 14:01:10 +0000

    test: adding tests for MCQ single

---
+++ src/components/PupilJourneyComponents/QuizMCQSingleAnswer/QuizMCQSingleAnswer.test.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import "@testing-library/jest-dom";
+import { OakThemeProvider, oakDefaultTheme } from "@oak-academy/oak-components";
+
+import {
+  QuizEngineContextType,
+  QuizEngineContext,
+} from "@/components/PupilJourneyComponents/QuizEngineProvider";
+import { QuizRenderer } from "@/components/PupilJourneyComponents/QuizRenderer";
+import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
+import { quizQuestions } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.fixture";
+
+const questionsArrayFixture = quizQuestions || [];
+
+const getContext = (): QuizEngineContextType => ({
+  currentQuestionData: questionsArrayFixture[0],
+  currentQuestionIndex: 0,
+  questionState: [
+    {
+      mode: "init",
+      offerHint: false,
+      grade: 0,
+    },
+  ],
+  updateQuestionMode: (mode) => mode,
+  handleSubmitMCAnswer: () => {},
+  handleNextQuestion: () => {},
+  score: 0,
+  maxScore: 1,
+  isComplete: false,
+});
+
+describe("QuizMCQSingleAnswer", () => {
+  it("renders the question answers", () => {
+    const context = getContext();
+
+    const { getByText } = renderWithTheme(
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <QuizEngineContext.Provider value={context}>
+          <QuizRenderer />
+        </QuizEngineContext.Provider>
+      </OakThemeProvider>,
+    );
+
+    if (context?.currentQuestionData?.answers?.["multiple-choice"]) {
+      for (const answer of context.currentQuestionData.answers[
+        "multiple-choice"
+      ]) {
+        for (const t of answer.answer) {
+          if (t.type === "text") {
+            const answerText = getByText(t.text);
+            expect(answerText).toBeInTheDocument();
+          }
+        }
+      }
+    }
+  });
+});
