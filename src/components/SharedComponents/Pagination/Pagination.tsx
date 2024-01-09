cdd7da83ec75a06f2167cf99a5756bb79541310d
John Roberts <john.roberts@thenational.academy>
2024-01-09 22:26:14 +0000

    chore(deps): resolve href ref

--- src/components/SharedComponents/Pagination/Pagination.tsx
+++ src/components/SharedComponents/Pagination/Pagination.tsx
@@ -1,5 +1,5 @@
 import Router, { useRouter } from "next/router";
-import { resolveHref } from "next/dist/shared/lib/router/utils/resolve-href";
+import { resolveHref } from "next/dist/client/resolve-href";
 import React, { FC, RefObject, useEffect } from "react";
 
 import { Span } from "@/components/SharedComponents/Typography";
