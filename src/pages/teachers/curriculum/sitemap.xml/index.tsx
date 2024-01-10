3b2bb308e54b172f5c11168fcf0a1b3ac4311846
Jim Cresswell <jim.cresswell@gmail.com>
2024-01-10 11:26:29 +0000

    chore(updates): fix sitemap types

--- src/pages/teachers/curriculum/sitemap.xml/index.tsx
+++ src/pages/teachers/curriculum/sitemap.xml/index.tsx
@@ -1,7 +1,7 @@
 import path from "node:path/posix";
 
 import { GetServerSideProps } from "next";
-import { getServerSideSitemap } from "next-sitemap";
+import { getServerSideSitemapLegacy } from "next-sitemap";
 
 import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
 
@@ -53,7 +53,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 
-  return getServerSideSitemap(context, fields);
+  return getServerSideSitemapLegacy(context, fields);
 };
 
 export default function Sitemap() {}
