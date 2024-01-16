2b6e72413a9f239a6e17473ca8454607b7fc75ac
Natassja Blore <natassja.blore@thenational.academy>
2024-01-16 13:27:27 +0000

    feat: add keystage pages to sitemap

---
+++ src/pages/teachers/key-stages/sitemap.xml/index.tsx
@@ -0,0 +1,27 @@
+import { GetServerSideProps } from "next";
+import { getServerSideSitemap } from "next-sitemap";
+
+import { getServerSideSitemapFields } from "@/node-lib/isr";
+import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // SITEMAP_BASE_URL is written to the .env file during next.config.js execution.
+  const sitemapBaseUrl = process.env.SITEMAP_BASE_URL || "";
+
+  const keyStages = await curriculumApi2023.teachersHomePage();
+
+  const keystageSlugs = keyStages.keyStages.map((ks) => `${ks.slug}/subjects`);
+
+  const fields = getServerSideSitemapFields(
+    context,
+    sitemapBaseUrl,
+    "/key-stages/",
+    keystageSlugs,
+  );
+
+  return getServerSideSitemap(context, fields);
+};
+
+// Default export to prevent next.js errors
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function Sitemap() {}
