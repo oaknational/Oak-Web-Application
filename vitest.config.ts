f493058b36e046ef28b7c7953aa5ba72bdd0724f
mantagen <contact.mantagen@gmail.com>
2024-01-19 11:30:44 +0000

    build(vitest): attempt github workflow with globals true

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: "jsdom",
     setupFiles: ["vitest.setup.ts"],
     coverage: {
