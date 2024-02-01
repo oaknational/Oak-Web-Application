571a328eaf6c17177bfb8cf92b70499b1eb43d4e
Kyle Huggins <kyle.huggins7@gmail.com>
2024-02-01 11:21:02 +0000

    chore: update spacing sizes

--- src/components/TeacherComponents/QuizQuestionsMatchAnswers/QuizQuestionsMatchAnswers.tsx
+++ src/components/TeacherComponents/QuizQuestionsMatchAnswers/QuizQuestionsMatchAnswers.tsx
@@ -14,7 +14,13 @@ export const QuizQuestionsMatchAnswers = ({
   questionNumber: number;
 }) => {
   return (
-    <Flex $flexDirection={"column"} $gap={4} $alignItems={"start"} role="list">
+    <Flex
+      $flexDirection={"column"}
+      $gap={4}
+      $alignItems={"start"}
+      role="list"
+      $width={"100%"}
+    >
       {answers.map((item, i) => {
         const match_option = item.match_option[0];
         const correct_choice = item.correct_choice[0];
@@ -34,7 +40,7 @@ export const QuizQuestionsMatchAnswers = ({
                 {removeMarkdown(correct_choice.text)}
               </VisuallyHidden>
               <Icon $mr={8} name={"tick"} />
-              <Flex $flexWrap={"wrap"}>
+              <Flex $flexWrap={"wrap"} $width={["100%", "100%", "max-content"]}>
                 <P
                   $whiteSpace={"nowrap"}
                   $font={["body-2-bold", "body-1-bold"]}
