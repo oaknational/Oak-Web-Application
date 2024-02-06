ecb541338fe034451aab99297f8f56bf83b2b256
Natassja Blore <natassja.blore@thenational.academy>
2024-02-06 12:33:53 +0000

    feat: update title for eyfs subjects page

--- src/components/TeacherViews/SubjectListing.view.tsx
+++ src/components/TeacherViews/SubjectListing.view.tsx
@@ -11,11 +11,16 @@ import Flex from "@/components/SharedComponents/Flex";
 const SubjectListingPage: FC<SubjectListingPageProps> = (props) => {
   const { subjects, keyStageSlug, keyStageTitle } = props;
 
+  const title =
+    keyStageSlug === "early-years-foundation-stage"
+      ? `${keyStageTitle} areas of learning`
+      : `${keyStageTitle} subjects`;
+
   return (
     <Flex $flexDirection={"column"}>
       <MaxWidth $maxWidth={[480, 840, 1280]} $ph={[12]}>
         <Heading $font={"heading-3"} tag={"h1"} $mt={[32, 40]} $mb={40}>
-          {`${keyStageTitle} subjects`}
+          {title}
         </Heading>
         <GridList $rg={16} $cg={16} $gridAutoRows={"1fr"} $mb={72}>
           {subjects.map((subject, i) => {
