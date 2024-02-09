82df29b9b70b129743b3b9de5c9fcee98463892a
Carl Whittaker <carl359@gmail.com>
2024-02-09 16:25:42 +0000

    refactor: remove redundant returns
    so that sonarcloud does not chastise me for a crime I did not commit

--- src/components/SharedComponents/helpers/downloadAndShareHelpers/downloadLessonResources.tsx
+++ src/components/SharedComponents/helpers/downloadAndShareHelpers/downloadLessonResources.tsx
@@ -24,8 +24,6 @@ const downloadLessonResources = async (
   if (downloadResourcesLink) {
     createAndClickHiddenDownloadLink(downloadResourcesLink);
   }
-
-  return;
 };
 
 export default downloadLessonResources;
