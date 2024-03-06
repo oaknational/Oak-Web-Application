5148eb17e57348f102222ef00dbee13249c75504
Natassja Blore <natassja.blore@thenational.academy>
2024-03-06 10:45:45 +0000

    fix: add counts to fixture

--- src/node-lib/curriculum-api-2023/fixtures/specialistProgrammes.fixture.ts
+++ src/node-lib/curriculum-api-2023/fixtures/specialistProgrammes.fixture.ts
@@ -11,11 +11,15 @@ export const specialistProgrammeListingPageDataFixture = (
         programmeSlug: "early-development",
         developmentStageSlug: "early-development-1",
         developmentStageTitle: "Early development 1",
+        unitCount: 10,
+        lessonCount: 100,
       },
       {
         programmeSlug: "creative-arts",
         developmentStageSlug: "creative-arts-1",
         developmentStageTitle: "Creative arts 1",
+        unitCount: 10,
+        lessonCount: 100,
       },
     ],
 
