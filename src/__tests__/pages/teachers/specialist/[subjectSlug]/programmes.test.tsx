022508deddc1157cef1d33784e2f702bc101025a
Natassja Blore <natassja.blore@thenational.academy>
2024-03-06 14:07:10 +0000

    test: add tests for programme page

---
+++ src/__tests__/pages/teachers/specialist/[subjectSlug]/programmes.test.tsx
@@ -0,0 +1,79 @@
+import { screen } from "@testing-library/dom";
+
+import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
+import curriculumApi from "@/node-lib/curriculum-api-2023";
+import SpecialistProgrammeListingPage, {
+  getStaticProps,
+} from "@/pages/teachers/specialist/[subjectSlug]/programmes";
+import { specialistProgrammeListingPageDataFixture } from "@/node-lib/curriculum-api-2023/fixtures/specialistProgrammes.fixture";
+
+const render = renderWithProviders();
+
+describe("pages/specialist/subjects", () => {
+  it("renders therapies and specialist", () => {
+    render(
+      <SpecialistProgrammeListingPage
+        curriculumData={specialistProgrammeListingPageDataFixture()}
+      />,
+    );
+
+    const developmentStageCard = screen.getByText("Early development 1", {
+      exact: false,
+    });
+
+    expect(developmentStageCard).toBeInTheDocument();
+  });
+  it("should link to unit listing page when there are multiple programmes for a subject", () => {
+    render(
+      <SpecialistProgrammeListingPage
+        curriculumData={specialistProgrammeListingPageDataFixture()}
+      />,
+    );
+
+    const specialistCard = screen.getByText("Early development 1").closest("a");
+
+    expect(specialistCard).toHaveAttribute(
+      "href",
+      "/teachers/specialist/programmes/early-development/units",
+    );
+  });
+});
+
+jest.mock("@/node-lib/curriculum-api-2023", () => ({
+  specialistProgrammeListing: jest.fn(),
+}));
+
+describe("getStaticProps", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+  });
+  it("should return  404 page when there are no programmes", async () => {
+    const result = await getStaticProps({ params: { subjectSlug: "fake" } });
+    expect(result).toEqual({ notFound: true });
+  });
+  it("Should call the api", async () => {
+    await getStaticProps({ params: { subjectSlug: "numeracy" } });
+
+    expect(curriculumApi.specialistProgrammeListing).toHaveBeenCalledTimes(1);
+    expect(curriculumApi.specialistProgrammeListing).toHaveBeenCalledWith({
+      subjectSlug: "numeracy",
+    });
+  });
+  it("should fetch the data and return the props", async () => {
+    const curriculumData = specialistProgrammeListingPageDataFixture();
+    (curriculumApi.specialistProgrammeListing as jest.Mock).mockResolvedValue(
+      curriculumData,
+    );
+
+    const result = await getStaticProps({
+      params: { subjectSlug: "communication-and-language" },
+    });
+
+    expect(result).toEqual({
+      props: {
+        curriculumData,
+      },
+    });
+  });
+});
