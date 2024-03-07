33106f11888ab458206d593b21b1222694266f08
Natassja Blore <natassja.blore@thenational.academy>
2024-03-07 13:36:35 +0000

    test: mock specialist lesson listing

--- src/node-lib/curriculum-api-2023/queries/specialistLessonListing/specialistLessonListing.query.test.ts
+++ src/node-lib/curriculum-api-2023/queries/specialistLessonListing/specialistLessonListing.query.test.ts
@@ -3,6 +3,39 @@ import sdk from "../../sdk";
 import specialistLessonListingQuery from "./specialistLessonListing.query";
 import { SpecialistLessonListingDataSchema } from "./specialistLessonListing.schema";
 
+jest.mock("../../sdk", () => {
+  return {
+    specialistLessonListing: jest.fn(() =>
+      Promise.resolve({
+        specialistLessonListing: [
+          {
+            unit_slug: "staying-safe-al-5556",
+            programme_slug: "independent-living-applying-learning",
+            combined_programme_fields: {
+              subject: "Staying safe",
+              subject_slug: "staying-safe",
+              developmentstage: "Applying learning",
+              developmentstage_slug: "applying-learning",
+              developmentstage_display_order: 3,
+            },
+            lesson_slug: "staying-safe-al-5556",
+            lesson_title: "Staying safe",
+            expired: false,
+            pupil_lesson_outcome: "Pupils will be able to stay safe",
+            worksheet_url: "https://www.example.com",
+            video_mux_playback_id: "123",
+            video_title: "Staying safe",
+            starter_quiz: 1,
+            exit_quiz: 1,
+            contains_copyright_content: false,
+            unit_title: "Staying safe",
+          },
+        ],
+      }),
+    ),
+  };
+});
+
 describe("specialist programme listing", () => {
   test("it runs", async () => {
     const res = await specialistLessonListingQuery(sdk)({
@@ -15,7 +48,14 @@ describe("specialist programme listing", () => {
   test("it throws an error if no lessons are found", async () => {
     await expect(
       async () =>
-        await specialistLessonListingQuery(sdk)({
+        await specialistLessonListingQuery({
+          ...sdk,
+          specialistLessonListing: jest.fn(() =>
+            Promise.resolve({
+              specialistLessonListing: [],
+            }),
+          ),
+        })({
           unitSlug: "blah",
           programmeSlug: "fake-programme",
         }),
@@ -26,9 +66,6 @@ describe("specialist programme listing", () => {
       unitSlug: "staying-safe-al-5556",
       programmeSlug: "independent-living-applying-learning",
     });
-
-    console.log("diego res", res);
-
     expect(SpecialistLessonListingDataSchema.parse(res)).toEqual(res);
   });
 });
