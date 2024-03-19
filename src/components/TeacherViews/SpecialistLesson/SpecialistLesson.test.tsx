0ffc7d1b3cc1ef2425efeb71904993caff518c35
Kyle Huggins <kyle.huggins7@gmail.com>
2024-03-19 10:47:34 +0000

    chore: amend test

--- src/components/TeacherViews/SpecialistLesson/SpecialistLesson.test.tsx
+++ src/components/TeacherViews/SpecialistLesson/SpecialistLesson.test.tsx
@@ -9,12 +9,16 @@ const render = renderWithProviders();
 
 describe("SpecialistLessonDownloads", () => {
   test("renders component", () => {
-    const { getByText } = render(
+    const { getAllByText, getByText } = render(
       <SpecialistLesson lesson={specialistLessonOverviewFixture()} />,
     );
-    const title = getByText("Communication and language - Applying learning");
+    const title = getAllByText(specialistLessonOverviewFixture().lessonTitle);
+    const unitTitle = getByText(
+      "Communication and language - Applying learning",
+    );
 
-    expect(title).toBeInTheDocument();
+    expect(title[0]).toBeInTheDocument();
+    expect(unitTitle).toBeInTheDocument();
   });
 
   test("renders with the correct resources", () => {
