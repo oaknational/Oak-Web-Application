48da5d3dc0fa10d8c4df25458a4d807571513175
Carl Whittaker <carl359@gmail.com>
2024-03-19 17:19:29 +0000

    feat(PUPIL-485): display a tooltip when a pupil tries to submit an incomplete answer

--- src/components/PupilViews/PupilQuiz/PupilQuiz.view.test.tsx
+++ src/components/PupilViews/PupilQuiz/PupilQuiz.view.test.tsx
@@ -3,16 +3,34 @@ import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
 import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
 import { act, fireEvent } from "@testing-library/react";
+import { ValueOf } from "next/dist/shared/lib/constants";
 
 import { PupilViewsQuiz } from "./PupilQuiz.view";
 
 import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
-import { quizQuestions } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.fixture";
+import {
+  matchAnswers,
+  mcqCorrectAnswer,
+  mcqIncorrectAnswer,
+  mcqTextAnswers,
+  orderAnswers,
+  quizQuestions,
+  shortAnswers,
+} from "@/node-lib/curriculum-api-2023/fixtures/quizElements.fixture";
 import {
   LessonEngineContext,
   LessonSection,
 } from "@/components/PupilComponents/LessonEngineProvider";
 import { createLessonEngineContext } from "@/components/PupilComponents/pupilTestHelpers/createLessonEngineContext";
+import { AnswersSchema } from "@/node-lib/curriculum-api-2023/shared.schema";
+
+jest.mock("@oaknational/oak-components", () => {
+  return {
+    ...jest.requireActual("@oaknational/oak-components"),
+    OakQuizMatch: () => null,
+    OakQuizOrder: () => null,
+  };
+});
 
 describe("PupilQuizView", () => {
   it("renders heading, mode and answer when there is currentQuestionData", () => {
@@ -25,17 +43,6 @@ describe("PupilQuizView", () => {
     );
     const heading = getByText("Starter Quiz");
     expect(heading).toBeInTheDocument();
-  });
-
-  it("disables submit button when mode is init", () => {
-    const { getByRole } = renderWithTheme(
-      <OakThemeProvider theme={oakDefaultTheme}>
-        <LessonEngineContext.Provider value={createLessonEngineContext()}>
-          <PupilViewsQuiz questionsArray={quizQuestions} />
-        </LessonEngineContext.Provider>
-      </OakThemeProvider>,
-    );
-    expect(getByRole("button", { name: /Check/ })).toBeDisabled();
   });
 
   it("renders Next button when questionState.mode is feedback", () => {
@@ -89,6 +96,54 @@ describe("PupilQuizView", () => {
       });
 
       expect(getByRole("button", { name: label })).toBeInTheDocument();
+    },
+  );
+
+  it.each([
+    ["short-answer", shortAnswers, "You need to type an answer to move on!"],
+    [
+      "multiple-choice",
+      mcqTextAnswers,
+      "You need to select an answer to move on!",
+    ],
+    [
+      "multiple-choice",
+      [mcqCorrectAnswer, mcqCorrectAnswer, mcqIncorrectAnswer],
+      "You need to select answers to move on!",
+    ],
+    ["order", orderAnswers, "You need to order to move on!"],
+    ["match", matchAnswers, "You need to match to move on!"],
+  ] satisfies Array<[keyof AnswersSchema, ValueOf<AnswersSchema>, string]>)(
+    "displays a tooltip for the %p question type when it is incomplete",
+    (questionType, answers, tooltipText) => {
+      const { getByRole } = renderWithTheme(
+        <OakThemeProvider theme={oakDefaultTheme}>
+          <LessonEngineContext.Provider value={createLessonEngineContext()}>
+            <PupilViewsQuiz
+              questionsArray={[
+                {
+                  questionId: 1,
+                  questionUid: "test-question",
+                  questionType,
+                  questionStem: [],
+                  feedback: "",
+                  hint: "",
+                  active: true,
+                  answers: {
+                    [questionType]: answers,
+                  },
+                },
+              ]}
+            />
+          </LessonEngineContext.Provider>
+        </OakThemeProvider>,
+      );
+
+      act(() => {
+        fireEvent.click(getByRole("button", { name: /Check/ }));
+      });
+
+      expect(getByRole("tooltip")).toHaveTextContent(tooltipText);
     },
   );
 });
