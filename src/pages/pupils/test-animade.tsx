160d93de3f3c751da7e6583b16bfebb36b140cbc
ben <ben.protheroe@thenational.academy>
2024-03-19 17:36:08 +0000

    feat(lottie-file): extend lottie animations

--- src/pages/pupils/test-animade.tsx
+++ src/pages/pupils/test-animade.tsx
@@ -1,18 +1,45 @@
 import { NextPage } from "next";
 import Lottie from "react-lottie-player";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { OakFlex, OakHeading } from "@oaknational/oak-components";
+import { useState } from "react";
 
 import lottieJson from "../../lotties/lottie.json";
 
 const TestPage: NextPage = () => {
+  const [width, setWidth] = useState(150);
+  const handleIncreaseSize = () => {
+    setWidth((prevState) => prevState + 20);
+  };
+  const handleDecreaseSize = () => {
+    setWidth((prevState) => prevState * 0.8);
+  };
   return (
-    <div>
-      <Lottie
+    <OakFlex $flexDirection={"column"} $alignItems={"center"}>
+      <button type="button" onClick={handleIncreaseSize}>
+        Increase size
+      </button>
+      <button type="button" onClick={handleDecreaseSize}>
+        Decrease size
+      </button>
+      <OakHeading tag="h1">local asset</OakHeading>
+      <Lottie loop animationData={lottieJson} play style={{ width }} />
+      <OakHeading tag="h1">cloudinary asset loop</OakHeading>
+      <Player
+        src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/08_Ambient_HeadTilt_du99fc.json"
+        className="player"
         loop
-        animationData={lottieJson}
-        play
-        style={{ width: 1000, height: 1000 }}
+        autoplay
+        style={{ width }}
       />
-    </div>
+      <OakHeading tag="h1">cloudinary asset no loop</OakHeading>
+      <Player
+        src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/08_Ambient_HeadTilt_du99fc.json"
+        className="player"
+        autoplay
+        style={{ width }}
+      />
+    </OakFlex>
   );
 };
 
