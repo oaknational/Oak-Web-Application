7d2001293d678552e90046c21d9d87a4fda749ee
Kyle Huggins <kyle.huggins7@gmail.com>
2024-03-26 13:54:35 +0000

    chore: change lesson to unit

--- src/components/TeacherComponents/UnitListItemLessonCount/UnitListItemLessonCount.tsx
+++ src/components/TeacherComponents/UnitListItemLessonCount/UnitListItemLessonCount.tsx
@@ -26,7 +26,7 @@ export const UnitListItemLessonCount = ({
           <OakSpan $font={["body-3", "heading-light-7"]} $color={textColor}>
             {!!lessonCount &&
               `${lessonCount} ${lessonCount > 1 ? "lessons" : "lesson"}`}
-            {expired && `This lesson is currently unavailable.`}
+            {expired && `This unit is currently unavailable.`}
           </OakSpan>
         )}
 
