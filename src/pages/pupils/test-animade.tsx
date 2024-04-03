419fda8678b80002d8eac2bf3016c04d011469bc
ben <ben.protheroe@thenational.academy>
2024-04-03 17:55:42 +0100

    feat(lottie-test): add new lottie files to test

--- src/pages/pupils/test-animade.tsx
+++ src/pages/pupils/test-animade.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from "next";
 import { Player } from "@lottiefiles/react-lottie-player";
-import { OakFlex, OakHeading } from "@oaknational/oak-components";
+import {
+  OakFlex,
+  OakGrid,
+  OakGridArea,
+  OakHeading,
+  OakLabel,
+} from "@oaknational/oak-components";
 import { useState } from "react";
+import { DotLottiePlayer } from "@dotlottie/react-player";
 
 import lottieJson from "../../lotties/lottie.json";
 
@@ -38,6 +45,99 @@ const TestPage: NextPage = () => {
         autoplay
         style={{ width }}
       />
+
+      <OakGrid>
+        <OakGridArea $colSpan={[12, 6, 3]} $mh={"space-between-l"}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/01_Intro_qk9cya.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Intro</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/02_Outro_m7kguu.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Outro</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/03_Celebratory_Fanfare_bkamfs.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Celebratory_Fanfare</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/04_Celebratory_Off_The_Scale_h4soj0.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Celebratory_Off_The_Scale</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/05_Something_Wrong_-_Encouraging_v6nryd.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Something_Wrong_-_Encouraging</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/06_Explaining_zrg5qj.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Explaining</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/07_Exercising_avpugw.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Exercising</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/08_Ambient_Head_Tilt_bcefeg.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Ambient_Head_Tilt</OakLabel>
+        </OakGridArea>
+        <OakGridArea $colSpan={[12, 6, 3]}>
+          <OakFlex>
+            <DotLottiePlayer
+              src="https://res.cloudinary.com/oak-web-application/raw/upload/v1710780142/lotties/09_Ambient_Face_tiucju.lottie"
+              autoplay
+              loop
+             />
+          </OakFlex>
+          <OakLabel>Ambient_Face</OakLabel>
+        </OakGridArea>
+      </OakGrid>
     </OakFlex>
   );
 };
