076f5dc5fa4e2afe074665f0bc0761f48960d487
Simon Katan <simon.katan@thenational.academy>
2024-04-09 17:11:40 +0100

    fix: deprecated_fields is optional

--- src/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema.ts
+++ src/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema.ts
@@ -85,7 +85,7 @@ const unitDataSchema = z.object({
   description: z.string().nullable(),
   slug: z.string(),
   tags: z.array(z.number()).nullable(),
-  deprecated_fields: z.object({}).nullable(),
+  deprecated_fields: z.object({}).nullable().optional(),
   title: z.string(),
   _state: _stateSchema,
   _cohort: _cohortSchema,
