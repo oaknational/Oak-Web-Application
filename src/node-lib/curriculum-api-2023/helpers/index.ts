0d16a251b77ae085eba030bb325a6db472e15c9c
Kyle Huggins <kyle.huggins7@gmail.com>
2024-04-18 11:24:17 +0100

    chore: explanation for function

--- src/node-lib/curriculum-api-2023/helpers/index.ts
+++ src/node-lib/curriculum-api-2023/helpers/index.ts
@@ -4,8 +4,9 @@ export const toSentenceCase = (str: string) => {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 };
 
+/* When including Core on the MV it returns on all subjects even if they do not have Core tier data,
+ this function removes core from programmes that shouldn't have Core as an option*/
 const hasCoreTierSet = new Set(["maths-ks4"]);
-
 export const filterOutCoreTier = (
   subjectSlug?: string | null,
   keyStageSlug?: string | null,
