5c6c9f0a64bdd91045dfa53cabe6aa782e7019b2
Jamie Blair <info@orangemug.co.uk>
2024-04-23 11:33:56 +0100

    chore: update testing-library to latest version

--- src/components/PupilViews/PupilIntro/PupilIntro.view.test.tsx
+++ src/components/PupilViews/PupilIntro/PupilIntro.view.test.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "@testing-library/jest-dom/extend-expect";
 import "@testing-library/jest-dom";
 import { fireEvent } from "@testing-library/react";
 import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
