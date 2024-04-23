fa072fd96f96004240330a352fe9640fe4fe38f7
ben <ben.protheroe@thenational.academy>
2024-04-23 13:46:14 +0100

    feat(lessonlisting): add test suite for page

--- src/__tests__/pages/pupils/programmes/[programmesSlug]/units/[unitSlug]/lessons.test.tsx
+++ src/__tests__/pages/pupils/programmes/[programmesSlug]/units/[unitSlug]/lessons.test.tsx
@@ -1,10 +1,37 @@
-import {
+import { render } from "@testing-library/react";
+
+import PupilLessonListingPage, {
   getStaticPaths,
   getStaticProps,
 } from "@/pages/pupils/beta//programmes/[programmeSlug]/units/[unitSlug]/lessons";
 import * as curriculumApi2023 from "@/node-lib/curriculum-api-2023/__mocks__/index";
+import { lessonBrowseDataFixture } from "@/node-lib/curriculum-api-2023/fixtures/lessonBrowseData.fixture";
 
 describe("pages/pupils/programmes/[programmeSlug]/units/[unitSlug]/lessons/[lessonSlug]/index", () => {
+  describe("renders", () => {
+    it("should render the subjectTitle, unitTitle, and yearDescription", () => {
+      const { getByText } = render(
+        <PupilLessonListingPage
+          curriculumData={{
+            browseData: [lessonBrowseDataFixture({})],
+          }}
+        />,
+      );
+      expect(getByText("subject")).toBeInTheDocument();
+      expect(getByText("unit-title")).toBeInTheDocument();
+      expect(getByText("year-description")).toBeInTheDocument();
+    });
+    it("should render the lesson titles as a tags", () => {
+      const { getByText } = render(
+        <PupilLessonListingPage
+          curriculumData={{
+            browseData: [lessonBrowseDataFixture({})],
+          }}
+        />,
+      );
+      expect(getByText("lesson-title")).toBeInTheDocument();
+    });
+  });
   describe("getStaticPaths", () => {
     it("Should not generate pages at build time", async () => {
       const res = await getStaticPaths();
