94bb297ff2362ac00e7e6fd173602396fd94955a
ben <ben.protheroe@thenational.academy>
2024-04-24 14:45:49 +0100

    feat(unitlistingquery): update schema import

--- src/node-lib/curriculum-api-2023/queries/pupilUnitListing/pupilUnitListing.test.ts
+++ src/node-lib/curriculum-api-2023/queries/pupilUnitListing/pupilUnitListing.test.ts
@@ -1,32 +1,9 @@
-import {
-  programmeFieldsFixture,
-  syntheticUnitvariantsWithLessonIds,
-  unitDataFixture,
-  unitvariantFixture,
-} from "@oaknational/oak-curriculum-schema";
+import { syntheticUnitvariantsWithLessonIdsFixture } from "@oaknational/oak-curriculum-schema";
 
 import { pupilUnitListingQuery } from "./pupilUnitListing.query";
 
 import { PupilUnitListingQuery } from "@/node-lib/curriculum-api-2023/generated/sdk";
 import sdk from "@/node-lib/curriculum-api-2023/sdk";
-
-export const syntheticUnitvariantsWithLessonIdsFixture = ({
-  overrides = {},
-}: {
-  overrides?: Partial<syntheticUnitvariantsWithLessonIds>;
-} = {}): syntheticUnitvariantsWithLessonIds => ({
-  unit_slug: "unit-slug",
-  programme_slug: "programme-slug",
-  is_legacy: false,
-  lesson_count: 1,
-  unit_data: unitDataFixture(),
-  null_unitvariant: unitvariantFixture(),
-  programme_fields: programmeFieldsFixture(),
-  supplementary_data: {
-    unit_order: 1,
-  },
-  ...overrides,
-});
 
 describe("pupilUnitListing()", () => {
   test("it returns the units if found", async () => {
