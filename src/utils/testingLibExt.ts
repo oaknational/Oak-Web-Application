78150c578c9d141e5f481353dc7e8ddc868246a5
Jamie Blair <info@orangemug.co.uk>
2024-04-30 12:07:50 +0100

    chore: added usage comment to renderHookWithInitialState

--- src/utils/testingLibExt.ts
+++ src/utils/testingLibExt.ts
@@ -1,6 +1,18 @@
 import { renderHook } from "@testing-library/react";
 import { useEffect } from "react";
 
+/*
+ * renderHook from "@testing-library/react" with initial state
+ * @example
+ *   const hook = renderHookWithInitialState(() => {
+ *      return useFoobar();
+ *   })
+ *
+ *   // Pre-rendered result
+ *   expect(hook.initialResult).toEqual(...)
+ *   // Rendered result
+ *   expect(hook.result.current).toEqual(...)
+ */
 export function renderHookWithInitialState(fn: () => unknown) {
   let initialResult: unknown;
   const out = renderHook(() => {
