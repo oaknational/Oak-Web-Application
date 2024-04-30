d190e99f71fe334970c8e3f2e3529d5b25669243
Jamie Blair <info@orangemug.co.uk>
2024-04-30 17:44:44 +0100

    test: added tests for useFetch(...)

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,45 @@
+import useSWR from "swr";
+
+import OakError, { ErrorInfo } from "@/errors/OakError";
+
+/*
+ * Wraps and reports errors in an OakError object
+ */
+export function useFetch<T>(url: string, errorCode: ErrorInfo["code"]) {
+  const { data, error, isLoading } = useSWR<T, OakError>(
+    url,
+    (queryUrl: string) => {
+      return fetch(queryUrl)
+        .then((res) => {
+          console.log("res.ok[%s]", queryUrl, res.ok);
+          if (res.ok) {
+            return res.json();
+          } else {
+            const error = new OakError({
+              code: errorCode,
+              meta: {
+                status: res.status,
+                statusText: res.statusText,
+                queryUrl,
+              },
+            });
+
+            // TODO: Where should I be reporting
+            // reportError(error);
+            throw error;
+          }
+        })
+        .catch((err) => {
+          const error = new OakError({
+            code: errorCode,
+            originalError: err,
+          });
+
+          // TODO: Where should I be reporting
+          // reportError(error);
+          throw error;
+        });
+    },
+  );
+  return { data, error, isLoading } as const;
+}
