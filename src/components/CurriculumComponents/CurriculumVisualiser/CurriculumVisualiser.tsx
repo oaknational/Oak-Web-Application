178f2b421ba26a51419159b7f769b61298fc4ae9
Sonali Singh <sonali.singh@live.co.uk>
2024-05-07 17:45:56 +0100

    refactor: moved units tab sidebarto render outside of year group

--- src/components/CurriculumComponents/CurriculumVisualiser/CurriculumVisualiser.tsx
+++ src/components/CurriculumComponents/CurriculumVisualiser/CurriculumVisualiser.tsx
@@ -240,6 +240,7 @@ const CurriculumVisualiser: FC<CurriculumVisualiserProps> = ({
           .map((year, index) => {
             const { units, childSubjects, domains, tiers, disciplines } =
               yearData[year] as YearData[string];
+
             return (
               <Box
                 key={year}
@@ -351,14 +352,15 @@ const CurriculumVisualiser: FC<CurriculumVisualiserProps> = ({
                   data-testid="unit-cards"
                 >
                   {units
-                    .filter((unit: Unit) =>
-                      isVisibleUnit(
+                    .filter((unit: Unit) => {
+                      const isVisible = isVisibleUnit(
                         yearSelection,
                         duplicateUnitSlugs,
                         year,
                         unit,
-                      ),
-                    )
+                      );
+                      return isVisible;
+                    })
                     .map((unit: Unit, index: number) => {
                       const isHighlighted = isHighlightedUnit(
                         unit,
@@ -449,33 +451,33 @@ const CurriculumVisualiser: FC<CurriculumVisualiserProps> = ({
                         </Card>
                       );
                     })}
-                  <UnitsTabSidebar
-                    displayModal={displayModal}
-                    onClose={handleCloseModal}
-                    lessons={currentUnitLessons}
-                    programmeSlug={createProgrammeSlug(unitData, examboardSlug)}
-                    unitOptionsAvailable={unitOptionsAvailable}
-                    unitSlug={unitData?.slug}
-                    unitVariantID={unitVariantID}
-                  >
-                    <UnitModal
-                      setCurrentUnitLessons={setCurrentUnitLessons}
-                      setUnitVariantID={setUnitVariantID}
-                      unitData={unitData}
-                      displayModal={displayModal}
-                      setUnitOptionsAvailable={setUnitOptionsAvailable}
-                      unitOptionsAvailable={unitOptionsAvailable}
-                      isHighlighted={
-                        unitData
-                          ? isHighlightedUnit(unitData, selectedThread)
-                          : false
-                      }
-                    />
-                  </UnitsTabSidebar>
                 </OakFlex>
               </Box>
             );
           })}
+      {displayModal && (
+        <UnitsTabSidebar
+          displayModal={displayModal}
+          onClose={handleCloseModal}
+          lessons={currentUnitLessons}
+          programmeSlug={createProgrammeSlug(unitData, examboardSlug)}
+          unitOptionsAvailable={unitOptionsAvailable}
+          unitSlug={unitData?.slug}
+          unitVariantID={unitVariantID}
+        >
+          <UnitModal
+            setCurrentUnitLessons={setCurrentUnitLessons}
+            setUnitVariantID={setUnitVariantID}
+            unitData={unitData}
+            displayModal={displayModal}
+            setUnitOptionsAvailable={setUnitOptionsAvailable}
+            unitOptionsAvailable={unitOptionsAvailable}
+            isHighlighted={
+              unitData ? isHighlightedUnit(unitData, selectedThread) : false
+            }
+          />
+        </UnitsTabSidebar>
+      )}
     </OakGridArea>
   );
 };
