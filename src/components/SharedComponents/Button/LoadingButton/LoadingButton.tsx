4cc42c8b6c5343f7b3c978cf2ed267235305aa22
Jamie Blair <info@orangemug.co.uk>
2024-05-14 18:11:28 +0100

    refactor: revert changes to <LoadingButton/>

--- src/components/SharedComponents/Button/LoadingButton/LoadingButton.tsx
+++ src/components/SharedComponents/Button/LoadingButton/LoadingButton.tsx
@@ -21,13 +21,10 @@ type LoadingButtonProps = {
   disabled: boolean;
   success?: boolean;
   ariaLabel?: string;
-  onClick?: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
+  onClick: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>;
 } & (
   | {
       type: "button";
-    }
-  | {
-      type: "submit";
     }
   | { type: "link"; href: string; external: boolean }
 );
@@ -125,9 +122,9 @@ const LoadingButton: FC<LoadingButtonProps> = (props) => {
 
   const onClick: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement> = (
     e,
-  ) => (disabled ? e.preventDefault() : props.onClick?.(e));
+  ) => (disabled ? e.preventDefault() : props.onClick(e));
 
-  return props.type === "button" || props.type === "submit" ? (
+  return props.type === "button" ? (
     <StyledButton
       onClick={props.onClick}
       aria-disabled={disabled}
