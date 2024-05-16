b2c111b2ed15b851a3618077ef8729283e477df4
Joe Baker <joe.baker@thenational.academy>
2024-05-16 16:18:48 +0100

    test: improve test coverage

--- src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.test.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.test.ts
@@ -6,6 +6,7 @@ import lessonListing, {
   getTransformedLessons,
   getTransformedUnit,
 } from "./lessonListing.query";
+import lessonListingSchema from "./lessonListing.schema";
 
 describe("lessonListing()", () => {
   describe("lessonListing query", () => {
@@ -19,6 +20,99 @@ describe("lessonListing()", () => {
           unitSlug: "unit-slug",
         });
       }).rejects.toThrow(`Resource not found`);
+    });
+
+    test("it returns data in the correct shape", async () => {
+      const res = await lessonListing({
+        ...sdk,
+        lessonListing: jest.fn(() =>
+          Promise.resolve({
+            unit: [syntheticUnitvariantLessonsFixture()],
+          }),
+        ),
+      })({
+        programmeSlug: "programme-slug",
+        unitSlug: "unit-slug",
+      });
+
+      expect(lessonListingSchema.parse(res)).toEqual({
+        programmeSlug: "programme-slug",
+        keyStageSlug: "ks1",
+        keyStageTitle: "Key stage 1",
+        subjectSlug: "maths",
+        subjectTitle: "Maths",
+        unitSlug: "unit-slug",
+        unitTitle: "unit-title",
+        tierSlug: null,
+        tierTitle: null,
+        examBoardSlug: null,
+        examBoardTitle: null,
+        yearTitle: "Year 1",
+        lessons: [
+          {
+            lessonSlug: "lesson-slug",
+            lessonTitle: "lesson-title",
+            description: "lesson-description",
+            pupilLessonOutcome: "pupil-lesson-outcome",
+            expired: false,
+            quizCount: 0,
+            videoCount: 0,
+            presentationCount: 0,
+            worksheetCount: 0,
+            hasCopyrightMaterial: false,
+            orderInUnit: 1,
+            lessonCohort: "2023-2024",
+          },
+        ],
+      });
+    });
+    test("it returns lessons in the correct order", async () => {
+      const lessonListingFixture2 = syntheticUnitvariantLessonsFixture({
+        overrides: {
+          supplementary_data: { unit_order: 2, order_in_unit: 2 },
+          lesson_data: {
+            lesson_id: 1,
+            lesson_uid: "lesson-uid",
+            title: "lesson-title-2",
+            description: "lesson-description-2",
+            slug: "lesson-slug",
+            pupil_lesson_outcome: "pupil-lesson-outcome",
+            key_learning_points: [{}],
+            equipment_and_resources: null,
+            content_guidance_details: null,
+            content_guidance: null,
+            supervision_level: null,
+            thirdpartycontent_list: null,
+            misconceptions_and_common_mistakes: null,
+            keywords: null,
+            video_id: null,
+            sign_language_video_id: null,
+            quiz_id_starter: null,
+            quiz_id_exit: null,
+            asset_id_slidedeck: null,
+            asset_id_worksheet: null,
+            copyright_content: null,
+            _state: "published",
+            _cohort: "2023-2024",
+            deprecated_fields: null,
+          },
+        },
+      });
+
+      const res = await lessonListing({
+        ...sdk,
+        lessonListing: jest.fn(() =>
+          Promise.resolve({
+            unit: [lessonListingFixture2, syntheticUnitvariantLessonsFixture()],
+          }),
+        ),
+      })({
+        programmeSlug: "programme-slug",
+        unitSlug: "unit-slug",
+      });
+
+      expect(res.lessons[0]?.lessonTitle).toEqual("lesson-title");
+      expect(res.lessons[1]?.lessonTitle).toEqual("lesson-title-2");
     });
 
     test("throws a Zod error if the response is invalid", async () => {
