d0241ffce48e61fe983ea8a44555192daeda61cd
Joe Baker <joe.baker@thenational.academy>
2024-05-16 12:12:51 +0100

    feat: lesson listing new query

--- src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.test.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.test.ts
@@ -1,45 +1,96 @@
+import { syntheticUnitvariantLessonsFixture } from "@oaknational/oak-curriculum-schema";
+
 import sdk from "../../sdk";
 
-import lessonListing from "./lessonListing.query";
+import lessonListing, {
+  getTransformedLessons,
+  getTransformedUnit,
+} from "./lessonListing.query";
 
 describe("lessonListing()", () => {
-  test("throws a not found error if no unit is found", async () => {
-    await expect(async () => {
-      await lessonListing({
-        ...sdk,
-        lessonListing: jest.fn(() => Promise.resolve({ unit: [] })),
-      })({
-        programmeSlug: "programme-slug",
-        unitSlug: "unit-slug",
-      });
-    }).rejects.toThrow(`Resource not found`);
-  });
+  describe("lessonListing query", () => {
+    test("throws a not found error if no unit is found", async () => {
+      await expect(async () => {
+        await lessonListing({
+          ...sdk,
+          lessonListing: jest.fn(() => Promise.resolve({ unit: [] })),
+        })({
+          programmeSlug: "programme-slug",
+          unitSlug: "unit-slug",
+        });
+      }).rejects.toThrow(`Resource not found`);
+    });
 
-  test("throws a Zod error if the response is invalid", async () => {
-    await expect(async () => {
-      await lessonListing({
-        ...sdk,
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        lessonListing: jest.fn(() =>
-          Promise.resolve({
-            unit: [
-              {
-                programmeSlug: "programme-slug",
-                unitTitle: "unit-title",
-                subjectSlug: "subject-slug",
-                subjectTitle: "subject-title",
-                keyStageSlug: "key-stage-slug",
-                keyStageTitle: "key-stage-title",
-                lessons: [],
-              },
-            ],
-          }),
-        ),
-      })({
+    test("throws a Zod error if the response is invalid", async () => {
+      await expect(async () => {
+        await lessonListing({
+          ...sdk,
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          lessonListing: jest.fn(() =>
+            Promise.resolve({
+              unit: [
+                {
+                  programmeSlug: "programme-slug",
+                  unitTitle: "unit-title",
+                  subjectSlug: "subject-slug",
+                  subjectTitle: "subject-title",
+                  keyStageSlug: "key-stage-slug",
+                  keyStageTitle: "key-stage-title",
+                  lessons: [],
+                },
+              ],
+            }),
+          ),
+        })({
+          programmeSlug: "programme-slug",
+          unitSlug: "unit-slug",
+        });
+      }).rejects.toThrow(`lesson_slug`);
+    });
+  });
+  describe("transform functions", () => {
+    test("getTransformedUnit returns the correct data", async () => {
+      const transformedLessons = getTransformedUnit(
+        syntheticUnitvariantLessonsFixture(),
+        [],
+      );
+      expect(transformedLessons).toEqual({
+        examBoardSlug: null,
+        examBoardTitle: null,
+        keyStageSlug: "ks1",
+        keyStageTitle: "Key stage 1",
+        lessons: [],
         programmeSlug: "programme-slug",
+        subjectSlug: "maths",
+        subjectTitle: "Maths",
+        tierSlug: null,
+        tierTitle: null,
         unitSlug: "unit-slug",
+        unitTitle: "unit-title",
+        yearTitle: "Year 1",
       });
-    }).rejects.toThrow(`lesson_slug`);
+    });
+    test("getTransformedLessons returns the correct data", async () => {
+      const transformedLessons = getTransformedLessons({
+        unit: [syntheticUnitvariantLessonsFixture()],
+      });
+      expect(transformedLessons).toEqual([
+        {
+          description: "lesson-description",
+          expired: false,
+          hasCopyrightMaterial: false,
+          lessonCohort: "2023-2024",
+          lessonSlug: "lesson-slug",
+          lessonTitle: "lesson-title",
+          orderInUnit: 1,
+          presentationCount: 0,
+          pupilLessonOutcome: "pupil-lesson-outcome",
+          quizCount: 0,
+          videoCount: 0,
+          worksheetCount: 0,
+        },
+      ]);
+    });
   });
 });
--- src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.ts
@@ -7,10 +7,59 @@ import { Sdk } from "../../sdk";
 import OakError from "../../../../errors/OakError";
 import { lessonListSchema } from "../../shared.schema";
 import { toSentenceCase } from "../../helpers";
+import { LessonListingQuery } from "../../generated/sdk";
 
 import lessonListingSchema, {
   LessonListingPageData,
 } from "./lessonListing.schema";
+
+export const getTransformedLessons = (res: LessonListingQuery) => {
+  return res.unit
+    .map((l) => {
+      const lesson = syntheticUnitvariantLessonsSchema.parse(l);
+      const transformedLesson = {
+        lessonSlug: lesson.lesson_slug,
+        lessonTitle: lesson.lesson_data.title,
+        description:
+          lesson.lesson_data.description ||
+          lesson.lesson_data.pupil_lesson_outcome,
+        pupilLessonOutcome: lesson.lesson_data.pupil_lesson_outcome,
+        expired: lesson.lesson_data.deprecated_fields?.expired || false,
+        quizCount:
+          (lesson.lesson_data.quiz_id_starter ? 1 : 0) +
+          (lesson.lesson_data.quiz_id_exit ? 1 : 0),
+        videoCount: lesson.lesson_data.video_id ? 1 : 0,
+        presentationCount: lesson.lesson_data.asset_id_slidedeck ? 1 : 0,
+        worksheetCount: lesson.lesson_data.asset_id_worksheet ? 1 : 0,
+        hasCopyrightMaterial: false, // this is hardcoded to false in previous lesson listing mv (data tools)
+        orderInUnit: lesson.supplementary_data.order_in_unit,
+        lessonCohort: lesson.lesson_data._cohort,
+      };
+      return transformedLesson;
+    })
+    .sort((a, b) => a.orderInUnit - b.orderInUnit);
+};
+
+export const getTransformedUnit = (
+  unit: SyntheticUnitvariantLessons,
+  parsedLessons: LessonListingPageData["lessons"],
+): LessonListingPageData => {
+  return {
+    programmeSlug: unit.programme_slug,
+    keyStageSlug: unit.programme_fields.keystage_slug,
+    keyStageTitle: toSentenceCase(unit.programme_fields.keystage_description),
+    subjectSlug: unit.programme_fields.subject_slug,
+    subjectTitle: unit.programme_fields.subject,
+    unitSlug: unit.unit_slug,
+    unitTitle: unit.unit_data.title,
+    tierSlug: unit.programme_fields.tier_slug,
+    tierTitle: unit.programme_fields.tier_description,
+    examBoardSlug: unit.programme_fields.examboard_slug,
+    examBoardTitle: unit.programme_fields.examboard,
+    yearTitle: unit.programme_fields.year_description,
+    lessons: parsedLessons,
+  };
+};
 
 const lessonListingQuery =
   (sdk: Sdk) => async (args: { programmeSlug: string; unitSlug: string }) => {
@@ -21,59 +70,10 @@ const lessonListingQuery =
     if (!unit) {
       throw new OakError({ code: "curriculum-api/not-found" });
     }
-
-    const unitLessons = res.unit
-      .map((l) => {
-        const lesson = syntheticUnitvariantLessonsSchema.parse(l);
-        const transformedLesson = {
-          lessonSlug: lesson.lesson_slug,
-          lessonTitle: lesson.lesson_data.title,
-          description:
-            lesson.lesson_data.description ||
-            lesson.lesson_data.pupil_lesson_outcome,
-          pupilLessonOutcome: lesson.lesson_data.pupil_lesson_outcome,
-          expired: lesson.lesson_data.deprecated_fields?.expired || false,
-          quizCount:
-            (lesson.lesson_data.quiz_id_starter ? 1 : 0) +
-            (lesson.lesson_data.quiz_id_exit ? 1 : 0),
-          videoCount: lesson.lesson_data.video_id ? 1 : 0,
-          presentationCount: lesson.lesson_data.asset_id_slidedeck ? 1 : 0,
-          worksheetCount: lesson.lesson_data.asset_id_worksheet ? 1 : 0,
-          hasCopyrightMaterial: false, // this is hardcoded to false in previous lesson listing mv  (data tools)
-          orderInUnit: lesson.supplementary_data.order_in_unit,
-          lessonCohort: lesson.lesson_data._cohort,
-        };
-        return transformedLesson;
-      })
-      .sort((a, b) => a.orderInUnit - b.orderInUnit);
-
-    const parsedUnit = syntheticUnitvariantLessonsSchema.parse(unit);
+    const unitLessons = getTransformedLessons(res);
     const parsedLessons = lessonListSchema.parse(unitLessons);
-
-    const getTransformedUnit = (
-      data: SyntheticUnitvariantLessons,
-    ): LessonListingPageData => {
-      return {
-        programmeSlug: data.programme_slug,
-        keyStageSlug: data.programme_fields.keystage_slug,
-        keyStageTitle: toSentenceCase(
-          data.programme_fields.keystage_description,
-        ),
-        subjectSlug: data.programme_fields.subject_slug,
-        subjectTitle: data.programme_fields.subject,
-        unitSlug: data.unit_slug,
-        unitTitle: data.unit_data.title,
-        tierSlug: data.programme_fields.tier_slug,
-        tierTitle: data.programme_fields.tier_description,
-        examBoardSlug: data.programme_fields.examboard_slug,
-        examBoardTitle: data.programme_fields.examboard,
-        yearTitle: data.programme_fields.year_description,
-        lessons: parsedLessons,
-      };
-    };
-
-    const transformedUnit = getTransformedUnit(parsedUnit);
-
+    const parsedUnit = syntheticUnitvariantLessonsSchema.parse(unit);
+    const transformedUnit = getTransformedUnit(parsedUnit, parsedLessons);
     return lessonListingSchema.parse(transformedUnit);
   };
 
