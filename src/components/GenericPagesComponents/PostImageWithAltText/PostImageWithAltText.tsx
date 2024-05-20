979f82eaa0d958f35057b7635642258173f5c249
Kyle Huggins <kyle.huggins7@gmail.com>
2024-05-20 12:59:00 +0100

    chore: amend the margin of images for cms blogs and webinars

--- src/components/GenericPagesComponents/PostImageWithAltText/PostImageWithAltText.tsx
+++ src/components/GenericPagesComponents/PostImageWithAltText/PostImageWithAltText.tsx
@@ -11,7 +11,7 @@ const PostImageWithAltText = (
     return null;
   }
 
-  return <CMSImage image={props.value} $mt={24} $mb={24} />;
+  return <CMSImage image={props.value} $mt={48} $mb={48} />;
 };
 
 export default PostImageWithAltText;
