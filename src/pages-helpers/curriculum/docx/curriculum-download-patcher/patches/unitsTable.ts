b881b4695948730111874d1af54bbca609a9b16f
Jamie Blair <info@orangemug.co.uk>
2024-05-20 12:14:48 +0100

    feat: units table docx page generation

--- src/pages-helpers/curriculum/docx/curriculum-download-patcher/patches/unitsTable.ts
+++ src/pages-helpers/curriculum/docx/curriculum-download-patcher/patches/unitsTable.ts
@@ -92,16 +92,91 @@ function buildYear(
       <w:p>
         <w:pPr>
             ${index > 0 && `<w:pageBreakBefore/>`}
+            <w:sz w:val="44"/>
+            <w:szCs w:val="44"/>
+            <w:sectPr w:rsidR="00F97FD2" w:rsidSect="006A40E7">
+              <w:type w:val="continuous"/>
+              <w:pgSz w:w="11909" w:h="16834"/>
+              <w:pgMar w:top="709" w:right="851" w:bottom="709" w:left="851" w:header="720" w:footer="720" w:gutter="0"/>
+              <w:cols w:space="720"/>
+            </w:sectPr>
         </w:pPr>
         <w:r>
             <w:rPr>
                 <w:color w:val="222222"/>
-                <w:sz w:val="44"/>
-                <w:szCs w:val="44"/>
-                <w:rFonts w:ascii="Lexend SemiBold" w:cs="Lexend SemiBold" w:eastAsia="Lexend SemiBold" w:hAnsi="Lexend SemiBold"/>
+                <w:sz w:val="56"/>
+                <w:szCs w:val="56"/>
+                <w:b/>
+                <w:rFonts w:ascii="Lexend" w:cs="Lexend" w:eastAsia="Lexend" w:hAnsi="Lexend"/>
             </w:rPr>
             <w:t xml:space="preserve">Year ${year} units</w:t>
         </w:r>
+      </w:p>
+      <w:p>
+        <w:pPr>
+            <w:rPr>
+                <w:sz w:val="28"/>
+                <w:szCs w:val="28"/>
+            </w:rPr>
+        </w:pPr>
+        <w:r>
+            <w:rPr>
+                <w:rFonts w:ascii="Lexend" w:eastAsia="Lexend" w:hAnsi="Lexend" w:cs="Lexend"/>
+                <w:b/>
+                <w:sz w:val="28"/>
+                <w:szCs w:val="28"/>
+                <w:u w:val="single"/>
+            </w:rPr>
+            <w:t>View interactive sequence online</w:t>
+        </w:r>
+        <w:r>
+                <w:rPr>
+                    <w:noProof/>
+                </w:rPr>
+                <w:drawing>
+                    <wp:anchor distT="114300" distB="114300" distL="114300" distR="114300" simplePos="0" relativeHeight="251691008" behindDoc="0" locked="0" layoutInCell="1" hidden="0" allowOverlap="1" wp14:anchorId="485FEB59" wp14:editId="5459E64E">
+                        <wp:simplePos x="0" y="0"/>
+                        <wp:positionH relativeFrom="column">
+                            <wp:posOffset>2476500</wp:posOffset>
+                        </wp:positionH>
+                        <wp:positionV relativeFrom="paragraph">
+                            <wp:posOffset>-28575</wp:posOffset>
+                        </wp:positionV>
+                        <wp:extent cx="275852" cy="275852"/>
+                        <wp:effectExtent l="0" t="0" r="0" b="0"/>
+                        <wp:wrapNone/>
+                        <wp:docPr id="50" name="image8.png" descr="Open link"/>
+                        <wp:cNvGraphicFramePr/>
+                        <a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
+                            <a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture">
+                                <pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture">
+                                    <pic:nvPicPr>
+                                        <pic:cNvPr id="50" name="image8.png" descr="Open link"/>
+                                        <pic:cNvPicPr preferRelativeResize="0"/>
+                                    </pic:nvPicPr>
+                                    <pic:blipFill>
+                                        <a:blip r:embed="rId30"/>
+                                        <a:srcRect/>
+                                        <a:stretch>
+                                            <a:fillRect/>
+                                        </a:stretch>
+                                    </pic:blipFill>
+                                    <pic:spPr>
+                                        <a:xfrm>
+                                            <a:off x="0" y="0"/>
+                                            <a:ext cx="275852" cy="275852"/>
+                                        </a:xfrm>
+                                        <a:prstGeom prst="rect">
+                                            <a:avLst/>
+                                        </a:prstGeom>
+                                        <a:ln/>
+                                    </pic:spPr>
+                                </pic:pic>
+                            </a:graphicData>
+                        </a:graphic>
+                    </wp:anchor>
+                </w:drawing>
+            </w:r>
       </w:p>
       <w:p>
         <w:r>
