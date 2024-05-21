b440edf49009fbe0f9a3358bf44d97513585076f
Natassja Blore <natassja.blore@thenational.academy>
2024-05-21 11:34:40 +0100

    chore: add more error logging

--- src/node-lib/curriculum-api-2023/queries/unitListing/units/getLessonCountsForUnits.ts
+++ src/node-lib/curriculum-api-2023/queries/unitListing/units/getLessonCountsForUnits.ts
@@ -15,49 +15,62 @@ export const getLessonCountsForUnit = async (units: Partial<UnitData>[][]) => {
     };
   });
 
-  const countsResponse = await getBatchedRequests(batchCountsRequests);
-  const parsedCounts = countsResponse.map((c) => lessonCounts.parse(c.data));
+  try {
+    const countsResponse = await getBatchedRequests(batchCountsRequests);
+    const parsedCounts = countsResponse.map((c) => lessonCounts.parse(c.data));
 
-  return parsedCounts.reduce(
-    (acc, counts) => {
-      const lessonCount = counts.lessonCount.aggregate.count;
-      const expiredLessonCount = counts.expiredLessonCount.aggregate.count;
+    return parsedCounts.reduce(
+      (acc, counts) => {
+        const lessonCount = counts.lessonCount.aggregate.count;
+        const expiredLessonCount = counts.expiredLessonCount.aggregate.count;
 
-      const unitSlug =
-        counts.lessonCount.nodes.find((n) => n.unit_slug)?.unit_slug ??
-        counts.expiredLessonCount.nodes.find((n) => n.unit_slug)?.unit_slug;
-      const unitId =
-        counts.lessonCount.nodes.find((n) => n.unit_data)?.unit_data ??
-        counts.expiredLessonCount.nodes.find((n) => n.unit_data)?.unit_data;
+        const unitSlug =
+          counts.lessonCount.nodes.find((n) => n.unit_slug)?.unit_slug ??
+          counts.expiredLessonCount.nodes.find((n) => n.unit_slug)?.unit_slug;
+        const unitId =
+          counts.lessonCount.nodes.find((n) => n.unit_data)?.unit_data ??
+          counts.expiredLessonCount.nodes.find((n) => n.unit_data)?.unit_data;
 
-      if (!unitSlug || !unitId) {
-        throw new OakError({
-          code: "curriculum-api/not-found",
-          originalError: `Lesson counts for unit not found, UnitSlug: ${unitSlug}, UnitId: ${unitId}`,
-        });
-      }
+        if (!unitSlug || !unitId) {
+          throw new OakError({
+            code: "curriculum-api/not-found",
+            originalError: `Lesson counts for unit not found, UnitSlug: ${unitSlug}, UnitId: ${unitId}`,
+          });
+        }
 
-      if (acc[unitId] && acc[unitId]?.[unitSlug]) {
-        throw new OakError({
-          code: "curriculum-api/uniqueness-assumption-violated",
-          originalError: `Duplicate lesson count data for unit ${unitSlug}`,
-        });
-      }
-      if (acc[unitId]) {
-        acc[unitId] = {
-          ...acc[unitId],
-          [unitSlug]: { lessonCount, expiredLessonCount },
-        };
-      } else {
-        acc[unitId] = { [unitSlug]: { lessonCount, expiredLessonCount } };
-      }
-      return acc;
-    },
-    {} as Record<
-      string,
-      {
-        [unitSlug: string]: { lessonCount: number; expiredLessonCount: number };
-      }
-    >,
-  );
+        if (acc[unitId] && acc[unitId]?.[unitSlug]) {
+          throw new OakError({
+            code: "curriculum-api/uniqueness-assumption-violated",
+            originalError: `Duplicate lesson count data for unit ${unitSlug}`,
+          });
+        }
+        if (acc[unitId]) {
+          acc[unitId] = {
+            ...acc[unitId],
+            [unitSlug]: { lessonCount, expiredLessonCount },
+          };
+        } else {
+          acc[unitId] = { [unitSlug]: { lessonCount, expiredLessonCount } };
+        }
+        return acc;
+      },
+      {} as Record<
+        string,
+        {
+          [unitSlug: string]: {
+            lessonCount: number;
+            expiredLessonCount: number;
+          };
+        }
+      >,
+    );
+  } catch (error) {
+    // TODO: remove console.error
+    console.error(
+      "Error in getLessonCountsForUnit",
+      batchCountsRequests,
+      error,
+    );
+    return {};
+  }
 };
