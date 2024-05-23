92a4e3560269e8800d41e09810c430cc628ea168
Jamie Blair <info@orangemug.co.uk>
2024-05-23 17:53:49 +0100

    fix: units table patch test for docx generation

--- src/pages-helpers/curriculum/docx/curriculum-download-patcher/patches/unitsTable.test.ts
+++ src/pages-helpers/curriculum/docx/curriculum-download-patcher/patches/unitsTable.test.ts
@@ -4,7 +4,7 @@ import { jsonXmlToXmlString } from "../../xml";
 import { unitsTablePatch } from "./unitsTable";
 
 describe("unitsTablePatch", () => {
-  const patcher = unitsTablePatch({
+  const inputData = {
     units: [
       {
         slug: "one",
@@ -22,7 +22,7 @@ describe("unitsTablePatch", () => {
       {
         slug: "two",
         title: "TEST_2",
-        year: "8",
+        year: "7",
         threads: [
           {
             title: "test1",
@@ -33,80 +33,30 @@ describe("unitsTablePatch", () => {
       {
         slug: "three",
         title: "TEST_3",
-        year: "8",
+        year: "7",
         threads: [],
       },
       {
         slug: "three",
         title: "TEST_3",
-        year: "8",
+        year: "7",
         threads: [],
       },
     ],
-  } as CombinedCurriculumData);
+  } as CombinedCurriculumData;
 
-  it("should modify node when valid element passed", async () => {
-    const output = await patcher({
-      type: "element",
-      name: "w:p",
-      elements: [
-        {
-          type: "element",
-          name: "w:r",
-          elements: [
-            {
-              type: "element",
-              name: "w:t",
-              elements: [
-                {
-                  type: "text",
-                  text: "{{=UNITS_TABLE}}",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    });
+  it("should return units table", async () => {
+    const output = await unitsTablePatch("7", {}, inputData.units);
 
     const xml = jsonXmlToXmlString(output);
 
     // Just check the data has made it's way through to the output XML
     expect(xml).toContain("Year 7 units");
     expect(xml).toContain("TEST_1");
-    expect(xml).toContain("Year 8 units");
     expect(xml).toContain("TEST_2");
     expect(xml).toContain("TEST_3");
 
     // Check the output hasn't changed
     expect(output).toMatchSnapshot();
-  });
-
-  it("should not modify invalid element", async () => {
-    const tree = {
-      type: "element",
-      name: "w:p",
-      elements: [
-        {
-          type: "element",
-          name: "w:r",
-          elements: [
-            {
-              type: "element",
-              name: "w:t",
-              elements: [
-                {
-                  type: "text",
-                  text: "{{=SOMETHING_ELSE}}",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    };
-
-    const output = await patcher(tree);
-    expect(output).toBe(tree);
   });
 });
