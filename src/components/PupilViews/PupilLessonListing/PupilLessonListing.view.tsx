2ec6b476e137ba279548a3c2fb5eefa06c343059
ben <ben.protheroe@thenational.academy>
2024-05-29 12:16:54 +0100

    feat(main): merge main

--- src/components/PupilViews/PupilLessonListing/PupilLessonListing.view.tsx
+++ src/components/PupilViews/PupilLessonListing/PupilLessonListing.view.tsx
@@ -48,8 +48,6 @@ export const PupilViewsLessonListing = (props: PupilLessonListingViewProps) => {
     breadcrumb.push(tierDescription);
   }
 
-  console.log(programmeFields);
-
   const optionality = programmeFields?.optionality;
 
   const LessonListingTitle = (
