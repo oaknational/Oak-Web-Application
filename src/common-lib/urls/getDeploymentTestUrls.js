3869e121a4a74ac4640ae59912f6ae21697f084c
Joe Baker <joe.baker@thenational.academy>
2024-06-05 12:19:01 +0100

    chore: add pupil lesson overview to percy

--- src/common-lib/urls/getDeploymentTestUrls.js
+++ src/common-lib/urls/getDeploymentTestUrls.js
@@ -53,6 +53,8 @@ function getDeploymentTestUrls() {
     "/teachers/lessons/duncan-as-a-father-figure/downloads",
     "/teachers/lessons/transverse-waves",
     "/teachers/lessons/transverse-waves/downloads",
+    // pupil lessons
+    "/pupils/lessons/introduction-exploring-delicious-fruits-and-vegetables-crvk4t/overview",
   ];
 }
 
