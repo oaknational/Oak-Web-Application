072b6d12035a04817e7f0eeebb4f5f9b9db6fe95
Natassja Blore <natassja.blore@thenational.academy>
2024-06-05 09:18:38 +0100

    feat: update share query

--- src/node-lib/curriculum-api-2023/queries/lessonShare/lessonShare.query.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonShare/lessonShare.query.ts
@@ -1,10 +1,45 @@
 import OakError from "../../../../errors/OakError";
 import { Sdk } from "../../sdk";
+import { toSentenceCase } from "../../helpers";
 
-import { lessonShareSchema } from "./lessonShare.schema";
+import {
+  RawLessonShareSchema,
+  lessonShareSchema,
+  rawLessonShareSchema,
+  rawShareBrowseData,
+} from "./lessonShare.schema";
 
 import errorReporter from "@/common-lib/error-reporter";
-import { LEGACY_COHORT } from "@/config/cohort";
+
+// TODO: extract to shared
+export const constructShareableResources = (lesson: RawLessonShareSchema) => {
+  const introQuiz = {
+    exists: lesson.starter_quiz !== null,
+    type: "intro-quiz-questions" as const,
+    label: "Starter Quiz",
+    metadata: `${lesson.starter_quiz?.length.toString()} questions` || "",
+  };
+  const exitQuiz = {
+    exists: lesson.exit_quiz !== null,
+    type: "exit-quiz-questions" as const,
+    label: "Exit Quiz",
+    metadata: `${lesson.exit_quiz?.length.toString()} questions` || "",
+  };
+  const video = {
+    exists: lesson.video_mux_playback_id !== null,
+    type: "video" as const,
+    label: "Video",
+    metadata: "mp4", // TODO: get video duration
+  };
+  const worksheet = {
+    exists: lesson.worksheet_asset_object_url !== null,
+    type: "worksheet-pdf" as const,
+    label: "Worksheet",
+    metadata: "pdf",
+  };
+
+  return [video, introQuiz, exitQuiz, worksheet];
+};
 
 const lessonShareQuery =
   (sdk: Sdk) =>
@@ -14,13 +49,14 @@ const lessonShareQuery =
     lessonSlug: string;
   }) => {
     const res = await sdk.lessonShare(args);
-    const pages = res.share;
+    const rawLesson = res.share;
+    const rawBrowseData = res.browse;
 
-    if (!pages || pages.length === 0) {
+    if (!rawLesson || rawLesson.length === 0 || !rawBrowseData) {
       throw new OakError({ code: "curriculum-api/not-found" });
     }
 
-    if (pages.length > 1) {
+    if (rawLesson.length > 1 || rawBrowseData.length > 1) {
       const error = new OakError({
         code: "curriculum-api/uniqueness-assumption-violated",
       });
@@ -31,13 +67,39 @@ const lessonShareQuery =
       });
     }
 
-    const page = pages[0];
+    const parsedRawLesson = rawLessonShareSchema.parse(rawLesson[0]);
+    const parsedRawBrowseData = rawShareBrowseData.parse(rawBrowseData[0]);
 
-    return lessonShareSchema.parse({
-      ...page,
-      isLegacy: page?.lessonCohort === LEGACY_COHORT,
+    const lesson = lessonShareSchema.parse({
       isSpecialist: false,
+      programmeSlug: args.programmeSlug,
+      keyStageSlug: parsedRawBrowseData.programme_fields.keystage_slug,
+      keyStageTitle: toSentenceCase(
+        parsedRawBrowseData.programme_fields.keystage_description,
+      ),
+      lessonSlug: args.lessonSlug,
+      lessonTitle: parsedRawLesson.lesson_title,
+      unitSlug: args.unitSlug,
+      unitTitle: parsedRawBrowseData.unit_title,
+      subjectSlug: parsedRawBrowseData.programme_fields.subject_slug,
+      subjectTitle: parsedRawBrowseData.programme_fields.subject,
+      examBoardSlug: parsedRawBrowseData.programme_fields.examboard_slug,
+      examBoardTitle:
+        parsedRawBrowseData.programme_fields.examboard_description,
+      tierSlug: parsedRawBrowseData.programme_fields.tier_slug,
+      tierTitle: parsedRawBrowseData.programme_fields.tier_description,
+      shareableResources: constructShareableResources(parsedRawLesson),
+      isLegacy: parsedRawBrowseData.is_legacy,
+      expired: parsedRawLesson.expired,
     });
+
+    return lesson;
+
+    // return lessonShareSchema.parse({
+    //   ...page,
+    //   isLegacy: page?.lessonCohort === LEGACY_COHORT,
+    //   isSpecialist: false,
+    // });
   };
 
 export default lessonShareQuery;
--- src/node-lib/curriculum-api-2023/queries/lessonShare/lessonShare.schema.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonShare/lessonShare.schema.ts
@@ -1,6 +1,27 @@
 import { z } from "zod";
+import {
+  programmeFieldsSchema,
+  quizQuestionSchema,
+} from "@oaknational/oak-curriculum-schema";
 
 import { lessonShareResourceSchema } from "../../shared.schema";
+
+export const rawLessonShareSchema = z.object({
+  lesson_title: z.string(),
+  expired: z.boolean().nullable(),
+  starter_quiz: z.array(quizQuestionSchema).nullable(),
+  exit_quiz: z.array(quizQuestionSchema).nullable(),
+  video_mux_playback_id: z.string().nullable(),
+  worksheet_asset_object_url: z.string().nullable(),
+});
+
+export const rawShareBrowseData = z.object({
+  unit_title: z.string(),
+  is_legacy: z.boolean(),
+  programme_fields: programmeFieldsSchema,
+});
+
+export type RawLessonShareSchema = z.infer<typeof rawLessonShareSchema>;
 
 export const lessonShareSchema = z.object({
   isSpecialist: z.literal(false),
@@ -17,7 +38,6 @@ export const lessonShareSchema = z.object({
   examBoardTitle: z.string().nullish(),
   tierSlug: z.string().nullish(),
   tierTitle: z.string().nullish(),
-  lessonCohort: z.string().nullish(),
   shareableResources: z.array(lessonShareResourceSchema),
   isLegacy: z.boolean(),
   expired: z.boolean().nullable(),
