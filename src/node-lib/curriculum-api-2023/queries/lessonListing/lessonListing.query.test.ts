a6508ea1523869d1fd1a02e95e607a56ee6d73a4
Natassja Blore <natassja.blore@thenational.academy>
2024-06-06 16:15:56 +0100

    fix: remove duplicate field in mock data

--- src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.test.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.test.ts
@@ -96,7 +96,6 @@ describe("lessonListing()", () => {
             _cohort: "2023-2024",
             updated_at: "2023-01-01T00:00:00.000Z",
             deprecated_fields: null,
-            updated_at: "2021",
           },
         },
       });
