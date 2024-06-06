81d2f3de595146666a287aa41b2dda1ba1394c48
Natassja Blore <natassja.blore@thenational.academy>
2024-06-06 08:12:06 +0100

    test: lesson share returns data in correct format

--- src/node-lib/curriculum-api-2023/queries/lessonShare/lessonShare.test.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonShare/lessonShare.test.ts
@@ -1,11 +1,13 @@
 import {
   lessonContentFixture,
+  multipleChoiceQuestion,
   programmeFieldsFixture,
 } from "@oaknational/oak-curriculum-schema";
 
 import sdk from "../../sdk";
 
 import lessonShare, { constructShareableResources } from "./lessonShare.query";
+import { lessonShareSchema } from "./lessonShare.schema";
 
 describe("lessonShare()", () => {
   test("throws a not found error if no lesson is found", async () => {
@@ -88,5 +90,43 @@ describe("lessonShare()", () => {
 
     const video = result.find((r) => r.type === "video");
     expect(video?.metadata).toBe("5 minutes");
+  });
+  test("returns the correct response", async () => {
+    const res = await lessonShare({
+      ...sdk,
+      lessonShare: jest.fn(() =>
+        Promise.resolve({
+          share: [
+            {
+              lesson_title: "Lesson Title",
+              starter_quiz: [multipleChoiceQuestion()],
+              exit_quiz: null,
+              video_mux_playback_id: "1",
+              video_duration: "15 mins",
+              worksheet_asset_object_url: "url",
+              expired: false,
+            },
+          ],
+          browse: [
+            {
+              unit_title: "Unit Title",
+              is_legacy: true,
+              programme_fields: programmeFieldsFixture(),
+            },
+          ],
+        }),
+      ),
+    })({
+      lessonSlug: "lesson-slug",
+      unitSlug: "unit-slug",
+      programmeSlug: "programme-slug",
+    });
+
+    expect(lessonShareSchema.parse(res)).toEqual(res);
+    expect(res.shareableResources).toHaveLength(4);
+    const starterQuiz = res.shareableResources.find(
+      (r) => r.type === "intro-quiz-questions",
+    );
+    expect(starterQuiz?.metadata).toBe("1 question");
   });
 });
