d9c9af7a83fef65e08a88ee0a014dc073bd64fe3
Jim Cresswell <jim.cresswell@gmail.com>
2024-06-11 12:07:13 +0100

    chore(Netlify): remove test page

--- src/pages/error/throw500.tsx
+++
@@ -1,8 +0,0 @@
-export async function getServerSideProps() {
-  throw new Error("don't catch me for a 500 response");
-  return { props: {} };
-}
-
-export default function Index() {
-  return <h1>You should not see me</h1>;
-}
