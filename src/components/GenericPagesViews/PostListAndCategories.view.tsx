a9ba233e10f47531ad1ec04bd93440138a7dad28
Natassja Blore <natassja.blore@thenational.academy>
2024-06-24 14:23:03 +0100

    feat: move category filters into aside element

--- src/components/GenericPagesViews/PostListAndCategories.view.tsx
+++ src/components/GenericPagesViews/PostListAndCategories.view.tsx
@@ -1,6 +1,10 @@
 import React, { FC } from "react";
-import { useTheme } from "styled-components";
-import { OakGrid, OakGridArea, OakHeading } from "@oaknational/oak-components";
+import {
+  OakBox,
+  OakGrid,
+  OakGridArea,
+  OakHeading,
+} from "@oaknational/oak-components";
 
 import { PostListingPageProps } from "@/components/GenericPagesViews/BlogIndex.view";
 import { PostListItemProps } from "@/components/SharedComponents/PostListItem";
@@ -9,7 +13,6 @@ import { PostCategoryPage } from "@/components/SharedComponents/PostCategoryList
 import usePostCategoryList from "@/components/SharedComponents/PostCategoryList/usePostCategoryList";
 import PostList from "@/components/SharedComponents/PostList";
 import usePostList from "@/components/SharedComponents/PostList/usePostList";
-import Box from "@/components/SharedComponents/Box";
 
 export type PostListAndCategoriesProps = Omit<PostListingPageProps, "blogs"> & {
   blogs: PostListItemProps[];
@@ -19,8 +22,6 @@ export type PostListAndCategoriesProps = Omit<PostListingPageProps, "blogs"> & {
 const PostListAndCategories: FC<PostListAndCategoriesProps> = (props) => {
   const { blogs, categories, categorySlug, page } = props;
   const blogCategoriesListProps = usePostCategoryList();
-  const theme = useTheme();
-  const HEADER_HEIGHT = theme.header.height;
   const blogListProps = usePostList({
     items: blogs,
     withImage: page === "blog-index" ? false : true,
@@ -32,12 +33,13 @@ const PostListAndCategories: FC<PostListAndCategoriesProps> = (props) => {
   return (
     <OakGrid $ph={["inner-padding-s", "inner-padding-none"]}>
       <OakGridArea $order={[0, 2]} $colSpan={[12, 4, 3]}>
-        <Box
+        <OakBox
           $display={["none", "block"]}
           $position={[null, "sticky"]}
-          $top={[null, HEADER_HEIGHT]}
-          $mt={[0, 24]}
-          $pt={[48]}
+          $top={[null, "all-spacing-12"]}
+          $mt={["space-between-none", "space-between-m"]}
+          $pt={["inner-padding-xl4"]}
+          as="aside"
         >
           <OakHeading
             tag="h3"
@@ -53,7 +55,7 @@ const PostListAndCategories: FC<PostListAndCategoriesProps> = (props) => {
             selectedCategorySlug={categorySlug}
             page={page}
           />
-        </Box>
+        </OakBox>
       </OakGridArea>
       {/* @todo is there a nicer way to make this 1 column spacer? */}
       <OakGridArea $order={1} $colSpan={[12, 1]} />
