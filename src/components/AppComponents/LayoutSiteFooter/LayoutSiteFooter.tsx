8324d901f00daa22035fa9cc59580a3974ea897b
Natassja Blore <natassja.blore@thenational.academy>
2024-06-26 15:25:05 +0100

    feat: add aria label to footer links which open in a new tab

--- src/components/AppComponents/LayoutSiteFooter/LayoutSiteFooter.tsx
+++ src/components/AppComponents/LayoutSiteFooter/LayoutSiteFooter.tsx
@@ -29,6 +29,7 @@ import useClickableCard from "@/hooks/useClickableCard";
 type LayoutFooterLinkProps = {
   text: string;
   icon?: IconName;
+  ariaLabel?: string;
 } & (
   | {
       href: string;
@@ -103,6 +104,7 @@ const FooterLink: FC<LayoutFooterLinkProps> = (props) => {
           htmlAnchorProps={{
             onClick: () =>
               track.teacherHubSelected({ navigatedFrom: "footer" }),
+            "aria-label": props.ariaLabel || undefined,
           }}
         >
           {props.text}
@@ -116,6 +118,7 @@ const FooterLink: FC<LayoutFooterLinkProps> = (props) => {
         <OwaLink
           {...primaryTargetProps}
           $focusStyles={["underline"]}
+          htmlAnchorProps={{ "aria-label": props.ariaLabel || undefined }}
           {...props}
         >
           {props.text}
@@ -131,6 +134,7 @@ const FooterLink: FC<LayoutFooterLinkProps> = (props) => {
           $focusStyles={["underline"]}
           page={null}
           href={props.href}
+          htmlAnchorProps={{ "aria-label": props.ariaLabel || undefined }}
         >
           {props.text}
         </OwaLink>
