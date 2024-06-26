e4e47e5b90610cefdde1b1f338b5e36425b97893
Kyle Huggins <kyle.huggins7@gmail.com>
2024-06-26 15:02:10 +0100

    chore: lesson listing copyright

--- src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.query.ts
@@ -31,7 +31,7 @@ export const getTransformedLessons = (res: LessonListingQuery) => {
         videoCount: lesson.lesson_data.video_id ? 1 : 0,
         presentationCount: lesson.lesson_data.asset_id_slidedeck ? 1 : 0,
         worksheetCount: lesson.lesson_data.asset_id_worksheet ? 1 : 0,
-        hasCopyrightMaterial: false, // this is hardcoded to false in previous lesson listing mv (data tools)
+        hasCopyrightMaterial: l.lesson_data.copyright_content ? true : false,
         orderInUnit: lesson.supplementary_data.order_in_unit,
         lessonCohort: lesson.lesson_data._cohort,
       };
