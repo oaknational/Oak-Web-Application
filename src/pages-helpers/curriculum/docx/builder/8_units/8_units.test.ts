da5a5c5aea8f50230b67e464733ce352ebcd75b3
Jamie Blair <info@orangemug.co.uk>
2024-07-18 12:55:01 +0100

    test: added simple tests for ./docx/builder/8_units

--- src/pages-helpers/curriculum/docx/builder/8_units/8_units.test.ts
+++ src/pages-helpers/curriculum/docx/builder/8_units/8_units.test.ts
@@ -1,32 +1,15 @@
 import { generateEmptyDocx } from "../../docx";
 import { zipToSimpleObject } from "../../zip";
-import { Slugs } from "../..";
 
 import generate from "./8_units";
-
-import { CombinedCurriculumData } from "@/pages/teachers/curriculum/docx-poc/[...slugs]";
+import { data, slugs } from "./8_units.fixture";
 
 describe("8_units", () => {
-  it.skip("simple", async () => {
+  it("simple", async () => {
     const zip = await generateEmptyDocx();
     await generate(zip, {
-      slugs: {
-        examboardSlug: "OCR",
-      } as Slugs,
-      data: {
-        units: [
-          { year: "7", title: "Test A" },
-          { year: "7", title: "Test B" },
-          { year: "8", title: "Test A" },
-          { year: "8", title: "Test B" },
-          { year: "9", title: "Test A" },
-          { year: "9", title: "Test B" },
-          { year: "10", title: "Test A" },
-          { year: "10", title: "Test B" },
-          { year: "11", title: "Test A" },
-          { year: "11", title: "Test B" },
-        ],
-      } as CombinedCurriculumData,
+      slugs: slugs,
+      data: data,
     });
 
     expect(zipToSimpleObject(zip)).toMatchSnapshot();
