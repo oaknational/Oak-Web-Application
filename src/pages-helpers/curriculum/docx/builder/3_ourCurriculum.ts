3f64d0efc400aac2d51cb77704cc0bea1fa1fa7e
Jamie Blair <info@orangemug.co.uk>
2024-07-25 18:14:10 +0100

    fix: docx "Our curriculum" accessibility (header structure)

--- src/pages-helpers/curriculum/docx/builder/3_ourCurriculum.ts
+++ src/pages-helpers/curriculum/docx/builder/3_ourCurriculum.ts
@@ -4,13 +4,12 @@ import { cdata, safeXml, xmlElementToJson } from "../xml";
 import {
   appendBodyElements,
   cmToEmu,
+  cmToTwip,
   createImage,
   insertImages,
   JSZipCached,
   wrapInBookmarkPoint,
 } from "../docx";
-
-import { generateGridCols } from "./helper";
 
 export default async function generate(zip: JSZipCached) {
   const images = await insertImages(zip, {
@@ -26,18 +25,6 @@ export default async function generate(zip: JSZipCached) {
       text: "Lessons and units are knowledge and vocabulary rich. Pupils will build on what they already know to develop deep knowledge and apply this knowledge in the form of skills.",
     },
     {
-      title: "Diverse",
-      text: "We prioritise creating a diverse curriculum by committing to diversity in teaching and teachers, and the language, texts and media we use, so all pupils feel positively represented.",
-    },
-    {
-      title: "Accessible",
-      text: "Creating an accessible curriculum that addresses the needs of all pupils and meets accessibility guidelines and requirements.",
-    },
-    {
-      title: "Evidence-informed",
-      text: "We take an evidence-informed approach applying the science of learning and subject-specific research.",
-    },
-    {
       title: "Sequenced and coherent",
       text: "Careful sequencing and attention to building coherence via vertical threads so that pupils build on prior knowledge and make meaningful connections.",
     },
@@ -45,103 +32,19 @@ export default async function generate(zip: JSZipCached) {
       title: "Flexible",
       text: "Our flexible curriculum enables schools to tailor our content to their curriculum and context.",
     },
+    {
+      title: "Accessible",
+      text: "Creating an accessible curriculum that addresses the needs of all pupils and meets accessibility guidelines and requirements.",
+    },
+    {
+      title: "Diverse",
+      text: "We prioritise creating a diverse curriculum by committing to diversity in teaching and teachers, and the language, texts and media we use, so all pupils feel positively represented.",
+    },
+    {
+      title: "Evidence-informed",
+      text: "We take an evidence-informed approach applying the science of learning and subject-specific research.",
+    },
   ];
-
-  function groupedIntoSets<T>(input: T[], count: number): T[][] {
-    const output: T[][] = [];
-    for (let i = 0; i < input.length; i += 2) {
-      const group: T[] = [];
-      output.push(group);
-      for (let j = 0; j < count; j++) {
-        group[j] = input[i + j]!;
-      }
-    }
-    return output;
-  }
-
-  function createTable() {
-    return safeXml`
-      <w:tbl>
-        <w:tblPr>
-          <w:tblW w:type="pct" w:w="100%" />
-          <w:tblBorders>
-            <w:top w:val="single" w:color="FFFFFF" w:sz="0" />
-            <w:left w:val="single" w:color="FFFFFF" w:sz="0" />
-            <w:bottom w:val="single" w:color="FFFFFF" w:sz="0" />
-            <w:right w:val="single" w:color="FFFFFF" w:sz="0" />
-            <w:insideH w:val="single" w:color="FFFFFF" w:sz="4" />
-            <w:insideV w:val="single" w:color="FFFFFF" w:sz="4" />
-          </w:tblBorders>
-        </w:tblPr>
-        <w:tblGrid>${generateGridCols(2)}</w:tblGrid>
-        ${groupedIntoSets(points, 2)
-          .flatMap((row) => {
-            const rows = row.map((point, columnIndex) => {
-              return safeXml`
-                <w:tc>
-                  <w:tcPr>
-                    <w:tcW w:type="pct" w:w="50%" />
-                    <w:tcBorders>
-                      <w:top w:val="single" w:color="FFFFFF" w:sz="0" />
-                      <w:start w:val="single" w:color="FFFFFF" w:sz="0" />
-                      <w:bottom w:val="single" w:color="FFFFFF" w:sz="0" />
-                      <w:end w:val="single" w:color="FFFFFF" w:sz="0" />
-                    </w:tcBorders>
-                    <w:tcMar>
-                      <w:top w:type="dxa" w:w="0" />
-                      <w:start
-                        w:type="dxa"
-                        w:w="${columnIndex === 0 ? "0" : "113"}"
-                      />
-                      <w:bottom w:type="dxa" w:w="226" />
-                      <w:end
-                        w:type="dxa"
-                        w:w="${columnIndex === 1 ? "0" : "113"}"
-                      />
-                    </w:tcMar>
-                  </w:tcPr>
-                  <w:p>
-                    <w:pPr>
-                      <w:pStyle w:val="Heading3" />
-                    </w:pPr>
-                    <w:r>
-                      <w:rPr>
-                        <w:rFonts
-                          w:ascii="Arial"
-                          w:hAnsi="Arial"
-                          w:cs="Arial"
-                        />
-                        <w:b />
-                        <w:color w:val="222222" />
-                        <w:sz w:val="36" />
-                      </w:rPr>
-                      <w:t>${cdata(point.title)}</w:t>
-                    </w:r>
-                  </w:p>
-                  <w:p>
-                    <w:r>
-                      <w:rPr>
-                        <w:rFonts
-                          w:ascii="Arial"
-                          w:hAnsi="Arial"
-                          w:cs="Arial"
-                        />
-                        <w:color w:val="222222" />
-                        <w:sz w:val="24" />
-                      </w:rPr>
-                      <w:t>${cdata(point.text)}</w:t>
-                    </w:r>
-                  </w:p>
-                </w:tc>
-              `;
-            });
-
-            return `<w:tr>${rows}</w:tr>`;
-          })
-          .join("")}
-      </w:tbl>
-    `;
-  }
 
   const pageXml = safeXml`
     <root>
@@ -189,7 +92,72 @@ export default async function generate(zip: JSZipCached) {
         </w:r>
       </w:p>
 
-      ${createTable()}
+      <w:p>
+        <w:pPr>
+          <w:sectPr>
+            <w:cols w:space="708" />
+            <w:docGrid w:linePitch="360" />
+            <w:pgSz w:w="11909" w:h="16834" />
+            <w:pgMar
+              w:top="${cmToTwip(1.5)}"
+              w:right="${cmToTwip(1.5)}"
+              w:bottom="${cmToTwip(1.5)}"
+              w:left="${cmToTwip(1.5)}"
+              w:header="${cmToTwip(1.5)}"
+              w:footer="${cmToTwip(1.5)}"
+              w:gutter="0"
+            />
+          </w:sectPr>
+        </w:pPr>
+      </w:p>
+      ${points.map((point, columnIndex) => {
+        return safeXml`
+          <w:p>
+            <w:pPr>
+              <w:pStyle w:val="Heading3" />
+            </w:pPr>
+            <w:r>
+              <w:rPr>
+                <w:rFonts w:ascii="Arial" w:hAnsi="Arial" w:cs="Arial" />
+                <w:b />
+                <w:color w:val="222222" />
+                <w:sz w:val="36" />
+              </w:rPr>
+              <w:t>${cdata(point.title)}</w:t>
+            </w:r>
+          </w:p>
+          <w:p>
+            <w:r>
+              <w:rPr>
+                <w:rFonts w:ascii="Arial" w:hAnsi="Arial" w:cs="Arial" />
+                <w:color w:val="222222" />
+                <w:sz w:val="24" />
+              </w:rPr>
+              <w:t>${cdata(point.text)}</w:t>
+            </w:r>
+          </w:p>
+          ${columnIndex === 2 ? "" : "<w:p/>"}
+        `;
+      })}
+      <w:p>
+        <w:pPr>
+          <w:sectPr>
+            <w:type w:val="continuous" />
+            <w:cols w:num="2" w:space="720" />
+            <w:docGrid w:linePitch="360" />
+            <w:pgSz w:w="11909" w:h="16834" />
+            <w:pgMar
+              w:top="${cmToTwip(1.5)}"
+              w:right="${cmToTwip(1.5)}"
+              w:bottom="${cmToTwip(1.5)}"
+              w:left="${cmToTwip(1.5)}"
+              w:header="${cmToTwip(1.5)}"
+              w:footer="${cmToTwip(1.5)}"
+              w:gutter="0"
+            />
+          </w:sectPr>
+        </w:pPr>
+      </w:p>
       <w:p>
         <w:pPr>
           <w:jc w:val="center" />
@@ -201,9 +169,25 @@ export default async function generate(zip: JSZipCached) {
             desc: "A bee hive diagram showing each of Oak's 6 curriculum principles",
           })}
         </w:r>
-        <w:r>
-          <w:br w:type="page" />
-        </w:r>
+      </w:p>
+      <w:p>
+        <w:pPr>
+          <w:sectPr>
+            <w:type w:val="continuous" />
+            <w:cols w:space="708" />
+            <w:docGrid w:linePitch="360" />
+            <w:pgSz w:w="11909" w:h="16834" />
+            <w:pgMar
+              w:top="${cmToTwip(1.5)}"
+              w:right="${cmToTwip(1.5)}"
+              w:bottom="${cmToTwip(1.5)}"
+              w:left="${cmToTwip(1.5)}"
+              w:header="${cmToTwip(1.5)}"
+              w:footer="${cmToTwip(1.5)}"
+              w:gutter="0"
+            />
+          </w:sectPr>
+        </w:pPr>
       </w:p>
     </root>
   `;
