4bb61f2d24f9ab4282ac30341a05bdc028cc9b30
Simon Katan <simon.katan@thenational.academy>
2024-07-29 09:42:17 +0100

    test: correct answers tests

--- src/components/PupilComponents/QuizCorrectAnswers/QuizCorrectAnswers.test.tsx
+++ src/components/PupilComponents/QuizCorrectAnswers/QuizCorrectAnswers.test.tsx
@@ -10,18 +10,30 @@ import { QuizCorrectAnswers } from "./QuizCorrectAnswers";
 
 import { quizQuestions } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.new.fixture";
 import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
+import { QuestionState } from "@/components/PupilComponents/QuizUtils/questionTypes";
 
 const questionsArrayFixture = quizQuestions || [];
 
-const getQuizEngineContext = (): NonNullable<QuizEngineContextType> =>
+const getQuizEngineContext = (
+  correctAnswer: QuestionState["correctAnswer"],
+): NonNullable<QuizEngineContextType> =>
   createQuizEngineContext({
     currentQuestionData: questionsArrayFixture[0],
+    currentQuestionIndex: 0,
+    currentQuestionState: {
+      mode: "init",
+      offerHint: false,
+      grade: 0,
+      correctAnswer,
+    },
     numQuestions: 1,
   });
 
 describe("QuizCorrectAnswers", () => {
-  it("renders all answers when currentQuestionData.question type is multiple choice", () => {
-    const context = getQuizEngineContext();
+  it("handles a single correct answer inside an array", () => {
+    const context = getQuizEngineContext([
+      "a group of words that contains a verb and makes complete sense",
+    ]);
 
     const { getByText } = renderWithTheme(
       <OakThemeProvider theme={oakDefaultTheme}>
@@ -33,8 +45,54 @@ describe("QuizCorrectAnswers", () => {
 
     expect(
       getByText(
-        "Correct answers: a group of words that contains a verb and makes complete sense",
+        "Correct answer: a group of words that contains a verb and makes complete sense",
       ),
     ).toBeInTheDocument();
+  });
+
+  it("handles a multiple correct answers inside an array", () => {
+    const context = getQuizEngineContext(["one", "two", "three"]);
+
+    const { getByText } = renderWithTheme(
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <QuizEngineContext.Provider value={context}>
+          <QuizCorrectAnswers />
+        </QuizEngineContext.Provider>
+      </OakThemeProvider>,
+    );
+
+    expect(getByText("Correct answers: one, two, three")).toBeInTheDocument();
+  });
+
+  it("handles a single correct answer", () => {
+    const context = getQuizEngineContext(
+      "a group of words that contains a verb",
+    );
+
+    const { getByText } = renderWithTheme(
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <QuizEngineContext.Provider value={context}>
+          <QuizCorrectAnswers />
+        </QuizEngineContext.Provider>
+      </OakThemeProvider>,
+    );
+
+    expect(
+      getByText("Correct answer: a group of words that contains a verb"),
+    ).toBeInTheDocument();
+  });
+
+  it("handles no correct answer", () => {
+    const context = getQuizEngineContext(undefined);
+
+    const { container } = renderWithTheme(
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <QuizEngineContext.Provider value={context}>
+          <QuizCorrectAnswers />
+        </QuizEngineContext.Provider>
+      </OakThemeProvider>,
+    );
+
+    expect(container).toBeEmptyDOMElement();
   });
 });
