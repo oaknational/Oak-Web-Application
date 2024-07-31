57abd4f1ae92888e85261f9a57156ac73bbbe23c
Simon Katan <simon.katan@thenational.academy>
2024-07-31 13:25:49 +0100

    feat: working on QuizResultMCQ

--- src/components/PupilComponents/QuizResultMCQ/QuizResultMCQ.stories.tsx
+++ src/components/PupilComponents/QuizResultMCQ/QuizResultMCQ.stories.tsx
@@ -3,6 +3,8 @@ import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
 
 import { QuizResultMCQ } from "./QuizResultMCQ";
 
+import { mcqTextAnswers } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.new.fixture";
+
 const meta = {
   component: QuizResultMCQ,
   decorators: [
@@ -24,12 +26,45 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 /*
- * A component to render MCQ questions where there is a single answer
+ * A component to display the results of a multiple choice quiz in the review section
  *
  */
-export const Default: Story = {
+export const CorrectSingle: Story = {
+  render: () => {
+    return <QuizResultMCQ {...args} />;
+  },
+  args: {
+    answers: mcqTextAnswers,
+    pupilAnswers: 2,
+  },
+};
+
+export const IncorrectSingle: Story = {
   render: (args) => {
     return <QuizResultMCQ {...args} />;
   },
-  args: {},
+  args: {
+    answers: mcqTextAnswers,
+    pupilAnswers: 0,
+  },
+};
+
+export const MixedMultiple: Story = {
+  render: (args) => {
+    return <QuizResultMCQ {...args} />;
+  },
+  args: {
+    answers: mcqTextAnswers,
+    pupilAnswers: [0, 2],
+  },
+};
+
+export const IncorrectMultiple: Story = {
+  render: (args) => {
+    return <QuizResultMCQ {...args} />;
+  },
+  args: {
+    answers: mcqTextAnswers,
+    pupilAnswers: [0, 1],
+  },
 };
--- src/components/PupilComponents/QuizResultMCQ/QuizResultMCQ.tsx
+++ src/components/PupilComponents/QuizResultMCQ/QuizResultMCQ.tsx
@@ -1,7 +1,55 @@
-export const QuizResultMCQ = () => {
+import React from "react";
+import {
+  OakFlex,
+  OakQuizResultItem,
+  OakSpan,
+} from "@oaknational/oak-components";
+
+import { MCAnswer } from "@/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema";
+import { PupilAnswerMCQ } from "@/components/PupilComponents/QuizUtils/questionTypes";
+
+export type QuizResultMCQProps = {
+  answers: MCAnswer[];
+  pupilAnswers: PupilAnswerMCQ;
+};
+
+export const QuizResultMCQ = ({
+  answers,
+  pupilAnswers,
+}: QuizResultMCQProps) => {
+  const resultItems = answers.map((answer, index) => {
+    const text = answer.answer.filter((answer) => answer?.type === "text")[0];
+    const image = answer.answer.filter((answer) => answer?.type === "image")[0];
+    const answerSelected = Array.isArray(pupilAnswers)
+      ? pupilAnswers.includes(index)
+      : pupilAnswers === index;
+
+    const feedbackState = (() => {
+      switch (true) {
+        case answerSelected && !answer.answerIsCorrect:
+          return "incorrect";
+        case answerSelected && answer.answerIsCorrect:
+          return "correct";
+        default:
+          return null;
+      }
+    })();
+
+    return (
+      <OakQuizResultItem
+        key={index}
+        standardText={text && text.text}
+        imageURL={image && image.imageObject.secureUrl}
+        imageAlt={"Image for option " + (index + 1)}
+        feedbackState={feedbackState}
+      />
+    );
+  });
+
   return (
-    <div>
-      <h1>Quiz Result MCQ</h1>
-    </div>
+    <OakFlex $flexDirection={"column"} $gap={"space-between-s"}>
+      <OakSpan $font={"body-3-bold"}>Your answer:</OakSpan>
+      {resultItems}
+    </OakFlex>
   );
 };
