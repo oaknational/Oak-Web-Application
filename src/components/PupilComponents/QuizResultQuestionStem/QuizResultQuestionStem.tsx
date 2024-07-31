8904f220dbfa9d4ebf20a40946f92286a6ed9334
ben <ben.protheroe@thenational.academy>
2024-07-31 12:26:42 +0100

    feat(result-stem): create results question stem and story for quiz stem

---
+++ src/components/PupilComponents/QuizResultQuestionStem/QuizResultQuestionStem.tsx
@@ -0,0 +1,87 @@
+import {
+  OakCloudinaryImage,
+  OakFlex,
+  OakSpan,
+} from "@oaknational/oak-components";
+
+import {
+  removeMarkdown,
+  shortAnswerTitleFormatter,
+} from "@/components/TeacherComponents/LessonOverviewQuizContainer/quizUtils";
+import {
+  ImageItem,
+  TextItem,
+} from "@/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema";
+import { getSizes } from "@/components/SharedComponents/CMSImage/getSizes";
+
+export const QuizResultQuestionStem = ({
+  questionStem,
+  index,
+  takeFullHeight,
+}: {
+  questionStem: (ImageItem | TextItem)[];
+  index: number;
+  takeFullHeight?: boolean;
+}) => {
+  const displayNumber = `Q${index + 1}.`;
+
+  return (
+    <OakFlex
+      $flexDirection={"column"}
+      $gap={"space-between-m"}
+      $color={"text-primary"}
+      $height={takeFullHeight ? "100%" : "auto"}
+      $justifyContent={["center", "flex-start"]}
+    >
+      <OakFlex
+        key="stem-header"
+        $mt={
+          takeFullHeight
+            ? ["space-between-none", "space-between-xl", "space-between-xxl"]
+            : "space-between-none"
+        }
+      >
+        {questionStem[0]?.type === "text" && (
+          <OakSpan
+            key={`q-${displayNumber}-stem-element-0`}
+            $font={"body-2-bold"}
+          >
+            {`${displayNumber} ${shortAnswerTitleFormatter(
+              removeMarkdown(questionStem[0].text),
+            )}`}
+          </OakSpan>
+        )}
+      </OakFlex>
+
+      {questionStem.map((stemItem, i) => {
+        if (stemItem.type === "text" && i > 0) {
+          return (
+            <OakSpan
+              key={`q-${displayNumber}-stem-element-${i}`}
+              $font={"body-2-bold"}
+            >
+              {shortAnswerTitleFormatter(removeMarkdown(stemItem.text))}
+            </OakSpan>
+          );
+        } else if (stemItem.type === "image") {
+          return (
+            <OakFlex key={`q-${displayNumber}-stem-element-${i}`}>
+              {stemItem.imageObject.publicId && (
+                <OakCloudinaryImage
+                  cloudinaryId={stemItem.imageObject.publicId}
+                  height={stemItem.imageObject.height}
+                  width={stemItem.imageObject.width}
+                  alt={""}
+                  $minWidth={"all-spacing-19"}
+                  placeholder="oak"
+                  sizes={getSizes(["100vw", 1200])}
+                  $background={"white"}
+                />
+              )}
+            </OakFlex>
+          );
+        }
+      })}
+    </OakFlex>
+  );
+};
---
+++ src/components/PupilComponents/QuizResultQuestionStem/index.ts
@@ -0,0 +1,1 @@
+export { QuizResultQuestionStem } from "./QuizResultQuestionStem";
