6c5f374ca43d3a259094d3aa5f2119507ff58083
Natassja Blore <natassja.blore@thenational.academy>
2024-08-02 07:59:57 +0100

    test: add initial tests for onboarding view

---
+++ src/components/TeacherViews/Onboarding/Onboarding.test.tsx
@@ -0,0 +1,24 @@
+import { screen } from "@testing-library/dom";
+
+import OnboardingView from "./Onboarding.view";
+
+import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
+
+describe("Onboarding view", () => {
+  it("renders a heading", () => {
+    renderWithProviders()(<OnboardingView />);
+    const heading = screen.getByRole("heading", { name: "Onboarding" });
+    expect(heading).toBeInTheDocument();
+  });
+  it("renders a school picker", () => {
+    renderWithProviders()(<OnboardingView />);
+    const schoolPicker = screen.getByTestId("search-combobox-input");
+    expect(schoolPicker).toBeInTheDocument();
+  });
+  it("renders a Continue button", () => {
+    renderWithProviders()(<OnboardingView />);
+    const continueButton = screen.getByRole("button", { name: "Continue" });
+    expect(continueButton).toBeInTheDocument();
+    expect(continueButton).toBeDisabled();
+  });
+});
