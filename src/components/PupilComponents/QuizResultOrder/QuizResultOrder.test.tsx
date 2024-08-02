f11a41381a9daa50de6b100d507e12bbff52d26e
Simon Katan <simon.katan@thenational.academy>
2024-08-02 11:16:13 +0100

    test: more tests to satisfy sonar-cloud

--- src/components/PupilComponents/QuizResultOrder/QuizResultOrder.test.tsx
+++ src/components/PupilComponents/QuizResultOrder/QuizResultOrder.test.tsx
@@ -1,11 +1,17 @@
 import { oakDefaultTheme, OakThemeProvider } from "@oaknational/oak-components";
 
+import { invariant } from "../pupilUtils/invariant";
+
 import { QuizResultOrder } from "./QuizResultOrder";
 
 import { orderAnswers } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.new.fixture";
 import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
 
 describe("QuizResultOrder", () => {
+  beforeAll(() => {
+    jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+
   it.each([
     { pupilAnswers: [0, 1, 2, 3] },
     { pupilAnswers: [1, 2, 0, 3] },
@@ -70,5 +76,44 @@ describe("QuizResultOrder", () => {
 
     expect(getAllByAltText("tick")).toHaveLength(3);
     expect(getAllByAltText("cross")).toHaveLength(1);
+  });
+
+  it("throws if the pupilAnswer is not in the answers", () => {
+    expect(() => {
+      renderWithTheme(
+        <OakThemeProvider theme={oakDefaultTheme}>
+          <QuizResultOrder
+            answers={orderAnswers}
+            feedback={["correct", "correct", "correct", "correct"]}
+            pupilAnswers={[0, 1, 2, 4]}
+          />
+        </OakThemeProvider>,
+      );
+    }).toThrow("Answer not found for index 4");
+  });
+
+  it("throws if an answer does not have text", () => {
+    invariant(orderAnswers[0], "Answer must exist");
+    invariant(orderAnswers[1], "Answer must exist");
+
+    const orderAnswersWithNoText = [
+      orderAnswers[0],
+      { ...orderAnswers[1], answer: [] },
+    ];
+    expect(() => {
+      renderWithTheme(
+        <OakThemeProvider theme={oakDefaultTheme}>
+          <QuizResultOrder
+            answers={orderAnswersWithNoText}
+            feedback={["correct", "correct"]}
+            pupilAnswers={[0, 1]}
+          />
+        </OakThemeProvider>,
+      );
+    }).toThrow("Text is missing from order answer");
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
   });
 });
