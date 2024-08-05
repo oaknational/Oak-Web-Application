bb9f71805e4268ab4ed90b1a519a4676d4c36674
Natassja Blore <natassja.blore@thenational.academy>
2024-08-05 14:24:12 +0100

    test: add more coverage and fix act errors with react-form

--- src/components/TeacherViews/Onboarding/Onboarding.test.tsx
+++ src/components/TeacherViews/Onboarding/Onboarding.test.tsx
@@ -1,38 +1,73 @@
 import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
 
 import OnboardingView from "./Onboarding.view";
 
 import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
 
 describe("Onboarding view", () => {
-  it("renders a heading", () => {
+  it("renders a heading", async () => {
     renderWithProviders()(<OnboardingView />);
-    const heading = screen.getByRole("heading", { name: "Select your school" });
+    const heading = await screen.findByRole("heading", {
+      name: "Select your school",
+    });
     expect(heading).toBeInTheDocument();
   });
-  it("renders a school picker", () => {
+  it("renders a school picker", async () => {
     renderWithProviders()(<OnboardingView />);
-    const schoolPicker = screen.getByTestId("search-combobox-input");
+    const schoolPicker = await screen.findByTestId("search-combobox-input");
     expect(schoolPicker).toBeInTheDocument();
   });
-  it("renders a Continue button", () => {
+  it("renders a Continue button", async () => {
     renderWithProviders()(<OnboardingView />);
-    const continueButton = screen.getByRole("button", { name: "Continue" });
+    const continueButton = await screen.findByRole("button", {
+      name: "Continue",
+    });
     expect(continueButton).toBeInTheDocument();
     expect(continueButton).toBeDisabled();
   });
-  it("renders terms and conditions text", () => {
+  it("renders terms and conditions text", async () => {
     renderWithProviders()(<OnboardingView />);
-    const tsAndCs = screen.getByText("Oak's terms & conditions", {
+    const tsAndCs = await screen.findByText("Oak's terms & conditions", {
       exact: false,
     });
     expect(tsAndCs).toBeInTheDocument();
     expect(tsAndCs).toHaveAttribute("href", "/legal/terms-and-conditions");
   });
-  it("renders contact us text", () => {
+  it("renders contact us text", async () => {
     renderWithProviders()(<OnboardingView />);
-    const contactUs = screen.getByText("Contact us", { exact: false });
+    const contactUs = await screen.findByText("Contact us", { exact: false });
     expect(contactUs).toBeInTheDocument();
     expect(contactUs).toHaveAttribute("href", "/contact-us");
+  });
+  it("it enables the continue button when a school is selected", async () => {
+    renderWithProviders()(<OnboardingView />);
+    const continueButton = await screen.findByRole("button", {
+      name: "Continue",
+    });
+    expect(continueButton).toBeDisabled();
+    const inputBox = await screen.findByRole("combobox");
+
+    const user = userEvent.setup();
+    await user.type(inputBox, "Bea");
+    const school = await screen.findByText("uvoir Primary School", {
+      exact: false,
+    });
+    await user.click(school);
+    expect(
+      await screen.findByRole("button", {
+        name: "Continue",
+      }),
+    ).toBeEnabled();
+  });
+  it("clears the input when a school is not completed", async () => {
+    renderWithProviders()(<OnboardingView />);
+
+    const inputBox = await screen.findByRole("combobox");
+
+    const user = userEvent.setup();
+    await user.type(inputBox, "Bea");
+    await user.tab();
+    expect(await screen.findByRole("combobox")).toHaveValue("");
   });
 });
