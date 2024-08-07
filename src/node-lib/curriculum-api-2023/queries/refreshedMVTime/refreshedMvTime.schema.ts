f30f288fbc2b7eec5519e0adaae2ef131150ade9
Jamie Blair <info@orangemug.co.uk>
2024-08-07 15:08:32 +0100

    fix: allow last_refresh_finish to be nullable for local development

--- src/node-lib/curriculum-api-2023/queries/refreshedMVTime/refreshedMvTime.schema.ts
+++ src/node-lib/curriculum-api-2023/queries/refreshedMVTime/refreshedMvTime.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const refreshedMvTimeSchema = z.object({
   data: z
     .object({
-      last_refresh_finish: z.string(),
+      last_refresh_finish: z.string().nullable(),
       materializedview_name: z.string(),
     })
     .strict()
