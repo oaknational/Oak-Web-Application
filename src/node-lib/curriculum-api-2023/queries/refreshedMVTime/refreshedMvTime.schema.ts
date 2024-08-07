3e3ba3073c722a866e4685970ddd32cbd861dfdf
Jamie Blair <info@orangemug.co.uk>
2024-08-07 15:15:38 +0100

    fix: rollback nullable, not valid type

--- src/node-lib/curriculum-api-2023/queries/refreshedMVTime/refreshedMvTime.schema.ts
+++ src/node-lib/curriculum-api-2023/queries/refreshedMVTime/refreshedMvTime.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const refreshedMvTimeSchema = z.object({
   data: z
     .object({
-      last_refresh_finish: z.string().nullable(),
+      last_refresh_finish: z.string(),
       materializedview_name: z.string(),
     })
     .strict()
