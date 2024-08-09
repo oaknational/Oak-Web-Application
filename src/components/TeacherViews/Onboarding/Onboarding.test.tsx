5c3b2aa52eee188f2455a55188ac3edbf3622428
Kyle Huggins <kyle.huggins7@gmail.com>
2024-08-09 13:52:01 +0100

    chore: add tests for check box component

--- src/components/TeacherViews/Onboarding/Onboarding.test.tsx
+++ src/components/TeacherViews/Onboarding/Onboarding.test.tsx
@@ -1,9 +1,16 @@
-import { screen } from "@testing-library/dom";
+import { fireEvent, screen } from "@testing-library/dom";
 import userEvent from "@testing-library/user-event";
+import { Controller, useForm } from "react-hook-form";
+import {
+  OakCheckBox,
+  oakDefaultTheme,
+  OakThemeProvider,
+} from "@oaknational/oak-components";
 
 import OnboardingView from "./Onboarding.view";
 
 import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
+import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
 
 describe("Onboarding view", () => {
   it("renders a heading", async () => {
@@ -47,6 +54,58 @@ describe("Onboarding view", () => {
         "Sign up to receive helpful content via email. Unsubscribe at any time.",
       ),
     ).toBeInTheDocument();
+  });
+  it("should render the Controller component and handle checkbox change", async () => {
+    const trigger = jest.fn();
+
+    const Wrapper = () => {
+      const { control } = useForm({
+        defaultValues: {
+          newsletterSignUp: true,
+        },
+      });
+      return (
+        <Controller
+          control={control}
+          name="newsletterSignUp"
+          render={({ field: { value, onChange, name, onBlur } }) => {
+            const onChangeHandler = (
+              e: React.ChangeEvent<HTMLInputElement>,
+            ) => {
+              onChange(e.target.checked);
+              trigger();
+            };
+            return (
+              <OakCheckBox
+                defaultChecked={value}
+                checked={value}
+                name={name}
+                onBlur={onBlur}
+                onChange={onChangeHandler}
+                value="Sign up to receive helpful content via email. Unsubscribe at any time."
+                id="newsletterSignUp"
+              />
+            );
+          }}
+        />
+      );
+    };
+
+    const { getByRole } = renderWithTheme(
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <Wrapper />
+      </OakThemeProvider>,
+    );
+
+    const checkbox = getByRole("checkbox", {
+      name: /Sign up to receive helpful content via email. Unsubscribe at any time./i,
+    });
+    expect(checkbox).toBeChecked();
+
+    await fireEvent.click(checkbox);
+
+    expect(trigger).toHaveBeenCalled();
+    expect(checkbox).not.toBeChecked();
   });
   it("it enables the continue button when a school is selected", async () => {
     renderWithProviders()(<OnboardingView />);
