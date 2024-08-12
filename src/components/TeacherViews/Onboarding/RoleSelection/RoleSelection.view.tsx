8af2654de4c2320874630ac6f72ba8476721e48a
Natassja Blore <natassja.blore@thenational.academy>
2024-08-12 09:53:50 +0100

    feat: add other text input

--- src/components/TeacherViews/Onboarding/RoleSelection/RoleSelection.view.tsx
+++ src/components/TeacherViews/Onboarding/RoleSelection/RoleSelection.view.tsx
@@ -8,8 +8,10 @@ import {
 } from "@oaknational/oak-components";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useState } from "react";
 
 import FieldError from "@/components/SharedComponents/FieldError";
+import Input from "@/components/SharedComponents/Input";
 
 const roleOptions: Record<string, string> = {
   "teacher-training": "Training to become a teacher",
@@ -18,6 +20,7 @@ const roleOptions: Record<string, string> = {
   "adult-helping-child": "Adult helping a child (including homeschooling)",
   "mat-or-lea": "Working at multi-academy trust or local educational authority",
   nonprofit: "Working at an educational nonprofit",
+  other: "Other",
 };
 
 const roleSelectFormSchema = z.object({
@@ -26,6 +29,7 @@ const roleSelectFormSchema = z.object({
       message: "Select a role",
     }),
   }),
+  other: z.string().optional(),
 });
 type RoleSelectFormValues = z.infer<typeof roleSelectFormSchema>;
 type RoleSelectFormProps = RoleSelectFormValues & {
@@ -33,6 +37,7 @@ type RoleSelectFormProps = RoleSelectFormValues & {
 };
 
 const RoleSelectionView = () => {
+  const [selectedRole, setSelectedRole] = useState<string | null>(null);
   const { formState, setValue, handleSubmit } = useForm<RoleSelectFormProps>({
     resolver: zodResolver(roleSelectFormSchema),
     mode: "onBlur",
@@ -40,6 +45,11 @@ const RoleSelectionView = () => {
 
   const onFormSubmit = async (values: RoleSelectFormValues) => {
     console.log("TODO: something with these values", values);
+  };
+
+  const handleChange = (role: string) => {
+    setValue("role", role);
+    setSelectedRole(role);
   };
 
   return (
@@ -63,7 +73,7 @@ const RoleSelectionView = () => {
         $flexDirection="column"
         $alignItems="flex-start"
         $gap="all-spacing-8"
-        onChange={(event) => setValue("role", event.target.value)}
+        onChange={(event) => handleChange(event.target.value)}
       >
         {Object.entries(roleOptions).map(([value, label]) => (
           <OakRadioButton key={value} id={value} label={label} value={value} />
@@ -71,6 +81,12 @@ const RoleSelectionView = () => {
       </OakRadioGroup>
       {formState.errors.role && (
         <FieldError id="role-error">{formState.errors.role.message}</FieldError>
+      )}
+      {selectedRole === "other" && (
+        <>
+          {}
+          <Input id="other" label="Please specify" required={true} />
+        </>
       )}
       <OakPrimaryButton
         width="100%"
