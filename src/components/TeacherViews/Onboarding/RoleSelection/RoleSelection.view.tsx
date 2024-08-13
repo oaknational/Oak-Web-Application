d474439db1960b61b1322c3b4b5a8d0581441076
Natassja Blore <natassja.blore@thenational.academy>
2024-08-13 10:38:27 +0100

    chore: update role text

--- src/components/TeacherViews/Onboarding/RoleSelection/RoleSelection.view.tsx
+++ src/components/TeacherViews/Onboarding/RoleSelection/RoleSelection.view.tsx
@@ -14,7 +14,8 @@ const roleOptions: Record<string, string> = {
   "teacher-training": "Training to become a teacher",
   "teacher-trainer": "Teacher trainer",
   "private-tutor": "Private tutor",
-  "adult-helping-child": "Adult helping a child (including homeschooling)",
+  "adult-helping-child":
+    "Adult helping a child, e.g. with revision, homeschooling",
   "mat-or-lea": "Working at multi-academy trust or local educational authority",
   nonprofit: "Working at an educational nonprofit",
   other: "Other",
