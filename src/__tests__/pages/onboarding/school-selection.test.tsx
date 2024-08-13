aace977e184832e6b412e2672e8af9cb31e255e0
Natassja Blore <natassja.blore@thenational.academy>
2024-08-13 16:10:38 +0100

    fix: test condition

--- src/__tests__/pages/onboarding/school-selection.test.tsx
+++ src/__tests__/pages/onboarding/school-selection.test.tsx
@@ -11,7 +11,7 @@ describe("Onboarding school selection page", () => {
   test("it renders the onboarding page", () => {
     renderWithProviders()(<SchoolSelection />);
 
-    const heading = screen.getByText("Do you work in a school?");
+    const heading = screen.getByText("Select your school");
     expect(heading).toBeInTheDocument();
   });
 });
