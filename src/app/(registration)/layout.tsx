6439ba32f1df0b57e77f54ace79c355e740e1440
Natassja Blore <natassja.blore@thenational.academy>
2024-08-19 14:27:41 +0100

    fix: refactor registration layout so the page is scrollable at high zoom levels

--- src/app/(registration)/layout.tsx
+++ src/app/(registration)/layout.tsx
@@ -2,36 +2,35 @@
 import CMSImage from "@/components/SharedComponents/CMSImage";
 import withFeatureFlag from "@/hocs/withFeatureFlag";
 import { getIllustrationAsset } from "@/image-data";
-import { OakBox, OakFlex, OakMaxWidth } from "@/styles/oakThemeApp";
+import { OakBox, OakFlex } from "@/styles/oakThemeApp";
 
 function RegistrationLayout({ children }: { children: React.ReactNode }) {
   return (
-    <OakBox $background={["white", "bg-decorative1-main"]} $overflow="hidden">
-      <OakMaxWidth
-        $justifyContent="center"
-        $alignItems={["center", "initial"]}
-        $height="100vh"
+    <OakFlex
+      $height="100vh"
+      $background={["white", "bg-decorative1-main"]}
+      $alignItems="center"
+      $justifyContent={["center", "flex-start"]}
+    >
+      <CMSImage
+        image={getIllustrationAsset("auth-acorn")}
+        $position="fixed"
+        $display={["none", "block"]}
+        $height="55%"
+        $width="auto"
+        $right={200}
+      />
+      <OakBox
+        $dropShadow="drop-shadow-standard"
+        $borderRadius="border-radius-l"
+        $display={["none", "block"]}
+        $width="max-content"
+        $ml="space-between-xxxl"
       >
-        <CMSImage
-          image={getIllustrationAsset("auth-acorn")}
-          $position="fixed"
-          $display={["none", "block"]}
-          $height="55%"
-          $width="auto"
-          $right={200}
-        />
-        <OakFlex $pl={["inner-padding-none", "inner-padding-xl8"]}>
-          <OakBox
-            $dropShadow="drop-shadow-standard"
-            $borderRadius="border-radius-l"
-            $display={["none", "block"]}
-          >
-            {children}
-          </OakBox>
-          <OakBox $display={["block", "none"]}>{children}</OakBox>
-        </OakFlex>
-      </OakMaxWidth>
-    </OakBox>
+        {children}
+      </OakBox>
+      <OakBox $display={["block", "none"]}>{children}</OakBox>
+    </OakFlex>
   );
 }
 
