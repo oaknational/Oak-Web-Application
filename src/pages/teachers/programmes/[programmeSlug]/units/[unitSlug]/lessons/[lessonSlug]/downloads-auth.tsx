6f275848cf80cbc21832b65b65de5331473281e8
Natassja Blore <natassja.blore@thenational.academy>
2024-08-30 08:50:20 +0100

    chore: remove onboarding required for testing

--- src/pages/teachers/programmes/[programmeSlug]/units/[unitSlug]/lessons/[lessonSlug]/downloads-auth.tsx
+++ src/pages/teachers/programmes/[programmeSlug]/units/[unitSlug]/lessons/[lessonSlug]/downloads-auth.tsx
@@ -3,12 +3,8 @@
 import LessonDownloadsPage from "./downloads";
 
 import { Wall } from "@/components/AppComponents/Wall";
-import { withOnboardingRequired } from "@/hocs/withOnboardingRequired";
 import { withPageAuthRequired } from "@/hocs/withPageAuthRequired";
 
 export { getStaticPaths, getStaticProps } from "./downloads";
 
-export default withPageAuthRequired(
-  withOnboardingRequired(LessonDownloadsPage, Wall),
-  Wall,
-);
+export default withPageAuthRequired(LessonDownloadsPage, Wall);
