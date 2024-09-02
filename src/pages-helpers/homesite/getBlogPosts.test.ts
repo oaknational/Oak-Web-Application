5f67b2a35ae1ae3d1d5b9f7afc907390cd3b62f4
Simon Katan <simon.katan@thenational.academy>
2024-09-02 17:06:00 +0100

    test: update test coverage

---
+++ src/pages-helpers/homesite/getBlogPosts.test.ts
@@ -0,0 +1,108 @@
+import { getBlogPosts } from "./getBlogPosts";
+
+import { BlogPostPreview, WebinarPreview } from "@/common-lib/cms-types";
+import CMSClient from "@/node-lib/cms";
+
+jest.mock("src/node-lib/cms");
+
+const mockCMSClient = CMSClient as jest.MockedObject<typeof CMSClient>;
+
+const pageData = {
+  heading: "",
+  id: "",
+  summaryPortableText: [],
+  notification: { enabled: false },
+  sidebarCard1: { title: "", bodyPortableText: [] },
+  sidebarCard2: { title: "", bodyPortableText: [] },
+};
+
+const mockPost = {
+  id: "1",
+  title: "Some blog post",
+  slug: "some-blog-post",
+  date: new Date("2022-12-01"),
+  category: { title: "Some category", slug: "some-category" },
+} as BlogPostPreview;
+
+const mockPost2 = {
+  id: "2",
+  title: "Some other post",
+  slug: "some-other-post",
+  date: new Date("2022-12-01"),
+  category: { title: "Some category", slug: "some-category" },
+} as BlogPostPreview;
+
+const mockPost3 = {
+  id: "2",
+  title: "Some other post",
+  slug: "some-other-post",
+  date: new Date("2022-12-01"),
+  category: { title: "Some category", slug: "some-category" },
+} as WebinarPreview;
+
+describe("getBlogPosts", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    jest.resetModules();
+
+    mockCMSClient.homepage.mockResolvedValue(pageData);
+    mockCMSClient.blogPosts.mockResolvedValue([]);
+    mockCMSClient.webinars.mockResolvedValue([]);
+  });
+
+  it("Should return no more than 4 posts", async () => {
+    mockCMSClient.blogPosts.mockResolvedValueOnce([
+      mockPost,
+      mockPost2,
+      mockPost,
+      mockPost2,
+      mockPost,
+      mockPost2,
+    ]);
+
+    const result = await getBlogPosts(false, 5);
+    expect(result.posts).toHaveLength(4);
+  });
+
+  it("Should sort posts by date ascending", async () => {
+    mockCMSClient.blogPosts.mockResolvedValueOnce([
+      { ...mockPost, id: "2", date: new Date("2022-01-01") },
+      { ...mockPost, id: "3", date: new Date("2021-01-01") },
+      { ...mockPost, id: "1", date: new Date("2023-01-01") },
+    ]);
+    const result = await getBlogPosts(false, 5);
+    const postIds = result.posts.map((p) => p.id);
+    expect(postIds).toEqual(["1", "2", "3"]);
+  });
+
+  it("Should filter out upcoming webinars", async () => {
+    mockCMSClient.webinars.mockResolvedValueOnce([
+      { ...mockPost3, id: "2", date: new Date("2022-01-01") },
+      { ...mockPost3, id: "3", date: new Date("2021-01-01") },
+      { ...mockPost3, id: "1", date: new Date("4023-01-01") },
+    ]);
+    const result = await getBlogPosts(false, 5);
+
+    const postIds = result.posts.map((p) => p.id as string);
+    expect(postIds).toEqual(["2", "3"]);
+  });
+
+  it("Should not fetch draft content by default", async () => {
+    mockCMSClient.blogPosts.mockResolvedValueOnce([mockPost]);
+    await getBlogPosts(false, 5);
+
+    expect(mockCMSClient.blogPosts).toHaveBeenCalledWith(
+      expect.objectContaining({
+        previewMode: false,
+      }),
+    );
+  });
+
+  it("should return null when the page data is missing", async () => {
+    mockCMSClient.homepage.mockResolvedValueOnce(null);
+
+    const propsResult = await getBlogPosts(false, 5);
+
+    expect(propsResult.pageData).toBeNull();
+  });
+});
