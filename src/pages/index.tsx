7888c5696df5b3d0b750880fe989463518e81941
Simon Katan <simon.katan@thenational.academy>
2024-09-02 16:07:10 +0100

    feat: redirect from old anchor targets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 import Teachers, {
   getStaticProps as getStaticPropsTeachers,
+  TeachersHomePageProps,
 } from "@/pages/teachers";
 
+const Home: NextPage<TeachersHomePageProps> = (props) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // clientside redirect for old tabs implementation
+    if (window.location.href.includes("#pupils")) {
+      router.push("/pupils");
+    } else if (window.location.href.includes("#curriculum")) {
+      router.push("/curriculum");
+    } else if (window.location.href.includes("#ai")) {
+      router.push("/ai");
+    }
+  }, [router]);
+
+  return <Teachers {...props} />;
+};
 export const getStaticProps = getStaticPropsTeachers;
 
-export default Teachers;
+export default Home;
