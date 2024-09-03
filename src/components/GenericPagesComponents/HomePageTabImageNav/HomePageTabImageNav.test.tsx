07b6fc808e3f10fd6f351d79e60dc3532345e6f9
Simon Katan <simon.katan@thenational.academy>
2024-09-03 09:30:53 +0100

    test: more tests

--- src/components/GenericPagesComponents/HomePageTabImageNav/HomePageTabImageNav.test.tsx
+++ src/components/GenericPagesComponents/HomePageTabImageNav/HomePageTabImageNav.test.tsx
@@ -3,10 +3,37 @@ import HomePageTabImageNav from "./HomePageTabImageNav";
 import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
 
 describe("HomePageTabImageNav Component", () => {
-  test("renders without errors", () => {
+  it("renders without errors", () => {
     const { container } = renderWithTheme(
       <HomePageTabImageNav current="teachers" />,
     );
     expect(container).toBeTruthy();
   });
+
+  it.each([
+    [/Curriculum Plans/i, "/curriculum"],
+    [/AI Experiments/i, "/ai"],
+    [/Pupils/i, "/pupils"],
+  ])(
+    "navigates to the correct tab when the button is clicked",
+    (name, path) => {
+      //mock window.location
+      Object.defineProperty(window, "location", {
+        value: {
+          href: "/",
+        },
+        writable: true,
+      });
+
+      const { getByRole } = renderWithTheme(
+        <HomePageTabImageNav current="teachers" />,
+      );
+      const curriculumButton = getByRole("link", {
+        name,
+      });
+      expect(curriculumButton).toBeTruthy();
+      curriculumButton.click();
+      expect(window.location.href).toBe(path);
+    },
+  );
 });
---
+++ src/pages-helpers/home/plan-a-lesson/getNavItems.test.tsx
@@ -0,0 +1,13 @@
+import { getNavItems } from "./getNavItems";
+
+import { testPlanALessonPageData } from "@/__tests__/pages/lesson-planning.fixture";
+
+describe("getNavItems function", () => {
+  it("should return valid navigation items excluding form items", () => {
+    const result = getNavItems({ ...testPlanALessonPageData });
+    expect(result).toEqual([
+      { title: "test nav content", href: "#test-nav-content" },
+      { title: "test content 2", href: "#test-content-2" },
+    ]);
+  });
+});
