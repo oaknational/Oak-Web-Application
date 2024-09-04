2d7f1c4dbe8ab50777a906cb6526d5fc75cdcc23
Natassja Blore <natassja.blore@thenational.academy>
2024-09-04 11:41:58 +0100

    refactor: use ref container to search for clerk classnames

--- src/app/(registration)/layout.tsx
+++ src/app/(registration)/layout.tsx
@@ -1,5 +1,11 @@
 "use client";
-import { useEffect, useState } from "react";
+import {
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 import { resolveOakHref } from "@/common-lib/urls";
 import CMSImage from "@/components/SharedComponents/CMSImage";
@@ -8,7 +14,7 @@ import withFeatureFlag from "@/hocs/withFeatureFlag";
 import { getIllustrationAsset } from "@/image-data";
 import { OakBox, OakFlex, OakLink, OakP } from "@/styles/oakThemeApp";
 
-const TermsAndConditions = () => {
+export const TermsAndConditions = () => {
   return (
     <OakBox>
       <OakP $font="body-2" color="text-primary" $textAlign="center">
@@ -41,41 +47,32 @@ const TermsAndConditions = () => {
 };
 
 function Layout({ children }: { children: React.ReactNode }) {
-  const [render, setRender] = useState(false);
+  const clerkRef = useRef<null | HTMLDivElement>(null);
+  const [clerkRendered, setClerkRendered] = useState(false);
 
-  const checkElement = () => {
-    const authElement =
-      [...document.getElementsByClassName("cl-rootBox cl-signUp-root")][0] ||
-      [...document.getElementsByClassName("cl-rootBox cl-signIn-root")][0];
+  const checkForClerkElement = useCallback(
+    (ref: MutableRefObject<HTMLDivElement | null>) => {
+      if (ref.current) {
+        // Clerk docs say these classnames are stable
+        const clerkSignUpElement = ref.current.getElementsByClassName(
+          "cl-rootBox cl-signUp-root",
+        )[0];
+        const clerkSignInElement = ref.current.getElementsByClassName(
+          "cl-rootBox cl-signIn-root",
+        )[0];
+        if (clerkSignUpElement || clerkSignInElement) {
+          setClerkRendered(true);
+        } else {
+          setTimeout(() => checkForClerkElement(ref), 100);
+        }
+      }
+    },
+    [],
+  );
 
-    if (authElement) {
-      setRender(true);
-    } else {
-      setTimeout(checkElement, 100);
-    }
-  };
   useEffect(() => {
-    checkElement();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    if (!render) checkElement();
-    const config = { attributes: true, childList: true, subtree: true };
-
-    const observer = new MutationObserver(() => setRender(false));
-
-    const authElement =
-      [...document.getElementsByClassName("cl-rootBox cl-signUp-root")][0] ||
-      [...document.getElementsByClassName("cl-rootBox cl-signIn-root")][0];
-    if (authElement) {
-      observer.observe(authElement, config);
-    }
-    return () => {
-      return observer.disconnect();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [render]);
+    checkForClerkElement(clerkRef);
+  }, [clerkRef, checkForClerkElement]);
 
   return (
     <RegistrationLayout
@@ -95,10 +92,11 @@ function Layout({ children }: { children: React.ReactNode }) {
           $borderRadius="border-radius-m2"
           $width="max-content"
           $mb="space-between-m"
+          ref={clerkRef}
         >
           {children}
         </OakBox>
-        {render && <TermsAndConditions />}
+        {clerkRendered && <TermsAndConditions />}
       </OakBox>
       <OakFlex
         $flexDirection="column"
@@ -107,7 +105,7 @@ function Layout({ children }: { children: React.ReactNode }) {
         $gap="space-between-m"
       >
         {children}
-        <TermsAndConditions />
+        {clerkRendered && <TermsAndConditions />}
       </OakFlex>
     </RegistrationLayout>
   );
