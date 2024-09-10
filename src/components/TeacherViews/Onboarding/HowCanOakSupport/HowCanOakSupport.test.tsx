32446cdb1ef3c2ce4affca45a6686fc6df4312b0
Natassja Blore <natassja.blore@thenational.academy>
2024-09-10 10:14:42 +0100

    test: add mock router

--- src/components/TeacherViews/Onboarding/HowCanOakSupport/HowCanOakSupport.test.tsx
+++ src/components/TeacherViews/Onboarding/HowCanOakSupport/HowCanOakSupport.test.tsx
@@ -1,8 +1,13 @@
-import { screen } from "@testing-library/dom";
+import { screen, waitFor } from "@testing-library/dom";
+import mockRouter from "next-router-mock";
 
 import HowCanOakSupport, { oakSupportMap } from "./HowCanOakSupport.view";
 
-import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
+import renderWithProviders, {
+  allProviders,
+} from "@/__tests__/__helpers__/renderWithProviders";
+
+jest.mock("next/router", () => require("next-router-mock"));
 
 describe("HowCanOakSupport", () => {
   it("renders the onboarding layout with the correct prompt", () => {
@@ -19,9 +24,20 @@ describe("HowCanOakSupport", () => {
     const checkboxes = screen.getAllByRole("checkbox");
     expect(checkboxes).toHaveLength(Object.keys(oakSupportMap).length);
   });
-  it("renders a continue button that is enabled by default", () => {
-    renderWithProviders()(<HowCanOakSupport />);
+  it("renders a continue button that is enabled by default", async () => {
+    mockRouter.push({
+      pathname: "/onboarding/how-can-oak-support",
+      query: {
+        newsletterSignUp: "true",
+        schoolName:
+          "Jefferson%20House%2C%20Cheshire%20West%20and%20Chester%2C%20CW7%201JT",
+        school: "142332-Jefferson%20House",
+      },
+    });
+    renderWithProviders({ ...allProviders, router: mockRouter })(
+      <HowCanOakSupport />,
+    );
     const continueButton = screen.getByRole("button", { name: /continue/i });
-    expect(continueButton).toBeEnabled();
+    await waitFor(() => expect(continueButton).toBeEnabled());
   });
 });
