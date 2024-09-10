f31d6937ce158b68adcbe5b258095d9c7e38a1d8
Natassja Blore <natassja.blore@thenational.academy>
2024-09-10 14:14:33 +0100

    fix: optionality title on canonical lesson page

--- src/node-lib/curriculum-api-2023/queries/lessonOverview/lessonOverview.query.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonOverview/lessonOverview.query.ts
@@ -126,10 +126,12 @@ export function getCopyrightContent(
 const getPathways = (res: LessonOverviewQuery): LessonPathway[] => {
   const pathways = res.browseData.map((l) => {
     const lesson = lessonBrowseDataSchema.parse(l);
+    const unitTitle =
+      lesson.programme_fields.optionality ?? lesson.unit_data.title;
     const pathway = {
       programmeSlug: lesson.programme_slug,
       unitSlug: lesson.unit_slug,
-      unitTitle: lesson.unit_data.title,
+      unitTitle,
       keyStageSlug: lesson.programme_fields.keystage_slug,
       keyStageTitle: lesson.programme_fields.keystage_description,
       subjectSlug: lesson.programme_fields.subject_slug,
