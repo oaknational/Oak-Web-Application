3dcd45630a1167d95d7becc8dbd0a045f300f084
ben <ben.protheroe@thenational.academy>
2024-09-16 10:01:15 +0100

    feat(story): fix story

--- src/components/PupilViews/PupilResults/PupilResults.view.tsx
+++ src/components/PupilViews/PupilResults/PupilResults.view.tsx
@@ -3,6 +3,7 @@ import { OakFlex, OakHeading } from "@oaknational/oak-components";
 import { LessonSectionResults } from "@/components/PupilComponents/LessonEngineProvider";
 import { QuestionsArray } from "@/components/PupilComponents/QuizEngineProvider";
 import { QuizResults } from "@/components/PupilComponents/QuizResults";
+import { MathJaxWrap } from "@/browser-lib/mathjax/MathJaxWrap";
 
 type PupilViewsResultsProps = {
   attemptData: {
@@ -26,23 +27,26 @@ export const PupilViewsResults = (props: PupilViewsResultsProps) => {
   const { sectionResults } = attemptData;
   return (
     <div>
-      <OakHeading tag="h1">Results Page</OakHeading>
-      <OakFlex $flexDirection={"column"}>
-        <OakHeading tag="h2">Starter Quiz</OakHeading>
-        <QuizResults
-          sectionResults={sectionResults}
-          quizArray={starterQuizQuestionsArray}
-          lessonSection={"starter-quiz"}
-        />
-      </OakFlex>
-      <OakFlex $flexDirection={"column"}>
-        <OakHeading tag="h2">Exit Quiz</OakHeading>
-        <QuizResults
-          sectionResults={sectionResults}
-          quizArray={exitQuizQuestionsArray}
-          lessonSection={"exit-quiz"}
-        />
-      </OakFlex>
+      <MathJaxWrap>
+        {" "}
+        <OakHeading tag="h1">Results Page</OakHeading>
+        <OakFlex $flexDirection={"column"}>
+          <OakHeading tag="h2">Starter Quiz</OakHeading>
+          <QuizResults
+            sectionResults={sectionResults}
+            quizArray={starterQuizQuestionsArray}
+            lessonSection={"starter-quiz"}
+          />
+        </OakFlex>
+        <OakFlex $flexDirection={"column"}>
+          <OakHeading tag="h2">Exit Quiz</OakHeading>
+          <QuizResults
+            sectionResults={sectionResults}
+            quizArray={exitQuizQuestionsArray}
+            lessonSection={"exit-quiz"}
+          />
+        </OakFlex>
+      </MathJaxWrap>
     </div>
   );
 };
