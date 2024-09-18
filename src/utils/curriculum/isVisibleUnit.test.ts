e191da9b70551b2da9b6a30150454c04899b2211
Assad Khan <assadkhan@outlook.com>
2024-09-18 11:44:56 +0100

    test: add placeholder test file for isVisibleUnit function

---
+++ src/utils/curriculum/isVisibleUnit.test.ts
@@ -0,0 +1,10 @@
+// import { isVisibleUnit } from "./isVisibleUnit.test";
+
+// import { YearSelection } from "@/components/CurriculumComponents/CurriculumVisualiser";
+// import { CurriculumUnitsTabData } from "@/node-lib/curriculum-api-2023";
+
+// TODO: Add a unit test for isVisibleUnit utility function
+
+describe("isVisibleUnit", () => {
+  it.todo("This function needs to be tested");
+});
