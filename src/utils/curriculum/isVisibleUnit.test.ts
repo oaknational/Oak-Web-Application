47010c5f8c60751f401ea0b3f837d900a44113ad
Assad Khan <assadkhan@outlook.com>
2024-09-18 19:40:26 +0100

    chore: add link to issue for adding unit tests for util function

--- src/utils/curriculum/isVisibleUnit.test.ts
+++ src/utils/curriculum/isVisibleUnit.test.ts
@@ -4,6 +4,7 @@
 // import { CurriculumUnitsTabData } from "@/node-lib/curriculum-api-2023";
 
 // TODO: Add a unit test for isVisibleUnit utility function
+// CUR-909: https://www.notion.so/oaknationalacademy/Write-tests-for-utility-functions-used-in-curriculum-visualiser-10526cc4e1b180a1b33bd8915d5ec4d5?pvs=4
 
 describe("isVisibleUnit", () => {
   it.todo("This function needs to be tested");
