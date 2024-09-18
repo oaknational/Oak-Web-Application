cb052098e924cf811693d13df3e0dc0665e0092a
ben <ben.protheroe@thenational.academy>
2024-09-18 17:56:20 +0100

    feat(test): test new code

---
+++ src/__tests__/pages/pupils/lessons/[lessonSlug]/results/[attemptId]/printable.test.tsx
@@ -0,0 +1,7 @@
+import CanonicalResultsPage from "@/pages/pupils/lessons/[lessonSlug]/results/[attemptId]/printable";
+
+describe("CanonicalResultsPage", () => {
+  it("should render", () => {
+    expect(CanonicalResultsPage).toBeTruthy();
+  });
+});
--- src/__tests__/pages/pupils/lessons/[lessonSlug]/results/[attemptId]/share.test.tsx
+++ src/__tests__/pages/pupils/lessons/[lessonSlug]/results/[attemptId]/share.test.tsx
@@ -1,4 +1,4 @@
-import CanonicalResultsPage from "@/pages/pupils/lessons/[lessonSlug]/results/[attemptId]";
+import CanonicalResultsPage from "@/pages/pupils/lessons/[lessonSlug]/results/[attemptId]/share";
 
 describe("CanonicalResultsPage", () => {
   it("should render", () => {
