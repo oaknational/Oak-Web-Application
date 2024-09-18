b446bc9fef1d6b076da69a1a7d223aeed8af25a7
Jamie Blair <info@orangemug.co.uk>
2024-09-18 16:19:49 +0100

    feat: added in new cycle 2 MVs + filters

--- src/node-lib/curriculum-api-2023/queries/subjectPhaseOptions/subjectPhaseOptions.query.ts
+++ src/node-lib/curriculum-api-2023/queries/subjectPhaseOptions/subjectPhaseOptions.query.ts
@@ -3,16 +3,20 @@ import subjectPhaseOptionsSchema from "./subjectPhaseOptions.schema";
 import OakError from "@/errors/OakError";
 import { Sdk } from "@/node-lib/curriculum-api-2023/sdk";
 
-const subjectPhaseOptionsQuery = (sdk: Sdk) => async () => {
-  const res = await sdk.subjectPhaseOptions();
-  const subjectPhaseOptions = res.options;
-  if (subjectPhaseOptions.length === 0) {
-    throw new OakError({ code: "curriculum-api/not-found" });
-  }
-  const results = subjectPhaseOptionsSchema.parse(subjectPhaseOptions);
-  return results.filter((result) => {
-    return result.state === "published";
-  });
-};
+type subjectPhaseOptionsQueryProps = { cycles?: string[] };
+const subjectPhaseOptionsQuery =
+  (sdk: Sdk) =>
+  async (props: subjectPhaseOptionsQueryProps = {}) => {
+    const { cycles = ["1"] } = props;
+    const res = await sdk.subjectPhaseOptions();
+    const subjectPhaseOptions = res.options;
+    if (subjectPhaseOptions.length === 0) {
+      throw new OakError({ code: "curriculum-api/not-found" });
+    }
+    const results = subjectPhaseOptionsSchema.parse(subjectPhaseOptions);
+    return results.filter((result) => {
+      return cycles.includes(result.cycle);
+    });
+  };
 
 export default subjectPhaseOptionsQuery;
