251a2d27967d94c2f8a06770a3f4dcef33701cff
Simon Katan <simon.katan@thenational.academy>
2024-10-01 17:00:20 +0100

    fix: type errors

--- src/components/PupilViews/PupilResults/PupilResults.view.tsx
+++ src/components/PupilViews/PupilResults/PupilResults.view.tsx
@@ -1,4 +1,5 @@
 import {
+  isValidIconName,
   oakDefaultTheme,
   OakFlex,
   OakHandDrawnHR,
@@ -89,6 +90,8 @@ export const PupilViewsResults = (props: PupilViewsResultsProps) => {
       ? Math.ceil((video.timeElapsed / video.duration) * 100)
       : 0;
 
+  const iconSlug = `subject-${subjectSlug}`;
+
   return (
     <OakThemeProvider theme={oakDefaultTheme}>
       <MathJaxWrap>
@@ -110,7 +113,7 @@ export const PupilViewsResults = (props: PupilViewsResultsProps) => {
           <OakQuizPrintableHeader
             alt="icon"
             breadcrumbs={[yearDescription, subject]}
-            iconName={`subject-${subjectSlug}`}
+            iconName={isValidIconName(iconSlug) ? iconSlug : "question-mark"}
             title={title}
             videoPercentage={percentageVideoWatched}
             worksheetDownloaded={worksheetDownloaded}
--- src/node-lib/curriculum-api-2023/queries/lessonDownloads/rawSyntheticUVLesson.schema.ts
+++ src/node-lib/curriculum-api-2023/queries/lessonDownloads/rawSyntheticUVLesson.schema.ts
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import { syntheticUnitvariantLessonsSchema } from "@oaknational/oak-curriculum-schema";
 
 export const rawSyntheticUVLessonSchema =
