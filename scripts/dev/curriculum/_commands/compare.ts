ba1f0f498a7f55317cd26d314ed6dc9164304380
Ben Titterington <titteringtonb@gmail.com>
2024-10-07 08:30:43 +0100

    feat: add comparison and some styling to curriculum screenshot script

--- scripts/dev/curriculum/_commands/compare.ts
+++ scripts/dev/curriculum/_commands/compare.ts
@@ -5,9 +5,12 @@ import { glob } from "glob";
 import { sortBy, uniq } from "lodash";
 import open from "open";
 
-function formatPath(input: string) {
-  return `./${input}`;
-}
+// function formatPath(input: string) {
+//   return `./${input}`;
+// }
+
+const removeFileExtension = (filename: string) =>
+  filename.substring(0, filename.lastIndexOf(".")) || filename;
 
 function outputPathFromLabel(label: string) {
   const labelName = label.replace(/^:/, "");
@@ -38,59 +41,122 @@ export default async function compare(
         <body>
             <style>
                 html {
-                    box-sizing: border-box;
+                  box-sizing: border-box;
+                  font-family: sans-serif;
+                  font-size: 14px;
+                }
+                html, body {
+                  margin: 0;
+                  padding: 0;
                 }
                 *, *:before, *:after {
-                    box-sizing: inherit;
+                  box-sizing: inherit;
+                }
+                .column-header{
+                  background: #eeeeee;
+                  padding: 8px;
+                  position: sticky;
+                  top: 8px;
+                  z-index: 9999;
+                  border-radius: 5px;
+                }
+                .nav-item{
+                  margin:8px 0;
+                  display:flex;
+                }
+                .nav-item a{
+                  padding:4px;
+                  border:1px solid #ccc; 
+                  background:#f6f6f6;
+                  border-radius:5px;
+                  text-decoration:none;
+                  text-wrap: nowrap;
+                  color:#333;
+                }
+                .nav-item a:hover{
+                  background:#e6e6e6;
+                }
+                .pill{
+                  background:#333;
+                  color:#fff;
+                  padding:2px 4px;
+                  border-radius:3px;
+                }
+                .image-holder{
+                  position: relative;
+                  overflow: hidden;
+                }
+                .diff-image{
+                  position: absolute;
+                  top: 0;
+                  left: 0;
+                  bottom: 0;
+                  right: 0;
+                  mix-blend-mode: difference;
+                }
+                .image-holder img{
+                  width: 100%;
                 }
             </style>
-            <div style="
-                display: grid;
-                grid-template-columns: 50% 50%;
-                gap: 8px;
-                position: sticky;
-                top: 0;
-                z-index: 99;
-            ">
-                <div style="
-                    background: #222222;
-                    color: #ffffff;
-                    padding: 8px;
-                ">${formatPath(relBasepath)}/*</div>
-                <div style="
-                    background: #222222;
-                    color: #ffffff;
-                    padding: 8px;
-                ">${formatPath(relTargetpath)}/*</div>
+            <div style="display: flex;">
+            <div style="width: fit-content;">
+              <div style="position: sticky; top: 0; padding:10px">
+                ${allFiles
+                  .map((filename) => {
+                    return `<div class="nav-item"><a href="#${filename}">${removeFileExtension(
+                      filename,
+                    )}</a></div>`;
+                  })
+                  .join("")}
+              </div>
             </div>
             <div style="
                 display: grid;
-                grid-template-columns: 50% 50%;
+                grid-template-columns: repeat(3, 1fr);
                 gap: 8px;
+                border-left: 1px solid #ccc;
             ">
                 ${allFiles
                   .map((filename) => {
                     let imageHtml = `
-                        <div style="background: #eeeeee; padding: 8px; position: sticky; top: 0; z-index: 9999;">
-                            ${formatPath(`${relBasepath}/${filename}`)}
+                        <span id="${filename}" style="grid-column: 1 / -1"></span>
+                        <div class="column-header" >
+                            <span class="pill">${basepathInput}</span> ${filename}
                         </div>
-                        <div style="background: #eeeeee; padding: 8px; position: sticky; top: 0; z-index: 9999;">
-                            ${formatPath(`${relTargetpath}/${filename}`)}
+                        <div class="column-header">
+                            <span class="pill">${targetpathInput}</span> ${filename}
+                        </div>
+                        <div class="column-header">
+                            Diff: ${filename}
                         </div>
                     `;
                     if (baseFilesStub.includes(filename)) {
-                      imageHtml += `<div style="position: relative; overflow: hidden;"><img width="100%" src="${relBasepath}/${filename}" /></div>`;
+                      imageHtml += `<div class="image-holder"><img src="${relBasepath}/${filename}" /></div>`;
                     } else {
                       imageHtml += `<div style="background: #eee; padding: 8px;">Missing</div>`;
                     }
                     if (targetFilesStub.includes(filename)) {
-                      imageHtml += `<div style="position: relative; overflow: hidden;"><img width="100%" src="${relTargetpath}/${filename}" /></div>`;
+                      imageHtml += `<div class="image-holder"><img src="${relTargetpath}/${filename}" /></div>`;
                     } else {
                       imageHtml += `<div style="background: #eee; padding: 8px;">Missing</div>`;
                     }
+
+                    if (
+                      baseFilesStub.includes(filename) &&
+                      targetFilesStub.includes(filename)
+                    ) {
+                      imageHtml += `<div style="filter:invert(1)" class="image-holder">
+                        <img src="${relBasepath}/${filename}" />
+                        <div class="diff-image"><img src="${relTargetpath}/${filename}" /></div>
+                      </div>`;
+                    } else {
+                      imageHtml += `<div style="background: #eee; padding: 8px;">Missing</div>`;
+                    }
+
                     return imageHtml;
                   })
                   .join("")}
+            </div>
             </div>
         </body>
     `;
