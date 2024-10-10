08c3822194aa9cd532dad688979895727de4d30f
Simon Katan <simon.katan@thenational.academy>
2024-10-10 15:26:19 +0100

    feat: exclusions applied to pupil browse queries

--- src/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.query.ts
+++ src/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.query.ts
@@ -10,7 +10,7 @@ import OakError from "@/errors/OakError";
 import { Sdk } from "@/node-lib/curriculum-api-2023/sdk";
 import {
   InputMaybe,
-  Published_Mv_Synthetic_Unitvariant_Lessons_By_Year_10_0_0_Bool_Exp,
+  Published_Mv_Synthetic_Unitvariant_Lessons_By_Year_12_0_0_Bool_Exp,
 } from "@/node-lib/curriculum-api-2023/generated/sdk";
 import keysToCamelCase from "@/utils/snakeCaseConverter";
 
@@ -24,7 +24,7 @@ export const pupilLessonQuery =
   }): Promise<{ content: LessonContent; browseData: LessonBrowseData }> => {
     const { lessonSlug, unitSlug, programmeSlug, isLegacy } = args;
 
-    const browseDataWhere: InputMaybe<Published_Mv_Synthetic_Unitvariant_Lessons_By_Year_10_0_0_Bool_Exp> =
+    const browseDataWhere: InputMaybe<Published_Mv_Synthetic_Unitvariant_Lessons_By_Year_12_0_0_Bool_Exp> =
       { lesson_slug: { _eq: lessonSlug } };
 
     if (unitSlug) {
@@ -46,7 +46,10 @@ export const pupilLessonQuery =
 
     const [browseDataSnake] = res.browseData;
 
-    if (!browseDataSnake) {
+    if (
+      !browseDataSnake ||
+      browseDataSnake.actions?.exclusions.includes("pupils")
+    ) {
       throw new OakError({ code: "curriculum-api/not-found" });
     }
 
