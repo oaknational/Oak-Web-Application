be1200437a1ef6ea2a39cf679f49c35a926da46f
Natassja Blore <natassja.blore@thenational.academy>
2024-10-10 09:00:43 +0100

    test: update test case

--- src/utils/getValidSubjectIconName.test.ts
+++ src/utils/getValidSubjectIconName.test.ts
@@ -8,8 +8,8 @@ describe("getValidSubjectIconName", () => {
   it("should return subject-maths when subject is valid", () => {
     expect(getValidSubjectIconName("maths")).toBe("subject-maths");
   });
-  it("should return error when subject is invalid", () => {
-    expect(getValidSubjectIconName("invalid")).toBe("error");
+  it("should return books icon when subject is invalid", () => {
+    expect(getValidSubjectIconName("invalid")).toBe("books");
   });
   it("should report an error when subject is invalid", () => {
     getValidSubjectIconName("invalid");
