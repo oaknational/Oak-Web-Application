e42acb7b50d56f1b89908d92c9f60d51bd21d46f
Kyle Huggins <kyle.huggins7@gmail.com>
2024-10-15 16:02:17 +0100

    chore: update search filters

--- src/components/TeacherComponents/SearchFilters/SearchFilters.tsx
+++ src/components/TeacherComponents/SearchFilters/SearchFilters.tsx
@@ -29,140 +29,138 @@ const SearchFilters: FC<SearchFiltersProps> = (props) => {
 
   return (
     <OakThemeProvider theme={oakDefaultTheme}>
-      <OakBox>
+      <OakBox
+        $mb="space-between-m2"
+        $bb={"border-solid-s"}
+        $borderColor={"grey40"}
+      >
         <OakFieldset>
-          <OakBox
+          <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
+            Curriculum
+          </OakP>
+          <OakFlex
+            $gap={"space-between-s"}
             $mb="space-between-m2"
-            $bb={"border-solid-s"}
-            $borderColor={"grey40"}
+            $flexDirection={"column"}
+            $flexWrap={"wrap"}
           >
-            <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
-              Curriculum
-            </OakP>
-            <OakFlex
-              $gap={"space-between-s"}
-              $mb="space-between-m2"
-              $flexDirection={"column"}
-              $flexWrap={"wrap"}
-            >
+            <OakSearchFilterCheckBox
+              name={"new"}
+              displayValue={"Show new only"}
+              key={`search-filters-curriculum-filter`}
+              aria-label={`Show new content filter`}
+              {...legacyFilter}
+              id={`search-filters-showNewContent:mobile:${isMobileFilter}`}
+              value="new"
+              onChange={() => {
+                legacyFilter.onChange();
+              }}
+            />
+            <OakFlex $alignItems={"flex-start"}>
+              <OakBox>
+                <TagPromotional size={"small"} />
+              </OakBox>
+              <OakP
+                $font={"body-3"}
+                $wordWrap={"normal"}
+                $color={"text-subdued"}
+              >
+                Resources designed for the classroom
+              </OakP>
+            </OakFlex>
+          </OakFlex>
+        </OakFieldset>
+      </OakBox>
+      <OakBox
+        $mb="space-between-m2"
+        $bb={"border-solid-s"}
+        $borderColor={"grey40"}
+      >
+        <OakFieldset>
+          <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
+            Key stages
+          </OakP>
+          <OakFlex
+            $gap={"space-between-xs"}
+            $mb="space-between-m2"
+            $flexDirection={"row"}
+            $flexWrap={"wrap"}
+          >
+            {keyStageFilters.map((keyStageFilter) => (
               <OakSearchFilterCheckBox
-                name={"new"}
-                displayValue={"Show new only"}
-                key={`search-filters-curriculum-filter`}
-                aria-label={`Show new content filter`}
-                {...legacyFilter}
-                id={`search-filters-showNewContent:mobile:${isMobileFilter}`}
-                value="new"
+                name={"keyStageFilters"}
+                displayValue={
+                  keyStageFilter.shortCode === "EYFS"
+                    ? keyStageFilter.shortCode
+                    : toSentenceCase(keyStageFilter.title)
+                }
+                key={`search-filters-keyStage-${keyStageFilter.slug}`}
+                aria-label={`${keyStageFilter.title} filter`}
+                id={`search-filters-keyStage-${keyStageFilter.slug}:mobile:${isMobileFilter}`}
+                value="Key stage filter"
+                {...keyStageFilter}
                 onChange={() => {
-                  legacyFilter.onChange();
+                  keyStageFilter.onChange();
                 }}
               />
-              <OakFlex $alignItems={"flex-start"}>
-                <OakBox>
-                  <TagPromotional size={"small"} />
-                </OakBox>
-                <OakP
-                  $font={"body-3"}
-                  $wordWrap={"normal"}
-                  $color={"text-subdued"}
-                >
-                  Resources designed for the classroom
-                </OakP>
-              </OakFlex>
-            </OakFlex>
-          </OakBox>
+            ))}
+          </OakFlex>
         </OakFieldset>
-        <OakBox
-          $mb="space-between-m2"
-          $bb={"border-solid-s"}
-          $borderColor={"grey40"}
-        >
-          <OakFieldset>
-            <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
-              Key stages
-            </OakP>
-            <OakFlex
-              $gap={"space-between-xs"}
-              $mb="space-between-m2"
-              $flexDirection={"row"}
-              $flexWrap={"wrap"}
-            >
-              {keyStageFilters.map((keyStageFilter) => (
+      </OakBox>
+      <OakBox
+        $mb="space-between-m2"
+        $bb={"border-solid-s"}
+        $borderColor={"grey40"}
+      >
+        <OakFieldset>
+          <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
+            Exam boards
+          </OakP>
+          <OakFlex
+            $gap={"space-between-xs"}
+            $mb="space-between-m2"
+            $flexWrap={"wrap"}
+          >
+            {examBoardFilters.map((examBoardFilter) => (
+              <OakSearchFilterCheckBox
+                value={"examBoardFilters"}
+                key={`search-filters-examBoard-${examBoardFilter.slug}`}
+                aria-label={`${examBoardFilter.title} filter`}
+                displayValue={examBoardFilter.title}
+                id={`search-filters-examBoard-${examBoardFilter.slug}:mobile:${isMobileFilter}`}
+                {...examBoardFilter}
+                onChange={() => {
+                  examBoardFilter.onChange();
+                }}
+              />
+            ))}
+          </OakFlex>
+        </OakFieldset>
+      </OakBox>
+      <OakBox>
+        <OakFieldset>
+          <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
+            Subjects
+          </OakP>
+          <OakFlex $gap={"space-between-xs"} $flexWrap={"wrap"}>
+            {subjectFilters.map((subjectFilter) => {
+              return (
                 <OakSearchFilterCheckBox
-                  name={"keyStageFilters"}
-                  displayValue={
-                    keyStageFilter.shortCode === "EYFS"
-                      ? keyStageFilter.shortCode
-                      : toSentenceCase(keyStageFilter.title)
-                  }
-                  key={`search-filters-keyStage-${keyStageFilter.slug}`}
-                  aria-label={`${keyStageFilter.title} filter`}
-                  id={`search-filters-keyStage-${keyStageFilter.slug}:mobile:${isMobileFilter}`}
-                  value="Key stage filter"
-                  {...keyStageFilter}
+                  value={"subjectFilters"}
+                  key={`search-filters-subject-${subjectFilter.slug}`}
+                  aria-label={`${subjectFilter.title} filter`}
+                  id={`search-filters-subject-${subjectFilter.slug}:mobile:${isMobileFilter}`}
+                  displayValue={subjectFilter.title}
+                  icon={getValidSubjectIconName(subjectFilter.slug)}
+                  {...subjectFilter}
                   onChange={() => {
-                    keyStageFilter.onChange();
+                    subjectFilter.onChange();
                   }}
                 />
-              ))}
-            </OakFlex>
-          </OakFieldset>
-        </OakBox>
-        <OakBox
-          $mb="space-between-m2"
-          $bb={"border-solid-s"}
-          $borderColor={"grey40"}
-        >
-          <OakFieldset>
-            <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
-              Exam boards
-            </OakP>
-            <OakFlex
-              $gap={"space-between-xs"}
-              $mb="space-between-m2"
-              $flexWrap={"wrap"}
-            >
-              {examBoardFilters.map((examBoardFilter) => (
-                <OakSearchFilterCheckBox
-                  value={"examBoardFilters"}
-                  key={`search-filters-examBoard-${examBoardFilter.slug}`}
-                  aria-label={`${examBoardFilter.title} filter`}
-                  displayValue={examBoardFilter.title}
-                  id={`search-filters-examBoard-${examBoardFilter.slug}:mobile:${isMobileFilter}`}
-                  {...examBoardFilter}
-                  onChange={() => {
-                    examBoardFilter.onChange();
-                  }}
-                />
-              ))}
-            </OakFlex>
-          </OakFieldset>
-        </OakBox>
-        <OakBox>
-          <OakFieldset>
-            <OakP as={"legend"} $mb="space-between-m" $font={"heading-7"}>
-              Subjects
-            </OakP>
-            <OakFlex $gap={"space-between-xs"} $flexWrap={"wrap"}>
-              {subjectFilters.map((subjectFilter) => {
-                return (
-                  <OakSearchFilterCheckBox
-                    value={"subjectFilters"}
-                    key={`search-filters-subject-${subjectFilter.slug}`}
-                    aria-label={`${subjectFilter.title} filter`}
-                    id={`search-filters-subject-${subjectFilter.slug}:mobile:${isMobileFilter}`}
-                    displayValue={subjectFilter.title}
-                    icon={getValidSubjectIconName(subjectFilter.slug)}
-                    {...subjectFilter}
-                    onChange={() => {
-                      subjectFilter.onChange();
-                    }}
-                  />
-                );
-              })}
-            </OakFlex>
-          </OakFieldset>
-        </OakBox>
+              );
+            })}
+          </OakFlex>
+        </OakFieldset>
       </OakBox>
     </OakThemeProvider>
   );
