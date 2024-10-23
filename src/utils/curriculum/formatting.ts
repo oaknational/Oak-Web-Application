48d39f7cdd2e0d1eaf39f269048d1f624dd57523
Jamie Blair <info@orangemug.co.uk>
2024-10-23 16:27:56 +0100

    chore: de-dup code in getPhaseText/getShortPhaseText (thanks @andykwoods)

--- src/utils/curriculum/formatting.ts
+++ src/utils/curriculum/formatting.ts
@@ -26,55 +26,40 @@ export function getPhaseText(
   phase: Pick<Phase, "slug">,
   keystages: { slug: string }[],
 ) {
-  if (phase.slug === "primary") {
-    const hasKs1 = hasKs(keystages, 1);
-    const hasKs2 = hasKs(keystages, 2);
-    if (hasKs1 && hasKs2) {
-      return "Key stage 1 and 2";
-    } else if (hasKs1) {
-      return "Key stage 1";
-    } else if (hasKs2) {
-      return "Key stage 2";
-    }
-  }
-  if (phase.slug === "secondary") {
-    const hasKs3 = hasKs(keystages, 3);
-    const hasKs4 = hasKs(keystages, 4);
-    if (hasKs3 && hasKs4) {
-      return "Key stage 3 and 4";
-    } else if (hasKs3) {
-      return "Key stage 3";
-    } else if (hasKs4) {
-      return "Key stage 4";
-    }
-  }
-  return "";
+  return buildPhaseText(phase, keystages, false);
 }
-
 export function getShortPhaseText(
   phase: Pick<Phase, "slug">,
   keystages: { slug: string }[],
+) {
+  return buildPhaseText(phase, keystages, true);
+}
+
+function buildPhaseText(
+  phase: Pick<Phase, "slug">,
+  keystages: { slug: string }[],
+  isShortPhase: boolean,
 ) {
   if (phase.slug === "primary") {
     const hasKs1 = hasKs(keystages, 1);
     const hasKs2 = hasKs(keystages, 2);
     if (hasKs1 && hasKs2) {
-      return "KS1 & KS2";
+      return isShortPhase ? "KS1 & KS2" : "Key stage 1 and 2";
     } else if (hasKs1) {
-      return "KS1";
+      return isShortPhase ? "KS1" : "Key stage 1";
     } else if (hasKs2) {
-      return "KS2";
+      return isShortPhase ? "KS2" : "Key stage 2";
     }
   }
   if (phase.slug === "secondary") {
     const hasKs3 = hasKs(keystages, 3);
     const hasKs4 = hasKs(keystages, 4);
     if (hasKs3 && hasKs4) {
-      return "KS3 & KS4";
+      return isShortPhase ? "KS3 & KS4" : "Key stage 3 and 4";
     } else if (hasKs3) {
-      return "KS3";
+      return isShortPhase ? "KS3" : "Key stage 3";
     } else if (hasKs4) {
-      return "KS4";
+      return isShortPhase ? "KS4" : "Key stage 4";
     }
   }
   return "";
