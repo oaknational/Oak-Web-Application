5dc2ed0a3ca6ad1d59e1e64a95ec5a1652eeb720
John Roberts <john.roberts@thenational.academy>
2024-10-29 14:47:28 +0100

    feat(deps): updates

--- src/utils/snakeCaseConverter.ts
+++ src/utils/snakeCaseConverter.ts
@@ -2,15 +2,16 @@ type SnakeToCamelCase<S extends string> =
   S extends `${infer T}_${infer U}${infer Rest}`
     ? `${T}${Uppercase<U>}${SnakeToCamelCase<Rest>}`
     : S;
-export type ConvertKeysToCamelCase<T> = T extends Array<infer U>
-  ? Array<ConvertKeysToCamelCase<U>>
-  : T extends object
-    ? {
-        [K in keyof T as SnakeToCamelCase<K & string>]: ConvertKeysToCamelCase<
-          T[K]
-        >;
-      }
-    : T;
+export type ConvertKeysToCamelCase<T> =
+  T extends Array<infer U>
+    ? Array<ConvertKeysToCamelCase<U>>
+    : T extends object
+      ? {
+          [K in keyof T as SnakeToCamelCase<
+            K & string
+          >]: ConvertKeysToCamelCase<T[K]>;
+        }
+      : T;
 export function convertKey(key: string): string {
   return key.replace(/(_\w)/g, (_, m) => m[1].toUpperCase());
 }
