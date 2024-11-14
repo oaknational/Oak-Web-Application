410ea8773b5c573256e795d2000115cece31f9bb
Jamie Blair <info@orangemug.co.uk>
2024-11-14 16:55:51 +0000

    chore: remove no-op MOCK_CURRIC_PARTNER_HACK/MOCK_ENABLE_CYCLE_2 from tests

--- src/utils/curriculum/features.test.ts
+++ src/utils/curriculum/features.test.ts
@@ -1,19 +1,6 @@
 import { getUnitFeatures } from "./features";
 import { Unit } from "./types";
 
-const MOCK_ENABLE_CYCLE_2 = jest.fn();
-const MOCK_CURRIC_PARTNER_HACK = jest.fn();
-jest.mock("./constants", () => ({
-  __esModule: true,
-  get ENABLE_CYCLE_2() {
-    return MOCK_ENABLE_CYCLE_2() ?? false;
-  },
-  get CURRIC_PARTNER_HACK() {
-    return MOCK_CURRIC_PARTNER_HACK() ?? false;
-  },
-  default: {},
-}));
-
 beforeEach(() => {
   jest.resetAllMocks();
   jest.clearAllMocks();
@@ -21,7 +8,6 @@ beforeEach(() => {
 
 describe("getUnitFeatures", () => {
   it("returns swimming rules when matching unit", () => {
-    MOCK_ENABLE_CYCLE_2.mockReturnValue(true);
     expect(
       getUnitFeatures({
         subject_slug: "physical-education",
@@ -47,7 +33,6 @@ describe("getUnitFeatures", () => {
   });
 
   it("returns computer science override when matching unit", () => {
-    MOCK_ENABLE_CYCLE_2.mockReturnValue(true);
     expect(
       getUnitFeatures({
         subject_slug: "computing",
@@ -74,7 +59,6 @@ describe("getUnitFeatures", () => {
   });
 
   it("returns subject category exclusion for english when matching unit", () => {
-    MOCK_ENABLE_CYCLE_2.mockReturnValue(true);
     expect(
       getUnitFeatures({
         subject_slug: "english",
@@ -114,7 +98,6 @@ describe("getUnitFeatures", () => {
   });
 
   it("returns nothing when hack enabled but not a matching unit", () => {
-    MOCK_ENABLE_CYCLE_2.mockReturnValue(true);
     expect(getUnitFeatures({ slug: "test" } as Unit)).toEqual(undefined);
   });
 });
