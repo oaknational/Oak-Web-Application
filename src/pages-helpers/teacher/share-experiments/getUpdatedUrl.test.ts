cb0a406ef425a52f4aea178724d4eef47342ce69
Simon Katan <simon.katan@thenational.academy>
2024-11-18 16:31:58 +0000

    feat: add test for override

--- src/pages-helpers/teacher/share-experiments/getUpdatedUrl.test.ts
+++ src/pages-helpers/teacher/share-experiments/getUpdatedUrl.test.ts
@@ -11,6 +11,16 @@ describe("getUpdatedUrl", () => {
     expect(result.shareId).toEqual("1234");
   });
 
+  it("should override the url with the share id in the cookie if available", () => {
+    const url = "http://example.com?sid-c2d05c=xxxxxxxx&sm=0";
+    const cookieShareId = "1234";
+    const lessonSlug = "lesson-1";
+    const result = getUpdatedUrl({ url, cookieShareId, lessonSlug });
+    expect(result.url).toEqual("http://example.com?sid-c2d05c=1234&sm=0");
+    expect(result.shareIdKey).toEqual("sid-c2d05c");
+    expect(result.shareId).toEqual("1234");
+  });
+
   it("should generate a new share id and store it in a cookie if no cookieId provided", () => {
     const url = "http://example.com";
     const lessonSlug = "lesson-1";
