e5161717dfcd6964e409086ca27acaa882e55912
Jamie Blair <info@orangemug.co.uk>
2024-12-16 11:39:38 +0000

    test: added missing tests for new subjectcategory grouped threads/units

--- src/pages-helpers/curriculum/docx/builder/8_units/8_units.test.ts
+++ src/pages-helpers/curriculum/docx/builder/8_units/8_units.test.ts
@@ -2,13 +2,31 @@ import { generateEmptyDocx } from "../../docx";
 import { zipToSnapshotObject } from "../helper";
 
 import generate from "./8_units";
-import { data, slugs } from "./8_units.fixture";
+import { data } from "./8_units.fixture";
 
 describe("8_units", () => {
   it("simple", async () => {
     const zip = await generateEmptyDocx();
     await generate(zip, {
-      slugs: slugs,
+      slugs: {
+        subjectSlug: "science",
+        phaseSlug: "primary",
+      },
+      data: data,
+    });
+
+    expect(await zipToSnapshotObject(zip.getJsZip())).toMatchSnapshot();
+  });
+
+  it("subjectcategory grouped", async () => {
+    const zip = await generateEmptyDocx();
+    await generate(zip, {
+      slugs: {
+        subjectSlug: "english",
+        phaseSlug: "secondary",
+        keyStageSlug: "secondary",
+        ks4OptionSlug: "aqa",
+      },
       data: data,
     });
 
