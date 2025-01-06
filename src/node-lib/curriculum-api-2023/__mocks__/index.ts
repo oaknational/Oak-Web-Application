3c5c95e897ef66220e904cd5262b281b0b21dbfc
Kyle Huggins <91190841+k-huggs@users.noreply.github.com>
2025-01-06 11:27:06 +0000

    fix: remove double importation

--- src/node-lib/curriculum-api-2023/__mocks__/index.ts
+++ src/node-lib/curriculum-api-2023/__mocks__/index.ts
@@ -23,8 +23,8 @@ import { refreshedMVTimeFixture } from "@/node-lib/curriculum-api-2023/fixtures/
 import { teachersSitemapDataFixtureCamelCase } from "@/node-lib/curriculum-api-2023/fixtures/teachersSiteMap.fixture";
 import { mockedSiteMapResponse } from "@/node-lib/curriculum-api-2023/fixtures/pupilSiteMap.fixture";
 import { type LessonDownloadsQuery } from "@/node-lib/curriculum-api-2023/queries/lessonDownloads/lessonDownloads.query";
-import lessonMediaFixture from "@/node-lib/curriculum-api-2023/fixtures/lessonMediaClips.fixture";
 import { LessonMediaClipsQueryReturn } from "@/node-lib/curriculum-api-2023/queries/lessonMediaClips/lessonMediaClips.query";
+import lessonMediaClipsFixtures from "@/node-lib/curriculum-api-2023/fixtures/lessonMediaClips.fixture";
 
 const curriculumApi: Pick<
   CurriculumApi,
@@ -123,7 +123,7 @@ const curriculumApi: Pick<
     return lessonDownloadsFixtures();
   }) as jest.Mocked<LessonDownloadsQuery>,
   lessonMediaClips: jest.fn(async () => {
-    return lessonMediaFixture();
+    return lessonMediaClipsFixtures();
   }) as jest.Mocked<LessonMediaClipsQueryReturn>,
   unitListing: jest.fn(async () => {
     return unitListingFixture();
