4a506393534a335077548bfb2781c21e2d39ec1d
Weronika Szalas <freelance.wszalas@thenational.academy>
2025-01-07 10:08:49 +0100

    fix: remove header underline flicker in deploy mode

--- src/components/AppComponents/AppHeaderUnderline/AppHeaderUnderline.tsx
+++ src/components/AppComponents/AppHeaderUnderline/AppHeaderUnderline.tsx
@@ -7,6 +7,7 @@ export const AppHeaderUnderline: FC = () => {
       $position={"absolute"}
       $zIndex={"behind"}
       $height={"all-spacing-1"}
+      $width={"100%"}
       $top={"all-spacing-12"}
       $right={"all-spacing-0"}
       $left={"all-spacing-0"}
@@ -15,6 +16,9 @@ export const AppHeaderUnderline: FC = () => {
         iconName="header-underline"
         $width={"100%"}
         $height={"100%"}
+        $position={"absolute"}
+        $left={"all-spacing-0"}
+        $top={"all-spacing-0"}
         $objectFit={"fill"}
       />
     </OakBox>
