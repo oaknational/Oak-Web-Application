f9d6ba442c7fa697ddce0476a9ff19bf3deeee3b
Natassja Blore <natassja.blore@thenational.academy>
2025-01-14 14:31:00 +0000

    feat: only require onboarding when download attempted

--- src/components/TeacherComponents/UnitDownloadButton/UnitDownloadButton.tsx
+++ src/components/TeacherComponents/UnitDownloadButton/UnitDownloadButton.tsx
@@ -8,12 +8,12 @@ import {
 } from "@oaknational/oak-components";
 import { useFeatureFlagVariantKey } from "posthog-js/react";
 import { z } from "zod";
+import { useRouter } from "next/router";
 
 import useUnitDownloadExistenceCheck from "../hooks/downloadAndShareHooks/useUnitDownloadExistenceCheck";
 
 import createAndClickHiddenDownloadLink from "@/components/SharedComponents/helpers/downloadAndShareHelpers/createAndClickHiddenDownloadLink";
 import { createUnitDownloadLink } from "@/components/SharedComponents/helpers/downloadAndShareHelpers/createDownloadLink";
-import { useRequireOnboarding } from "@/hooks/useRequireOnboarding";
 
 // teacher-unit-downloads experiment A/B test group keys and test values
 const variantKey = z
@@ -28,8 +28,25 @@ const unitButtonSignInText = {
   control: "Download unit", // should not be used
 };
 
-const UnitDownloadSignInButton = ({ variant }: { variant: VariantKey }) => (
-  <SignUpButton>
+// Used when a user is signed in but not onboarded
+const UnitDownloadOnboardButton = () => (
+  <OakPrimaryButton width="fit-content">
+    <OakFlex $alignItems="center" $gap="space-between-xs">
+      <OakTagFunctional label="New" $background="mint" $color="text-primary" />
+      Complete sign up to download this unit
+    </OakFlex>
+  </OakPrimaryButton>
+);
+
+// Used when a user is not signed in
+const UnitDownloadSignInButton = ({
+  variant,
+  redirectUrl,
+}: {
+  variant: VariantKey;
+  redirectUrl: string;
+}) => (
+  <SignUpButton forceRedirectUrl={redirectUrl}>
     <OakPrimaryButton
       iconName={variant === "option-a" ? "download" : undefined}
       isTrailingIcon
@@ -47,6 +64,7 @@ const UnitDownloadSignInButton = ({ variant }: { variant: VariantKey }) => (
   </SignUpButton>
 );
 
+// Used when the user is signed in and onboarded
 const DownloadButton = ({
   onUnitDownloadClick,
   downloadInProgress,
@@ -95,10 +113,17 @@ export type UnitDownloadButtonProps = {
   downloadInProgress: boolean;
 };
 
+/**
+ * Three possible states for the unit download button:
+ * - Sign in to download
+ * - Continue onboarding to download
+ * - Download
+ * If there is no download for this unit, or unit download is disabled, the button will not be shown (ie. legacy units)
+ */
 export default function UnitDownloadButton(props: UnitDownloadButtonProps) {
-  useRequireOnboarding();
   const { unitFileId } = props;
-  const { isSignedIn, isLoaded } = useUser();
+  const { isSignedIn, isLoaded, user } = useUser();
+  const router = useRouter();
   const featureFlag = useFeatureFlagVariantKey("teacher-unit-downloads");
   const parsedFeatureFlagKey = variantKey.safeParse(featureFlag);
 
@@ -135,12 +160,25 @@ export default function UnitDownloadButton(props: UnitDownloadButtonProps) {
 
   const unitDownloadDisabled =
     !parsedFeatureFlagKey.success || parsedFeatureFlagKey.data === "control";
+
   const showDownloadButton = !unitDownloadDisabled && hasCheckedFiles && exists;
+
   const showSignInButton =
     showDownloadButton && !unitDownloadDisabled && isLoaded && !isSignedIn;
 
-  return showSignInButton ? (
-    <UnitDownloadSignInButton variant={parsedFeatureFlagKey.data} />
+  const showOnboardButton =
+    showDownloadButton &&
+    !unitDownloadDisabled &&
+    user &&
+    !user.publicMetadata?.owa?.isOnboarded;
+
+  return showOnboardButton ? (
+    <UnitDownloadOnboardButton />
+  ) : showSignInButton ? (
+    <UnitDownloadSignInButton
+      variant={parsedFeatureFlagKey.data}
+      redirectUrl={`/onboarding?returnTo=${router.asPath}`}
+    />
   ) : showDownloadButton ? (
     <DownloadButton
       onUnitDownloadClick={onUnitDownloadClick}
