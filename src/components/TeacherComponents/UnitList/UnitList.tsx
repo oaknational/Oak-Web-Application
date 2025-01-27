f67bfd34c0844c3246845b50e67c0fcf49cd6f1e
Natassja Blore <natassja.blore@thenational.academy>
2025-01-27 11:29:36 +0000

    feat: hide curriculum download button for rshe

--- src/components/TeacherComponents/UnitList/UnitList.tsx
+++ src/components/TeacherComponents/UnitList/UnitList.tsx
@@ -1,4 +1,10 @@
 import React, { FC, MouseEvent } from "react";
+import { NextRouter, useRouter } from "next/router";
+
+import { UnitOption } from "../UnitListOptionalityCard/UnitListOptionalityCard";
+
+import { getPageItems, getProgrammeFactors } from "./helpers";
+
 import {
   OakFlex,
   OakUnitsContainer,
@@ -8,12 +14,6 @@ import {
   OakAnchorTarget,
   OakBox,
 } from "@oaknational/oak-components";
-import { NextRouter, useRouter } from "next/router";
-
-import { UnitOption } from "../UnitListOptionalityCard/UnitListOptionalityCard";
-
-import { getPageItems, getProgrammeFactors } from "./helpers";
-
 import {
   UnitListItemProps,
   SpecialistListItemProps,
@@ -245,18 +245,25 @@ const UnitList: FC<UnitListProps> = (props) => {
     });
   };
 
+  //TODO: Temporary measure until curriculum downloads are ready for RSHE
+  const hideNewCurriculumDownloadButton = subjectSlug === "rshe-pshe";
+
   const NewUnits = ({ category }: { category?: string }) =>
     newPageItems.length && phaseSlug ? (
       <OakUnitsContainer
         isLegacy={false}
         subject={category ?? subjectSlug}
         phase={phaseSlug}
-        curriculumHref={resolveOakHref({
-          page: "curriculum-units",
-          subjectPhaseSlug: `${linkSubject}-${phaseSlug}${
-            examBoardSlug ? `-${examBoardSlug}` : ""
-          }`,
-        })}
+        curriculumHref={
+          hideNewCurriculumDownloadButton
+            ? null
+            : resolveOakHref({
+                page: "curriculum-units",
+                subjectPhaseSlug: `${linkSubject}-${phaseSlug}${
+                  examBoardSlug ? `-${examBoardSlug}` : ""
+                }`,
+              })
+        }
         showHeader={paginationProps.currentPage === 1}
         unitCards={getUnitCards(newPageItems)}
       />
