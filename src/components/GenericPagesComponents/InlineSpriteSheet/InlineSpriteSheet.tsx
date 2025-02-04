9404c3d19e9b9c2c17c02ecc2b6ac949817b4f7b
Steve <steve@cloudminded.engineering>
2025-02-04 14:57:26 +0000

    test(performance): ignore issue w/ inline sprite sheet

--- src/components/GenericPagesComponents/InlineSpriteSheet/InlineSpriteSheet.tsx
+++ src/components/GenericPagesComponents/InlineSpriteSheet/InlineSpriteSheet.tsx
@@ -6,14 +6,10 @@
 import { FC } from "react";
 
 // eslint-disable-next-line import/no-unresolved
-import InlineSpriteSheetSvg from "@/image-data/generated/inline-sprite.svg";
+// import InlineSpriteSheetSvg from "@/image-data/generated/inline-sprite.svg";
 
 const InlineSpriteSheet: FC = () => {
-  return (
-    <div style={{ display: "none" }}>
-      <InlineSpriteSheetSvg />
-    </div>
-  );
+  return <div style={{ display: "none" }}>{/*<InlineSpriteSheetSvg />*/}</div>;
 };
 
 export default InlineSpriteSheet;
