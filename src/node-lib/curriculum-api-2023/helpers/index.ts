ade0dfb90856daec38487bb045573327c104992b
Natassja Blore <natassja.blore@thenational.academy>
2025-02-04 12:03:41 +0000

    chore: bump mv version

--- src/node-lib/curriculum-api-2023/helpers/index.ts
+++ src/node-lib/curriculum-api-2023/helpers/index.ts
@@ -1,6 +1,6 @@
 import {
   InputMaybe,
-  Published_Mv_Synthetic_Unitvariant_Lessons_By_Keystage_10_0_0_Bool_Exp,
+  Published_Mv_Synthetic_Unitvariant_Lessons_By_Keystage_13_1_0_Bool_Exp,
 } from "../generated/sdk";
 import { RawSyntheticUVLesson } from "../queries/lessonDownloads/rawSyntheticUVLesson.schema";
 import { lessonPathwaySchema } from "../shared.schema";
@@ -58,7 +58,7 @@ export const constructLessonBrowseQuery = ({
   programmeSlug?: string;
   lessonSlug?: string;
 }) => {
-  const browseDataWhere: InputMaybe<Published_Mv_Synthetic_Unitvariant_Lessons_By_Keystage_10_0_0_Bool_Exp> =
+  const browseDataWhere: InputMaybe<Published_Mv_Synthetic_Unitvariant_Lessons_By_Keystage_13_1_0_Bool_Exp> =
     {};
 
   browseDataWhere["lesson_slug"] = { _eq: lessonSlug };
