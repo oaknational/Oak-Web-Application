94096def35bfa0802410fcd4f07416ba3e5f831b
Sonali Singh <sonali.singh@live.co.uk>
2025-02-11 09:49:34 +0000

    chore: fix formatting in file

--- src/pages-helpers/curriculum/docx/builder/10_threadsDetail.ts
+++ src/pages-helpers/curriculum/docx/builder/10_threadsDetail.ts
@@ -35,8 +35,8 @@ function renderUnits(units: Unit[], numbering: { unitNumbering: string }) {
                 <w:sz w:val="24" />
               </w:rPr>
               <w:t xml:space="preserve">${cdata(
-                `Unit ${unit.order + 1}, `,
-              )}</w:t>
+                  `Unit ${unit.order + 1}, `,
+                )}</w:t>
             </w:r>
             <w:r>
               <w:rPr>
@@ -298,17 +298,15 @@ export default async function generate(
       <XML_FRAGMENT>
         ${threadTitle}
         ${contentElements.join("")}
-        ${
-          !isLast
-            ? safeXml`
+        ${!isLast
+          ? safeXml`
               <w:p>
                 <w:r>
                   <w:br w:type="page" />
                 </w:r>
               </w:p>
             `
-            : ""
-        }
+          : ""}
       </XML_FRAGMENT>
     `;
   });
