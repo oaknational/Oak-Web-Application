599f9e023add7a3f683636e24efb80ec8b7afadd
Jamie Blair <info@orangemug.co.uk>
2025-02-25 16:03:39 +0000

    fix: extracted out keystage and added in proper examboard formatting

--- src/components/CurriculumComponents/helpers/curriculumMetadata.test.ts
+++ src/components/CurriculumComponents/helpers/curriculumMetadata.test.ts
@@ -6,10 +6,23 @@ describe("buildCurriculumMetadata", () => {
       metadataType: "title",
       tab: "overview",
       keyStages: ["ks3", "ks4"],
-      subjectSlug: "Math",
+      subjectSlug: "math",
+      subjectTitle: "Math",
       ks4OptionSlug: "aqa",
     });
-    expect(result).toBe("KS3-4 Math AQA Curriculum Plans");
+    expect(result).toBe("KS3-4 math AQA curriculum explainer");
+  });
+
+  it('should return the title string for "overview" tab and "title" metadataType', () => {
+    const result = buildCurriculumMetadata({
+      metadataType: "title",
+      tab: "overview",
+      keyStages: ["ks3", "ks4"],
+      subjectSlug: "design-and-technology",
+      subjectTitle: "Design and technology",
+      ks4OptionSlug: "aqa",
+    });
+    expect(result).toBe("KS3-4 design and technology AQA curriculum explainer");
   });
 
   it('should return the description string for "overview" tab and "description" metadataType', () => {
@@ -17,10 +30,11 @@ describe("buildCurriculumMetadata", () => {
       metadataType: "description",
       tab: "overview",
       keyStages: ["ks1", "ks2"],
-      subjectSlug: "Science",
+      subjectSlug: "science",
+      subjectTitle: "Science",
     });
     expect(result).toBe(
-      "Looking for KS1-2 Science curriculum? We have sequenced curriculum plans, select by key stage. Our free resources are easy to browse and explore.",
+      "Looking for KS1-2 science curriculum? We have sequenced curriculum plans, select by key stage. Our free resources are easy to browse and explore.",
     );
   });
 
@@ -29,10 +43,11 @@ describe("buildCurriculumMetadata", () => {
       metadataType: "title",
       tab: "units",
       keyStages: ["ks3", "ks4"],
-      subjectSlug: "English",
+      subjectSlug: "english",
+      subjectTitle: "English",
       ks4OptionSlug: "edexcel",
     });
-    expect(result).toBe("KS3-4 English Edexcel Curriculum Unit Sequence");
+    expect(result).toBe("KS3-4 English Edexcel curriculum unit sequence");
   });
 
   it('should return the description string for "units" tab and "description" metadataType', () => {
@@ -40,10 +55,11 @@ describe("buildCurriculumMetadata", () => {
       metadataType: "description",
       tab: "units",
       keyStages: ["ks3", "ks4"],
-      subjectSlug: "History",
+      subjectSlug: "history",
+      subjectTitle: "History",
     });
     expect(result).toBe(
-      "Explore our free KS3-4 History curriculum unit sequences, easily select units and topics and view in our interactive tool now.",
+      "Explore our free KS3-4 history curriculum unit sequences, easily select units and topics and view in our interactive tool now.",
     );
   });
 
@@ -52,10 +68,11 @@ describe("buildCurriculumMetadata", () => {
       metadataType: "description",
       tab: "downloads",
       keyStages: ["ks3", "ks4"],
-      subjectSlug: "History",
+      subjectSlug: "history",
+      subjectTitle: "History",
     });
     expect(result).toBe(
-      "Explore our free KS3-4 History curriculum unit downloads.",
+      "Explore our free KS3-4 history curriculum unit downloads.",
     );
   });
 
@@ -64,10 +81,11 @@ describe("buildCurriculumMetadata", () => {
       metadataType: "title",
       tab: "downloads",
       keyStages: ["ks3", "ks4"],
-      subjectSlug: "English",
+      subjectSlug: "english",
+      subjectTitle: "English",
       ks4OptionSlug: "edexcel",
     });
-    expect(result).toBe("KS3-4 English Edexcel Curriculum Downloads");
+    expect(result).toBe("KS3-4 English Edexcel curriculum downloads");
   });
 
   it("should throw if invalid", () => {
@@ -77,7 +95,8 @@ describe("buildCurriculumMetadata", () => {
         metadataType: "foo",
         tab: "downloads",
         keyStages: ["ks3", "ks4"],
-        subjectSlug: "English",
+        subjectSlug: "english",
+        subjectTitle: "English",
         ks4OptionSlug: "edexcel",
       });
     }).toThrow();
