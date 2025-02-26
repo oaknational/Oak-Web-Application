9d46389a844be42a3eeec5ffda1f1dc6267d72f5
Natassja Blore <natassja.blore@thenational.academy>
2025-02-26 10:00:45 +0000

    feat: style registration page for mobile

--- src/components/TeacherComponents/RegistrationLayout/RegistrationLayout.tsx
+++ src/components/TeacherComponents/RegistrationLayout/RegistrationLayout.tsx
@@ -26,7 +26,8 @@ const RegistrationLayout = ({
   return useAlternateLayout ? (
     <OakGrid $width="100%" $height="100%">
       <OakGridArea
-        $colSpan={6}
+        $colSpan={[0, 0, 6]}
+        $display={["none", "none", "flex"]}
         $pa="inner-padding-xl2"
         $background="bg-decorative1-main"
         $justifyContent="center"
@@ -42,7 +43,7 @@ const RegistrationLayout = ({
         </OakFlex>
       </OakGridArea>
       <OakGridArea
-        $colSpan={6}
+        $colSpan={[12, 12, 6]}
         $pa="inner-padding-xl2"
         $background="white"
         $alignItems="center"
@@ -51,8 +52,15 @@ const RegistrationLayout = ({
         <OakFlex
           style={{ width: "400px" }}
           $flexDirection="column"
-          $gap="space-between-m"
+          $gap={["all-spacing-0", "all-spacing-0", "space-between-m"]}
         >
+          <OakBox
+            $display={["block", "block", "none"]}
+            $width="max-content"
+            $ph="inner-padding-xl3"
+          >
+            {bannerSlot}
+          </OakBox>
           {children}
           {termsSlot}
         </OakFlex>
