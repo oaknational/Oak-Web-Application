b916e00b27817c8518585a1e5a795ddf6094af85
Sonali Singh <sonali.singh@live.co.uk>
2025-03-03 12:40:37 +0000

    feat: add curriculum approach navigation evt

--- src/components/GenericPagesComponents/CurriculumTab/CurriculumTab.tsx
+++ src/components/GenericPagesComponents/CurriculumTab/CurriculumTab.tsx
@@ -14,6 +14,7 @@ import Illustration from "@/components/SharedComponents/Illustration";
 import SubjectPhasePicker from "@/components/SharedComponents/SubjectPhasePicker";
 import { resolveOakHref } from "@/common-lib/urls";
 import { SubjectPhasePickerData } from "@/components/SharedComponents/SubjectPhasePicker/SubjectPhasePicker";
+import useAnalytics from "@/context/Analytics/useAnalytics";
 
 type CurriculumDownloadTabProps = {
   curriculumPhaseOptions: SubjectPhasePickerData;
@@ -21,6 +22,7 @@ type CurriculumDownloadTabProps = {
 const CurriculumTab: FC<CurriculumDownloadTabProps> = ({
   curriculumPhaseOptions,
 }) => {
+  const { track } = useAnalytics();
   return (
     <OakBox $background={"mint"} $pv="inner-padding-xl" $ph={"inner-padding-m"}>
       <OakMaxWidth $pv={"inner-padding-xl"}>
@@ -65,6 +67,16 @@ const CurriculumTab: FC<CurriculumDownloadTabProps> = ({
             >
               <OakSecondaryLink
                 href={resolveOakHref({ page: "curriculum-landing-page" })}
+                onClick={() => {
+                  track.curriculumLandingPageAccessed({
+                    platform: "owa", // string ( allowed values: "owa", "aila-beta")
+                    product: "curriculum resources", // string ( allowed values: "ai lesson assistant", "curriculum visualiser", "curriculum resources", "pupil lesson activities", "teacher lesson resources")
+                    engagementIntent: "explore", // string ( allowed values: "explore", "refine", "use", "advocate")
+                    componentType: "oak_curriculum_principles", // string ( allowed values: "hamburger_menu_button", "text_input", "regenerate_response_button", "select_oak_lesson", "type_edit", "lesson_finish_check", "continue_button", "continue_text", "go_to_share_page_button", "example_lesson_button", "homepage_primary_create_a_lesson_button", "homepage_secondary_create_a_lesson_button", "footer_menu_link", "download_button", "homepage_button", "curriculum_visualiser_button", "see_lessons_in_unit_button", "year_group_button", "learning_tier_button", "subject_category_button", "unit_info_button", "lessons_in_unit", "previous_unit_desc", "following_unit_desc", "video", "filter_link", "keystage_keypad_button", "lesson_card", "lesson_download_button", "programme_card", "search_button", "search_result_item", "share_button", "subject_card", "unit_card", "homepage_tab", "landing_page_button", "why_this_why_now", "unit_sequence_tab", "download_tab", "explainer_tab", "aims_and_purpose", "oak_curriculum_principles", "oak_subject_principles", "national_curriculum", "curriculum_delivery", "curiculum_coherence", "recommendations_from_subject_specific_reports", "subject_specific_needs", "our_curriculum_partner")
+                    eventVersion: "2.0.0", // string ( allowed values: "2.0.0")
+                    analyticsUseCase: "Teacher", // string ( allowed values: "Pupil", "Teacher")
+                  });
+                }}
                 iconName="chevron-right"
                 isTrailingIcon
               >
