7067c93996a69922259d9a6e40fbe3ad52938871
Jim Cresswell <jim.cresswell@gmail.com>
2025-03-17 16:48:49 +0000

    chore: more type fixing

---
+++ scripts/build/fetch_secrets/helpers.test.ts
@@ -0,0 +1,22 @@
+import { getSecretNamesFromPublicConfig } from "./helpers.js";
+import { OakConfig } from "../fetch_config/config_types.js";
+
+describe("get_secret_names_from_public_config.js", () => {
+  test("it correctly gets the secret names", () => {
+    const secretNames = getSecretNamesFromPublicConfig({
+      bugsnag: {
+        apiKey: "123abc",
+      },
+      service: {
+        curriculumApiUrl: "https://example.com",
+        requiredSecrets: ["SERVICE_SECRET"],
+      },
+      oak: {
+        appBaseUrl: "http://localhost:3000",
+        requiredSecrets: ["OAK_SECRET_101"],
+      },
+    } as unknown as OakConfig);
+
+    expect(secretNames).toEqual(["SERVICE_SECRET", "OAK_SECRET_101"]);
+  });
+});
