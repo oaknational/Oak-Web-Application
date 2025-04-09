584c5f7361687d831860e4c9af5d1a775b9355c1
Assad Khan <assadkhan@outlook.com>
2025-04-09 12:46:50 +0100

    feat: add storybook cases

--- src/components/CurriculumComponents/CurricYearCard/index.stories.tsx
+++ src/components/CurriculumComponents/CurricYearCard/index.stories.tsx
@@ -1,28 +1,133 @@
 import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
 import { Meta, StoryObj } from "@storybook/react";
 
+import CurriculumUnitCard from "../CurriculumUnitCard/CurriculumUnitCard";
+
 import { CurricYearCard as Component } from ".";
+
+import { Unit } from "@/utils/curriculum/types";
 
 const meta: Meta<typeof Component> = {
   component: Component,
-  argTypes: {},
+  argTypes: {
+    yearTitle: { control: "text" },
+    yearSubheading: { control: "text" },
+    isExamboard: { control: "boolean" },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Component>;
 
-export const CurricYearCard: Story = {
+export const DefaultWithoutSubheading: Story = {
   args: {
     yearTitle: "Year 10",
-    yearSubheading: "Literature",
+    isExamboard: true,
+  },
+  render: function Render(args) {
+    return (
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <Component {...args}>
+          <div>Content for Year 10 Literature</div>
+        </Component>
+      </OakThemeProvider>
+    );
+  },
+};
+
+export const WithSubheading: Story = {
+  args: {
+    yearTitle: "Year 11",
+    yearSubheading: "Physics",
+    isExamboard: true,
+  },
+  render: function Render(args) {
+    return (
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <Component {...args}>
+          <div>Content for Key Stage 4 Science</div>
+        </Component>
+      </OakThemeProvider>
+    );
+  },
+};
+
+export const CorePathway: Story = {
+  args: {
+    yearTitle: "Year 11",
+    yearSubheading: "Core",
     isExamboard: false,
   },
   render: function Render(args) {
     return (
       <OakThemeProvider theme={oakDefaultTheme}>
         <Component {...args}>
-          <div>content goes here...</div>
+          <div>Content for Year 11 Mathematics</div>
         </Component>
+      </OakThemeProvider>
+    );
+  },
+};
+
+export const WithAdditionalContent: Story = {
+  args: {
+    yearTitle: "Year 11",
+    yearSubheading: "Physics",
+    isExamboard: true,
+    additional: (
+      <div style={{ display: "flex", flexDirection: "row", gap: "16px" }}>
+        <CurriculumUnitCard
+          unit={
+            {
+              title: "Mechanics",
+              unit_options: [],
+              connection_prior_unit_description: null,
+              connection_future_unit_description: null,
+              connection_future_unit_title: null,
+              connection_prior_unit_title: null,
+            } as unknown as Unit
+          }
+          index={0}
+          isHighlighted={false}
+          onClick={() => {}}
+        />
+        <CurriculumUnitCard
+          unit={
+            {
+              title: "Waves and Electricity",
+              unit_options: [],
+              connection_prior_unit_description: null,
+              connection_future_unit_description: null,
+              connection_future_unit_title: null,
+              connection_prior_unit_title: null,
+            } as unknown as Unit
+          }
+          index={1}
+          isHighlighted={false}
+          onClick={() => {}}
+        />
+        <CurriculumUnitCard
+          unit={
+            {
+              title: "Quantum Physics",
+              unit_options: [],
+              connection_prior_unit_description: null,
+              connection_future_unit_description: null,
+              connection_future_unit_title: null,
+              connection_prior_unit_title: null,
+            } as unknown as Unit
+          }
+          index={2}
+          isHighlighted={false}
+          onClick={() => {}}
+        />
+      </div>
+    ),
+  },
+  render: function Render(args) {
+    return (
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <Component {...args} />
       </OakThemeProvider>
     );
   },
