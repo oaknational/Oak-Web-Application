2f3a3400fcde92750d46cd7cfc4a0e9fecc32e98
Weronika Szalas <freelance.wszalas@thenational.academy>
2025-04-09 11:32:52 +0200

    feat: add beta leson listing page

---
+++ src/node-lib/curriculum-api-2023/queries/teacherPreviewLessonListing/teacherPreviewLessonListing.query.ts
@@ -0,0 +1,151 @@
+import { ProgrammeFields } from "@oaknational/oak-curriculum-schema";
+
+import {
+  lessonListingPageDataSchema,
+  LessonListingPageData,
+  partialSyntheticUnitvariantLessonsArraySchema,
+  partialSyntheticUnitvariantLessonsSchema,
+} from "@/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.schema";
+import { Sdk } from "@/node-lib/curriculum-api-2023/sdk";
+import OakError from "@/errors/OakError";
+import {
+  LessonListSchema,
+  Actions,
+} from "@/node-lib/curriculum-api-2023/shared.schema";
+import { TeacherPreviewLessonListingQuery } from "@/node-lib/curriculum-api-2023/generated/sdk";
+import { applyGenericOverridesAndExceptions } from "@/node-lib/curriculum-api-2023/helpers/overridesAndExceptions";
+import { getCorrectYear } from "@/node-lib/curriculum-api-2023/helpers/getCorrectYear";
+import { getIntersection } from "@/utils/getIntersection";
+import keysToCamelCase from "@/utils/snakeCaseConverter";
+
+export const getTransformedLessons = (
+  lessons: TeacherPreviewLessonListingQuery["lessons"],
+): LessonListSchema => {
+  return lessons
+    .map((l) => {
+      const lesson = partialSyntheticUnitvariantLessonsSchema.parse(l);
+      const hasCopyrightMaterial =
+        l.lesson_data.copyright_content?.find(
+          (c: { copyright_info: string }) =>
+            c.copyright_info === "This lesson contains copyright material.",
+        ) !== undefined;
+
+      const transformedLesson = {
+        lessonSlug: lesson.lesson_slug,
+        lessonTitle: lesson.lesson_data.title,
+        description:
+          lesson.lesson_data.description ||
+          lesson.lesson_data.pupil_lesson_outcome,
+        pupilLessonOutcome: lesson.lesson_data.pupil_lesson_outcome,
+        expired: Boolean(lesson.lesson_data.deprecated_fields?.expired),
+        quizCount:
+          (lesson.lesson_data.quiz_id_starter ? 1 : 0) +
+          (lesson.lesson_data.quiz_id_exit ? 1 : 0),
+        videoCount: lesson.lesson_data.video_id ? 1 : 0,
+        presentationCount: lesson.lesson_data.asset_id_slidedeck ? 1 : 0,
+        worksheetCount: lesson.lesson_data.asset_id_worksheet ? 1 : 0,
+        hasCopyrightMaterial,
+        orderInUnit: lesson.order_in_unit,
+        lessonCohort: lesson.lesson_data._cohort,
+        actions: (keysToCamelCase(lesson.actions) || null) as Actions,
+      };
+      return transformedLesson;
+    })
+    .sort((a, b) => a.orderInUnit - b.orderInUnit);
+};
+
+type PackagedUnitData = {
+  programmeFields: ProgrammeFields;
+  unitSlug: string;
+  unitvariantId: number;
+  programmeSlug: string;
+  unitTitle: string;
+  programmeSlugByYear: string[];
+};
+
+export const getPackagedUnit = (
+  packagedUnitData: PackagedUnitData,
+  unitLessons: LessonListSchema,
+): LessonListingPageData => {
+  const {
+    programmeFields,
+    unitSlug,
+    unitvariantId,
+    unitTitle,
+    programmeSlug,
+    programmeSlugByYear,
+  } = packagedUnitData;
+
+  console.log("programmeSlugByYear", programmeSlugByYear);
+  console.log("programmeFields", programmeFields);
+
+  const modifiedProgrammeFields = getCorrectYear({
+    programmeSlugByYear,
+    programmeFields,
+  });
+
+  const combinedActions = getIntersection<LessonListSchema[number]["actions"]>(
+    unitLessons.map((lesson) => lesson.actions),
+  ) as Actions;
+
+  return {
+    programmeSlug,
+    keyStageSlug: modifiedProgrammeFields.keystage_slug,
+    keyStageTitle: modifiedProgrammeFields.keystage_description,
+    subjectSlug: modifiedProgrammeFields.subject_slug,
+    subjectTitle: modifiedProgrammeFields.subject,
+    unitSlug,
+    unitvariantId,
+    unitTitle,
+    tierSlug: modifiedProgrammeFields.tier_slug,
+    tierTitle: modifiedProgrammeFields.tier_description,
+    examBoardSlug: modifiedProgrammeFields.examboard_slug,
+    examBoardTitle: modifiedProgrammeFields.examboard,
+    yearSlug: modifiedProgrammeFields.year_slug,
+    yearTitle: modifiedProgrammeFields.year_description,
+    lessons: unitLessons,
+    pathwaySlug: modifiedProgrammeFields.pathway_slug,
+    pathwayTitle: modifiedProgrammeFields.pathway,
+    pathwayDisplayOrder: modifiedProgrammeFields.pathway_display_order,
+    actions: combinedActions,
+  };
+};
+
+const teacherPreviewLessonListingQuery =
+  (sdk: Sdk) => async (args: { programmeSlug: string; unitSlug: string }) => {
+    const res = await sdk.teacherPreviewLessonListing(args);
+
+    const modifiedLessons = applyGenericOverridesAndExceptions<
+      TeacherPreviewLessonListingQuery["lessons"][number]
+    >({
+      journey: "teacher",
+      queryName: "lessonListingQuery",
+      browseData: res.lessons,
+    });
+
+    if (modifiedLessons.length === 0) {
+      throw new OakError({ code: "curriculum-api/not-found" });
+    }
+
+    const parsedModifiedLessons =
+      partialSyntheticUnitvariantLessonsArraySchema.parse(modifiedLessons);
+
+    const unitLessons = getTransformedLessons(parsedModifiedLessons);
+    const packagedUnitData = modifiedLessons.reduce((acc, lesson) => {
+      return {
+        ...acc,
+        programmeFields: lesson.programme_fields,
+        unitSlug: lesson.unit_slug ?? "",
+        unitvariantId: lesson.null_unitvariant_id ?? 0, // it should be unitvariant_id but we don't have it available on this mv
+        programmeSlug: lesson.programme_slug ?? "",
+        unitTitle:
+          lesson.programme_fields.optionality ?? lesson.unit_data.title,
+        programmeSlugByYear: lesson.programme_slug_by_year,
+      };
+    }, {} as PackagedUnitData);
+
+    const packagedUnit = getPackagedUnit(packagedUnitData, unitLessons);
+    return lessonListingPageDataSchema.parse(packagedUnit);
+  };
+
+export default teacherPreviewLessonListingQuery;
---
+++ src/pages/teachers/beta/programmes/[programmeSlug]/units/[unitSlug]/lessons.tsx
@@ -0,0 +1,334 @@
+import React, { useEffect, useState } from "react";
+import {
+  NextPage,
+  GetStaticProps,
+  GetStaticPropsResult,
+  GetStaticPathsResult,
+} from "next";
+import {
+  OakGrid,
+  OakGridArea,
+  OakThemeProvider,
+  oakDefaultTheme,
+  OakMaxWidth,
+} from "@oaknational/oak-components";
+import { useUser } from "@clerk/nextjs";
+
+import AppLayout from "@/components/SharedComponents/AppLayout";
+import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
+import LessonList from "@/components/TeacherComponents/LessonList";
+import usePagination from "@/components/SharedComponents/Pagination/usePagination";
+import {
+  getFallbackBlockingConfig,
+  shouldSkipInitialBuild,
+} from "@/node-lib/isr";
+import { RESULTS_PER_PAGE } from "@/utils/resultsPerPage";
+import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
+import { LessonListingPageData } from "@/node-lib/curriculum-api-2023/queries/lessonListing/lessonListing.schema";
+import getPageProps from "@/node-lib/getPageProps";
+import HeaderListing from "@/components/TeacherComponents/HeaderListing";
+import isSlugLegacy from "@/utils/slugModifiers/isSlugLegacy";
+import { LessonListItemProps } from "@/components/TeacherComponents/LessonListItem";
+import { KeyStageTitleValueType } from "@/browser-lib/avo/Avo";
+import useAnalytics from "@/context/Analytics/useAnalytics";
+import { NEW_COHORT } from "@/config/cohort";
+import { SpecialistLesson } from "@/node-lib/curriculum-api-2023/queries/specialistLessonListing/specialistLessonListing.schema";
+import removeLegacySlugSuffix from "@/utils/slugModifiers/removeLegacySlugSuffix";
+import isSlugEYFS from "@/utils/slugModifiers/isSlugEYFS";
+import PaginationHead from "@/components/SharedComponents/Pagination/PaginationHead";
+import { isLessonListItem } from "@/components/TeacherComponents/LessonListItem/LessonListItem";
+import { useShareExperiment } from "@/pages-helpers/teacher/share-experiments/useShareExperiment";
+import { TeacherShareButton } from "@/components/TeacherComponents/TeacherShareButton/TeacherShareButton";
+import { ExpiringBanner } from "@/components/SharedComponents/ExpiringBanner";
+import { CurriculumTrackingProps } from "@/pages-helpers/teacher/share-experiments/shareExperimentTypes";
+
+export type LessonListingPageProps = {
+  curriculumData: LessonListingPageData;
+};
+
+/**
+ * This function takes a unit and returns an array of lessons with the unit data
+ * embedded in each lesson.
+ *
+ * We do this so that we don't have to send duplicate unit data for each lesson.
+ * This data gets stored in the browser and is used to render the lesson list,
+ * so it's important to keep it as small as possible.
+ */
+function getHydratedLessonsFromUnit(unit: LessonListingPageData) {
+  const { lessons, ...rest } = unit;
+  return lessons.map((lesson) => ({
+    ...lesson,
+    ...rest,
+  }));
+}
+
+const LessonListPage: NextPage<LessonListingPageProps> = ({
+  curriculumData,
+}) => {
+  const {
+    unitSlug,
+    unitvariantId,
+    keyStageTitle,
+    keyStageSlug,
+    unitTitle,
+    subjectTitle,
+    programmeSlug,
+    subjectSlug,
+    actions,
+  } = curriculumData;
+
+  const [showExpiredLessonsBanner, setShowExpiredLessonsBanner] =
+    useState<boolean>(actions?.displayExpiringBanner ?? false);
+
+  const unitListingHref = `/teachers/key-stages/${keyStageSlug}/subjects/${subjectSlug}/programmes`;
+  const { shareUrl, browserUrl, shareActivated } = useShareExperiment({
+    programmeSlug: programmeSlug ?? undefined,
+    source: "lesson-listing",
+    curriculumTrackingProps: {
+      lessonName: null,
+      lessonSlug: null,
+      unitName: unitTitle,
+      unitSlug: unitSlug,
+      subjectSlug,
+      subjectTitle,
+      keyStageSlug,
+      keyStageTitle: keyStageTitle as CurriculumTrackingProps["keyStageTitle"],
+    },
+    overrideExistingShareId: true,
+  });
+
+  useEffect(() => {
+    if (window.location.href !== browserUrl) {
+      window.history.replaceState({}, "", browserUrl);
+    }
+  }, [browserUrl]);
+
+  const teacherShareButton = (
+    <TeacherShareButton
+      variant="primary"
+      shareUrl={shareUrl}
+      shareActivated={shareActivated}
+      label="Share unit with colleague"
+    />
+  );
+
+  const lessons = getHydratedLessonsFromUnit(curriculumData);
+  const hasNewContent = lessons[0]?.lessonCohort === NEW_COHORT;
+  const paginationProps = usePagination({
+    totalResults: lessons.length,
+    pageSize: RESULTS_PER_PAGE,
+    items: lessons,
+  });
+
+  const {
+    currentPageItems,
+    paginationTitle,
+    prevPageUrlObject,
+    nextPageUrlObject,
+    isFirstPage,
+    isLastPage,
+  } = paginationProps;
+
+  const { track } = useAnalytics();
+
+  const trackLessonSelected = ({
+    ...props
+  }: LessonListItemProps | SpecialistLesson) => {
+    if (isLessonListItem(props)) {
+      track.lessonAccessed({
+        platform: "owa",
+        product: "teacher lesson resources",
+        engagementIntent: "use",
+        componentType: "lesson_card",
+        eventVersion: "2.0.0",
+        analyticsUseCase: "Teacher",
+        lessonName: props.lessonTitle,
+        lessonSlug: props.lessonSlug,
+        unitName: unitTitle,
+        unitSlug: unitSlug,
+        keyStageSlug: keyStageSlug,
+        keyStageTitle: keyStageTitle as KeyStageTitleValueType,
+        yearGroupName: props.yearTitle,
+        yearGroupSlug: props.yearSlug,
+      });
+    }
+  };
+
+  const isNew = hasNewContent ?? false;
+  const { isSignedIn } = useUser();
+  const showRiskAssessmentBanner = !!actions?.isPePractical && isSignedIn;
+
+  return (
+    <AppLayout
+      seoProps={{
+        ...getSeoProps({
+          title: `Unit: ${unitTitle} | ${keyStageSlug.toUpperCase()} ${subjectTitle}${paginationTitle}`,
+          description: `Free lessons and teaching resources about ${unitTitle.toLowerCase()}`,
+        }),
+      }}
+      $background="white"
+    >
+      <PaginationHead
+        prevPageUrlObject={prevPageUrlObject}
+        nextPageUrlObject={nextPageUrlObject}
+        isFirstPage={isFirstPage}
+        isLastPage={isLastPage}
+      />
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <HeaderListing
+          breadcrumbs={[
+            {
+              oakLinkProps: {
+                page: "home",
+              },
+              label: "Home",
+            },
+            {
+              oakLinkProps: {
+                page: "subject-index",
+                keyStageSlug,
+              },
+              label: keyStageTitle,
+            },
+            {
+              oakLinkProps: {
+                page: "unit-index",
+                programmeSlug:
+                  subjectSlug === "maths" && !isSlugEYFS(programmeSlug)
+                    ? removeLegacySlugSuffix(programmeSlug)
+                    : programmeSlug,
+              },
+              label: subjectTitle,
+            },
+
+            {
+              oakLinkProps: {
+                page: "lesson-index",
+                unitSlug,
+                programmeSlug: programmeSlug,
+              },
+
+              label: unitTitle,
+              disabled: true,
+            },
+          ]}
+          background={"pink30"}
+          subjectIconBackgroundColor={"pink"}
+          title={unitTitle}
+          programmeFactor={keyStageTitle} // this should be changed to year LESQ-242
+          isNew={isNew}
+          hasCurriculumDownload={isSlugLegacy(programmeSlug)}
+          {...curriculumData}
+          shareButton={teacherShareButton}
+          unitDownloadFileId={
+            unitSlug.endsWith(unitvariantId.toString())
+              ? unitSlug
+              : `${unitSlug}-${unitvariantId}`
+          }
+          onUnitDownloadSuccess={() =>
+            track.unitDownloadInitiated({
+              platform: "owa",
+              product: "teacher lesson resources",
+              engagementIntent: "use",
+              componentType: "unit_download_button",
+              eventVersion: "2.0.0",
+              analyticsUseCase: "Teacher",
+              unitName: unitTitle,
+              unitSlug: unitSlug,
+              keyStageSlug: keyStageSlug,
+              keyStageTitle: keyStageTitle as KeyStageTitleValueType,
+              subjectSlug: subjectSlug,
+              subjectTitle: subjectTitle,
+            })
+          }
+          showRiskAssessmentBanner={showRiskAssessmentBanner}
+        />
+        <OakMaxWidth $ph={"inner-padding-m"}>
+          <OakGrid>
+            <OakGridArea
+              $colSpan={[12, 9]}
+              $mt={["space-between-s", "space-between-m2"]}
+            >
+              <LessonList
+                {...curriculumData}
+                lessonCount={lessons.length}
+                currentPageItems={currentPageItems}
+                paginationProps={paginationProps}
+                headingTag={"h2"}
+                unitTitle={unitTitle}
+                onClick={trackLessonSelected}
+                expiringBanner={
+                  <ExpiringBanner
+                    isOpen={showExpiredLessonsBanner}
+                    isResourcesMessage={true}
+                    onwardHref={unitListingHref}
+                    onClose={() => {
+                      setShowExpiredLessonsBanner(false);
+                    }}
+                  />
+                }
+              />
+            </OakGridArea>
+          </OakGrid>
+        </OakMaxWidth>
+      </OakThemeProvider>
+    </AppLayout>
+  );
+};
+
+export type URLParams = {
+  programmeSlug: string;
+  unitSlug: string;
+};
+
+export const getStaticPaths = async () => {
+  if (shouldSkipInitialBuild) {
+    return getFallbackBlockingConfig();
+  }
+
+  const config: GetStaticPathsResult<URLParams> = {
+    fallback: "blocking",
+    paths: [],
+  };
+  return config;
+};
+
+export const getStaticProps: GetStaticProps<
+  LessonListingPageProps,
+  URLParams
+> = async (context) => {
+  return getPageProps({
+    page: "lesson-listing::getStaticProps",
+    context,
+    getProps: async () => {
+      if (!context.params) {
+        throw new Error("no context.params");
+      }
+      const { programmeSlug, unitSlug } = context.params;
+      if (!programmeSlug || !unitSlug) {
+        throw new Error("unexpected context.params");
+      }
+
+      const curriculumData =
+        await curriculumApi2023.teacherPreviewLessonListing({
+          programmeSlug,
+          unitSlug,
+        });
+
+      if (!curriculumData) {
+        return {
+          notFound: true,
+        };
+      }
+
+      const results: GetStaticPropsResult<LessonListingPageProps> = {
+        props: {
+          curriculumData,
+        },
+      };
+      return results;
+    },
+  });
+};
+
+export default LessonListPage;
