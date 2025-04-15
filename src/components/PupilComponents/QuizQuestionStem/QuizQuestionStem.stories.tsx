7b4ca85be69564f11438c08c8483014667d1545f
Jamie Blair <info@orangemug.co.uk>
2025-04-15 12:29:40 +0100

    chore: comment out some troublesome storybook stories for now

--- src/components/PupilComponents/QuizQuestionStem/QuizQuestionStem.stories.tsx
+++ src/components/PupilComponents/QuizQuestionStem/QuizQuestionStem.stories.tsx
@@ -1,62 +1,62 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
+// import type { Meta, StoryObj } from "@storybook/react";
+// import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
 
-import { QuizQuestionStem } from "./QuizQuestionStem";
+// import { QuizQuestionStem } from "./QuizQuestionStem";
 
-import { quizQuestions } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.new.fixture";
+// import { quizQuestions } from "@/node-lib/curriculum-api-2023/fixtures/quizElements.new.fixture";
 
-const meta: Meta<typeof QuizQuestionStem> = {
-  component: QuizQuestionStem,
-  decorators: [
-    (Story) => (
-      <OakThemeProvider theme={oakDefaultTheme}>
-        <Story />
-      </OakThemeProvider>
-    ),
-  ],
+// const meta: Meta<typeof QuizQuestionStem> = {
+//   component: QuizQuestionStem,
+//   decorators: [
+//     (Story) => (
+//       <OakThemeProvider theme={oakDefaultTheme}>
+//         <Story />
+//       </OakThemeProvider>
+//     ),
+//   ],
 
-  argTypes: {},
-} as Meta;
+//   argTypes: {},
+// } as Meta;
 
-export default meta;
+// export default meta;
 
-type Story = StoryObj<typeof meta>;
+// type Story = StoryObj<typeof meta>;
 
-const starterQuiz = quizQuestions;
-const mcqText = starterQuiz ? starterQuiz[0] : null;
-const mcqStemImage = starterQuiz ? starterQuiz[1] : null;
+// const starterQuiz = quizQuestions;
+// const mcqText = starterQuiz ? starterQuiz[0] : null;
+// const mcqStemImage = starterQuiz ? starterQuiz[1] : null;
 
-/*
- * This is the view users will see on encountering an expired lesson
- *
- */
+// /*
+//  * This is the view users will see on encountering an expired lesson
+//  *
+//  */
 
-export const Default: Story = {
-  render: (args) => {
-    return <QuizQuestionStem {...args} />;
-  },
-  args: {
-    questionStem: mcqText?.questionStem || [],
-    index: 0,
-  },
-};
+// export const Default: Story = {
+//   render: (args) => {
+//     return <QuizQuestionStem {...args} />;
+//   },
+//   args: {
+//     questionStem: mcqText?.questionStem || [],
+//     index: 0,
+//   },
+// };
 
-export const Text: Story = {
-  render: (args) => {
-    return <QuizQuestionStem {...args} />;
-  },
-  args: {
-    questionStem: mcqText?.questionStem || [],
-    index: 0,
-  },
-};
+// export const Text: Story = {
+//   render: (args) => {
+//     return <QuizQuestionStem {...args} />;
+//   },
+//   args: {
+//     questionStem: mcqText?.questionStem || [],
+//     index: 0,
+//   },
+// };
 
-export const Image: Story = {
-  render: (args) => {
-    return <QuizQuestionStem {...args} />;
-  },
-  args: {
-    questionStem: mcqStemImage?.questionStem || [],
-    index: 0,
-  },
-};
+// export const Image: Story = {
+//   render: (args) => {
+//     return <QuizQuestionStem {...args} />;
+//   },
+//   args: {
+//     questionStem: mcqStemImage?.questionStem || [],
+//     index: 0,
+//   },
+// };
