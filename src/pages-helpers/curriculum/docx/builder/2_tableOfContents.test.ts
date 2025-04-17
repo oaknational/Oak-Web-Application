98060fc34c9b2b66988bda45fd92b77390f90701
Jamie Blair <info@orangemug.co.uk>
2025-04-17 13:34:01 +0100

    test: added missing pathways snapshot tests to ./docx/builder/2_tableOfContents

--- src/pages-helpers/curriculum/docx/builder/2_tableOfContents.test.ts
+++ src/pages-helpers/curriculum/docx/builder/2_tableOfContents.test.ts
@@ -5,7 +5,7 @@ import generate from "./2_tableOfContents";
 import { zipToSnapshotObject } from "./helper";
 
 describe("2_tableOfContents", () => {
-  it("simple", async () => {
+  it("without pathways", async () => {
     const zip = await generateEmptyDocx();
     await generate(zip, {
       data: {
@@ -19,6 +19,30 @@ describe("2_tableOfContents", () => {
         ],
       } as unknown as CombinedCurriculumData,
       ks4Options: [],
+    });
+
+    expect(await zipToSnapshotObject(zip.getJsZip())).toMatchSnapshot();
+  });
+
+  it("with pathways", async () => {
+    const zip = await generateEmptyDocx();
+    await generate(zip, {
+      data: {
+        subjectTitle: "Physical education",
+        units: [
+          { year: "7", threads: [] },
+          { year: "8", threads: [] },
+          { year: "9", threads: [] },
+          { year: "10", threads: [], pathway: "core" },
+          { year: "11", threads: [], pathway: "core" },
+          { year: "10", threads: [], pathway: "gcse" },
+          { year: "11", threads: [], pathway: "gcse" },
+        ],
+      } as unknown as CombinedCurriculumData,
+      ks4Options: [
+        { slug: "core", title: "Core" },
+        { slug: "gcse", title: "GCSE" },
+      ],
     });
 
     expect(await zipToSnapshotObject(zip.getJsZip())).toMatchSnapshot();
