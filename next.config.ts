191e3a98898bca6a7bcf40e014b5bafa08032bca
Jim Cresswell <jim.cresswell@gmail.com>
2025-04-17 14:09:14 +0100

    chore(next 15): explicitly exclude swc everything from server bundle

--- next.config.ts
+++ next.config.ts
@@ -115,7 +115,7 @@ export default async (phase: NextConfig["phase"]): Promise<NextConfig> => {
     // Attempt to reduce the size of the build by excluding some packages.
     serverExternalPackages: ["sharp", "@swc/core", "@swc/core-darwin-arm64"],
     outputFileTracingExcludes: {
-      "**/*": ["node_modules/@swc/core-darwin-arm64"],
+      "**/*": ["node_modules/@swc/**/*"],
     },
 
     webpack: function getWebpackConfig(
