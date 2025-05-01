78b9f49712c730aedc80bad23fa87d7270c00a0e
ben <ben.protheroe@thenational.academy>
2025-05-01 17:45:18 +0100

    feat: add test to beta lesson overview

--- src/components/TeacherViews/LessonOverview/LessonOverview.test.tsx
+++ src/components/TeacherViews/LessonOverview/LessonOverview.test.tsx
@@ -1,4 +1,22 @@
-import { getDedupedPupilLessonOutcome } from "./LessonOverview.view";
+import {
+  getDedupedPupilLessonOutcome,
+  LessonOverview,
+} from "./LessonOverview.view";
+
+import lessonOverviewFixture from "@/node-lib/curriculum-api-2023/fixtures/lessonOverview.fixture";
+import renderWithTheme from "@/__tests__/__helpers__/renderWithTheme";
+
+const lessonMediaClipsStarted = jest.fn();
+
+jest.mock("@/context/Analytics/useAnalytics", () => ({
+  __esModule: true,
+  default: () => ({
+    track: {
+      lessonMediaClipsStarted: (...args: []) =>
+        lessonMediaClipsStarted(...args),
+    },
+  }),
+}));
 
 describe("isPupilLessonOutcomeInKeyLearningPoints", () => {
   it("should return plo if the pupil lesson outcome is not in the key learning points", () => {
@@ -25,5 +43,52 @@ describe("isPupilLessonOutcomeInKeyLearningPoints", () => {
       { keyLearningPoint: "pupil lesson outcome" },
     ]);
     expect(result).toBe(null);
+  });
+});
+describe("lessonOverview.view", () => {
+  describe("tracking", () => {
+    it("should call track.lessonMediaClipsStarted when play all is clicked for media clips", () => {
+      const { getByText } = renderWithTheme(
+        <LessonOverview
+          lesson={{
+            ...lessonOverviewFixture(),
+            isSpecialist: false,
+            isCanonical: false,
+            hasMediaClips: true,
+          }}
+          isBeta={false}
+        />,
+      );
+      const playAllButton = getByText("Play all");
+      playAllButton.click();
+      expect(lessonMediaClipsStarted).toHaveBeenCalledWith({
+        analyticsUseCase: "Teacher",
+        componentType: "go_to_media_clips_page_button",
+        engagementIntent: "use",
+        eventVersion: "2.0.0",
+        examBoard: null,
+        keyStageSlug: "ks2",
+        keyStageTitle: "Key Stage 2",
+        learningCycle: undefined,
+        lessonName: "Adverbial complex sentences",
+        lessonReleaseCohort: "2020-2023",
+        lessonReleaseDate: "2024-09-29T14:00:00.000Z",
+        lessonSlug:
+          "lesson-4-in-grammar-1-simple-compound-and-adverbial-complex-sentences",
+        mediaClipsButtonName: "play all",
+        pathway: null,
+        phase: null,
+        platform: "owa",
+        product: "media clips",
+        releaseGroup: "legacy",
+        subjectSlug: "english",
+        subjectTitle: "English",
+        tierName: null,
+        unitName: "Simple, Compound and Adverbial Complex Sentences",
+        unitSlug: "grammar-1-simple-compound-and-adverbial-complex-sentences",
+        yearGroupName: null,
+        yearGroupSlug: null,
+      });
+    });
   });
 });
