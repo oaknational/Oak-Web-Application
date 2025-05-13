b150a091460b02a054eff44f3a9eaed5aa2cfa9e
Natassja Blore <natassja.blore@thenational.academy>
2025-05-13 13:33:49 +0100

    feat: add no saved content component

--- src/components/TeacherComponents/MyLibraryHeader/MyLibraryHeader.tsx
+++ src/components/TeacherComponents/MyLibraryHeader/MyLibraryHeader.tsx
@@ -1,5 +1,4 @@
 import {
-  OakMaxWidth,
   OakFlex,
   OakHeading,
   OakImage,
@@ -10,53 +9,52 @@ import { getCloudinaryImageUrl } from "@/utils/getCloudinaryImageUrl";
 
 export default function MyLibraryHeader() {
   return (
-    <OakMaxWidth $pv="inner-padding-xl">
+    <OakFlex
+      $borderRadius={["border-radius-square", "border-radius-xl"]}
+      $background="bg-decorative1-main"
+      $pv="inner-padding-xl"
+    >
       <OakFlex
-        $borderRadius={["border-radius-square", "border-radius-xl"]}
-        $background="bg-decorative1-main"
+        $flexDirection="column"
+        $justifyContent="center"
+        $gap="space-between-s"
+        $pa={["inner-padding-m", "inner-padding-xl4"]}
+        $pv={["inner-padding-xl", "inner-padding-xl4"]}
       >
-        <OakFlex
-          $flexDirection="column"
-          $justifyContent="center"
-          $gap="space-between-s"
-          $pa={["inner-padding-m", "inner-padding-xl4"]}
-          $pv={["inner-padding-xl", "inner-padding-xl4"]}
-        >
-          <OakFlex>
-            <OakHeading tag="h1" $font={["heading-4", "heading-1"]}>
-              My library
-            </OakHeading>
-            <OakImage
-              $display={["block", "none"]}
-              $height={"all-spacing-15"}
-              $width={"all-spacing-15"}
-              alt="a woman reading a book"
-              src={getCloudinaryImageUrl(
-                "v1747129947/svg-illustrations/reading_fiction_e43vpo.svg",
-              )}
-            />
-          </OakFlex>
-          <OakP $font="body-1">
-            All your content in one handy place. Whether it's units you're
-            teaching this term, or ideas and inspiration for curriculum
-            development and lesson planning. Save what you need to your library.
-          </OakP>
-        </OakFlex>
-        <OakFlex
-          $pr="inner-padding-xl4"
-          $pt="inner-padding-xl4"
-          $display={["none", "block"]}
-        >
+        <OakFlex>
+          <OakHeading tag="h1" $font={["heading-4", "heading-1"]}>
+            My library
+          </OakHeading>
           <OakImage
-            $height={"all-spacing-19"}
-            $width={"all-spacing-19"}
+            $display={["block", "none"]}
+            $height={"all-spacing-15"}
+            $width={"all-spacing-15"}
             alt="a woman reading a book"
             src={getCloudinaryImageUrl(
               "v1747129947/svg-illustrations/reading_fiction_e43vpo.svg",
             )}
           />
         </OakFlex>
+        <OakP $font="body-1">
+          All your content in one handy place. Whether it's units you're
+          teaching this term, or ideas and inspiration for curriculum
+          development and lesson planning. Save what you need to your library.
+        </OakP>
       </OakFlex>
-    </OakMaxWidth>
+      <OakFlex
+        $pr="inner-padding-xl4"
+        $pt="inner-padding-xl4"
+        $display={["none", "block"]}
+      >
+        <OakImage
+          $height={"all-spacing-19"}
+          $width={"all-spacing-19"}
+          alt="a woman reading a book"
+          src={getCloudinaryImageUrl(
+            "v1747129947/svg-illustrations/reading_fiction_e43vpo.svg",
+          )}
+        />
+      </OakFlex>
+    </OakFlex>
   );
 }
--- src/pages/teachers/my-library.tsx
+++ src/pages/teachers/my-library.tsx
@@ -1,3 +1,5 @@
+import { OakMaxWidth } from "@oaknational/oak-components";
+
 import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
 import { Wall } from "@/components/AppComponents/Wall";
 import AppLayout from "@/components/SharedComponents/AppLayout";
@@ -5,11 +7,18 @@ import withFeatureFlag from "@/hocs/withFeatureFlag";
 import { withOnboardingRequired } from "@/hocs/withOnboardingRequired";
 import { withPageAuthRequired } from "@/hocs/withPageAuthRequired";
 import MyLibraryHeader from "@/components/TeacherComponents/MyLibraryHeader/MyLibraryHeader";
+import NoSavedContent from "@/components/TeacherComponents/NoSavedContent/NoSavedContent";
 
 function MyLibraryPage() {
   return (
     <AppLayout seoProps={getSeoProps({ title: "My Library" })}>
-      <MyLibraryHeader />
+      <OakMaxWidth
+        $gap={["space-between-m", "space-between-l"]}
+        $pv="inner-padding-xl"
+      >
+        <MyLibraryHeader />
+        <NoSavedContent />
+      </OakMaxWidth>
     </AppLayout>
   );
 }
