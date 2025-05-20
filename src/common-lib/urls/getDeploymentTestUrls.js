842446f166c967c4dd5da2c0a82c023786e76440
Jamie Blair <info@orangemug.co.uk>
2025-05-20 13:20:00 +0100

    test: added curric unit options modal to pa11y test URLs (CUR-1296)

--- src/common-lib/urls/getDeploymentTestUrls.js
+++ src/common-lib/urls/getDeploymentTestUrls.js
@@ -80,6 +80,7 @@ function getDeploymentTestUrls() {
     "/teachers/curriculum/previous-downloads",
     "/teachers/curriculum",
     "/teachers/curriculum/english-primary/units/speaking-and-listening",
+    "/teachers/curriculum/english-primary/units/king-tut-or-healthy-lifestyle-non-chronological-report",
     "/teachers/curriculum/english-primary/units/king-tut-or-healthy-lifestyle-non-chronological-report-629",
   ];
 }
