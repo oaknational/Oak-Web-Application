24e1179de64931c449616921ebc479d673c4ef63
Reece Charsville <reece@reececharsville.com>
2025-05-22 16:38:00 +0100

    feat: added A/B variants of downloads page that render based on posthog experiment

---
+++ src/components/TeacherComponents/helpers/downloadAndShareHelpers/groupResources.ts
@@ -0,0 +1,48 @@
+import { LessonDownloadsPageData } from "@/node-lib/curriculum-api-2023/queries/lessonDownloads/lessonDownloads.schema";
+
+type Download = LessonDownloadsPageData["downloads"][number];
+type AdditionalFile = LessonDownloadsPageData["additionalFiles"][number];
+
+export const getGroupName = (
+  type: Download["type"] | AdditionalFile["type"],
+): string => {
+  switch (type) {
+    case "presentation":
+    case "lesson-guide-pdf":
+      return "Slide deck";
+    case "intro-quiz-questions":
+    case "intro-quiz-answers":
+    case "exit-quiz-questions":
+    case "exit-quiz-answers":
+      return "Quizzes";
+    case "worksheet-pdf":
+    case "worksheet-pptx":
+      return "Worksheet";
+    case "supplementary-pdf":
+    case "supplementary-docx":
+      return "Additional material";
+    case "curriculum-pdf":
+    case "additional-files":
+    default:
+      return "Lesson files";
+  }
+};
+
+export const groupDownloadResources = (
+  downloads?: LessonDownloadsPageData["downloads"],
+  additionalFiles?: LessonDownloadsPageData["additionalFiles"],
+): Record<string, Array<Download | AdditionalFile>> => {
+  const combined = [...(downloads || []), ...(additionalFiles || [])];
+  return combined.reduce(
+    (
+      all: Record<string, Array<Download | AdditionalFile>>,
+      item: Download | AdditionalFile,
+    ) => {
+      const groupName = getGroupName(item.type);
+      if (!all[groupName]) all[groupName] = [];
+      all[groupName].push(item);
+      return all;
+    },
+    {},
+  );
+};
