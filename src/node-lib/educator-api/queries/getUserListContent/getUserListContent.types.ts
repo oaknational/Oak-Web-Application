c9fc1ba3882c881dc66ee75b32ed6e9ae6566a0d
Natassja Blore <natassja.blore@thenational.academy>
2025-05-27 10:12:19 +0100

    chore: rename types file

---
+++ src/node-lib/educator-api/queries/getUserListContent/getUserListContent.types.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+const lesson_data = z.object({
+  slug: z.string(),
+  title: z.string(),
+  order: z.number(),
+  _state: z.string(),
+});
+
+const users_content_lists = z.object({
+  created_at: z.string(),
+  content: z.object({
+    unit_slug: z.string(),
+    programme_slug: z.string(),
+    browse_mv: z.array(
+      z.object({
+        supplementary_data: z.array(lesson_data),
+        unit_title: z.string(),
+        year: z.string(),
+        keystage: z.string(),
+        subject: z.string(),
+      }),
+    ),
+  }),
+});
+
+const users_content = z.array(
+  z.object({
+    users_content_lists: users_content_lists.nullable(),
+  }),
+);
+
+export type UsersContent = z.infer<typeof users_content>;
+
+export const getUserListContentResponse = z.object({
+  users_content: users_content,
+});
