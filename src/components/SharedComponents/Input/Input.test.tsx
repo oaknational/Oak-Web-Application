04073d554c6399246a1556ad4c4654b9cff83c1a
Jamie Blair <info@orangemug.co.uk>
2025-07-02 12:57:31 +0100

    test: added missing tests to <Input/>

--- src/components/SharedComponents/Input/Input.test.tsx
+++ src/components/SharedComponents/Input/Input.test.tsx
@@ -44,4 +44,31 @@ describe("Input", () => {
     const input = getByRole("textbox");
     expect(input).toHaveAccessibleDescription("Error You done wrong");
   });
+
+  test("withoutMarginBottom removes bottom margin", () => {
+    const { getByRole, rerender } = renderWithTheme(
+      <Input
+        id="test-input"
+        label="A particular label"
+        value="Hello world"
+        onChange={jest.fn()}
+        error="You done wrong"
+      />,
+    );
+    const box1 = getByRole("alert");
+    expect(window.getComputedStyle(box1).marginBottom).toEqual("1.5rem");
+
+    rerender(
+      <Input
+        id="test-input"
+        label="A particular label"
+        value="Hello world"
+        onChange={jest.fn()}
+        error="You done wrong"
+        withoutMarginBottom={true}
+      />,
+    );
+    const box2 = getByRole("alert");
+    expect(window.getComputedStyle(box2).marginBottom).toEqual("0rem");
+  });
 });
