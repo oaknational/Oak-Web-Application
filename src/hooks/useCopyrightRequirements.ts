6040035e262c1fa706a29528d495febdaa4fb3ff
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-07-14 13:35:37 +0100

    feat: add useCopyrightRequirements hook

---
+++ src/hooks/useCopyrightRequirements.ts
@@ -0,0 +1,40 @@
+import { useUser } from "@clerk/nextjs";
+import { useFeatureFlagEnabled } from "posthog-js/react";
+
+interface UseCopyrightRequirementsProps {
+  loginRequired?: boolean;
+  geoRestricted?: boolean;
+}
+
+interface UseCopyrightRequirementsReturn {
+  showSignedOutLoginRequired?: boolean;
+  showSignedOutGeoRestricted?: boolean;
+  showGeoBlocked?: boolean;
+}
+
+export function useCopyrightRequirements({
+  loginRequired,
+  geoRestricted,
+}: UseCopyrightRequirementsProps): UseCopyrightRequirementsReturn {
+  const { user, isSignedIn } = useUser();
+  const featureFlagEnabled = useFeatureFlagEnabled(
+    "teachers-copyright-restrictions",
+  );
+
+  if (!featureFlagEnabled) {
+    return {
+      showGeoBlocked: false,
+      showSignedOutLoginRequired: false,
+      showSignedOutGeoRestricted: false,
+    };
+  }
+
+  const userRegionAuthorised =
+    user?.publicMetadata?.owa?.isRegionAuthorised ?? false;
+
+  return {
+    showSignedOutLoginRequired: !isSignedIn && loginRequired,
+    showSignedOutGeoRestricted: !isSignedIn && geoRestricted,
+    showGeoBlocked: isSignedIn && geoRestricted && !userRegionAuthorised,
+  };
+}
