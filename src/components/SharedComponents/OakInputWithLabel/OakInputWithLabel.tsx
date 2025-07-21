118d07054b95ccac2b7180f1da09f7671f95f97e
Natassja Blore <natassja.blore@thenational.academy>
2025-07-21 15:35:32 +0100

    feat: add component for oak input with label

---
+++ src/components/SharedComponents/OakInputWithLabel/OakInputWithLabel.tsx
@@ -0,0 +1,75 @@
+import {
+  OakFlex,
+  OakBox,
+  OakFieldError,
+  OakJauntyAngleLabel,
+  OakTextInput,
+} from "@oaknational/oak-components";
+import { useState } from "react";
+import { RefCallBack } from "react-hook-form";
+
+export const OakInputWithLabel = ({
+  error,
+  id,
+  autocomplete,
+  label,
+  onChange,
+  onBlur,
+  ref,
+  required,
+  placeholder = "",
+  name,
+}: {
+  label: string;
+  required: boolean;
+  error?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+  ref: RefCallBack;
+  placeholder?: string;
+  id: string;
+  autocomplete?: string;
+  name: string;
+}) => {
+  const [hasFocus, setHasFocus] = useState(false);
+
+  return (
+    <OakFlex $position="relative" $flexDirection="column" ref={ref}>
+      {error && (
+        <OakBox id={error} role="alert" $mv="space-between-s">
+          <OakFieldError>{error}</OakFieldError>
+        </OakBox>
+      )}
+      <OakJauntyAngleLabel
+        label={label + (required ? " (required)" : "")}
+        $color={!!error || hasFocus ? "white" : "black"}
+        htmlFor={"label"}
+        id={label + "-label"}
+        $font={"heading-7"}
+        $background={error ? "red" : hasFocus ? "blue" : "lemon"}
+        $zIndex="in-front"
+        $position="absolute"
+        $top={"-20px"}
+        $left={"5px"}
+        $borderRadius="border-radius-square"
+        data-testid="jaunty-label"
+      />
+      <OakTextInput
+        id={id}
+        data-testid="text-input"
+        placeholder={placeholder}
+        onChange={onChange}
+        onFocus={() => setHasFocus(true)}
+        onBlur={(e) => {
+          onBlur(e);
+          setHasFocus(false);
+        }}
+        $pv="inner-padding-none"
+        wrapperWidth="100%"
+        $height="all-spacing-10"
+        autoComplete={autocomplete}
+        name={name}
+      />
+    </OakFlex>
+  );
+};
