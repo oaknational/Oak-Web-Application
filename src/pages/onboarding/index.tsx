847025be2de61590b0d4072070ed77cefbe368b4
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-07-30 15:48:36 +0100

    feat: implement authentication check and redirect in onboarding page

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,9 +1,33 @@
 import { NextPage } from "next";
-import { oakDefaultTheme, OakThemeProvider } from "@oaknational/oak-components";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import {
+  OakBox,
+  oakDefaultTheme,
+  OakThemeProvider,
+} from "@oaknational/oak-components";
+import { useUser } from "@clerk/nextjs";
 
 import OnboardingView from "@/components/TeacherViews/Onboarding/Onboarding.view";
 
 const OnboardingComponent: NextPage = () => {
+  const { isSignedIn, isLoaded } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoaded && !isSignedIn) {
+      router.replace("/sign-up");
+    }
+  }, [isLoaded, isSignedIn, router]);
+
+  if (!isLoaded || !isSignedIn) {
+    return (
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <OakBox $background={"bg-decorative1-main"} />
+      </OakThemeProvider>
+    );
+  }
+
   return (
     <OakThemeProvider theme={oakDefaultTheme}>
       <OnboardingView />
