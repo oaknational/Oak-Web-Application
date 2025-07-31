2ba9d1350a24ba6cf1a285ba70fe0aeb09ade705
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-07-31 09:19:08 +0100

    refactor: use exiting withPageAuthRequired component

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,33 +1,11 @@
 import { NextPage } from "next";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import {
-  OakBox,
-  oakDefaultTheme,
-  OakThemeProvider,
-} from "@oaknational/oak-components";
-import { useUser } from "@clerk/nextjs";
+import { oakDefaultTheme, OakThemeProvider } from "@oaknational/oak-components";
 
 import OnboardingView from "@/components/TeacherViews/Onboarding/Onboarding.view";
+import { withPageAuthRequired } from "@/hocs/withPageAuthRequired";
+import { Wall } from "@/components/AppComponents/Wall";
 
 const OnboardingComponent: NextPage = () => {
-  const { isSignedIn, isLoaded } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isLoaded && !isSignedIn) {
-      router.replace("/sign-up");
-    }
-  }, [isLoaded, isSignedIn, router]);
-
-  if (!isLoaded || !isSignedIn) {
-    return (
-      <OakThemeProvider theme={oakDefaultTheme}>
-        <OakBox $background={"bg-decorative1-main"} />
-      </OakThemeProvider>
-    );
-  }
-
   return (
     <OakThemeProvider theme={oakDefaultTheme}>
       <OnboardingView />
@@ -35,4 +13,4 @@ const OnboardingComponent: NextPage = () => {
   );
 };
 
-export default OnboardingComponent;
+export default withPageAuthRequired(OnboardingComponent, Wall);
