ead4903ceb75a8e513b0899c216467a4e92323a2
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-08-15 12:02:41 +0100

    feat: hide content from non onboarded users on overview and d/l pages

--- src/components/TeacherComponents/CopyrightRestrictionBanner/CopyrightRestrictionBanner.tsx
+++ src/components/TeacherComponents/CopyrightRestrictionBanner/CopyrightRestrictionBanner.tsx
@@ -90,7 +90,7 @@ const SignedOutCopyrightBanner = ({
                 query: { returnTo: router.asPath },
               })}
             >
-              sign in.
+              complete sign up.
             </OakSecondaryLink>
           ) : (
             <SignUpButton
@@ -222,10 +222,8 @@ const CopyrightRestrictionBanner = (props: CopyrightRestrictionBannerProps) => {
     showSignedOutLoginRequired ||
     showSignedInNotOnboarded;
 
-  const showOnboardingLink =
-    showSignedInNotOnboarded &&
-    !(showSignedOutGeoRestricted || showSignedOutLoginRequired);
-
+  const showOnboardingLink = showSignedInNotOnboarded;
+  console.log(showSignedInNotOnboarded);
   return showGeoBlocked ? (
     <SignedInGeorestrictedBanner isUnit={isUnit} />
   ) : showSignedOutCopyrightBanner ? (
