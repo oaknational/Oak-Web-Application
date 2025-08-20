dbb068d3e7698027f0e4536ed7c24faa8dd7be2d
Natassja Blore <natassja.blore@thenational.academy>
2025-08-20 14:25:48 +0100

    feat: add promo stamp styling and example usage on campaign page

--- src/pages/teachers/beta/campaigns/[campaignSlug].tsx
+++ src/pages/teachers/beta/campaigns/[campaignSlug].tsx
@@ -4,30 +4,56 @@ import {
   GetStaticPropsResult,
   NextPage,
 } from "next";
+import { OakHeading } from "@oaknational/oak-components";
+import { PortableTextComponents } from "@portabletext/react";
 
 import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
 import { CampaignPage } from "@/common-lib/cms-types/campaignPage";
-import Layout from "@/components/AppComponents/Layout";
 import CMSClient from "@/node-lib/cms";
 import getPageProps from "@/node-lib/getPageProps";
 import {
   shouldSkipInitialBuild,
   getFallbackBlockingConfig,
 } from "@/node-lib/isr";
+import { PortableTextWithDefaults } from "@/components/SharedComponents/PortableText";
+import AppLayout from "@/components/SharedComponents/AppLayout";
 
 export type CampaignSinglePageProps = {
   campaign: CampaignPage;
+};
+
+const textComponents: PortableTextComponents = {
+  block: {
+    normal: (props) => {
+      return (
+        <OakHeading $font="heading-2" tag="h2">
+          {props.children}
+        </OakHeading>
+      );
+    },
+  },
 };
 
 const CampaignSinglePage: NextPage<CampaignSinglePageProps> = (props) => {
   console.log("CampaignSinglePage props:", props);
 
   return (
-    <Layout
+    <AppLayout
       seoProps={getSeoProps({
         title: props.campaign.seo?.title || props.campaign.title,
       })}
-     />
+    >
+      {props.campaign.content.map((content) => {
+        if (content.type === "CampaignIntro") {
+          return (
+            <PortableTextWithDefaults
+              value={content.headingPortableTextWithPromo}
+              components={textComponents}
+            />
+          );
+        }
+      })}
+    </AppLayout>
   );
 };
 
