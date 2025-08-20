0273c4208dbb17cc5cf6aad6c377f9c9d5aaf775
Natassja Blore <natassja.blore@thenational.academy>
2025-08-20 15:02:55 +0100

    refactor: use SSR with feature flag and move page out of beta route

--- src/pages/campaigns/[campaignSlug].tsx
+++ src/pages/campaigns/[campaignSlug].tsx
@@ -1,22 +1,16 @@
-import {
-  GetStaticPathsResult,
-  GetStaticProps,
-  GetStaticPropsResult,
-  NextPage,
-} from "next";
+import { GetServerSideProps, GetServerSidePropsResult, NextPage } from "next";
 import { OakHeading } from "@oaknational/oak-components";
 import { PortableTextComponents } from "@portabletext/react";
 
 import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
 import { CampaignPage } from "@/common-lib/cms-types/campaignPage";
 import CMSClient from "@/node-lib/cms";
-import getPageProps from "@/node-lib/getPageProps";
-import {
-  shouldSkipInitialBuild,
-  getFallbackBlockingConfig,
-} from "@/node-lib/isr";
 import { PortableTextWithDefaults } from "@/components/SharedComponents/PortableText";
 import AppLayout from "@/components/SharedComponents/AppLayout";
+import getBrowserConfig from "@/browser-lib/getBrowserConfig";
+import { getFeatureFlag } from "@/node-lib/posthog/getFeatureFlag";
+import { getPosthogIdFromCookie } from "@/node-lib/posthog/getPosthogId";
+import getPageProps from "@/node-lib/getPageProps";
 
 export type CampaignSinglePageProps = {
   campaign: CampaignPage;
@@ -59,32 +53,56 @@ const CampaignSinglePage: NextPage<CampaignSinglePageProps> = (props) => {
 
 type URLParams = { campaignSlug: string };
 
-export const getStaticPaths = async () => {
-  if (shouldSkipInitialBuild) {
-    return getFallbackBlockingConfig();
-  }
+// TODO: Uncomment when ready for static generation
+// export const getStaticPaths = async () => {
+//   if (shouldSkipInitialBuild) {
+//     return getFallbackBlockingConfig();
+//   }
 
-  const campaignPages = await CMSClient.campaigns();
+//   const campaignPages = await CMSClient.campaigns();
 
-  const paths = campaignPages.map((campaign) => ({
-    params: { campaignSlug: campaign.slug },
-  }));
+//   const paths = campaignPages.map((campaign) => ({
+//     params: { campaignSlug: campaign.slug },
+//   }));
 
-  const config: GetStaticPathsResult<URLParams> = {
-    fallback: "blocking",
-    paths,
-  };
-  return config;
-};
+//   const config: GetStaticPathsResult<URLParams> = {
+//     fallback: "blocking",
+//     paths,
+//   };
+//   return config;
+// };
 
-export const getStaticProps: GetStaticProps<
+const posthogApiKey = getBrowserConfig("posthogApiKey");
+
+export const getServerSideProps: GetServerSideProps<
   CampaignSinglePageProps,
   URLParams
 > = async (context) => {
   return getPageProps({
-    page: "campaign-single::getStaticProps",
+    page: "campaign-single::getServerSideProps",
     context,
+    withIsr: false,
     getProps: async () => {
+      const posthogUserId = getPosthogIdFromCookie(
+        context.req.cookies,
+        posthogApiKey,
+      );
+
+      let variantKey: string | boolean | undefined;
+
+      if (posthogUserId) {
+        variantKey = await getFeatureFlag({
+          featureFlagKey: "mythbusting-campaign",
+          posthogUserId,
+        });
+      }
+
+      if (variantKey !== true) {
+        return {
+          notFound: true,
+        };
+      }
+
       const campaignSlug = context.params?.campaignSlug as string;
       const isPreviewMode = context.preview === true;
 
@@ -101,7 +119,7 @@ export const getStaticProps: GetStaticProps<
         };
       }
 
-      const results: GetStaticPropsResult<CampaignSinglePageProps> = {
+      const results: GetServerSidePropsResult<CampaignSinglePageProps> = {
         props: {
           campaign: campaignPageResult,
         },
