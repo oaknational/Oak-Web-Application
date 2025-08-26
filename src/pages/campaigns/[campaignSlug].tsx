e5c1e7bdb4d381418401fef6fc5949b45295035a
Natassja Blore <natassja.blore@thenational.academy>
2025-08-26 14:28:54 +0100

    feat: add desktop and mobile layout for header

--- src/pages/campaigns/[campaignSlug].tsx
+++ src/pages/campaigns/[campaignSlug].tsx
@@ -1,6 +1,5 @@
 import { GetServerSideProps, GetServerSidePropsResult, NextPage } from "next";
 import { OakFlex, OakHeading } from "@oaknational/oak-components";
-import { PortableTextComponents } from "@portabletext/react";
 
 import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
 import { CampaignPage } from "@/common-lib/cms-types/campaignPage";
@@ -11,24 +10,32 @@ import { getFeatureFlag } from "@/node-lib/posthog/getFeatureFlag";
 import { getPosthogIdFromCookie } from "@/node-lib/posthog/getPosthogId";
 import getPageProps from "@/node-lib/getPageProps";
 import CMSImage from "@/components/SharedComponents/CMSImage";
+import KeyStageKeypad from "@/components/SharedComponents/KeyStageKeypad";
+import curriculumApi2023, {
+  KeyStagesData,
+} from "@/node-lib/curriculum-api-2023";
+import SearchForm from "@/components/SharedComponents/SearchForm";
+import useSearch from "@/context/Search/useSearch";
 
 export type CampaignSinglePageProps = {
   campaign: CampaignPage;
+  keyStages: KeyStagesData;
 };
 
-const h2: PortableTextComponents = {
-  block: {
-    normal: (props) => {
-      return (
-        <OakHeading $font="heading-2" tag="h2">
-          {props.children}
-        </OakHeading>
-      );
-    },
-  },
-};
+// const h2: PortableTextComponents = {
+//   block: {
+//     normal: (props) => {
+//       return (
+//         <OakHeading $font="heading-2" tag="h2">
+//           {props.children}
+//         </OakHeading>
+//       );
+//     },
+//   },
+// };
 
 const CampaignSinglePage: NextPage<CampaignSinglePageProps> = (props) => {
+  const { setSearchTerm } = useSearch({});
   return (
     <AppLayout
       seoProps={{
@@ -42,21 +49,69 @@ const CampaignSinglePage: NextPage<CampaignSinglePageProps> = (props) => {
       }}
     >
       <OakFlex
-        $background="bg-decorative5-very-subdued"
-        $width="all-spacing-24"
-        data-testid="campaign-header"
-        $pa="inner-padding-xl6"
+        $alignItems="center"
+        $flexDirection="column"
+        $width="100%"
+        $pv={"inner-padding-xl2"}
+        $ph={["inner-padding-l", "inner-padding-l", "inner-padding-none"]}
       >
-        <OakFlex $flexDirection="column">
-          <OakHeading tag="h1" $font="heading-2">
-            {props.campaign.header.heading}
-          </OakHeading>
+        <OakFlex
+          $background="bg-decorative5-very-subdued"
+          data-testid="campaign-header"
+          $width={["100%", "100%", "all-spacing-24"]}
+          $pa={["inner-padding-xl2", "inner-padding-xl2", "inner-padding-xl6"]}
+          $borderRadius="border-radius-xl"
+          $justifyContent={["center", "center", "initial"]}
+          $gap="space-between-l"
+        >
+          <OakFlex $flexDirection="column" $gap="space-between-m2">
+            <CMSImage
+              $display={["block", "block", "none"]}
+              image={props.campaign.header.image}
+              format={null}
+              $width="none"
+              $objectFit="contain"
+            />
+            <OakHeading tag="h1" $font={["heading-4", "heading-2"]}>
+              {props.campaign.header.heading}
+            </OakHeading>
+            <OakFlex
+              $width={["100%", "100%", "all-spacing-21"]}
+              $flexDirection="column"
+              $gap="space-between-m2"
+            >
+              {/* TODO: restyle keystage keypad btns */}
+              <KeyStageKeypad
+                title="View subjects by key stage"
+                titleTag="h3"
+                keyStages={props.keyStages.keyStages}
+                trackingOnClick={() => {}}
+              />
+              <OakFlex $flexDirection="column" $gap="space-between-s">
+                <OakHeading tag="h3" $font="heading-7">
+                  Or search by keyword
+                </OakHeading>
+                {/* TODO: update anlytics search types */}
+                <SearchForm
+                  searchContext="search"
+                  placeholderText="Search by keyword or topic"
+                  searchTerm=""
+                  handleSubmit={(value) => {
+                    setSearchTerm(value);
+                  }}
+                  analyticsSearchSource={"search page search box"}
+                />
+              </OakFlex>
+            </OakFlex>
+          </OakFlex>
+          <CMSImage
+            $display={["none", "none", "block"]}
+            image={props.campaign.header.image}
+            format={null}
+            $width="none"
+            $objectFit="contain"
+          />
         </OakFlex>
-        <CMSImage
-          image={props.campaign.header.image}
-          $objectFit="contain"
-          format={null}
-        />
       </OakFlex>
     </AppLayout>
   );
@@ -124,7 +179,9 @@ export const getServerSideProps: GetServerSideProps<
         },
       );
 
-      if (!campaignPageResult) {
+      const keyStages = await curriculumApi2023.keyStages();
+
+      if (!campaignPageResult || !keyStages) {
         return {
           notFound: true,
         };
@@ -133,6 +190,7 @@ export const getServerSideProps: GetServerSideProps<
       const results: GetServerSidePropsResult<CampaignSinglePageProps> = {
         props: {
           campaign: campaignPageResult,
+          keyStages,
         },
       };
       return results;
