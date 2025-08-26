23cfeec434d2234096a65abb11581fe0c6c65440
Natassja Blore <natassja.blore@thenational.academy>
2025-08-26 14:57:08 +0100

    feat: update keystage keypad component to use oak components

--- src/browser-lib/fixtures/keyStageKeypad.ts
+++ src/browser-lib/fixtures/keyStageKeypad.ts
@@ -5,19 +5,6 @@ const keyStageKeypad: KeyStageKeypadProps = {
   keyStages: keyStagesFixture().keyStages,
   title: "Select key stage",
   trackingOnClick: () => {},
-  years: [
-    { title: "Year 1", shortCode: "1", slug: "/" },
-    { title: "Year 2", shortCode: "2", slug: "/" },
-    { title: "Year 3", shortCode: "3", slug: "/" },
-    { title: "Year 4", shortCode: "4", slug: "/" },
-    { title: "Year 5", shortCode: "5", slug: "/" },
-    { title: "Year 6", shortCode: "6", slug: "/" },
-    { title: "Year 7", shortCode: "7", slug: "/" },
-    { title: "Year 8", shortCode: "8", slug: "/" },
-    { title: "Year 9", shortCode: "9", slug: "/" },
-    { title: "Year 10", shortCode: "10", slug: "/" },
-    { title: "Year 11", shortCode: "11", slug: "/" },
-  ],
 };
 
 export default keyStageKeypad;
