1eb9541d057b39bcd3ed90e5ae6236455dc3a322
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-08-26 13:50:54 +0100

    feat: add text and image to campaigns page

--- src/pages/campaigns/[campaignSlug].tsx
+++ src/pages/campaigns/[campaignSlug].tsx
@@ -1,5 +1,10 @@
 import { GetServerSideProps, GetServerSidePropsResult, NextPage } from "next";
-import { OakHeading } from "@oaknational/oak-components";
+import {
+  OakBox,
+  OakFlex,
+  OakHeading,
+  OakMaxWidth,
+} from "@oaknational/oak-components";
 import { PortableTextComponents } from "@portabletext/react";
 
 import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
@@ -11,6 +16,8 @@ import getBrowserConfig from "@/browser-lib/getBrowserConfig";
 import { getFeatureFlag } from "@/node-lib/posthog/getFeatureFlag";
 import { getPosthogIdFromCookie } from "@/node-lib/posthog/getPosthogId";
 import getPageProps from "@/node-lib/getPageProps";
+import CMSImage from "@/components/SharedComponents/CMSImage";
+import { Image } from "@/common-lib/cms-types";
 
 export type CampaignSinglePageProps = {
   campaign: CampaignPage;
@@ -20,7 +27,7 @@ const h2: PortableTextComponents = {
   block: {
     normal: (props) => {
       return (
-        <OakHeading $font="heading-2" tag="h2">
+        <OakHeading $font={["heading-5", "heading-4", "heading-2"]} tag="h2">
           {props.children}
         </OakHeading>
       );
@@ -41,16 +48,50 @@ const CampaignSinglePage: NextPage<CampaignSinglePageProps> = (props) => {
         noFollow: true,
       }}
     >
-      {props.campaign.content.map((content) => {
-        if (content.type === "CampaignIntro") {
-          return (
-            <PortableTextWithDefaults
-              value={content.headingPortableTextWithPromo}
-              components={h2}
-            />
-          );
-        }
-      })}
+      <OakMaxWidth>
+        {props.campaign.content.map((content) => {
+          if (content.type === "CampaignIntro") {
+            return (
+              <PortableTextWithDefaults
+                value={content.headingPortableTextWithPromo}
+                components={h2}
+              />
+            );
+          }
+          if (content.type === "CampaignPromoBanner") {
+            // we get an array here but seem to expect an object
+            const media: Image = content.media[0] as Image;
+            return (
+              <OakFlex
+                $flexDirection={["column", "row"]}
+                $width={"100%"}
+                $gap={"space-between-m2"}
+                $borderRadius={"border-radius-xl"}
+                $pv={["inner-padding-xl5"]}
+                $ph={["inner-padding-xl"]}
+              >
+                <OakFlex
+                  $flexDirection={"column"}
+                  $gap={["space-between-m", "space-between-l"]}
+                >
+                  <PortableTextWithDefaults
+                    value={content.headingPortableTextWithPromo}
+                    components={h2}
+                  />
+
+                  <PortableTextWithDefaults
+                    value={content.bodyPortableTextWithPromo}
+                  />
+                </OakFlex>
+
+                <OakBox>
+                  <CMSImage image={media} />
+                </OakBox>
+              </OakFlex>
+            );
+          }
+        })}
+      </OakMaxWidth>
     </AppLayout>
   );
 };
