31881211307247cfe949b1aaa1e00faa84376c22
Natassja Blore <natassja.blore@thenational.academy>
2025-08-28 13:16:28 +0100

    test: add tests for Banners

--- src/components/SharedComponents/Banners/Banners.test.tsx
+++ src/components/SharedComponents/Banners/Banners.test.tsx
@@ -2,10 +2,31 @@ import Banners from "./Banners";
 
 import render from "@/__tests__/__helpers__/renderWithTheme";
 
+const mockFeatureFlagEnabled = jest.fn();
+jest.mock("posthog-js/react", () => {
+  return {
+    useFeatureFlagEnabled: () => mockFeatureFlagEnabled(),
+  };
+});
+
 describe("Banners", () => {
-  test("when cycle 2 enabled", async () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  test("shows curriculum banner when feature flag is disabled", async () => {
     const { container } = render(<Banners />);
     expect(container).toHaveTextContent("See curriculum plans");
+    expect(container).toMatchSnapshot();
+  });
+  test("shows mythbusting banner when mythbusting campaign feature flag enabled", async () => {
+    mockFeatureFlagEnabled.mockReturnValue(true);
+    const { container } = render(<Banners />);
+    expect(container).toHaveTextContent("Learn why");
+    expect(container).toMatchSnapshot();
+  });
+  test("when hideIfFeatureFlagDisabled is true and mythbusting campaign disabled", async () => {
+    const { container } = render(<Banners hideIfFeatureFlagDisabled />);
+    expect(container).toBeEmptyDOMElement();
     expect(container).toMatchSnapshot();
   });
 });
