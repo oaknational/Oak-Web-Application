c0f4b25a6daa6581d6dcb3ed032baee4f0582cc1
Natassja Blore <natassja.blore@thenational.academy>
2025-09-03 13:56:08 +0100

    feat: add stem to QuizQuestionsOrderAnswers and convert order answers

--- src/pages-helpers/shared/lesson-pages/mathjax.ts
+++ src/pages-helpers/shared/lesson-pages/mathjax.ts
@@ -2,33 +2,68 @@ import { LessonOverviewQuizData } from "@/node-lib/curriculum-api-2023/shared.sc
 import convertToMml from "@/utils/mathjax";
 import { LessonOverviewPageData } from "@/node-lib/curriculum-api-2023/queries/lessonOverview/lessonOverview.schema";
 
+// TODO: This should escape text here, rather than just replacing
+export const generateHtml = (text: string) => {
+  return text.replace(/\$\$([^$]|$[^\$])*\$\$/g, (item: string) => {
+    return convertToMml({ math: item });
+  });
+};
+
 export function convertQuestionMath(questions: LessonOverviewQuizData) {
   return questions?.map((question) => {
+    let newQuestionStem = question.questionStem;
     if (question.questionStem) {
-      return {
-        ...question,
-        questionStem: question.questionStem.map((questionStem) => {
-          if (questionStem.type === "text") {
-            // TODO: This should escape text here, rather than just replacing
-            const html = questionStem.text.replace(
-              /\$\$([^$]|$[^\$])*\$\$/g,
-              (item: string) => {
-                return convertToMml({ math: item });
-              },
-            );
-            return {
-              type: questionStem.type,
-              text: questionStem.text,
-              html,
-            };
-          } else {
-            return questionStem;
-          }
-        }),
-      };
-    } else {
-      return question;
+      newQuestionStem = question.questionStem.map((questionStem) => {
+        if (questionStem.type === "text") {
+          const html = generateHtml(questionStem.text);
+          return {
+            type: questionStem.type,
+            text: questionStem.text,
+            html,
+          };
+        } else {
+          return questionStem;
+        }
+      });
     }
+    let newAnswers = question.answers;
+    if (question.answers) {
+      const multipleChoiceAnswers = question.answers["multiple-choice"];
+      const matchAnswers = question.answers["match"];
+      const orderAnswers = question.answers["order"];
+      const shortAnswers = question.answers["short-answer"];
+      if (orderAnswers) {
+        newAnswers = {
+          ...newAnswers,
+          order: orderAnswers.map((item) => {
+            if (item.answer && item.answer.length > 0) {
+              const newItemAnswer = item.answer.map((answerItem) => {
+                if (answerItem.type === "text") {
+                  const html = generateHtml(answerItem.text);
+                  return {
+                    ...answerItem,
+                    html,
+                  };
+                } else {
+                  return answerItem;
+                }
+              });
+              return {
+                ...item,
+                answer: newItemAnswer,
+              };
+            } else {
+              return item;
+            }
+          }),
+        };
+      }
+    }
+    return {
+      ...question,
+      questionStem: newQuestionStem,
+      answers: newAnswers,
+    };
   });
 }
 
