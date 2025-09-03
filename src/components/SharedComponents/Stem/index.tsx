7437f4ee926c3b663a5332ea28c1f46d6d902718
Jamie Blair <info@orangemug.co.uk>
2025-09-03 17:36:18 +0100

    feat: moved over to using portabletext for stems

--- src/components/SharedComponents/Stem/index.tsx
+++ src/components/SharedComponents/Stem/index.tsx
@@ -1,25 +1,184 @@
 import { TextItem } from "@oaknational/oak-curriculum-schema";
+import { OakCodeRenderer, OakSpan } from "@oaknational/oak-components";
+import {
+  MissingComponentHandler,
+  PortableTextTypeComponentProps,
+} from "@portabletext/react";
+import { Fragment } from "react";
+import styled from "styled-components";
 
-import { removeMarkdown } from "@/components/TeacherComponents/LessonOverviewQuizContainer/quizUtils";
+import { PortableTextWithDefaults } from "../PortableText";
+
+import convertToMml from "@/utils/mathjax";
 import { MathJaxWrap } from "@/browser-lib/mathjax/MathJaxWrap";
+import { shortAnswerTitleFormatter } from "@/components/TeacherComponents/LessonOverviewQuizContainer/quizUtils";
+
+type PortableTextSpan = { _type: "span"; text: string };
+type PortableTextText = {
+  _type: "block";
+  style: string;
+  children: (PortableTextMath | PortableTextSpan | PortableTextCodeInline)[];
+};
+type PortableTextCodeBlock = { _type: "codeblock"; text: string };
+type PortableTextCodeInline = { _type: "codeinline"; text: string };
+type PortableTextMath = { _type: "math"; text: string; html: string };
+type PortableTextItem =
+  | PortableTextText
+  | PortableTextCodeBlock
+  | PortableTextSpan;
+
+function ifLastItemIsBlock(out: PortableTextItem[]) {
+  const lastItem = out.at(-1);
+  if (lastItem && lastItem._type === "block") {
+    return lastItem;
+  } else {
+    const newItem: PortableTextText = {
+      _type: "block",
+      style: "normal",
+      children: [],
+    };
+    out.push(newItem);
+    return newItem;
+  }
+}
+
+export function stemToPortableText(text: string) {
+  const regexp =
+    /(?:(\$\$([^$]|$[^$])*\$\$)|(```(?:[\s\S]*?)```|(?:`.*?`))|(.+?))/g;
+  let match = regexp.exec(text);
+  const out: PortableTextItem[] = [];
+  while (match) {
+    if (match[1]) {
+      const block = ifLastItemIsBlock(out);
+      block.children.push({
+        _type: "math",
+        text: match[1],
+        html: convertToMml({ math: match[1] }),
+      });
+    } else if (match[3]) {
+      if (match[3].startsWith("```")) {
+        out.push({
+          _type: "codeblock",
+          text: match[3],
+        });
+      } else {
+        const block = ifLastItemIsBlock(out);
+        block.children.push({
+          _type: "codeinline",
+          text: match[3],
+        });
+      }
+    } else {
+      const block = ifLastItemIsBlock(out);
+      const lastItem = block.children.at(-1);
+      if (lastItem && lastItem._type === "span") {
+        lastItem.text += match[4];
+      } else {
+        block.children.push({
+          _type: "span",
+          text: String(match[4]),
+        });
+      }
+    }
+    match = regexp.exec(text);
+  }
+  return out;
+}
 
 const ENABLE_SSR = true;
 
+const UnderlineSpan = styled.span`
+  display: inline-block;
+  border-bottom: 2px solid black;
+  padding-bottom: 2px;
+  min-width: 48px;
+  position: relative;
+`;
+
+const stemComponents = {
+  types: {
+    codeblock: (
+      props: PortableTextTypeComponentProps<{ text: string; type: "code" }>,
+    ) => {
+      const text = props.value.text;
+
+      const shortAnswerRegex = /\{\{(?:[^{}]|(?!\{\{|\}\})\w)*\}\}/g;
+      if (!text) return "";
+      if (shortAnswerRegex.test(text)) {
+        return (
+          <Fragment>
+            {" "}
+            <UnderlineSpan
+              // This is an empty box with a line under it, to indicate an answer would go here.
+              role="presentation"
+              title="An empty space to write an answer in"
+              data-testid="underline"
+            />{" "}
+            <OakCodeRenderer
+              string={text}
+              $font="code-3"
+              $mt={"space-between-ssx"}
+            />
+          </Fragment>
+        );
+      } else {
+        return (
+          <OakCodeRenderer
+            string={text}
+            $font="code-3"
+            $mt={"space-between-ssx"}
+          />
+        );
+      }
+    },
+    codeinline: (
+      props: PortableTextTypeComponentProps<{ text: string; type: "code" }>,
+    ) => {
+      const text = props.value.text;
+      return (
+        <OakCodeRenderer
+          string={text}
+          $font="code-3"
+          $mt={"space-between-ssx"}
+        />
+      );
+    },
+    math: (
+      props: PortableTextTypeComponentProps<{
+        html: string;
+        text: string;
+        type: "math";
+      }>,
+    ) => {
+      return <OakSpan dangerouslySetInnerHTML={{ __html: props.value.html }} />;
+    },
+  },
+  marks: {},
+};
+
+const logMissingPortableTextComponents: MissingComponentHandler = (
+  message,
+  options,
+) => {
+  console.log(message, {
+    type: options.type,
+    nodeType: options.nodeType,
+  });
+};
+
 type StemProps = {
-  stem: TextItem;
+  stem?: TextItem;
 };
 export function Stem({ stem }: StemProps) {
-  // TODO: This should also deal with rendering <OakCodeRenderer/> as I've removed that else where
-  // See `shortAnswerTitleFormatter` for the logic behind it all
   if (ENABLE_SSR) {
     return (
-      <div
-        dangerouslySetInnerHTML={{
-          __html: removeMarkdown(stem.html),
-        }}
+      <PortableTextWithDefaults
+        value={stem?.portableText ?? []}
+        components={stemComponents}
+        onMissingComponent={logMissingPortableTextComponents}
       />
     );
   } else {
-    return <MathJaxWrap>{stem.text}</MathJaxWrap>;
+    return <MathJaxWrap>{shortAnswerTitleFormatter(stem.text)}</MathJaxWrap>;
   }
 }
--- src/pages-helpers/pupil/lessons-pages/getProps.ts
+++ src/pages-helpers/pupil/lessons-pages/getProps.ts
@@ -17,8 +17,11 @@ import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
 import { invariant } from "@/utils/invariant";
 import { WorksheetInfo } from "@/components/PupilViews/PupilIntro";
 import { getWorksheetInfo } from "@/components/PupilComponents/pupilUtils/getWorksheetInfo";
-import convertToMml from "@/utils/mathjax";
-import { LessonContent, QuizQuestion } from "@/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema";
+import {
+  LessonContent,
+  QuizQuestion,
+} from "@/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema";
+import { stemToPortableText } from "@/components/SharedComponents/Stem";
 
 export type PupilLessonPageURLParams = {
   lessonSlug: string;
@@ -29,44 +32,144 @@ export type PupilLessonPageURLParams = {
 
 type PageType = "preview" | "canonical" | "browse";
 
-export type QuizQuestionWithHtml = QuizQuestion & {
-  questionStem?: QuizQuestion["questionStem"] & {
-    html?: string
-  } | null
-}
-function convertQuestionMath (questions: QuizQuestion[]): QuizQuestionWithHtml[] {
-  return questions.map(question => {
-    if (question.questionStem) {
-      return {
-        ...question,
-        questionStem: question.questionStem.map(questionStem => {
-          if (questionStem.type === "text") {
-            // TODO: This should escape text here, rather than just replacing
-            const html = questionStem.text.replace(/\$\$([^$]|$[^\$])*\$\$/g, (item: string) => {
-              return convertToMml({math: item});
-            })
-            return {
-              type: questionStem.type,
-              text: questionStem.text,
-              html,
-            };
-          } else {
-            return questionStem;
-          }
-        })
-      };
-    } else {
-      return question;
-    }
-  })
-} 
+// TODO: Hack of a type here...
+type Answer = NonNullable<
+  NonNullable<QuizQuestion["answers"]>["multiple-choice"]
+>[number]["answer"][number];
 
-function convertQuizes (content: LessonContent) {
+function convertAnswer<T extends Answer>(answer: T) {
+  if (answer && answer.type === "text") {
+    const portableText = stemToPortableText(answer.text);
+    return {
+      ...answer,
+      portableText,
+    };
+  }
+  return answer;
+}
+
+function convertMultipleChoice(
+  answer: NonNullable<QuizQuestion["answers"]>["multiple-choice"],
+) {
+  return answer?.map((a) => {
+    return {
+      ...a,
+      answer: a.answer.map((b) => convertAnswer(b)),
+    };
+  });
+}
+
+function convertShortAnswer(
+  answer: NonNullable<QuizQuestion["answers"]>["short-answer"],
+) {
+  return answer?.map((a) => {
+    return {
+      ...a,
+      answer: a.answer.map((b) => convertAnswer(b)),
+    };
+  });
+}
+
+function convertOrder(answer: NonNullable<QuizQuestion["answers"]>["order"]) {
+  return answer?.map((a) => {
+    return {
+      ...a,
+      answer: a.answer.map((b) => convertAnswer(b)),
+    };
+  });
+}
+
+function convertMatch(answer: NonNullable<QuizQuestion["answers"]>["match"]) {
+  return answer?.map((a) => {
+    return {
+      correctChoice: a.correctChoice.map((b) => convertAnswer(b)),
+      // HACK
+      matchOption: a.matchOption?.map((b) => convertAnswer(b)),
+    };
+  });
+}
+
+function convertAnswers(answers: QuizQuestion["answers"]) {
+  let out = answers;
+  if (out && out["multiple-choice"]) {
+    out = {
+      ...out,
+      "multiple-choice": convertMultipleChoice(out["multiple-choice"]),
+    };
+  }
+  if (out && out["short-answer"]) {
+    out = {
+      ...out,
+      "short-answer": convertShortAnswer(out["short-answer"]),
+    };
+  }
+  if (out && out["order"]) {
+    out = {
+      ...out,
+      order: convertOrder(out["order"]),
+    };
+  }
+  if (out && out["match"]) {
+    out = {
+      ...out,
+      match: convertMatch(out["match"]),
+    };
+  }
+  return out;
+}
+
+function convertQuestionStem(
+  questionStem: NonNullable<QuizQuestion["questionStem"]>[number],
+) {
+  if (questionStem.type === "text") {
+    // TODO: This should escape text here, rather than just replacing
+    const portableText = stemToPortableText(questionStem.text);
+    return {
+      type: questionStem.type,
+      text: questionStem.text,
+      portableText,
+    };
+  } else {
+    return questionStem;
+  }
+}
+
+export type QuizQuestionWithHtml = QuizQuestion & {
+  questionStem?:
+    | (QuizQuestion["questionStem"] & {
+        html?: string;
+      })
+    | null;
+};
+function convertQuestionMath(
+  questions: QuizQuestion[],
+): QuizQuestionWithHtml[] {
+  return questions.map((question) => {
+    let out = question;
+    if (question.questionStem) {
+      out = {
+        ...out,
+        questionStem: question.questionStem.map((stem) =>
+          convertQuestionStem(stem),
+        ),
+      };
+    }
+    if (question.answers) {
+      out = {
+        ...out,
+        answers: convertAnswers(question.answers),
+      };
+    }
+    return out;
+  });
+}
+
+function convertQuizes(content: LessonContent) {
   return {
     ...content,
     starterQuiz: convertQuestionMath(content.starterQuiz),
     exitQuiz: convertQuestionMath(content.exitQuiz),
-  }
+  };
 }
 
 export const getProps = ({
