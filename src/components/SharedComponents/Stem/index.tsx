b3803191eb69d1083a640dc0c9d785c970392ce3
Jamie Blair <info@orangemug.co.uk>
2025-09-03 12:30:58 +0100

    feat: added ENABLE_SSR to enable/disable SSR

--- src/components/SharedComponents/Stem/index.tsx
+++ src/components/SharedComponents/Stem/index.tsx
@@ -1,6 +1,9 @@
 import { TextItem } from "@oaknational/oak-curriculum-schema";
 
 import { removeMarkdown } from "@/components/TeacherComponents/LessonOverviewQuizContainer/quizUtils";
+import { MathJaxWrap } from "@/browser-lib/mathjax/MathJaxWrap";
+
+const ENABLE_SSR = true;
 
 type StemProps = {
   stem: TextItem;
@@ -8,11 +11,15 @@ type StemProps = {
 export function Stem({ stem }: StemProps) {
   // TODO: This should also deal with rendering <OakCodeRenderer/> as I've removed that else where
   // See `shortAnswerTitleFormatter` for the logic behind it all
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: removeMarkdown(stem.html),
-      }}
-    />
-  );
+  if (ENABLE_SSR) {
+    return (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: removeMarkdown(stem.html),
+        }}
+      />
+    );
+  } else {
+    return <MathJaxWrap>{stem.text}</MathJaxWrap>;
+  }
 }
