ddf65bc5ebd259d0118834126de3c8c5103c3d1e
Jamie Blair <info@orangemug.co.uk>
2025-09-03 12:26:43 +0100

    feat: removed <MathJaxWrap/> and added <Stem/> into <QuizResultQuestionStem/>

--- src/components/PupilComponents/QuizResultQuestionStem/QuizResultQuestionStem.tsx
+++ src/components/PupilComponents/QuizResultQuestionStem/QuizResultQuestionStem.tsx
@@ -7,15 +7,11 @@ import {
 import { CodeRenderWrapper } from "../CodeRendererWrapper/CodeRendererWrapper";
 
 import {
-  removeMarkdown,
-  shortAnswerTitleFormatter,
-} from "@/components/TeacherComponents/LessonOverviewQuizContainer/quizUtils";
-import {
   ImageItem,
   TextItem,
 } from "@/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema";
 import { getSizes } from "@/components/SharedComponents/CMSImage/getSizes";
-import { MathJaxWrap } from "@/browser-lib/mathjax/MathJaxWrap";
+import { Stem } from "@/components/SharedComponents/Stem";
 
 export interface QuizQuestionStemProps {
   questionStem: (ImageItem | TextItem)[];
@@ -30,59 +26,54 @@ export const QuizResultQuestionStem = (props: QuizQuestionStemProps) => {
 
   return (
     <CodeRenderWrapper>
-      <MathJaxWrap>
-        <OakFlex
-          $flexDirection={"column"}
-          $gap={"space-between-m"}
-          $color={"text-primary"}
-          $justifyContent={["center", "flex-start"]}
-        >
-          <OakFlex key="stem-header">
-            {questionStem[0]?.type === "text" && (
+      <OakFlex
+        $flexDirection={"column"}
+        $gap={"space-between-m"}
+        $color={"text-primary"}
+        $justifyContent={["center", "flex-start"]}
+      >
+        <OakFlex key="stem-header">
+          {questionStem[0]?.type === "text" && (
+            <OakSpan
+              key={`q-${displayIndex}-stem-element-0`}
+              $font={"body-2-bold"}
+            >
+              {questionNumber} <Stem stem={questionStem[0]} />
+            </OakSpan>
+          )}
+        </OakFlex>
+
+        {questionStem.map((stemItem, i) => {
+          if (stemItem.type === "text" && i > 0) {
+            return (
               <OakSpan
-                key={`q-${displayIndex}-stem-element-0`}
+                key={`q-${displayIndex}-stem-element-${i}`}
                 $font={"body-2-bold"}
               >
-                {questionNumber}{" "}
-                {shortAnswerTitleFormatter(
-                  removeMarkdown(questionStem[0].text),
-                )}
+                <Stem stem={stemItem} />
               </OakSpan>
-            )}
-          </OakFlex>
-
-          {questionStem.map((stemItem, i) => {
-            if (stemItem.type === "text" && i > 0) {
-              return (
-                <OakSpan
-                  key={`q-${displayIndex}-stem-element-${i}`}
-                  $font={"body-2-bold"}
-                >
-                  {shortAnswerTitleFormatter(removeMarkdown(stemItem.text))}
-                </OakSpan>
-              );
-            } else if (stemItem.type === "image") {
-              return (
-                <OakFlex key={`q-${displayIndex}-stem-element-${i}`}>
-                  {stemItem.imageObject.publicId && (
-                    <OakCloudinaryImage
-                      cloudinaryId={stemItem.imageObject.publicId}
-                      height={stemItem.imageObject.height}
-                      width={stemItem.imageObject.width}
-                      alt={"An image in a quiz"}
-                      $minWidth={"all-spacing-19"}
-                      placeholder="oak"
-                      sizes={getSizes(["100vw", 1200])}
-                      $background={"white"}
-                      role="presentation"
-                    />
-                  )}
-                </OakFlex>
-              );
-            }
-          })}
-        </OakFlex>
-      </MathJaxWrap>
+            );
+          } else if (stemItem.type === "image") {
+            return (
+              <OakFlex key={`q-${displayIndex}-stem-element-${i}`}>
+                {stemItem.imageObject.publicId && (
+                  <OakCloudinaryImage
+                    cloudinaryId={stemItem.imageObject.publicId}
+                    height={stemItem.imageObject.height}
+                    width={stemItem.imageObject.width}
+                    alt={"An image in a quiz"}
+                    $minWidth={"all-spacing-19"}
+                    placeholder="oak"
+                    sizes={getSizes(["100vw", 1200])}
+                    $background={"white"}
+                    role="presentation"
+                  />
+                )}
+              </OakFlex>
+            );
+          }
+        })}
+      </OakFlex>
     </CodeRenderWrapper>
   );
 };
