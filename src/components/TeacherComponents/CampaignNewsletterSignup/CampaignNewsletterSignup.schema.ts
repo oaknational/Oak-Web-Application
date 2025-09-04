953f42c49b66808bf27ee863886389f97b1b3686
Sonali Singh <sonali.singh@live.co.uk>
2025-09-04 09:37:05 +0100

    feat: create error schema for sign up form

---
+++ src/components/TeacherComponents/CampaignNewsletterSignup/CampaignNewsletterSignup.schema.ts
@@ -0,0 +1,55 @@
+import { z } from "zod";
+
+const ERRORS = {
+  school: "Select school, type ‘homeschool’ or tick ‘My school isn’t listed’",
+  email: "Please enter a valid email address",
+  name: "Please enter your name",
+} as const;
+
+export const schoolIdSchema = z
+  .string({
+    errorMap: () => ({
+      message: ERRORS.school,
+    }),
+  })
+  .min(1, ERRORS.school);
+
+export const nameInputSchema = z
+  .string({
+    errorMap: () => ({
+      message: ERRORS.name,
+    }),
+  })
+  .min(1, ERRORS.name);
+
+export const emailRequiredSchema = z
+  .string()
+  .email({
+    message: ERRORS.email,
+  })
+  .or(z.literal(""));
+
+const schoolCombinedSchema = z
+  .object({
+    schoolId: schoolIdSchema.optional(),
+    schoolNotListed: z.boolean(),
+  })
+  .superRefine((values, context) => {
+    if (!values.schoolNotListed && values.schoolId === undefined) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: ERRORS.school,
+        path: ["schoolId"],
+      });
+    }
+  });
+
+const partialSchema = z.object({
+  email: emailRequiredSchema,
+  name: nameInputSchema,
+});
+
+export const newsletterSignupFormSubmitSchema = z.intersection(
+  partialSchema,
+  schoolCombinedSchema,
+);
