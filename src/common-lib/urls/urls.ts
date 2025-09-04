22ffa4ba4c843719cea335c9b6702817275ed35b
Joe Baker <joe.baker@thenational.academy>
2025-09-04 15:28:29 +0100

    chore: new doman

--- src/common-lib/urls/urls.ts
+++ src/common-lib/urls/urls.ts
@@ -662,7 +662,7 @@ export const OAK_PAGES: {
     pageType: "labs",
   }),
   "labs-teaching-materials": createOakPageConfig({
-    url: "https://oak-ai-lesson-assistant-nfrpajtpo.vercel.thenational.academy/aila/teaching-materials",
+    url: "oak-ai-lesson-assistant-git-fix-only-fetch-owa-data-when-7502a8.vercel.thenational.academy/aila/teaching-materials",
     analyticsPageName: "[external] Labs",
     configType: "external",
     pageType: "labs-teaching-materials",
