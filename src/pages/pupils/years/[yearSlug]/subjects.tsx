bab5a8729bd68a008f8cacce08185926dbb1d988
ben <ben.protheroe@thenational.academy>
2025-09-05 09:02:04 +0100

    chore: set pupil pages to noindex, follow

--- src/pages/pupils/years/[yearSlug]/subjects.tsx
+++ src/pages/pupils/years/[yearSlug]/subjects.tsx
@@ -30,6 +30,8 @@ const PupilSubjectListing = (props: SubjectListingPageProps) => {
             title: `${yearDescription} - Subject listing`,
             description: `Subject listing for ${yearDescription}`,
           }),
+          noIndex: true,
+          noFollow: false,
         }}
       >
         <PupilViewsSubjectListing subjects={curriculumData} />
