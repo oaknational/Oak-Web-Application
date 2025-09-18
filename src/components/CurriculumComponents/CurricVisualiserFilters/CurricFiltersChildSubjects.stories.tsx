04f78dad254c9650e05b4c56f3ffc23b08612403
Jamie Blair <info@orangemug.co.uk>
2025-09-18 15:56:54 +0100

    fix: curric component storybooks

--- src/components/CurriculumComponents/CurricVisualiserFilters/CurricFiltersChildSubjects.stories.tsx
+++ src/components/CurriculumComponents/CurricVisualiserFilters/CurricFiltersChildSubjects.stories.tsx
@@ -1,11 +1,18 @@
-import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
 import { Meta, StoryObj } from "@storybook/react";
+import { oakDefaultTheme, OakThemeProvider } from "@oaknational/oak-components";
 
 import { CurricFiltersChildSubjects as Component } from "./CurricFiltersChildSubjects";
 import { ks4Setup } from "./CurricFiltersChildSubjects.fixtures";
 
 const meta: Meta<typeof Component> = {
   component: Component,
+  decorators: [
+    (Story) => (
+      <OakThemeProvider theme={oakDefaultTheme}>
+        <Story />
+      </OakThemeProvider>
+    ),
+  ],
   argTypes: {},
 };
 
@@ -26,10 +33,6 @@ export const CurricFiltersChildSubjects: Story = {
     onChangeFilters: () => {},
   },
   render: function Render(args) {
-    return (
-      <OakThemeProvider theme={oakDefaultTheme}>
-        <Component {...args} />
-      </OakThemeProvider>
-    );
+    return <Component {...args} />;
   },
 };
