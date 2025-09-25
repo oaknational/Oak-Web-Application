6be0f49d559606b13d95bbb6f6a7d32bbcf9bbaf
Joe Baker <joe.baker@thenational.academy>
2025-09-25 16:48:09 +0100

    feat: add title to schema

--- src/pages/api/search/intent.ts
+++ src/pages/api/search/intent.ts
@@ -8,39 +8,39 @@ import OakError from "@/errors/OakError";
 
 const reportError = errorReporter("search-intent");
 
-const DUMMY_DIRECT_MATCH_RESPONSE = {
+export const DUMMY_DIRECT_MATCH_RESPONSE = {
   directMatch: {
-    subject: "maths",
+    subject: { slug: "maths", title: "Maths" },
     keyStage: null,
     year: null,
     examBoard: null,
   },
   suggestedFilters: [
-    { type: "subject", value: "maths" },
-    { type: "key-stage", value: "ks1" },
-    { type: "key-stage", value: "ks2" },
-    { type: "key-stage", value: "ks3" },
-    { type: "key-stage", value: "ks4" },
-    { type: "exam-board", value: "aqa" },
-    { type: "exam-board", value: "edexcel" },
+    { type: "subject", slug: "maths", title: "Maths" },
+    { type: "key-stage", slug: "ks1", title: "Key Stage 1" },
+    { type: "key-stage", slug: "ks2", title: "Key Stage 2" },
+    { type: "key-stage", slug: "ks3", title: "Key Stage 3" },
+    { type: "key-stage", slug: "ks4", title: "Key Stage 4" },
+    { type: "exam-board", slug: "aqa", title: "AQA" },
+    { type: "exam-board", slug: "edexcel", title: "Edexcel" },
   ],
 } satisfies z.infer<typeof searchIntentSchema>;
 
 const DUMMY_AI_RESPONSE = {
   directMatch: null,
   suggestedFilters: [
-    { type: "subject", value: "maths" },
-    { type: "key-stage", value: "ks4" },
-    { type: "exam-board", value: "aqa" },
-    { type: "subject", value: "science" },
+    { type: "subject", slug: "maths", title: "Maths" },
+    { type: "key-stage", slug: "ks4", title: "Key Stage 4" },
+    { type: "exam-board", slug: "aqa", title: "AQA" },
+    { type: "subject", slug: "science", title: "Science" },
   ],
 } satisfies z.infer<typeof searchIntentSchema>;
 
 const handler: NextApiHandler = async (req, res) => {
   let searchTerm: string;
-
+  const { query } = req.query;
   try {
-    const parsed = intentRequestSchema.parse(req.query);
+    const parsed = intentRequestSchema.parse({ searchTerm: query });
     searchTerm = parsed.searchTerm;
   } catch (err) {
     return res.status(400).json({ error: "Invalid search term" });
@@ -49,6 +49,7 @@ const handler: NextApiHandler = async (req, res) => {
   try {
     // TEMP: return direct match response if searchTerm is "maths"
     if (searchTerm === "maths") {
+      console.log("isMaths");
       const payload = searchIntentSchema.parse(DUMMY_DIRECT_MATCH_RESPONSE);
       return res.status(200).json(payload);
     }
@@ -63,7 +64,7 @@ const handler: NextApiHandler = async (req, res) => {
       },
     });
     reportError(error);
-
+    console.log("Error:", error);
     return res.status(500).json({ error: JSON.stringify(error) });
   }
 };
