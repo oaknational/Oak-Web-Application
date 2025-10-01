1d2e1f7b741c67fbb73a18fc4500cf5b34872e02
Jamie Blair <info@orangemug.co.uk>
2025-10-01 11:04:15 +0100

    fix: redirect '/teachers' to homepage '/' and change associated links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,30 @@
-import { GetStaticProps, NextPage } from "next";
+import { GetStaticProps, GetStaticPropsResult, NextPage } from "next";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-import { HomePageProps } from "@/pages-helpers/home/getBlogPosts";
-import TeachersHomePage, {
-  getStaticProps as getStaticPropsTeachers,
-  TeachersHomePageProps,
-} from "@/pages/teachers/index";
+import {
+  HomePageProps as BlogPostProps,
+  getBlogPosts,
+  SerializedPost,
+} from "@/pages-helpers/home/getBlogPosts";
+import AppLayout from "@/components/SharedComponents/AppLayout";
+import TeachersTab from "@/components/GenericPagesComponents/TeachersTab";
+import HomePageTabImageNav from "@/components/GenericPagesComponents/HomePageTabImageNav";
+import Banners from "@/components/SharedComponents/Banners";
+import { HomePageLowerView } from "@/components/GenericPagesViews/HomePageLower/HomePageLower.view";
+import curriculumApi2023, {
+  KeyStagesData,
+} from "@/node-lib/curriculum-api-2023";
+import { HomePage as CmsHomePage } from "@/common-lib/cms-types";
+import getPageProps from "@/node-lib/getPageProps";
 
-const HomePage: NextPage<TeachersHomePageProps> = (props) => {
+export type HomePageProps = BlogPostProps & {
+  curriculumData: KeyStagesData;
+  pageData: CmsHomePage;
+  posts: SerializedPost[];
+};
+
+const HomePage: NextPage<HomePageProps> = (props) => {
   const router = useRouter();
   useEffect(() => {
     // clientside redirect for old tabs implementation
@@ -19,14 +35,68 @@ const HomePage: NextPage<TeachersHomePageProps> = (props) => {
     } else if (window.location.href.includes("#ai")) {
       router.push("/ai");
     } else if (window.location.href.includes("#teachers")) {
-      router.push("/teachers");
+      router.push("/");
     }
   }, [router]);
 
-  return <TeachersHomePage {...props} />;
+  const { curriculumData, posts, pageData } = props;
+
+  const testimonials = pageData?.testimonials;
+  const intro = pageData?.intro;
+  const campaignPromoBanner = pageData.campaignPromoBanner;
+
+  return (
+    <AppLayout
+      seoProps={{
+        title: "Free, time-saving teacher resources | Oak National Academy",
+        description:
+          "Explore our free, time-saving teacher resources from Oak National Academy. Browse and download worksheets, quizzes and slides from KS1 to KS4. ",
+      }}
+      $background={"white"}
+    >
+      <Banners />
+      <HomePageTabImageNav current={"teachers"} />
+      <TeachersTab keyStages={curriculumData.keyStages} aria-current="page" />
+      <HomePageLowerView
+        campaignPromoBanner={campaignPromoBanner}
+        posts={posts}
+        testimonials={testimonials}
+        introVideo={intro}
+      />
+    </AppLayout>
+  );
 };
 
-export const getStaticProps: GetStaticProps<HomePageProps> =
-  getStaticPropsTeachers;
+export const getStaticProps: GetStaticProps<HomePageProps> = async (
+  context,
+) => {
+  return getPageProps({
+    page: "teachers-home-page::getStaticProps",
+    context,
+    getProps: async () => {
+      const { pageData, posts } = await getBlogPosts(
+        context.preview === true,
+        5,
+      );
+
+      if (!pageData) {
+        return {
+          notFound: true,
+        };
+      }
+
+      const curriculumData = await curriculumApi2023.keyStages();
+
+      const results: GetStaticPropsResult<HomePageProps> = {
+        props: {
+          pageData,
+          curriculumData,
+          posts,
+        },
+      };
+      return results;
+    },
+  });
+};
 
 export default HomePage;
--- src/pages/teachers/index.tsx
+++ src/pages/teachers/index.tsx
@@ -1,88 +1,15 @@
-import { GetStaticProps, GetStaticPropsResult, NextPage } from "next";
+import { NextPage } from "next";
 
-import AppLayout from "@/components/SharedComponents/AppLayout";
-import TeachersTab from "@/components/GenericPagesComponents/TeachersTab";
-import HomePageTabImageNav from "@/components/GenericPagesComponents/HomePageTabImageNav";
-import Banners from "@/components/SharedComponents/Banners";
-import { HomePageLowerView } from "@/components/GenericPagesViews/HomePageLower/HomePageLower.view";
-import getPageProps from "@/node-lib/getPageProps";
-import curriculumApi2023, {
-  KeyStagesData,
-} from "@/node-lib/curriculum-api-2023";
-import {
-  getBlogPosts,
-  SerializedPost,
-} from "@/pages-helpers/home/getBlogPosts";
-import { HomePage } from "@/common-lib/cms-types";
-
-export type TeachersHomePageProps = HomePageProps & {
-  curriculumData: KeyStagesData;
+const TeachersHomePage: NextPage = () => {
+  return <div />;
 };
 
-export type HomePageProps = {
-  pageData: HomePage;
-  posts: SerializedPost[];
-};
-
-const TeachersHomePage: NextPage<TeachersHomePageProps> = (props) => {
-  const { curriculumData, posts, pageData } = props;
-
-  const testimonials = pageData?.testimonials;
-  const intro = pageData?.intro;
-  const campaignPromoBanner = pageData.campaignPromoBanner;
-
-  return (
-    <AppLayout
-      seoProps={{
-        title: "Free, time-saving teacher resources | Oak National Academy",
-        description:
-          "Explore our free, time-saving teacher resources from Oak National Academy. Browse and download worksheets, quizzes and slides from KS1 to KS4. ",
-      }}
-      $background={"white"}
-    >
-      <Banners />
-      <HomePageTabImageNav current={"teachers"} />
-      <TeachersTab keyStages={curriculumData.keyStages} aria-current="page" />
-      <HomePageLowerView
-        campaignPromoBanner={campaignPromoBanner}
-        posts={posts}
-        testimonials={testimonials}
-        introVideo={intro}
-      />
-    </AppLayout>
-  );
-};
-
-export const getStaticProps: GetStaticProps<HomePageProps> = async (
-  context,
-) => {
-  return getPageProps({
-    page: "teachers-home-page::getStaticProps",
-    context,
-    getProps: async () => {
-      const { pageData, posts } = await getBlogPosts(
-        context.preview === true,
-        5,
-      );
-
-      if (!pageData) {
-        return {
-          notFound: true,
-        };
-      }
-
-      const curriculumData = await curriculumApi2023.keyStages();
-
-      const results: GetStaticPropsResult<TeachersHomePageProps> = {
-        props: {
-          pageData,
-          curriculumData,
-          posts,
-        },
-      };
-      return results;
+export const getStaticProps = async () => {
+  return {
+    redirect: {
+      destination: "/",
     },
-  });
+  };
 };
 
 export default TeachersHomePage;
