5d66fd6ec4b70c207e3aa638a07e08d468e8932c
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-10-14 10:30:10 +0100

    chore: move gateway url to secrets

--- scripts/build/write_env_file/index.ts
+++ scripts/build/write_env_file/index.ts
@@ -197,7 +197,7 @@ async function main() {
     AI_SEARCH_ENABLED:
       process.env.AI_SEARCH_ENABLED || oakConfig.oak.aiSearchEnabled,
     AI_GATEWAY_URL:
-      process.env.AI_GATEWAY_URL || oakConfig.aiGateway.gatewayUrl,
+      process.env.AI_GATEWAY_URL || secretsFromNetwork.AI_GATEWAY_URL,
     AI_GATEWAY_API_KEY:
       process.env.AI_GATEWAY_API_KEY || secretsFromNetwork.AI_GATEWAY_API_KEY,
 
