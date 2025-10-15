e3d9d052980084be05830319c581c8de63fe10fd
Natassja Blore <natassja.blore@thenational.academy>
2025-10-15 17:07:33 +0100

    fix: handle edge cases with search query matches

--- src/components/TeacherComponents/SearchSuggestionBanner/getSearchSuggestionBannerProps.test.ts
+++ src/components/TeacherComponents/SearchSuggestionBanner/getSearchSuggestionBannerProps.test.ts
@@ -6,6 +6,9 @@ import { SearchIntent } from "@/common-lib/schemas/search-intent";
 const mathsDescription = OAK_SUBJECTS.find(
   (s) => s.slug == "maths",
 )?.description;
+const citizenshipDescription = OAK_SUBJECTS.find(
+  (s) => s.slug == "citizenship",
+)?.description;
 
 describe("getSearchSuggestionBannerProps", () => {
   test("it returns no suggestion if no direct subject match", () => {
@@ -108,12 +111,12 @@ describe("getSearchSuggestionBannerProps", () => {
       },
       suggestedFilters: [
         {
-          title: "Key stage 1",
+          title: "Key Stage 1",
           slug: "ks1",
           type: "key-stage",
         },
         {
-          title: "Key stage 4",
+          title: "Key Stage 4",
           slug: "ks4",
           type: "key-stage",
         },
@@ -131,11 +134,77 @@ describe("getSearchSuggestionBannerProps", () => {
       links: [
         {
           keystageSlug: "ks1",
-          keystageTitle: "Key stage 1",
+          keystageTitle: "Key Stage 1",
         },
         {
           keystageSlug: "ks4",
-          keystageTitle: "Key stage 4",
+          keystageTitle: "Key Stage 4",
+        },
+      ],
+    });
+  });
+  test("it returns valid keystages for the subject", () => {
+    const searchIntent: SearchIntent = {
+      directMatch: {
+        subject: { slug: "citizenship", title: "Citizenship" },
+        keyStage: { slug: "ks1", title: "Key Stage 1" },
+        examBoard: null,
+        year: null,
+      },
+      suggestedFilters: [
+        {
+          title: "Key Stage 1",
+          slug: "ks1",
+          type: "key-stage",
+        },
+      ],
+    };
+    const result = getSearchSuggestionBannerProps(searchIntent);
+    expect(result).toMatchObject({
+      title: "Citizenship",
+      body: citizenshipDescription,
+      links: [
+        {
+          keystageSlug: "ks3",
+          keystageTitle: "Key Stage 3",
+        },
+        {
+          keystageSlug: "ks4",
+          keystageTitle: "Key Stage 4",
+        },
+      ],
+    });
+  });
+  test("it returns valid keystages for direct matches without suggestions", () => {
+    const searchIntent: SearchIntent = {
+      directMatch: {
+        subject: { slug: "maths", title: "Maths" },
+        keyStage: null,
+        examBoard: null,
+        year: { slug: "year-1", title: "Year 1" },
+      },
+      suggestedFilters: [],
+    };
+    const result = getSearchSuggestionBannerProps(searchIntent);
+    expect(result).toMatchObject({
+      title: "Maths",
+      body: mathsDescription,
+      links: [
+        {
+          keystageSlug: "ks1",
+          keystageTitle: "Key Stage 1",
+        },
+        {
+          keystageSlug: "ks2",
+          keystageTitle: "Key Stage 2",
+        },
+        {
+          keystageSlug: "ks3",
+          keystageTitle: "Key Stage 3",
+        },
+        {
+          keystageSlug: "ks4",
+          keystageTitle: "Key Stage 4",
         },
       ],
     });
--- src/components/TeacherComponents/SearchSuggestionBanner/getSearchSuggestionBannerProps.ts
+++ src/components/TeacherComponents/SearchSuggestionBanner/getSearchSuggestionBannerProps.ts
@@ -7,6 +7,10 @@ export function getSearchSuggestionBannerProps(props: SearchIntent) {
   const { directMatch, suggestedFilters } = props;
   let suggestion: SearchSuggestionBannerProps | null = null;
 
+  const suggestedKeystageFilters = suggestedFilters.filter(
+    (filter) => filter.type === "key-stage",
+  );
+
   if (directMatch?.subject) {
     const description = OAK_SUBJECTS.find(
       (subject) => subject.slug === directMatch.subject?.slug,
@@ -19,24 +23,52 @@ export function getSearchSuggestionBannerProps(props: SearchIntent) {
       links: [],
     };
 
-    if (directMatch?.keyStage || directMatch?.examBoard) {
-      suggestion.metadata = directMatch?.keyStage?.title ?? "Key stage 4";
-      suggestion.links.push({
-        keystageSlug: directMatch?.keyStage?.slug ?? "ks4",
-        keystageTitle: directMatch?.keyStage?.title ?? "Key stage 4",
-      });
-    } else {
-      const suggestedKeystages = suggestedFilters.filter(
-        (filter) => filter.type === "key-stage",
-      );
+    const subjectData = OAK_SUBJECTS.find(
+      (subject) => subject.slug === directMatch.subject?.slug,
+    );
 
-      suggestedKeystages.forEach((keystage) => {
+    const hasKeystageMatch = directMatch?.keyStage || directMatch?.examBoard;
+    const isValidKeystageForSubject = subjectData?.keyStages.some((ks) => {
+      if (directMatch.keyStage) {
+        return ks.slug === directMatch.keyStage.slug;
+      } else {
+        return ks.slug === "ks4";
+      }
+    });
+
+    if (hasKeystageMatch) {
+      if (isValidKeystageForSubject) {
+        suggestion.links.push({
+          keystageSlug: directMatch?.keyStage?.slug ?? "ks4",
+          keystageTitle: directMatch?.keyStage?.title ?? "Key stage 4",
+        });
+      } else {
+        subjectData?.keyStages.forEach((ks) => {
+          suggestion?.links.push({
+            keystageSlug: ks.slug,
+            keystageTitle: ks.title,
+          });
+        });
+      }
+    } else if (suggestedKeystageFilters.length > 0) {
+      suggestedKeystageFilters.forEach((keystage) => {
         suggestion?.links.push({
           keystageSlug: keystage.slug,
           keystageTitle: keystage.title,
         });
       });
+    } else {
+      subjectData?.keyStages.forEach((ks) => {
+        suggestion?.links.push({
+          keystageSlug: ks.slug,
+          keystageTitle: ks.title,
+        });
+      });
     }
+  }
+
+  if (suggestion?.links.length === 1) {
+    suggestion.metadata = suggestion.links[0]?.keystageTitle;
   }
 
   return suggestion;
