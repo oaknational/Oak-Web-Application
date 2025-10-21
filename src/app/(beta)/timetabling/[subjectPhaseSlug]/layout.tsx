f98d2ba59d1b36976f881c886077318ddbcc08fc
Assad Khan <assadkhan@outlook.com>
2025-10-21 23:32:12 +0100

    refactor: remove provider

--- src/app/(beta)/timetabling/[subjectPhaseSlug]/layout.tsx
+++ src/app/(beta)/timetabling/[subjectPhaseSlug]/layout.tsx
@@ -1,7 +1,5 @@
 import { notFound } from "next/navigation";
 
-import TimetablingProvider from "./TimetablingProvider";
-
 import { useFeatureFlag } from "@/utils/featureFlags";
 
 export default async function Layout({
@@ -14,5 +12,5 @@ export default async function Layout({
     return notFound();
   }
 
-  return <TimetablingProvider>{children}</TimetablingProvider>;
+  return <>{children}</>;
 }
