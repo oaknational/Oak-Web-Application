c04ab40915abc136bb230ad8e1e48619a66a0dfe
Jamie Blair <info@orangemug.co.uk>
2025-10-23 13:24:24 +0100

    feat: added component <CurricLessonItem/>

---
+++ src/components/CurriculumComponents/CurricLessonItem/index.tsx
@@ -0,0 +1,53 @@
+import {
+  OakFlex,
+  OakSecondaryLink,
+  OakSpan,
+} from "@oaknational/oak-components";
+
+type CurricLessonItemProps = {
+  number: number;
+  title: string;
+  href?: string;
+};
+export function CurricLessonItem({
+  number,
+  title,
+  href,
+}: CurricLessonItemProps) {
+  return (
+    <OakFlex $borderRadius={"border-radius-s"} $overflow={"hidden"}>
+      <OakFlex
+        $background={"pink"}
+        $ph={"inner-padding-l"}
+        $alignItems={"center"}
+        $font={"heading-6"}
+        data-testid="curric-lesson-item-number"
+      >
+        {number}
+      </OakFlex>
+      <OakFlex
+        $flexGrow={1}
+        $alignItems={"center"}
+        $background={"bg-decorative4-very-subdued"}
+        $ph={"inner-padding-l"}
+        $pv={"inner-padding-s"}
+        $gap={"all-spacing-6"}
+      >
+        <OakFlex $flexGrow={1} data-testid="curric-lesson-title">
+          <OakSpan $font={"heading-light-7"}>{title}</OakSpan>
+        </OakFlex>
+        {href && (
+          <OakFlex>
+            <OakSecondaryLink
+              href={href}
+              isTrailingIcon={true}
+              iconName="arrow-right"
+            >
+              Go to lesson
+            </OakSecondaryLink>
+          </OakFlex>
+        )}
+      </OakFlex>
+    </OakFlex>
+  );
+}
