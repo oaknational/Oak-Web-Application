1750c7b9ac40d31e1b71de235746f5ecc9557446
ben <ben.protheroe@thenational.academy>
2025-10-23 16:37:26 +0100

    fix: review changes

--- src/node-lib/pupil-api/pupilDataStore/index.ts
+++ src/node-lib/pupil-api/pupilDataStore/index.ts
@@ -1,6 +1,6 @@
-import { getLessonAttempt } from "@/node-lib/pupil-api/functions/getLessonAttempt";
-import { logLessonAttempt } from "@/node-lib/pupil-api/functions/logLessonAttempt";
-import { getTeacherNote } from "@/node-lib/pupil-api/functions/getTeacherNote";
+import { getLessonAttempt } from "@/node-lib/pupil-api/lesson-attempt/getLessonAttempt";
+import { logLessonAttempt } from "@/node-lib/pupil-api/lesson-attempt/logLessonAttempt";
+import { getTeacherNote } from "@/node-lib/pupil-api/teacher-notes/getTeacherNote";
 
 export const pupilDatastore = {
   getLessonAttempt,
