1dc79a6b253bf5d211e59e054fcff2fe7b93f45c
Dorothy Yau <dorothy.yau@apolitical.co>
2025-12-11 15:50:56 +0000

    feat: update useSelectedArea to use next/navigation

--- src/hooks/useSelectedArea.ts
+++ src/hooks/useSelectedArea.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 import {
   SelectedArea,
@@ -7,29 +7,19 @@ import {
 } from "@/components/AppComponents/AppHeader/AppHeader";
 
 const useSelectedArea = () => {
-  const { asPath, pathname } = useRouter();
-  const serverBasedSelectedArea = pathname?.includes("/pupils")
-    ? siteAreas.pupils
-    : siteAreas.teachers;
+  const pathname = usePathname();
 
   const [clientArea, setClientArea] = useState<SelectedArea>(
-    serverBasedSelectedArea,
+    siteAreas.teachers,
   );
 
   useEffect(() => {
-    // This prevents a hydration error. The server contains information on the requested path, but not when it's a 404 or 500 error.
-    // For example if we are on /pupils/thisPageDoesNotExist, on the server pathname is /404 and asPath is undefined .
-    // We have to wait until we are client-side and then we can query asPath to get the url that we are on.
-    // For info - One solution to this is to set getServerSideProps on the 404 & 500 pages. This means the asPath would be set and the pages would be ssr.
-    // But that also makes the 404 & 500 pages a larger load on the server in a large incident, therefore I think it's better to keep them as fully static pages.
-    const clientBasedSelectedArea = asPath?.includes("/pupils")
+    // This prevents a hydration error. See: https://nextjs.org/docs/app/api-reference/functions/use-pathname#avoid-hydration-mismatch-with-rewrites
+    const clientArea = pathname?.includes("/pupils")
       ? siteAreas.pupils
       : siteAreas.teachers;
-
-    if (serverBasedSelectedArea !== clientBasedSelectedArea) {
-      setClientArea(clientBasedSelectedArea);
-    }
-  }, [asPath, serverBasedSelectedArea]);
+    setClientArea(clientArea);
+  }, [pathname]);
 
   return clientArea;
 };
