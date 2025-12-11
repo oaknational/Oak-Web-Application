dcdeed4540342ddb32a5264a49eb5e4fc2501fda
Dorothy Yau <dorothy.yau@apolitical.co>
2025-12-11 17:11:25 +0000

    fix: update useselectedarea to prevent flashing

--- src/hooks/useSelectedArea.ts
+++ src/hooks/useSelectedArea.ts
@@ -8,18 +8,20 @@ import {
 
 const useSelectedArea = () => {
   const pathname = usePathname();
+  const initialClientArea = pathname?.includes("/pupils")
+    ? siteAreas.pupils
+    : siteAreas.teachers;
 
-  const [clientArea, setClientArea] = useState<SelectedArea>(
-    siteAreas.teachers,
-  );
-
+  const [clientArea, setClientArea] = useState<SelectedArea>(initialClientArea);
   useEffect(() => {
     // This prevents a hydration error. See: https://nextjs.org/docs/app/api-reference/functions/use-pathname#avoid-hydration-mismatch-with-rewrites
     const clientArea = pathname?.includes("/pupils")
       ? siteAreas.pupils
       : siteAreas.teachers;
-    setClientArea(clientArea);
-  }, [pathname]);
+    if (initialClientArea !== clientArea) {
+      setClientArea(clientArea);
+    }
+  }, [pathname, initialClientArea]);
 
   return clientArea;
 };
