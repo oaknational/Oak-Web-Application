7c8c2b06941163b823d3f370243fcdec9d98bfb2
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2025-12-11 11:23:58 +0000

    feat: remove suspense boundary

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,5 +1,3 @@
-import { Suspense } from "react";
-
 import AnalyticsWrapper from "./AnalyticsWrapper";
 
 export default function CoreLayout({
@@ -8,9 +6,5 @@ export default function CoreLayout({
   children: React.ReactNode;
 }>) {
   // TODO: [integrated-journey] Top Nav & Footer
-  return (
-    <Suspense fallback={null}>
-      <AnalyticsWrapper>{children}</AnalyticsWrapper>
-    </Suspense>
-  );
+  return <AnalyticsWrapper>{children}</AnalyticsWrapper>;
 }
