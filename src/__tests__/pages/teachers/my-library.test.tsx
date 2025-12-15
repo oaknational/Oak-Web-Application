e6e2258651c7ec500fc625acdaf57ec4f02c5d53
Natassja Blore <natassja.blore@thenational.academy>
2025-12-15 15:18:54 +0000

    fix: test mock

--- src/__tests__/pages/teachers/my-library.test.tsx
+++ src/__tests__/pages/teachers/my-library.test.tsx
@@ -10,7 +10,7 @@ jest.mock("posthog-js/react", () => ({
   useFeatureFlagVariantKey: () => true,
   useFeatureFlagEnabled: () => true,
 }));
-jest.mock("next/navigation", () => require("next-router-mock"));
+jest.mock("next/navigation");
 globalThis.matchMedia = jest.fn().mockReturnValue({
   matches: true,
 });
