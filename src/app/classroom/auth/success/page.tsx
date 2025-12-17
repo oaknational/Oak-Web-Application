280f296e1256196377febe53c3dcaa0fdf897cf2
Reece Charsville <reece@reececharsville.com>
2025-12-17 11:27:52 +0000

    fix: add suspense boundary for success page

--- src/app/classroom/auth/success/page.tsx
+++ src/app/classroom/auth/success/page.tsx
@@ -2,17 +2,20 @@
 
 import { GoogleClassroomAuthSuccessView } from "@oaknational/google-classroom-addon/ui";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 function AuthSuccessPage() {
   const searchParams = useSearchParams();
   const session = searchParams?.get("s") ?? null;
   const accessToken = searchParams?.get("at") ?? null;
-  if (!session || !accessToken) return <>An error occurred.</>;
+  if (!session || !accessToken) return <Suspense>An error occurred.</Suspense>;
   return (
-    <GoogleClassroomAuthSuccessView
-      session={decodeURIComponent(session)}
-      accessToken={decodeURIComponent(accessToken)}
-    />
+    <Suspense>
+      <GoogleClassroomAuthSuccessView
+        session={decodeURIComponent(session)}
+        accessToken={decodeURIComponent(accessToken)}
+      />
+    </Suspense>
   );
 }
 
