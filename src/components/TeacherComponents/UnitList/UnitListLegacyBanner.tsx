09fddbbf6784c624bceb846c5f313c843aa08bef
Kyle Huggins <91190841+k-huggs@users.noreply.github.com>
2025-12-18 13:39:57 +0000

    refactor: convert list into object for maintainability

--- src/components/TeacherComponents/UnitList/UnitListLegacyBanner.tsx
+++ src/components/TeacherComponents/UnitList/UnitListLegacyBanner.tsx
@@ -40,67 +40,85 @@ export const UnitListLegacyBanner: FC<UnitListLegacyBannerProps> = ({
     return <></>;
   }
 
-  const getBannerCopy = (): [React.ReactNode, React.ReactNode] => {
+  const getBannerCopy = (): {
+    headerText: React.ReactNode;
+    bodyText: React.ReactNode;
+  } => {
     switch (true) {
       case hasNewUnits && userType === "teacher":
         // Teacher pages, legacy unit is being removed but new units exist
-        return [
-          "These resources were made for remote use during the pandemic, not classroom teaching.",
-          <OakP>
-            Switch to our new teaching resources now - designed by teachers and
-            leading subject experts, and tested in classrooms.
-          </OakP>,
-        ];
+        return {
+          headerText:
+            "These resources were made for remote use during the pandemic, not classroom teaching.",
+          bodyText: (
+            <OakP>
+              Switch to our new teaching resources now - designed by teachers
+              and leading subject experts, and tested in classrooms.
+            </OakP>
+          ),
+        };
       case hasNewUnits && userType === "pupil":
         // Pupil pages, legacy unit is being removed but new units exist
-        return [
-          "These lessons were made for home learning during the pandemic.",
-          <OakP>
-            Scroll up to take a look at the brand-new and improved resources
-            we've made for you.
-          </OakP>,
-        ];
+        return {
+          headerText:
+            "These lessons were made for home learning during the pandemic.",
+          bodyText: (
+            <OakP>
+              Scroll up to take a look at the brand-new and improved resources
+              we've made for you.
+            </OakP>
+          ),
+        };
       case !hasNewUnits && userType === "pupil":
         // Pupil pages, legacy unit is being removed but no new units exist
-        return [
-          "New units on the way!",
-          <>
-            <OakP>
-              These resources were made for remote use during the pandemic, not
-              classroom teaching.
-            </OakP>
-            <OakP $mt={"spacing-24"}>
-              We’re busy creating new lessons for you.
-            </OakP>
-          </>,
-        ];
+        return {
+          headerText: "New units on the way!",
+          bodyText: (
+            <>
+              <OakP>
+                These resources were made for remote use during the pandemic,
+                not classroom teaching.
+              </OakP>
+              <OakP $mt={"spacing-24"}>
+                We're busy creating new lessons for you.
+              </OakP>
+            </>
+          ),
+        };
       default:
         // Teacher pages, legacy unit is being removed but no new units exist
-        return [
-          <OakHeading tag="h2" $font={["heading-5", "heading-4", "heading-4"]}>
-            New units on the way!
-          </OakHeading>,
-          <>
-            <OakP>
-              These resources were made for remote use during the pandemic, not
-              classroom teaching.
-            </OakP>
-            <OakP>
-              We have been busy creating new units. We'll let you know when they
-              are ready -and send you other helpful content and resources.
-              Unsubscribe at any time. Read our{" "}
-              <OakLink href="https://www.thenational.academy/legal/privacy-policy">
-                privacy policy
-              </OakLink>
-            </OakP>
-          </>,
-        ];
+        return {
+          headerText: (
+            <OakHeading
+              tag="h2"
+              $font={["heading-5", "heading-4", "heading-4"]}
+            >
+              New units on the way!
+            </OakHeading>
+          ),
+          bodyText: (
+            <>
+              <OakP>
+                These resources were made for remote use during the pandemic,
+                not classroom teaching.
+              </OakP>
+              <OakP>
+                We have been busy creating new units. We'll let you know when
+                they are ready -and send you other helpful content and
+                resources. Unsubscribe at any time. Read our{" "}
+                <OakLink href="https://www.thenational.academy/legal/privacy-policy">
+                  privacy policy
+                </OakLink>
+              </OakP>
+            </>
+          ),
+        };
     }
   };
 
   // Capture emails for newsletter if user is a teacher and no new units exist
   const shouldUseEmailCapture = userType === "teacher" && !hasNewUnits;
-  const [headerText, bodyText] = getBannerCopy();
+  const { headerText, bodyText } = getBannerCopy();
 
   if (shouldUseEmailCapture) {
     return (
