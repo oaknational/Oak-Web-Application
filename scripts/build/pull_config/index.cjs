06fc862c8cf8d897586111a9e9e35097035592e9
Jim Cresswell <jim.cresswell@gmail.com>
2026-01-06 14:26:25 +0000

    build: useful error if config pulled without gcloud

--- scripts/build/pull_config/index.cjs
+++ scripts/build/pull_config/index.cjs
@@ -12,7 +12,20 @@ const execFile = util.promisify(require("child_process").execFile);
 const OAK_CONFIG_PATH = path.join(__dirname, "../../../oak-config/");
 const GCLOUD_BUCKET_PATH = "gs://oak-config/";
 
+async function checkGcloudInstalled() {
+  try {
+    await execFile("gcloud", ["--version"]);
+  } catch (error) {
+    console.error("\x1b[31m%s\x1b[0m", "Error: gcloud is not installed on the system.");
+    console.error(
+      "Please install it to continue: https://docs.cloud.google.com/sdk/docs/install-sdk"
+    );
+    process.exit(1);
+  }
+}
+
 async function run() {
+  await checkGcloudInstalled();
   await execFile("gcloud", [
     "storage",
     "cp",
