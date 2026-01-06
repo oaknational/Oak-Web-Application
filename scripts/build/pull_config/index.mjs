4368bbed7411421942a44fe401b0f2f84cd9d15f
Jim Cresswell <jim.cresswell@gmail.com>
2026-01-06 15:40:47 +0000

    fix: oops

---
+++ scripts/build/pull_config/index.mjs
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+/**
+ * This script will pull the specifed config from the cloud-storage bucket into
+ * the directory /oak-config/ (over-writing previous contents).
+ */
+
+import { promisify } from "util";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import { execFile as execFileCb } from "child_process";
+import chalk from "chalk";
+
+const execFile = promisify(execFileCb);
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const OAK_CONFIG_PATH = join(__dirname, "../../../oak-config/");
+const GCLOUD_BUCKET_PATH = "gs://oak-config/";
+
+async function checkGcloudInstalled() {
+  try {
+    await execFile("gcloud", ["--version"]);
+  } catch (error) {
+    console.error(
+      chalk.red.bold("🛑 gcloud --version errored. Do you need to install gcloud? Probably! 😱")
+    );
+    console.error(chalk.dim(error));
+    console.error(
+      chalk.cyan(
+        "👉 Install gcloud cli here: https://docs.cloud.google.com/sdk/docs/install-sdk ☁️"
+      )
+    );
+    process.exit(1);
+  }
+}
+
+async function run() {
+  await checkGcloudInstalled();
+  await execFile("gcloud", [
+    "storage",
+    "cp",
+    `${GCLOUD_BUCKET_PATH}*`,
+    `${OAK_CONFIG_PATH}`,
+  ]);
+  console.log(chalk.green.bold("✅ Config pulled successfully!"));
+}
+
+run();
