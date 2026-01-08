06b4075cd92be2a0e404929ff3768cb4d2e81954
Natassja Blore <natassja.blore@thenational.academy>
2026-01-08 15:33:59 +0000

    feat: update links in footer

--- src/browser-lib/fixtures/footerSections.ts
+++ src/browser-lib/fixtures/footerSections.ts
@@ -1,3 +1,4 @@
+import { resolveOakHref } from "@/common-lib/urls";
 import { FooterSections } from "@/components/AppComponents/LayoutSiteFooter/LayoutSiteFooter";
 
 const footerSections: FooterSections = {
@@ -6,8 +7,8 @@ const footerSections: FooterSections = {
     links: [
       {
         text: "Learn online",
-        type: "page",
-        page: "pupil-year-index",
+        type: "link",
+        href: resolveOakHref({ page: "pupil-year-index" }),
       },
     ],
   },
@@ -16,63 +17,98 @@ const footerSections: FooterSections = {
     links: [
       {
         text: "EYFS",
-        href: "/teachers/key-stages/early-years-foundation-stage/subjects",
+        type: "link",
+        href: resolveOakHref({
+          page: "subject-index",
+          keyStageSlug: "early-years-foundation-stage",
+        }),
       },
       {
         text: "Specialist",
+        type: "link",
         href: "/teachers/specialist/subjects",
       },
       {
         text: "Key stage 1",
-        href: "/teachers/key-stages/ks1/subjects",
+        type: "link",
+        href: resolveOakHref({ page: "subject-index", keyStageSlug: "ks1" }),
       },
       {
         text: "Key stage 2",
-        href: "/teachers/key-stages/ks2/subjects",
+        type: "link",
+        href: resolveOakHref({ page: "subject-index", keyStageSlug: "ks2" }),
       },
       {
         text: "Key stage 3",
-        href: "/teachers/key-stages/ks3/subjects",
+        type: "link",
+        href: resolveOakHref({ page: "subject-index", keyStageSlug: "ks3" }),
       },
       {
         text: "Key stage 4",
-        href: "/teachers/key-stages/ks4/subjects",
+        type: "link",
+        href: resolveOakHref({ page: "subject-index", keyStageSlug: "ks4" }),
       },
 
       {
         text: "Curriculum plans",
-        type: "page",
-        page: "curriculum-landing-page",
+        type: "link",
+        href: resolveOakHref({ page: "curriculum-landing-page" }),
       },
-      { text: "Plan a lesson", type: "page", page: "lesson-planning" },
-      { text: "Support your team", type: "page", page: "support-your-team" },
+      {
+        text: "Plan a lesson",
+        type: "link",
+        href: resolveOakHref({ page: "lesson-planning" }),
+      },
+      {
+        text: "Support your team",
+        type: "link",
+        href: resolveOakHref({ page: "support-your-team" }),
+      },
     ],
   },
   oak: {
     title: "Oak",
     links: [
-      { text: "Home", type: "page", page: "home" },
-      { text: "About us", type: "page", page: "about-who-we-are" },
+      { text: "Home", type: "link", href: resolveOakHref({ page: "home" }) },
+      {
+        text: "About us",
+        type: "link",
+        href: resolveOakHref({ page: "about-who-we-are" }),
+      },
       {
         text: "Careers",
+        type: "link",
         href: "https://jobs.thenational.academy",
         icon: "external",
         ariaLabel: "Careers (opens in a new tab)",
       },
-      { text: "Contact us", type: "page", page: "contact" },
+      {
+        text: "Contact us",
+        type: "link",
+        href: resolveOakHref({ page: "contact" }),
+      },
       {
         text: "Help",
-        type: "page",
-        page: "help",
+        type: "link",
+        href: "help",
         icon: "external",
         ariaLabel: "Help (opens in a new tab)",
       },
-      { text: "Blog", type: "page", page: "blog-index" },
-      { text: "Webinars", type: "page", page: "webinar-index" },
+      {
+        text: "Blog",
+        type: "link",
+        href: resolveOakHref({ page: "blog-index" }),
+      },
+      {
+        text: "Webinars",
+        type: "link",
+        href: resolveOakHref({ page: "webinar-index" }),
+      },
       {
         text: "Status",
         href: "https://status.thenational.academy",
         icon: "external",
+        type: "link",
         ariaLabel: "Status (opens in a new tab)",
       },
     ],
@@ -81,24 +117,39 @@ const footerSections: FooterSections = {
   legal: {
     title: "Legal",
     links: [
-      { text: "Terms & conditions", href: "/legal/terms-and-conditions" },
-      { text: "Privacy policy", href: "/legal/privacy-policy" },
-      { text: "Cookie policy", href: "/legal/cookie-policy" },
+      {
+        text: "Terms & conditions",
+        type: "link",
+        href: "/legal/terms-and-conditions",
+      },
+      { text: "Privacy policy", type: "link", href: "/legal/privacy-policy" },
+      { text: "Cookie policy", type: "link", href: "/legal/cookie-policy" },
       { text: "Manage cookie settings", type: "consent-manager-toggle" },
 
-      { text: "Copyright notice", href: "/legal/copyright-notice" },
+      {
+        text: "Copyright notice",
+        type: "link",
+        href: "/legal/copyright-notice",
+      },
       {
         text: "Accessibility statement",
+        type: "link",
         href: "/legal/accessibility-statement",
       },
-      { text: "Safeguarding statement", href: "/legal/safeguarding-statement" },
+      {
+        text: "Safeguarding statement",
+        type: "link",
+        href: "/legal/safeguarding-statement",
+      },
       {
         text: "Physical activity disclaimer",
+        type: "link",
         href: "/legal/physical-activity-disclaimer",
       },
-      { text: "Complaints", href: "/legal/complaints" },
+      { text: "Complaints", type: "link", href: "/legal/complaints" },
       {
         text: "Freedom of information requests",
+        type: "link",
         href: "/legal/freedom-of-information-requests",
       },
     ],
