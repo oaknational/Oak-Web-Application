a526ba9399024820ad1ad45b1a1c089122eb6bbc
Reece Charsville <reece@reececharsville.com>
2026-01-08 12:19:03 +0000

    refactor: reverted change to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,7 +27,6 @@ export const config: MiddlewareConfig = {
     // Skip Next.js internals and all static files, unless found in search params
     // "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // API routes except /api/classroom/* which is used for Google Classroom Add-on
-    // "/(api|trpc)((?!/classroom))(.*)",
-    "/:path*", // apply to all routes
+    "/(api|trpc)((?!/classroom))(.*)",
   ],
 };
