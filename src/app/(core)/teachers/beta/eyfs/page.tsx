cebd3444dd3875810870935185caebc63dd1f660
Natassja Blore <natassja.blore@thenational.academy>
2026-01-08 13:48:34 +0000

    fix: app route error handling

--- src/app/(core)/teachers/beta/eyfs/page.tsx
+++ src/app/(core)/teachers/beta/eyfs/page.tsx
@@ -3,14 +3,19 @@ import { notFound } from "next/navigation";
 import EYFSSubjectsView from "./EYFSSubjectsView";
 
 import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
+import OakError from "@/errors/OakError";
 
 export default async function EYFSPage() {
   try {
     const curriculumData = await curriculumApi2023.eyfsListing();
     return <EYFSSubjectsView curriculumData={curriculumData} />;
-  } catch {
-    return {
-      notFound,
-    };
+  } catch (error) {
+    if (error instanceof OakError) {
+      if (error.config.responseStatusCode === 404) {
+        return notFound();
+      }
+    }
+    // TD: [integrated journey] error reporting
+    throw error;
   }
 }
