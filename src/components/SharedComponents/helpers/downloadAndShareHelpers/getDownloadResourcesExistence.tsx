b9fdc315774e9324a67b3a5f4b4366f76effd4e5
ben <ben.protheroe@thenational.academy>
2026-01-12 17:02:16 +0000

    fix: remore exports

--- src/components/SharedComponents/helpers/downloadAndShareHelpers/getDownloadResourcesExistence.tsx
+++ src/components/SharedComponents/helpers/downloadAndShareHelpers/getDownloadResourcesExistence.tsx
@@ -44,30 +44,7 @@ const schema = z.object({
     .optional(),
 });
 
-export const legacySchema = z.object({
-  data: z
-    .object({
-      resources: z.object({
-        "exit-quiz-answers": z.boolean().optional(),
-        "exit-quiz-questions": z.boolean().optional(),
-        "intro-quiz-answers": z.boolean().optional(),
-        "intro-quiz-questions": z.boolean().optional(),
-        presentation: z.boolean().optional(),
-        "worksheet-pdf": z.boolean().optional(),
-        "worksheet-pptx": z.boolean().optional(),
-      }),
-    })
-    .optional(),
-  error: z
-    .object({
-      message: z.string(),
-    })
-    .optional(),
-});
 export type DownloadsApiCheckFilesResponseSchema = z.infer<typeof schema>;
-export type LegacyDownloadsApiCheckFilesResponseSchema = z.infer<
-  typeof legacySchema
->;
 
 const getDownloadExistence = async (
   meta: Meta,
