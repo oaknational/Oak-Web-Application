2459a374c0faec406a63a3bb863a5c96c33da5fe
ben <ben.protheroe@thenational.academy>
2026-01-14 16:37:20 +0000

    feat: next response

--- src/app/api/classroom/auth/callback/route.ts
+++ src/app/api/classroom/auth/callback/route.ts
@@ -1,4 +1,4 @@
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { redirect } from "next/navigation";
 import { codeSchema } from "@oaknational/google-classroom-addon/types";
 
@@ -10,7 +10,7 @@ export async function GET(request: NextRequest) {
   const subscribeToNewsletter = searchParams.get("subscribeToNewsletter");
   const parsedCode = codeSchema.safeParse(code);
   if (!parsedCode.success) {
-    return Response.json("code is required", { status: 400 });
+    return NextResponse.json("code is required", { status: 400 });
   }
   const tempSignUpToNewsletter = async (email: string) => {
     // This is temp until we have actual mailing list to subscribe to
