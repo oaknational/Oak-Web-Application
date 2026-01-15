7d8b1e1be6743e222c0b43515f892e98bed60a23
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2026-01-15 17:14:59 +0000

    feat: fix about us and guidance links and types

--- src/node-lib/curriculum-api-2023/fixtures/topNav.fixture.ts
+++ src/node-lib/curriculum-api-2023/fixtures/topNav.fixture.ts
@@ -1,4 +1,5 @@
 import { TopNavProps } from "@/components/AppComponents/TopNav/TopNav";
+import { OakLinkPropsSimple } from "@/common-lib/urls";
 
 export const topNavFixture: TopNavProps = {
   teachers: {
@@ -8,8 +9,26 @@ export const topNavFixture: TopNavProps = {
       phaseTitle: "Secondary",
       keystages: [],
     },
-    aboutUs: [],
-    guidance: [],
+    aboutUs: [
+      {
+        slug: "about-us/who-we-are" as OakLinkPropsSimple["page"],
+        title: "Who we are",
+      },
+      {
+        slug: "about-us/our-mission" as OakLinkPropsSimple["page"],
+        title: "Our mission",
+      },
+    ],
+    guidance: [
+      {
+        slug: "guidance/teaching-tips" as OakLinkPropsSimple["page"],
+        title: "Teaching tips",
+      },
+      {
+        slug: "guidance/safeguarding" as OakLinkPropsSimple["page"],
+        title: "Safeguarding",
+      },
+    ],
   },
   pupils: {
     primary: { phaseSlug: "primary", phaseTitle: "Primary", years: [] },
