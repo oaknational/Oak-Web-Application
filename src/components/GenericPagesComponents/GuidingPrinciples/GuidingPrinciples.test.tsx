60cf953e63f81acd98a7e5660ed03db9437e230b
Jamie Blair <info@orangemug.co.uk>
2026-02-05 15:06:31 +0000

    fix: added guiding principles to /about-us/oaks-curricula refactor from curric page

---
+++ src/components/GenericPagesComponents/GuidingPrinciples/GuidingPrinciples.test.tsx
@@ -0,0 +1,24 @@
+import { GuidingPrinciples } from "./";
+
+import { renderWithProvidersByName } from "@/__tests__/__helpers__/renderWithProviders";
+
+const render = renderWithProvidersByName(["oakTheme"]);
+
+describe("GetInvolvedWorkWithUs", () => {
+  it("renders correctly", () => {
+    const { baseElement, getByRole, getAllByRole } = render(
+      <GuidingPrinciples
+        $background="bg-primary"
+        accentColor="bg-decorative4-subdued"
+      />,
+    );
+    expect(baseElement).toMatchSnapshot();
+
+    expect(getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Our guiding principles",
+    );
+
+    const headingEls = getAllByRole("heading", { level: 3 });
+    expect(headingEls.length).toEqual(6);
+  });
+});
