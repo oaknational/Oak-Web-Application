97f19f8b223364a74bf984bc612f144dbf1269ce
Natassja Blore <natassja.blore@thenational.academy>
2026-02-19 11:18:16 +0000

    refactor: use early return instead of ternary

--- src/app/(core)/programmes/[subjectPhaseSlug]/[tab]/Components/UnitSequence/getSavePropsForUnitCard.ts
+++ src/app/(core)/programmes/[subjectPhaseSlug]/[tab]/Components/UnitSequence/getSavePropsForUnitCard.ts
@@ -6,23 +6,25 @@ import { Unit } from "@/utils/curriculum/types";
 export const getSavePropsForUnitCard = (unit: Unit) => {
   const isOptionalityUnit = unit.unit_options.length > 0;
 
-  return isOptionalityUnit
-    ? undefined
-    : {
-        unitSlug: unit.slug,
-        unitTitle: unit.title,
-        programmeSlug: createTeacherProgrammeSlug(
-          unit,
-          unit.examboard_slug,
-          unit.tier_slug ?? undefined,
-          unit.pathway_slug ?? undefined,
-        ),
-        trackingProps: {
-          savedFrom: "unit_listing_save_button" as const,
-          keyStageSlug: unit.keystage_slug,
-          keyStageTitle: getKeyStageTitle(unit.keystage_slug),
-          subjectTitle: unit.subject,
-          subjectSlug: unit.subject_slug,
-        },
-      };
+  if (isOptionalityUnit) {
+    return;
+  }
+
+  return {
+    unitSlug: unit.slug,
+    unitTitle: unit.title,
+    programmeSlug: createTeacherProgrammeSlug(
+      unit,
+      unit.examboard_slug,
+      unit.tier_slug ?? undefined,
+      unit.pathway_slug ?? undefined,
+    ),
+    trackingProps: {
+      savedFrom: "unit_listing_save_button" as const,
+      keyStageSlug: unit.keystage_slug,
+      keyStageTitle: getKeyStageTitle(unit.keystage_slug),
+      subjectTitle: unit.subject,
+      subjectSlug: unit.subject_slug,
+    },
+  };
 };
