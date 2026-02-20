7677da69bcc46e0796a16e7677c1f535d86a81ee
Carl Whittaker <carl359@gmail.com>
2026-02-20 11:19:50 +0000

    feat(LESQ-1697): add `ProgrammeFiltersThreads`

---
+++ src/app/(core)/programmes/[subjectPhaseSlug]/[tab]/Components/Filters/ProgrammeFiltersThreads.tsx
@@ -0,0 +1,77 @@
+import {
+  OakBox,
+  OakRadioGroup,
+  OakRadioAsButton,
+  OakRadioAsButtonProps,
+} from "@oaknational/oak-components";
+import { useId } from "react";
+
+import { Thread, CurriculumFilters } from "@/utils/curriculum/types";
+import { highlightedUnitCount } from "@/utils/curriculum/filtering";
+import { CurriculumUnitsFormattedData } from "@/pages-helpers/curriculum/docx/tab-helpers";
+import { ComponentTypeValueType } from "@/browser-lib/avo/Avo";
+
+export type ProgrammeFiltersThreadsProps = {
+  filters: CurriculumFilters;
+  onChangeFilters: (
+    newFilters: CurriculumFilters,
+    source?: ComponentTypeValueType,
+  ) => void;
+  data: CurriculumUnitsFormattedData;
+  radioWidth?: OakRadioAsButtonProps["width"];
+};
+
+export function ProgrammeFiltersThreads({
+  filters,
+  onChangeFilters,
+  data,
+  radioWidth,
+}: Readonly<ProgrammeFiltersThreadsProps>) {
+  const id = useId();
+  const { yearData, threadOptions } = data;
+
+  function getDisplayValue(threadOption: Thread) {
+    const isSelected = filters.threads.includes(threadOption.slug);
+
+    if (isSelected) {
+      return `${threadOption.title} (${highlightedUnitCount(yearData, filters, filters.threads)} highlighted)`;
+    }
+
+    return threadOption.title;
+  }
+
+  return (
+    <OakRadioGroup
+      name={`threads-${id}`}
+      onChange={(e) => {
+        const threads = e.target.value.trim() === "" ? [] : [e.target.value];
+        onChangeFilters({ ...filters, threads });
+      }}
+      value={filters.threads.at(0) ?? ""}
+      $gap="spacing-12"
+    >
+      <OakBox as="legend" $font="heading-7" $mb="spacing-16">
+        Highlight a thread
+      </OakBox>
+      <OakRadioAsButton
+        variant="with-radio"
+        colorScheme="transparent"
+        displayValue={"None highlighted"}
+        value={""}
+        width={radioWidth}
+      />
+      {threadOptions.map((threadOption) => {
+        return (
+          <OakRadioAsButton
+            key={threadOption.slug}
+            variant="with-radio"
+            colorScheme="transparent"
+            value={threadOption.slug}
+            displayValue={getDisplayValue(threadOption)}
+            width={radioWidth}
+          />
+        );
+      })}
+    </OakRadioGroup>
+  );
+}
