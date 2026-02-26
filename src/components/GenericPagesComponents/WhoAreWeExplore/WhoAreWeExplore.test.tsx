f09ca0f7961c476cff0413dff0a4aac47eb8de11
Cat Miller <cat.miller@thenational.academy>
2026-02-26 18:18:54 +0100

    feat: add whoweareexplore test for analytics

--- src/components/GenericPagesComponents/WhoAreWeExplore/WhoAreWeExplore.test.tsx
+++ src/components/GenericPagesComponents/WhoAreWeExplore/WhoAreWeExplore.test.tsx
@@ -1,8 +1,14 @@
+import { fireEvent } from "@testing-library/react";
+import { aboutUsExplored } from "@/browser-lib/avo/Avo";
 import { WhoAreWeExplore } from "./";
-
 import { renderWithProvidersByName } from "@/__tests__/__helpers__/renderWithProviders";
 
 const render = renderWithProvidersByName(["oakTheme"]);
+
+jest.mock("@/browser-lib/avo/Avo", () => ({
+  ...jest.requireActual("@/browser-lib/avo/Avo"),
+  aboutUsExplored: jest.fn(),
+}));
 
 describe("WhoAreWeExplore", () => {
   it("renders correctly", () => {
@@ -14,16 +20,19 @@ describe("WhoAreWeExplore", () => {
             iconName: "search",
             title: "ITEM_ONE",
             href: "#item_one",
+            componentType: "about_oak",
           },
           {
             iconName: "search",
             title: "ITEM_TWO",
             href: "#item_two",
+            componentType: "get_involved",
           },
           {
             iconName: "search",
             title: "ITEM_THREE",
             href: "#item_three",
+            componentType: "meet_the_team",
           },
         ]}
       />,
@@ -41,5 +50,30 @@ describe("WhoAreWeExplore", () => {
     expect(elements[2]).toHaveTextContent("ITEM_THREE");
     expect(elements[2]).toHaveAttribute("href", "#item_three");
     expect(baseElement).toMatchSnapshot();
+  });
+
+  it("should call aboutUsExplored with the correct analytics object when an item is clicked", () => {
+    const { getByRole } = render(
+      <WhoAreWeExplore
+        title={"TEST_TITLE"}
+        items={[
+          {
+            iconName: "search",
+            title: "ITEM_ONE",
+            href: "#item_one",
+            componentType: "about_oak",
+          },
+        ]}
+      />,
+    );
+
+    const link = getByRole("link", { name: "ITEM_ONE" });
+    fireEvent.click(link);
+
+    expect(aboutUsExplored).toHaveBeenCalledWith(
+      expect.objectContaining({
+        componentType: "about_oak",
+      })
+    );
   });
 });
