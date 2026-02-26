03b2fa9ae003e7115a07a86623d194e9626fd0c7
Cat Miller <cat.miller@thenational.academy>
2026-02-26 17:25:30 +0100

    feat: add tests for analytics builders

--- src/utils/analytics-builders/index.ts
+++ src/utils/analytics-builders/index.ts
@@ -5,11 +5,21 @@ import {
   EventVersion,
   AnalyticsUseCase,
   ComponentTypeValueType,
+  AboutUsAccessedProperties,
   AboutUsContactInitiatedProperties,
-  AboutUsExploredProperties,
-  AboutUsAccessedProperties
+  AboutUsExploredProperties
 } from "@/browser-lib/avo/Avo";
 
+export const buildAboutUsAccessedAnalytics = (componentType: ComponentTypeValueType): AboutUsAccessedProperties => (
+  {
+    platform: Platform.OWA,
+    product: Product.ABOUT_US,
+    engagementIntent: EngagementIntent.EXPLORE,
+    componentType,
+    eventVersion: EventVersion["2_0_0"],
+    analyticsUseCase: AnalyticsUseCase.TEACHER,
+  }
+);
 
 export const buildAboutUsContactInitiatedAnalytics = (componentType: ComponentTypeValueType): AboutUsContactInitiatedProperties => (
   {
@@ -23,17 +33,6 @@ export const buildAboutUsContactInitiatedAnalytics = (componentType: ComponentTy
 );
 
 export const buildAboutUsExploredAnalytics = (componentType: ComponentTypeValueType): AboutUsExploredProperties => (
-  {
-    platform: Platform.OWA,
-    product: Product.ABOUT_US,
-    engagementIntent: EngagementIntent.EXPLORE,
-    componentType,
-    eventVersion: EventVersion["2_0_0"],
-    analyticsUseCase: AnalyticsUseCase.TEACHER,
-  }
-);
-
-export const buildAboutUsAccessedAnalytics = (componentType: ComponentTypeValueType): AboutUsAccessedProperties => (
   {
     platform: Platform.OWA,
     product: Product.ABOUT_US,
