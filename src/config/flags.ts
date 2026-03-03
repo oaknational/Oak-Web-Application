72719c063f2fec3240e6e9f2537766e201a3d1ac
Carl Whittaker <carl359@gmail.com>
2026-03-03 17:35:27 +0000

    feat(LESQ-1837): enable top nav with a hard coded flag
    we'll use this to remove the flicker caused by the async Posthog flag check.
    Once we're confident that the new top nav is stable, we can remove this
    along with the old nav and the Posthog flag check.

---
+++ src/config/flags.ts
@@ -0,0 +1,3 @@
+// Note: Enabling this will enable the new TopNav regardless the state of the feature flag.
+// Set to false to roll back to feature flag control.
+export const ENABLE_NEW_TOPNAV = true;
