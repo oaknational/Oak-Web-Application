c07e3b5ce4e20d55cb9d19906c4bc8dd47bc9673
Natassja Blore <natassja.blore@thenational.academy>
2026-03-16 12:02:20 +0000

    fix: use actionSchemaCamel to get correct action type in takedown banner

--- src/node-lib/curriculum-api-2023/queries/unitListing/unitListing.schema.ts
+++ src/node-lib/curriculum-api-2023/queries/unitListing/unitListing.schema.ts
@@ -14,13 +14,12 @@ import {
   ProgrammeFields,
   tierDescriptions,
   pathways,
-  actionsSchema,
   years,
   pathwaySlugs,
+  actionsSchemaCamel,
 } from "@oaknational/oak-curriculum-schema";
 
 import { ConvertKeysToCamelCase } from "@/utils/snakeCaseConverter";
-import { zodToCamelCase } from "@/node-lib/curriculum-api-2023/helpers/zodToCamelCase";
 
 export const learningThemesSchema = z.object({
   themeTitle: z.string(),
@@ -63,7 +62,7 @@ const reshapedUnitData = z.object({
   learningThemes: z.array(learningThemesSchema).nullable(),
   subjectCategories: z.array(subjectCategorySchema).nullish(),
   groupUnitsAs: z.string().nullish(),
-  actions: zodToCamelCase(actionsSchema).nullish(),
+  actions: actionsSchemaCamel.nullish(),
 });
 
 export type ReshapedUnitData = z.infer<typeof reshapedUnitData>;
