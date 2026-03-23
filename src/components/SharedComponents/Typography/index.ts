5f2c041f7bc42fcf0fa98b8389f5a2af484d0083
Natassja Blore <natassja.blore@thenational.academy>
2026-03-23 14:56:51 +0000

    feat: remove Typography component

--- src/components/SharedComponents/Typography/index.ts
+++ src/components/SharedComponents/Typography/index.ts
@@ -1,3 +1,2 @@
 export { default as Heading, getNextHeadingTag } from "./Heading.deprecated";
 export type { HeadingTag } from "./Heading.deprecated";
-export { default } from "./Typography.deprecated";
