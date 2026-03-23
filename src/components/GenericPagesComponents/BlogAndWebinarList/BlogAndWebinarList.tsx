917cbe913d559dd4d57e4e60ca7e47b8980d55c9
Natassja Blore <natassja.blore@thenational.academy>
2026-03-23 09:49:50 +0000

    feat: update blogAndWebinarList

--- src/components/GenericPagesComponents/BlogAndWebinarList/BlogAndWebinarList.tsx
+++ src/components/GenericPagesComponents/BlogAndWebinarList/BlogAndWebinarList.tsx
@@ -4,13 +4,14 @@ import {
   OakFlex,
   OakHeading,
   OakTypography,
+  OakSecondaryLink,
 } from "@oaknational/oak-components";
 import { FC } from "react";
 
-import OwaLink from "@/components/SharedComponents/OwaLink";
 import PostList, {
   PostListProps,
 } from "@/components/SharedComponents/PostList";
+import { resolveOakHref } from "@/common-lib/urls";
 
 export type BlogAndWebinarListProps = {
   blogListPosts: PostListProps;
@@ -54,10 +55,14 @@ const BlogAndWebinarList: FC<BlogAndWebinarListProps> = ({
         </OakHeading>
         <OakFlex $flexDirection={"row"}>
           <OakTypography $mr={"spacing-16"} $font={"heading-7"}>
-            <OwaLink page={"webinar-index"}>All webinars</OwaLink>
+            <OakSecondaryLink href={resolveOakHref({ page: "webinar-index" })}>
+              All webinars
+            </OakSecondaryLink>
           </OakTypography>
           <OakTypography $font={"heading-7"}>
-            <OwaLink page={"blog-index"}>All blogs</OwaLink>
+            <OakSecondaryLink href={resolveOakHref({ page: "blog-index" })}>
+              All blogs
+            </OakSecondaryLink>
           </OakTypography>
         </OakFlex>
       </OakFlex>
