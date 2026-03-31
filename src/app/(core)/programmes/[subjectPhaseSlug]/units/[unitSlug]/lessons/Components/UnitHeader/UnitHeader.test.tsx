c5b4ba71dc3e7b7f565a0dbffb551a805507654d
Natassja Blore <natassja.blore@thenational.academy>
2026-03-31 10:35:39 +0100

    test: add test suite for unit header

---
+++ src/app/(core)/programmes/[subjectPhaseSlug]/units/[unitSlug]/lessons/Components/UnitHeader/UnitHeader.test.tsx
@@ -0,0 +1,24 @@
+import { screen } from "@testing-library/dom";
+
+import UnitHeader, { UnitHeaderProps } from "./UnitHeader";
+
+import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
+
+const render = renderWithProviders();
+
+const defaultProps: UnitHeaderProps = {
+  heading: "Unit 1 in maths",
+  phase: "primary",
+  prevUnit: null,
+  nextUnit: null,
+  subjectIcon: "subject-maths",
+};
+
+describe("UnitHeader", () => {
+  it("renders a heading", () => {
+    render(<UnitHeader {...defaultProps} />);
+
+    const heading = screen.getByRole("heading", { name: defaultProps.heading });
+    expect(heading).toBeInTheDocument();
+  });
+});
