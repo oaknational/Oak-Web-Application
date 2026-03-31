8e5e89f141ea35ae24138465081f97a09744b017
Natassja Blore <natassja.blore@thenational.academy>
2026-03-31 14:52:45 +0100

    feat: try handling all errors and log in handle

--- src/node-lib/curriculum-api-2023/sdk.ts
+++ src/node-lib/curriculum-api-2023/sdk.ts
@@ -1,9 +1,13 @@
 import { GraphQLClient } from "graphql-request";
 import { DocumentNode } from "graphql";
+import polly from "polly-js";
 
 import getServerConfig from "../getServerConfig";
 
 import { getSdk } from "./generated/sdk";
+
+import errorReporter from "@/common-lib/error-reporter";
+import OakError from "@/errors/OakError";
 
 const curriculumApiUrl = getServerConfig("curriculumApi2023Url");
 const curriculumApiAuthType = getServerConfig("curriculumApiAuthType");
@@ -23,43 +27,43 @@ const headers: Headers = {
   "user-agent": "OWA Curriculum API Client",
 };
 
-// const reportError = errorReporter("Graphql sdk");
+const reportError = errorReporter("Graphql sdk");
 const graphqlClient = new GraphQLClient(curriculumApiUrl, { headers });
 
-// const retryCount = 3;
-// const withRetries = <T>(
-//   action: (requestHeaders?: Record<string, string>) => Promise<T>,
-//   operationName: string,
-// ) =>
-//   polly()
-//     .logger((err) => console.warn("GraphQLClient:Error", operationName, err))
-//     .handle((err: Error) => {
-//       // Retry timeout errors
-//       return err.message.includes("connect ETIMEDOUT");
-//     })
-//     .waitAndRetry(retryCount)
-//     .executeForPromise((info) => {
-//       if (info.count === retryCount) {
-//         console.error("GraphqlClient:MaxRetries", {
-//           ...info,
-//           queryName: operationName,
-//         });
-//         // report timeout errors
-//         const timeoutError = new OakError({
-//           code: "graphql/timeout",
-//           meta: { queryName: operationName, action: action.toString() },
-//         });
-//         reportError(timeoutError);
-//       } else if (info.count >= 0) {
-//         console.warn("GraphqlClient:RetryingCall", {
-//           ...info,
-//           queryName: operationName, // name of the query being retried
-//         });
-//       }
-//       return action();
-//     });
+const retryCount = 3;
+const withRetries = <T>(
+  action: (requestHeaders?: Record<string, string>) => Promise<T>,
+  operationName: string,
+) =>
+  polly()
+    .handle((err: Error) => {
+      // Retry timeout errors
+      console.warn("GraphQLClient:Error: ", err);
+      return true;
+    })
+    .waitAndRetry(retryCount)
+    .executeForPromise((info) => {
+      if (info.count === retryCount) {
+        console.error("GraphqlClient:MaxRetries", {
+          ...info,
+          queryName: operationName,
+        });
+        // report timeout errors
+        const timeoutError = new OakError({
+          code: "graphql/timeout",
+          meta: { queryName: operationName, action: action.toString() },
+        });
+        reportError(timeoutError);
+      } else if (info.count > 0) {
+        console.warn("GraphqlClient:RetryingCall", {
+          ...info,
+          queryName: operationName, // name of the query being retried
+        });
+      }
+      return action();
+    });
 
-const sdk = getSdk(graphqlClient);
+const sdk = getSdk(graphqlClient, withRetries);
 
 /*
  * batched queries not currently supported with the sdk
