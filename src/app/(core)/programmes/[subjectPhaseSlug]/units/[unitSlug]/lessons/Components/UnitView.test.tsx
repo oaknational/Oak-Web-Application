df4f60401fde109c05dcaec2918b5c11a1a17287
Natassja Blore <natassja.blore@thenational.academy>
2026-04-08 10:15:15 +0100

    test: add tests for unit info

--- src/app/(core)/programmes/[subjectPhaseSlug]/units/[unitSlug]/lessons/Components/UnitView.test.tsx
+++ src/app/(core)/programmes/[subjectPhaseSlug]/units/[unitSlug]/lessons/Components/UnitView.test.tsx
@@ -1,3 +1,5 @@
+import { screen } from "@testing-library/dom";
+
 import { UnitView } from "./UnitView";
 import type { UnitViewProps } from "./UnitView";
 import { LessonList } from "./LessonList";
@@ -6,24 +8,25 @@ import renderWithProviders from "@/__tests__/__helpers__/renderWithProviders";
 
 jest.mock("./LessonList");
 
+const render = renderWithProviders();
+
+const lessons = [
+  {
+    lessonSlug: "lesson-1",
+    lessonTitle: "Lesson 1",
+  },
+  {
+    lessonSlug: "lesson-2",
+    lessonTitle: "Lesson 2",
+  },
+];
 describe("UnitView", () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
   it("renders LessonList and passes lesson props with computed lessonCount", () => {
-    const lessons = [
-      {
-        lessonSlug: "lesson-1",
-        lessonTitle: "Lesson 1",
-      },
-      {
-        lessonSlug: "lesson-2",
-        lessonTitle: "Lesson 2",
-      },
-    ];
-
-    renderWithProviders()(
+    render(
       <UnitView
         programmeSlug="biology-secondary-ks3"
         unitSlug="cells"
@@ -58,5 +61,77 @@ describe("UnitView", () => {
       }),
       {},
     );
+  });
+  it("renders why this why now", () => {
+    render(
+      <UnitView
+        programmeSlug="biology-secondary-ks3"
+        unitSlug="cells"
+        unitTitle="Cells"
+        unitDescription="Learn about cells"
+        subjectTitle="Biology"
+        subjectSlug="biology"
+        keyStageSlug="ks3"
+        keyStageTitle="Key Stage 3"
+        lessons={lessons as UnitViewProps["lessons"]}
+        unitIndex={2}
+        unitCount={12}
+        threads={[]}
+        phaseSlug="secondary"
+        whyThisWhyNow={"mock why this why now"}
+      />,
+    );
+
+    expect(screen.getAllByText("mock why this why now")[0]).toBeInTheDocument();
+  });
+  it("renders prior knowledge requirements in a list", () => {
+    render(
+      <UnitView
+        programmeSlug="biology-secondary-ks3"
+        unitSlug="cells"
+        unitTitle="Cells"
+        unitDescription="Learn about cells"
+        subjectTitle="Biology"
+        subjectSlug="biology"
+        keyStageSlug="ks3"
+        keyStageTitle="Key Stage 3"
+        lessons={lessons as UnitViewProps["lessons"]}
+        unitIndex={2}
+        unitCount={12}
+        threads={[]}
+        phaseSlug="secondary"
+        priorKnowledgeRequirements={["Requirement 1", "Requirement 2"]}
+      />,
+    );
+
+    const listItems = screen.getAllByRole("listitem");
+    expect(listItems).toHaveLength(2);
+    expect(listItems[0]).toHaveTextContent("Requirement 1");
+    expect(listItems[1]).toHaveTextContent("Requirement 2");
+  });
+  it("renders threads", () => {
+    render(
+      <UnitView
+        programmeSlug="biology-secondary-ks3"
+        unitSlug="cells"
+        unitTitle="Cells"
+        unitDescription="Learn about cells"
+        subjectTitle="Biology"
+        subjectSlug="biology"
+        keyStageSlug="ks3"
+        keyStageTitle="Key Stage 3"
+        lessons={lessons as UnitViewProps["lessons"]}
+        unitIndex={2}
+        unitCount={12}
+        threads={["Thread 1", "Thread 2"]}
+        phaseSlug="secondary"
+      />,
+    );
+
+    const thread1 = screen.getAllByText("Thread 1");
+    expect(thread1[0]).toBeInTheDocument();
+
+    const thread2 = screen.getAllByText("Thread 2");
+    expect(thread2[0]).toBeInTheDocument();
   });
 });
