481e3b3eb3f81e02d48c2e3adabe9d3154203a88
Reece Charsville <reece@reececharsville.com>
2026-04-13 14:10:32 +0100

    feat: added pupil exp comps and stories for video page comps driven by props

---
+++ src/components/PupilComponents/Views/PupilLessonVideo/index.ts
@@ -0,0 +1,4 @@
+export * from "./PupilLessonVideo.view";
+export * from "./PupilLessonVideoTopNav";
+export * from "./PupilLessonVideoTranscript";
+export * from "./PupilLessonVideoAdditionalFilesCard";
