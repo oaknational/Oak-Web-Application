18daec277ff490f8c1ce30fd0083096d43afabeb
Kyle Huggins <91190841+k-huggs@users.noreply.github.com>
2026-04-13 14:37:11 +0100

    refactor: reduce if statements...

--- src/components/PupilViews/PupilExperience/PupilExperience.view.tsx
+++ src/components/PupilViews/PupilExperience/PupilExperience.view.tsx
@@ -267,6 +267,17 @@ type PupilExperienceLayoutProps = PupilExperienceViewProps & {
   assignmentToken?: string | null;
 };
 
+const getAgeRestrictionString = (ageRestriction: string | undefined | null) => {
+  switch (ageRestriction) {
+    case "7_and_above":
+      return `To view this lesson, you must be in year 7 and above`;
+    case "10_and_above":
+      return `To view this lesson, you must be in year 10 and above`;
+    default:
+      return `This lesson is age restricted.`;
+  }
+};
+
 const PupilExperienceLayout = ({
   browseData,
   lessonContent,
@@ -527,19 +538,6 @@ const PupilExperienceLayout = ({
     },
     [courseWork],
   );
-
-  const getAgeRestrictionString = (
-    ageRestriction: string | undefined | null,
-  ) => {
-    switch (ageRestriction) {
-      case "7_and_above":
-        return `To view this lesson, you must be in year 7 and above`;
-      case "10_and_above":
-        return `To view this lesson, you must be in year 10 and above`;
-      default:
-        return `This lesson is age restricted.`;
-    }
-  };
 
   const { track } = usePupilAnalytics();
   const [isOpen, setIsOpen] = useState<boolean>(
