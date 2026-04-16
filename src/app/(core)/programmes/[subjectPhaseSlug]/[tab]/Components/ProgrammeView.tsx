ac45fd5ca24b4f5bb0559b9e48f0f52b0af0e229
Coup <mia.coupland@thenational.academy>
2026-04-16 08:23:26 +0100

    fix: wrap unit tabs in OakBox nav for tab screen reader adjustment

--- src/app/(core)/programmes/[subjectPhaseSlug]/[tab]/Components/ProgrammeView.tsx
+++ src/app/(core)/programmes/[subjectPhaseSlug]/[tab]/Components/ProgrammeView.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { OakMaxWidth, OakTabs } from "@oaknational/oak-components";
+import { OakBox, OakMaxWidth, OakTabs } from "@oaknational/oak-components";
 import { useEffect, useMemo, useState } from "react";
 import { notFound, usePathname, useSearchParams } from "next/navigation";
 
@@ -144,32 +144,37 @@ export const ProgrammeView = ({
         bullets={subjectPhaseSanityData?.bullets}
       />
       {curriculumInfo.nonCurriculum ? null : (
-        <OakMaxWidth
+        <OakBox
+          as="nav"
+          aria-label="Programme page tabs"
           data-testid="programme-tabs"
-          $ph={["spacing-20", "spacing-20", "spacing-0"]}
-          $mb={["spacing-0", "spacing-48", "spacing-48"]}
         >
-          <OakTabs<TabName>
-            sizeVariant={["compact", "default"]}
-            colorVariant="black"
-            activeTab={tabSlugToName[activeTab]}
-            onTabClick={(tabName, event) => {
-              const tabSlug = tabNameToSlug[tabName];
-              // Prevents a full page reload using client side nav
-              event.preventDefault();
-              globalThis.history.pushState(null, "", tabSlug);
-            }}
-            tabs={TAB_NAMES.map((tab) => ({
-              label: tab,
-              type: "link",
-              href: resolveOakHref({
-                page: "teacher-programme",
-                subjectPhaseSlug,
-                tab: tabNameToSlug[tab],
-              }),
-            }))}
-          />
-        </OakMaxWidth>
+          <OakMaxWidth
+            $ph={["spacing-20", "spacing-20", "spacing-0"]}
+            $mb={["spacing-0", "spacing-48", "spacing-48"]}
+          >
+            <OakTabs<TabName>
+              sizeVariant={["compact", "default"]}
+              colorVariant="black"
+              activeTab={tabSlugToName[activeTab]}
+              onTabClick={(tabName, event) => {
+                const tabSlug = tabNameToSlug[tabName];
+                // Prevents a full page reload using client side nav
+                event.preventDefault();
+                globalThis.history.pushState(null, "", tabSlug);
+              }}
+              tabs={TAB_NAMES.map((tab) => ({
+                label: tab,
+                type: "link",
+                href: resolveOakHref({
+                  page: "teacher-programme",
+                  subjectPhaseSlug,
+                  tab: tabNameToSlug[tab],
+                }),
+              }))}
+            />
+          </OakMaxWidth>
+        </OakBox>
       )}
       <TabContent
         tabSlug={activeTab}
