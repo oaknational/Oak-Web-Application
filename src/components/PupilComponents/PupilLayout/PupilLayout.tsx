24420af2227c86abc29cfb091be2dc17938635b3
Reece Charsville <reece@reececharsville.com>
2026-04-16 11:12:11 +0100

    feat: added usePupilStores hook to init stores and added as optional to main pupil layout

--- src/components/PupilComponents/PupilLayout/PupilLayout.tsx
+++ src/components/PupilComponents/PupilLayout/PupilLayout.tsx
@@ -2,10 +2,19 @@ import Head from "next/head";
 import { FC } from "react";
 
 import Seo, { SeoProps } from "@/browser-lib/seo/Seo";
+import { usePupilStores } from "@/components/PupilComponents/Views/ViewHelpers";
+import {
+  LessonBrowseData,
+  LessonContent,
+} from "@/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema";
 
 export type PupilLayoutProps = {
   children?: React.ReactNode;
   seoProps: SeoProps;
+  pupilStores?: {
+    browseData: LessonBrowseData;
+    lessonContent: LessonContent;
+  };
 };
 
 /**
@@ -13,7 +22,8 @@ export type PupilLayoutProps = {
  */
 
 export const PupilLayout: FC<PupilLayoutProps> = (props) => {
-  const { seoProps, children } = props;
+  const { seoProps, children, pupilStores } = props;
+  usePupilStores(pupilStores);
 
   return (
     <>
