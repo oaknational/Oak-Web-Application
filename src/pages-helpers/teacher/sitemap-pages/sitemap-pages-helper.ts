142cb3a45bc9322e5538c157e3ceeaa4e4c81cab
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2026-04-22 11:34:41 +0100

    feat: remove specialist link from footer, remove from site-map

--- src/pages-helpers/teacher/sitemap-pages/sitemap-pages-helper.ts
+++ src/pages-helpers/teacher/sitemap-pages/sitemap-pages-helper.ts
@@ -13,49 +13,9 @@ export async function buildAllUrlFields({
   firstHalf: boolean;
   teachersSitemapData: TeachersSitemapBrowseData;
 }) {
-  const {
-    programmes,
-    units,
-    lessons,
-    keyStages,
-    specialistProgrammes,
-    specialistUnits,
-    specialistLessons,
-  } = teachersSitemapData;
+  const { programmes, units, lessons, keyStages } = teachersSitemapData;
 
   const baseUrl = getBrowserConfig("clientAppBaseUrl");
-
-  const specialistSubjectsURLs = `${baseUrl}${resolveOakHref({
-    page: "specialist-subject-index",
-  })}`;
-
-  const specialistProgrammesURLs = specialistProgrammes.map(
-    (programme) =>
-      `${baseUrl}${resolveOakHref({
-        page: "specialist-unit-index",
-        programmeSlug: programme.programmeSlug,
-      })}`,
-  );
-
-  const specialistUnitsURLs = specialistUnits.map(
-    (unit) =>
-      `${baseUrl}${resolveOakHref({
-        page: "specialist-lesson-index",
-        programmeSlug: unit.programmeSlug,
-        unitSlug: unit.unitSlug,
-      })}
-            `,
-  );
-
-  const specialistLessonsURLs = specialistLessons.map(
-    (lesson) =>
-      `${baseUrl}${resolveOakHref({
-        page: "specialist-lesson-overview",
-        programmeSlug: lesson.programmeSlug,
-        unitSlug: lesson.unitSlug,
-        lessonSlug: lesson.lessonSlug,
-      })}`,
-  );
 
   const keyStageSubjectsURLs = keyStages.map(
     (keyStage) =>
@@ -97,10 +57,6 @@ export async function buildAllUrlFields({
       ...programmesURLs,
       ...unitsURLs,
       ...lessonsURLs,
-      specialistSubjectsURLs,
-      ...specialistProgrammesURLs,
-      ...specialistUnitsURLs,
-      ...specialistLessonsURLs,
     ].map((url) => ({
       urls: url.trim(),
     })),
