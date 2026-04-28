e9ce366c0529d5007e2e0cece4513287456fa31e
Coup <mia.coupland@thenational.academy>
2026-04-28 09:57:23 +0100

    fix: remove unused properties

--- src/components/AppComponents/TopNav/TeachersTopNavHamburger/HamburgerSubMenu.tsx
+++ src/components/AppComponents/TopNav/TeachersTopNavHamburger/HamburgerSubMenu.tsx
@@ -133,9 +133,6 @@ export function SubmenuContent(
       );
       if (!keystage) return null;
       const subjects = keystage.children.filter((s) => !s.nonCurriculum);
-      const nonCurriculumSubjects = keystage.children.filter(
-        (s) => s.nonCurriculum,
-      );
       return (
         <SubmenuContainer
           description={keystage.description}
@@ -161,9 +158,7 @@ export function SubmenuContent(
             }}
             selectedMenu={phase}
             subjects={subjects}
-            nonCurriculumSubjects={nonCurriculumSubjects}
             keyStageSlug={keystage.slug}
-            keyStageTitle={keystage.title}
           />
         </SubmenuContainer>
       );
