1d6c87481ba0637e636f5161866cf77da9e9a257
Reece Charsville <reece@reececharsville.com>
2026-05-07 12:51:16 +0100

    test: added tests on some default values to increase coverage

--- src/components/PupilComponents/Views/PupilLessonIntro/PupilLessonIntroReadyCard/PupilLessonIntroReadyCard.test.tsx
+++ src/components/PupilComponents/Views/PupilLessonIntro/PupilLessonIntroReadyCard/PupilLessonIntroReadyCard.test.tsx
@@ -5,6 +5,18 @@ import { renderWithProvidersByName } from "@/__tests__/__helpers__/renderWithPro
 const render = renderWithProvidersByName(["oakTheme"]);
 
 describe("PupilLessonIntroReadyCard", () => {
+  it("renders the default copy", () => {
+    render(<PupilLessonIntroReadyCard />);
+
+    expect(document.body).toHaveTextContent("Are you ready to learn?");
+    expect(document.body).toHaveTextContent(
+      "Are you sitting in a quiet space away from distractions?",
+    );
+    expect(document.body).toHaveTextContent(
+      "Do you have all the equipment you need?",
+    );
+  });
+
   it("renders the configured copy", () => {
     render(
       <PupilLessonIntroReadyCard
--- src/components/PupilComponents/Views/PupilLessonQuiz/helpers/pickQuizTooltip.test.ts
+++ src/components/PupilComponents/Views/PupilLessonQuiz/helpers/pickQuizTooltip.test.ts
@@ -1,6 +1,37 @@
 import { pickQuizTooltip } from "./pickQuizTooltip";
 
+import { QuizQuestionAnswers } from "@/node-lib/curriculum-api-2023/queries/pupilLesson/pupilLesson.schema";
+
 describe("pickQuizTooltip", () => {
+  it.each([
+    [{ order: [{}] }, "You need to order to move on!"],
+    [{ match: [{}] }, "You need to match to move on!"],
+    [{ "short-answer": [{}] }, "You need to type an answer to move on!"],
+    [
+      {
+        "multiple-choice": [
+          { answerIsCorrect: true },
+          { answerIsCorrect: true },
+        ],
+      },
+      "You need to select answers to move on!",
+    ],
+    [
+      {
+        "multiple-choice": [
+          { answerIsCorrect: true },
+          { answerIsCorrect: false },
+        ],
+      },
+      "You need to select an answer to move on!",
+    ],
+  ] as Array<[QuizQuestionAnswers, string]>)(
+    "returns the right tooltip for %s",
+    (answers, expected) => {
+      expect(pickQuizTooltip(answers)).toBe(expected);
+    },
+  );
+
   it("returns undefined when there is no recognised answer type", () => {
     expect(pickQuizTooltip({})).toBeUndefined();
   });
