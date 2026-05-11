13a55990c1873a5077a48cf38aa0fc5feb6b33f5
Reece Charsville <reece@reececharsville.com>
2026-05-11 15:24:23 +0100

    fix: removed currentSection from getQuizNextStep call

--- src/pages-helpers/pupil/lessons-pages/new/QuizPageContent.tsx
+++ src/pages-helpers/pupil/lessons-pages/new/QuizPageContent.tsx
@@ -235,7 +235,6 @@ export const QuizPageContent = ({
     const nextStep = getQuizNextStep({
       currentQuestionIndex,
       numQuestions,
-      currentSection: section,
       isReadOnly,
     });
 
