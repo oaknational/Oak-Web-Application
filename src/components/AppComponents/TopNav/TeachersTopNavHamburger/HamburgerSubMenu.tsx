14d3729ffe09bf29807b7d75e4bd08b58fa61fa2
Coup <mia.coupland@thenational.academy>
2026-06-08 12:58:08 +0100

    refactor: add phase to examboard panel in hamburger menu

--- src/components/AppComponents/TopNav/TeachersTopNavHamburger/HamburgerSubMenu.tsx
+++ src/components/AppComponents/TopNav/TeachersTopNavHamburger/HamburgerSubMenu.tsx
@@ -168,6 +168,7 @@ export function SubmenuContent(
           >
             <ExamBoardPanel
               examBoards={selectedExamBoardSubject.examBoards}
+              phaseSlug={phase}
               keystageSlug={keystage.slug}
               selectedSubject={selectedExamBoardSubject}
               onClick={(subjectSlug, keystageSlug) => {
