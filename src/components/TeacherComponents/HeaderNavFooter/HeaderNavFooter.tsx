4236c850d08c70c792a4cff9e23ef7a641badecf
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2026-06-10 12:17:39 +0100

    feat: mobile stuck behaviour

--- src/components/TeacherComponents/HeaderNavFooter/HeaderNavFooter.tsx
+++ src/components/TeacherComponents/HeaderNavFooter/HeaderNavFooter.tsx
@@ -30,8 +30,8 @@ function useDetectStuck() {
     }
 
     const observer = new IntersectionObserver(
-      ([event]) => setIsStuck(event ? event.intersectionRatio < 1 : false),
-      { threshold: [1], rootMargin: "-1px 0px 0px 0px" },
+      ([entry]) => setIsStuck(entry ? !entry.isIntersecting : false),
+      { threshold: [0], rootMargin: "0px" },
     );
     observer.observe(ref.current);
 
@@ -45,80 +45,96 @@ const HeaderNavFooter = (props: HeaderNavFooterProps) => {
   const { ref, isStuck } = useDetectStuck();
 
   return (
-    <OakFlex
-      $zIndex={"in-front"}
-      ref={ref}
-      $position={"sticky"}
-      $top={"spacing-0"}
-      $width={"100%"}
-      $justifyContent={"center"}
-    >
+    <>
+      <OakBox ref={ref} $height="spacing-0" />
+      {/**
+       * Most of the complexity here comes from having to pin to the bottom
+       * of the screen on mobile viewports. position: sticky assumes you're
+       * pinning to the top.
+       *
+       * Positioning per breakpoint:
+       *
+       * |          | not stuck | stuck            |
+       * | -------- | --------- | ---------------- |
+       * | mobile   | static    | fixed, bottom: 0 |
+       * | tablet+  |     sticky, top: 0           |
+       */}
       <OakFlex
-        $background={
-          isStuck
-            ? `bg-decorative${props.backgroundColorLevel}-very-subdued`
-            : `bg-decorative${props.backgroundColorLevel}-subdued`
-        }
-        $pv={"spacing-24"}
-        $ph={["spacing-20", "spacing-40"]}
-        $flexDirection={["column", "row"]}
-        $maxWidth={isStuck ? ["spacing-1280"] : "auto"}
-        $mh={isStuck ? "spacing-40" : "auto"}
+        $zIndex={"in-front"}
+        $position={[isStuck ? "fixed" : "static", "sticky"]}
+        $bottom={[isStuck ? "spacing-0" : null, null]}
+        $top={[null, "spacing-0"]}
+        $left={[isStuck ? "spacing-0" : null, null]}
         $width={"100%"}
-        $gap={"spacing-24"}
-        $justifyContent="center"
-        $dropShadow={isStuck ? "drop-shadow-standard" : "drop-shadow-none"}
-        $bbr={isStuck ? "border-radius-l" : "border-radius-square"}
+        $justifyContent={"center"}
       >
         <OakFlex
-          $justifyContent={isStuck ? "start" : "space-between"}
+          $background={
+            isStuck
+              ? `bg-decorative${props.backgroundColorLevel}-very-subdued`
+              : `bg-decorative${props.backgroundColorLevel}-subdued`
+          }
+          $pv={"spacing-24"}
+          $ph={["spacing-20", "spacing-40"]}
+          $flexDirection={["column", "row"]}
+          $maxWidth={isStuck ? ["spacing-1280"] : "auto"}
+          $mh={isStuck ? "spacing-40" : "auto"}
           $width={"100%"}
-          $gap={"spacing-16"}
-          $maxWidth="spacing-1280"
+          $gap={"spacing-24"}
+          $justifyContent="center"
+          $dropShadow={isStuck ? "drop-shadow-standard" : "drop-shadow-none"}
+          $bbr={isStuck ? "border-radius-l" : "border-radius-square"}
         >
-          {props.actionButton}
-          <OakBox
-            $bl={"border-solid-m"}
-            $display={isStuck ? ["none", "none", "block"] : "none"}
-            $height={"spacing-24"}
-            $borderColor={`border-decorative${props.backgroundColorLevel}`}
-          />
-          <OakBox $display={isStuck ? ["none", "none", "block"] : "none"}>
-            <OakP>{props.title}</OakP>
-          </OakBox>
           <OakFlex
-            as="nav"
-            $display={isStuck ? "none" : ["none", "flex"]}
-            $gap={"spacing-32"}
-            $alignItems={"center"}
+            $justifyContent={isStuck ? "start" : "space-between"}
+            $width={"100%"}
+            $gap={"spacing-16"}
+            $maxWidth="spacing-1280"
           >
-            <PrevNextButtons $display={"flex"} {...props} />
+            {props.actionButton}
             <OakBox
               $bl={"border-solid-m"}
-              $display={"flex"}
+              $display={isStuck ? ["none", "none", "block"] : "none"}
               $height={"spacing-24"}
               $borderColor={`border-decorative${props.backgroundColorLevel}`}
             />
+            <OakBox $display={isStuck ? ["none", "none", "block"] : "none"}>
+              <OakP>{props.title}</OakP>
+            </OakBox>
+            <OakFlex
+              as="nav"
+              $display={isStuck ? "none" : ["none", "flex"]}
+              $gap={"spacing-32"}
+              $alignItems={"center"}
+            >
+              <PrevNextButtons $display={"flex"} {...props} />
+              <OakBox
+                $bl={"border-solid-m"}
+                $display={"flex"}
+                $height={"spacing-24"}
+                $borderColor={`border-decorative${props.backgroundColorLevel}`}
+              />
+            </OakFlex>
           </OakFlex>
-        </OakFlex>
 
-        <PrevNextButtons
-          $display={isStuck ? "none" : ["flex", "none"]}
-          {...props}
-        />
-        <OakSmallPrimaryInvertedButton
-          $display={isStuck ? "none" : "block"}
-          width={["100%", "auto"]}
-          iconName="list"
-          isTrailingIcon
-          $textWrap={"nowrap"}
-          element="a"
-          href={props.viewHref}
-        >
-          {`View ${props.type === "unit" ? "all units" : "unit"}`}
-        </OakSmallPrimaryInvertedButton>
+          <PrevNextButtons
+            $display={isStuck ? "none" : ["flex", "none"]}
+            {...props}
+          />
+          <OakSmallPrimaryInvertedButton
+            $display={isStuck ? "none" : "block"}
+            width={["100%", "auto"]}
+            iconName="list"
+            isTrailingIcon
+            $textWrap={"nowrap"}
+            element="a"
+            href={props.viewHref}
+          >
+            {`View ${props.type === "unit" ? "all units" : "unit"}`}
+          </OakSmallPrimaryInvertedButton>
+        </OakFlex>
       </OakFlex>
-    </OakFlex>
+    </>
   );
 };
 
