622638e8f77d3b40fb56423e4f8e06be600c7eed
Coup <mia.coupland@thenational.academy>
2026-06-25 08:36:08 +0100

    fix: try adding fallback div

--- src/app/(core)/teachers/search/page.tsx
+++ src/app/(core)/teachers/search/page.tsx
@@ -5,6 +5,19 @@ import { SearchView } from "./SearchView";
 
 import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
 
+function SearchPageFallback() {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    />
+  );
+}
+
 export const metadata: Metadata = {
   title: "Search for Free Teaching Resources",
   description: "Search for Free Teaching Resources",
@@ -18,7 +31,7 @@ async function SearchPage() {
   const curriculumData = await curriculumApi2023.searchPage();
 
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<SearchPageFallback />}>
       <SearchView curriculumData={curriculumData} />
     </Suspense>
   );
