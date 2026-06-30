431802a69f8d9d463cd8de06ae12c96a002a034e
Alex Cupit <alexcupit@outlook.com>
2026-06-30 13:45:25 +0100

    feat: oaks impact intial build

--- src/pages/about-us/oaks-impact.tsx
+++ src/pages/about-us/oaks-impact.tsx
@@ -3,12 +3,18 @@ import {
   GetServerSidePropsResult,
   NextPage,
 } from "next/dist/types";
-import { OakHeading } from "@oaknational/oak-components";
+import { OakBox } from "@oaknational/oak-components";
 
 import getBrowserConfig from "@/browser-lib/getBrowserConfig";
 import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
 import Layout from "@/components/AppComponents/AppLayout";
 import { TopNavProps } from "@/components/AppComponents/TopNav/TopNav";
+import { AboutUsLayout } from "@/components/GenericPagesComponents/AboutUsLayout";
+import {
+  AboutSharedHeader,
+  AboutSharedHeaderImage,
+} from "@/components/GenericPagesComponents/AboutSharedHeader";
+import { SupportYou } from "@/components/GenericPagesComponents/SupportYou";
 import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
 import getPageProps from "@/node-lib/getPageProps";
 import { getFeatureFlag } from "@/node-lib/posthog/getFeatureFlag";
@@ -18,6 +24,11 @@ export type OaksImpactPageProps = {
   topNav: TopNavProps;
 };
 
+const placeholderImage = {
+  _id: "ef2a05d634b1ade34d33664c44fa36cb62e1aaba-3000x2001-jpg",
+  url: "https://sanity-asset-cdn.thenational.academy/images/cuvjke51/production/ef2a05d634b1ade34d33664c44fa36cb62e1aaba-3000x2001.jpg",
+};
+
 const OaksImpact: NextPage<OaksImpactPageProps> = ({ topNav }) => {
   return (
     <Layout
@@ -25,9 +36,55 @@ const OaksImpact: NextPage<OaksImpactPageProps> = ({ topNav }) => {
       $background={"bg-primary"}
       topNavProps={topNav}
     >
-      <OakHeading tag="h1" $font="heading-1">
-        Hello World
-      </OakHeading>
+      <AboutUsLayout>
+        <AboutSharedHeader
+          title="Oak's Impact"
+          titleHighlight="bg-decorative2-main"
+          content="How our world-class curriculum is making a difference in schools and trusts across the country"
+        >
+          <AboutSharedHeaderImage
+            imageAlt="Oak's Impact"
+            imageUrl={placeholderImage.url}
+          />
+        </AboutSharedHeader>
+        <OakBox
+          $ma={"spacing-48"}
+          $pa={"spacing-48"}
+          $borderColor="red"
+          $ba="border-solid-xxl"
+        >
+          TODO: Stats
+        </OakBox>
+        <OakBox
+          $ma={"spacing-48"}
+          $pa={"spacing-48"}
+          $borderColor="red"
+          $ba="border-solid-xxl"
+        >
+          TODO: Case Studies
+        </OakBox>
+        <OakBox
+          $ma={"spacing-48"}
+          $pa={"spacing-48"}
+          $borderColor="red"
+          $ba="border-solid-xxl"
+        >
+          TODO: Quotes
+        </OakBox>
+        <SupportYou
+          title="Discover how Oak can support you"
+          headingTag="h2"
+          body="To explore the impact Oak’s curricula could have in your school or trust, fill out the form below and one of our experts will be in touch shortly."
+          link={{
+            href: "https://share.hsforms.com/2yBT-92_WT6CvX1b6L3Iw8Qbvumd",
+            text: "Get in touch with an expert",
+          }}
+          image={{
+            asset: placeholderImage,
+            altText: "Oak's Impact",
+          }}
+        />
+      </AboutUsLayout>
     </Layout>
   );
 };
