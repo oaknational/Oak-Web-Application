be6e1615c86d94a3270b2157836904f97161df07
Natassja Blore <natassja.blore@thenational.academy>
2026-07-01 10:16:05 +0100

    feat: add ssr version of page for experiment

---
+++ src/app/(core)/teachers/programmes/[slug]/units/[unitSlug]/lessons/ssr/page.tsx
@@ -0,0 +1,32 @@
+import { UnitView } from "../Components/UnitView";
+import {
+  getCachedUnitData,
+  redirectUnitPageIfNeeded,
+} from "../getCachedUnitData";
+
+import withPageErrorHandling, {
+  AppPageProps,
+} from "@/hocs/withPageErrorHandling";
+import { getFeatureFlagValue } from "@/utils/featureFlags";
+
+type LessonsPageParams = { slug: string; unitSlug: string };
+
+export const dynamic = "force-dynamic";
+
+export { generateMetadata } from "../generateMetadata";
+
+const InnerUnitPage = async (props: AppPageProps<LessonsPageParams>) => {
+  const { slug: programmeSlug, unitSlug } = await props.params;
+
+  await redirectUnitPageIfNeeded({ programmeSlug, unitSlug });
+
+  const isEnabled = await getFeatureFlagValue("test-flag", "string");
+
+  const data = await getCachedUnitData(programmeSlug, unitSlug);
+
+  return <UnitView {...data} isEnabled={isEnabled === "test"} />;
+};
+
+const UnitPage = withPageErrorHandling(InnerUnitPage, "unit-page::app");
+
+export default UnitPage;
