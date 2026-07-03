9d5a19a0e690aaf974c64445addc1ef4e9f12bb7
Coup <mia.coupland@thenational.academy>
2026-07-03 10:17:24 +0100

    fix: add role to focus target

--- src/components/TeacherComponents/LessonOverviewPresentation/LessonOverviewPresentation.tsx
+++ src/components/TeacherComponents/LessonOverviewPresentation/LessonOverviewPresentation.tsx
@@ -62,6 +62,7 @@ const LessonOverviewPresentation: FC<LessonOverviewPresentationProps> = ({
         >
           <FocusTarget
             as="div"
+            role="region"
             tabIndex={0}
             $width={"100%"}
             $height={"100%"}
