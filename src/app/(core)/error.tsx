3a60c23584d72dc3f39b1bfa6af2dfe6c7462a9b
Natassja Blore <natassja.blore@thenational.academy>
2026-07-06 10:45:10 +0100

    refactor: memoise reportError fns in error pages

--- src/app/(core)/error.tsx
+++ src/app/(core)/error.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useMemo } from "react";
+
 import ErrorFallback from "../components/ErrorHandling/ErrorFallback";
 
 import errorReporter from "@/common-lib/error-reporter";
@@ -11,11 +13,8 @@ export default function RootError({
   error: Error & { digest?: string };
   reset: () => void;
 }>) {
+  const reportError = useMemo(() => errorReporter("app::core-layout"), []);
   return (
-    <ErrorFallback
-      error={error}
-      reset={reset}
-      reportError={errorReporter("app::core-layout")}
-    />
+    <ErrorFallback error={error} reset={reset} reportError={reportError} />
   );
 }
--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useMemo } from "react";
+
 import errorReporter from "@/common-lib/error-reporter";
 import LayoutSiteFooter from "@/components/AppComponents/LayoutSiteFooter";
 import TopNavMinimal from "@/components/AppComponents/TopNav/TopNavMinimal";
@@ -12,14 +14,11 @@ export default function RootError({
   error: Error & { digest?: string };
   reset: () => void;
 }>) {
+  const reportError = useMemo(() => errorReporter("app::root-layout"), []);
   return (
     <>
       <TopNavMinimal />
-      <ErrorFallback
-        error={error}
-        reset={reset}
-        reportError={errorReporter("app::root-layout")}
-      />
+      <ErrorFallback error={error} reset={reset} reportError={reportError} />
       <LayoutSiteFooter />
     </>
   );
