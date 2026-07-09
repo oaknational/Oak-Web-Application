8cf0ebb4bd56973a519252fdd3e3a12e640b99fd
Natassja Blore <natassja.blore@thenational.academy>
2026-07-09 11:39:36 +0100

    feat: remove type casts on avo properties

--- src/context/TeacherBrowseAnalytics/utils/getAnalyticsProperties.ts
+++ src/context/TeacherBrowseAnalytics/utils/getAnalyticsProperties.ts
@@ -1,3 +1,5 @@
+import { ProgrammeFields } from "@oaknational/oak-curriculum-schema";
+
 import {
   ProgrammeState,
   ProgrammePathwayData,
@@ -7,29 +9,29 @@ import {
   LessonPathwayData,
 } from "../teacherBrowseAnalytics.types";
 
-import {
-  ExamBoardValueType,
-  KeyStageTitleValueType,
-  PathwayValueType,
-  TierNameValueType,
-} from "@/browser-lib/avo/Avo";
+import { KeyStageTitleValueType } from "@/browser-lib/avo/Avo";
 
 /**
  * Helper fns to transform curriculum data into the shape Avo expects
  * to be sent with events
  */
 
+const convertKsTitle = (ks: ProgrammeFields["keystage_description"]) => {
+  const sentenceCase = ks.charAt(0).toUpperCase() + ks.slice(1).toLowerCase();
+  return sentenceCase as KeyStageTitleValueType;
+};
+
 export const getProgrammeAnalyticsProperties = (
   programmeState: ProgrammeState,
 ): ProgrammePathwayData => {
   return {
-    examBoard: programmeState.examBoardTitle as ExamBoardValueType,
+    examBoard: programmeState.examBoardTitle,
     keyStageSlug: programmeState.keyStageSlug,
-    keyStageTitle: programmeState.keyStageTitle as KeyStageTitleValueType,
-    pathway: programmeState.pathwayTitle as PathwayValueType,
+    keyStageTitle: convertKsTitle(programmeState.keyStageTitle),
+    pathway: programmeState.pathwayTitle,
     subjectSlug: programmeState.subjectSlug,
     subjectTitle: programmeState.subjectTitle,
-    tierName: programmeState.tierTitle as TierNameValueType,
+    tierName: programmeState.tierTitle,
   };
 };
 
