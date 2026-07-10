ed61f849d7e3556cf208f26f73e9ae54e104f6f5
Cat Miller <cat.miller@thenational.academy>
2026-07-10 10:36:46 +0100

    refactor: use newguttermaxwidth instead of padding vals

--- src/components/GenericPagesComponents/OaksImpactCaseStudies/index.tsx
+++ src/components/GenericPagesComponents/OaksImpactCaseStudies/index.tsx
@@ -1,4 +1,11 @@
-import { OakBox, OakCard, OakFlex, OakHeading, OakMaxWidth } from "@oaknational/oak-components";
+import {
+  OakBox,
+  OakCard,
+  OakFlex,
+  OakHeading,
+} from "@oaknational/oak-components";
+
+import { NewGutterMaxWidth } from "@/components/GenericPagesComponents/NewGutterMaxWidth";
 
 export type OaksImpactCaseStudiesProps = {
   caseStudies: {
@@ -8,40 +15,35 @@ export type OaksImpactCaseStudiesProps = {
   }[];
 };
 
-export const OaksImpactCaseStudies = ( { caseStudies }: OaksImpactCaseStudiesProps) => {
-	return (
-		<OakBox $background={"bg-decorative2-subdued"}>
-			<OakMaxWidth>
-				<OakFlex
-					$flexDirection={"column"}
-					$ph={["spacing-32", "spacing-40", "spacing-100"]}
-					$pv={["spacing-56", "spacing-80"]}
-					$gap={"spacing-24"}
-				>
-					<OakHeading
-						tag={"h2"}
-						$font={["heading-5", "heading-3"]}
-					>
-						Case studies
-					</OakHeading>
-					<OakFlex
-						$gap={"spacing-16"}
-						$flexDirection={["column", "row"]}
-					>
-						{caseStudies.map((caseStudy, index) => (
-							<OakCard
-								key={index}
-								heading={caseStudy.heading}
-								href={caseStudy.href}
-								imageSrc={caseStudy.imageSrc}
-								aspectRatio="4/3"
-								linkText="Watch the video"
-								cardWidth={"100%"}
-							/>
-						))}
-					</OakFlex>
-				</OakFlex>
-			</OakMaxWidth>
-		</OakBox>
-	);
+export const OaksImpactCaseStudies = ({
+  caseStudies,
+}: OaksImpactCaseStudiesProps) => {
+  return (
+    <OakBox $background={"bg-decorative2-subdued"}>
+      <NewGutterMaxWidth>
+        <OakFlex
+          $flexDirection={"column"}
+          $pv={["spacing-56", "spacing-80"]}
+          $gap={"spacing-24"}
+        >
+          <OakHeading tag={"h2"} $font={["heading-5", "heading-3"]}>
+            Case studies
+          </OakHeading>
+          <OakFlex $gap={"spacing-16"} $flexDirection={["column", "row"]}>
+            {caseStudies.map((caseStudy, index) => (
+              <OakCard
+                key={index}
+                heading={caseStudy.heading}
+                href={caseStudy.href}
+                imageSrc={caseStudy.imageSrc}
+                aspectRatio="4/3"
+                linkText="Watch the video"
+                cardWidth={"100%"}
+              />
+            ))}
+          </OakFlex>
+        </OakFlex>
+      </NewGutterMaxWidth>
+    </OakBox>
+  );
 };
