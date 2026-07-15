6892afd4bee07482f2515c6510643c2af33dff44
Jamie Blair <info@orangemug.co.uk>
2026-07-15 09:54:19 +0100

    test: use fixture data in tests

--- src/components/GenericPagesComponents/OaksImpactStats/OaksImpactStats.test.tsx
+++ src/components/GenericPagesComponents/OaksImpactStats/OaksImpactStats.test.tsx
@@ -1,3 +1,5 @@
+import { fixtureData } from "./OaksImpactStats.fixtures";
+
 import { OaksImpactStats } from "./";
 
 import { renderWithProvidersByName } from "@/__tests__/__helpers__/renderWithProviders";
@@ -6,26 +8,11 @@ const render = renderWithProvidersByName(["oakTheme"]);
 
 describe("OaksImpactStats", () => {
   it("renders correctly", () => {
-    const data = {
-      headingText: "MAIN_HEADER",
-      headingCopy: "MAIN_HEADER_COPY",
-      link: {
-        href: "#",
-        text: "LINK",
-      },
-      items: new Array(3).fill(true).map((_item, index) => {
-        return {
-          image: {
-            alt: "",
-            src: "https://res.cloudinary.com/oak-web-application/image/upload/v1749031463/icons/clipboard_yll2yj.svg",
-          },
-          headingText: `ITEM_HEADING_${index}`,
-          body: `ITEM_HEADING_BODY_${index}`,
-        };
-      }),
-    };
+    const data = fixtureData;
     const { baseElement, getByRole } = render(<OaksImpactStats {...data} />);
     expect(baseElement).toMatchSnapshot();
-    expect(getByRole("heading", { level: 2 })).toHaveTextContent("MAIN_HEADER");
+    expect(getByRole("heading", { level: 2 })).toHaveTextContent(
+      "Oak is now used in 72% of schools",
+    );
   });
 });
