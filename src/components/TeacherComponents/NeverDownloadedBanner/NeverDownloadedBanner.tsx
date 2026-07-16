85956eca331de576f6ac519bc4a2ca0c1b9547b9
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2026-07-16 15:13:07 +0100

    fix: use client

--- src/components/TeacherComponents/NeverDownloadedBanner/NeverDownloadedBanner.tsx
+++ src/components/TeacherComponents/NeverDownloadedBanner/NeverDownloadedBanner.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useEffect, useState } from "react";
 import { OakInlineBanner } from "@oaknational/oak-components";
 import { useFeatureFlagEnabled, usePostHog } from "posthog-js/react";
