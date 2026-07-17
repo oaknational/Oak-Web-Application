75e09d1c685734fa60ecbade7095dc36eb406dd5
Alex Cupit <alexcupit@outlook.com>
2026-07-17 10:52:27 +0100

    chore: update snapshots

--- src/components/GenericPagesComponents/OaksImpactSchoolQuote/OaksImpactSchoolQuote.test.tsx
+++ src/components/GenericPagesComponents/OaksImpactSchoolQuote/OaksImpactSchoolQuote.test.tsx
@@ -14,10 +14,6 @@ describe("OaksImpactSchoolQuote", () => {
     expect(baseElement).toMatchSnapshot();
     expect(getByRole("heading")).toHaveTextContent("Ormiston Academies Trust");
     const imageEl = getByRole("img");
-    expect(imageEl).toHaveAttribute(
-      "src",
-      "http://localhost/_next/image?url=https%3A%2F%2Fsanity-asset-cdn.thenational.academy%2Fimages%2Fcuvjke51%2Fproduction%2Fb81ee19a35baa3192360a210fda34cc9b21f4fd6-5824x3264.jpg%3Fw%3D640%26fm%3Dwebp%26q%3D80%26fit%3Dclip%26auto%3Dformat&w=3840&q=75",
-    );
     expect(imageEl).toHaveAttribute("alt", "TEST_ALT");
   });
 
