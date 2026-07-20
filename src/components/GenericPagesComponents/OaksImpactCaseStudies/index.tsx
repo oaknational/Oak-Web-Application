d1496c123fcfee8a35e15b1ce772c0a74a17cc27
Cat Miller <cat.miller@thenational.academy>
2026-07-20 12:48:00 +0100

    fix: remove comment

--- src/components/GenericPagesComponents/OaksImpactCaseStudies/index.tsx
+++ src/components/GenericPagesComponents/OaksImpactCaseStudies/index.tsx
@@ -41,7 +41,6 @@ export const OaksImpactCaseStudies = ({
                 heading={caseStudy.video.title || ""}
                 headingLevel={"div"}
                 href={`#/case-studies/${caseStudy.slug.current}`}
-                // imageSrc={caseStudy.image.asset?.url}
                 imageSrc={
                   getProxiedSanityAssetUrl(caseStudy.image?.asset?.url) ?? ""
                 }
