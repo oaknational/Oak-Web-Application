a3998bcf1debf5fa6027f2974ed5288c78a5b1a6
Jamie Blair <info@orangemug.co.uk>
2026-07-21 14:49:13 +0100

    test: wrap in missing <OakThemeProvider/>

--- src/context/OakNotifications/useOakNotificationsContext.test.tsx
+++ src/context/OakNotifications/useOakNotificationsContext.test.tsx
@@ -1,14 +1,23 @@
 import { act } from "@testing-library/react";
+import { OakThemeProvider, oakDefaultTheme } from "@oaknational/oak-components";
 
 import { OakNotificationsProvider } from "./OakNotificationsProvider";
 import { useOakNotificationsContext } from "./useOakNotificationsContext";
 
 import renderHookWithTheme from "@/__tests__/__helpers__/renderHookWithTheme";
 
+function Wrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <OakThemeProvider theme={oakDefaultTheme}>
+      <OakNotificationsProvider>{children}</OakNotificationsProvider>
+    </OakThemeProvider>
+  );
+}
+
 describe("useToastContext", () => {
   test("oakToast properties should  be null by default", () => {
     const { result } = renderHookWithTheme(() => useOakNotificationsContext(), {
-      wrapper: OakNotificationsProvider,
+      wrapper: Wrapper,
     });
     const { currentToastProps } = result.current;
 
@@ -16,7 +25,7 @@ describe("useToastContext", () => {
   });
   test("oakToast properties should update  when set", () => {
     const { result } = renderHookWithTheme(() => useOakNotificationsContext(), {
-      wrapper: OakNotificationsProvider,
+      wrapper: Wrapper,
     });
     const { setCurrentToastProps } = result.current;
 
