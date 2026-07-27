3e5e25e349d31ca548bde2c3004df971d789214b
Natassja Blore <natassja.blore@thenational.academy>
2026-07-27 15:28:57 +0100

    refactor: invert if to remove extra return

--- src/components/TeacherComponents/ResourcePageSchoolPicker/useSchoolPicker.tsx
+++ src/components/TeacherComponents/ResourcePageSchoolPicker/useSchoolPicker.tsx
@@ -12,10 +12,8 @@ export const fetcher = async (queryUrl: string) => {
   try {
     const res = await fetch(queryUrl);
     if (!res.ok) {
-      if (res.status === 400) {
-        // Don't report bad request errors due to invalid input characters
-        return undefined;
-      } else {
+      if (res.status !== 400) {
+        // Don't report bad request errors due to invalid input characters ie. 400 response
         throw new Error(
           new OakError({
             code: "school-picker/fetch-suggestions",
@@ -43,6 +41,7 @@ export const fetcher = async (queryUrl: string) => {
     }
 
     reportError(oakError);
+    throw oakError;
   }
 };
 
