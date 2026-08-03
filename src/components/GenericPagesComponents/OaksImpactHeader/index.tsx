64ee5a2af18b5d0e490fa9de401f78c32938614c
Jamie Blair <info@orangemug.co.uk>
2026-08-03 16:40:14 +0100

    feat: added <OaksImpactHeader/> component

---
+++ src/components/GenericPagesComponents/OaksImpactHeader/index.tsx
@@ -0,0 +1,120 @@
+import {
+  OakVideo,
+  OakFlex,
+  OakHeading,
+  OakBox,
+  parseSpacing,
+  OakP,
+} from "@oaknational/oak-components";
+import styled from "styled-components";
+
+import { NewGutterMaxWidth } from "../NewGutterMaxWidth";
+
+import { Video } from "@/common-lib/cms-types";
+import VideoPlayer from "@/components/SharedComponents/VideoPlayer";
+
+const Layout = styled(OakFlex)`
+  @media (min-width: 0px) AND (max-width: 749px) {
+    flex-direction: column;
+    padding-top: ${parseSpacing("spacing-56")};
+    padding-bottom: ${parseSpacing("spacing-72")};
+    gap: ${parseSpacing("spacing-16")};
+  }
+  @media (min-width: 750px) AND (max-width: 999px) {
+    flex-direction: column;
+    padding-top: ${parseSpacing("spacing-80")};
+    padding-bottom: ${parseSpacing("spacing-80")};
+    gap: ${parseSpacing("spacing-16")};
+  }
+  @media (min-width: 1000px) AND (max-width: 1279px) {
+    flex-direction: row;
+    padding-top: ${parseSpacing("spacing-80")};
+    padding-bottom: ${parseSpacing("spacing-80")};
+    gap: ${parseSpacing("spacing-40")};
+  }
+  @media (min-width: 1280px) {
+    flex-direction: row;
+    padding-top: ${parseSpacing("spacing-56")};
+    padding-bottom: ${parseSpacing("spacing-56")};
+    gap: ${parseSpacing("spacing-16")};
+  }
+`;
+
+const VideoLayout = styled(OakBox)`
+  @media (min-width: 0px) AND (max-width: 749px) {
+    width: ${parseSpacing("100%")};
+  }
+  @media (min-width: 750px) AND (max-width: 999px) {
+    width: ${parseSpacing("100%")};
+  }
+  @media (min-width: 1000px) AND (max-width: 1279px) {
+    width: ${parseSpacing("spacing-480")};
+  }
+  @media (min-width: 1280px) {
+    width: ${parseSpacing("spacing-640")};
+  }
+`;
+
+export type OaksImpactHeaderProps = {
+  title: string;
+  body: string;
+  video?: Video;
+  videoDescription: string;
+};
+export function OaksImpactHeader({
+  videoDescription,
+  video,
+  title,
+  body,
+}: Readonly<OaksImpactHeaderProps>) {
+  return (
+    <NewGutterMaxWidth>
+      <Layout>
+        {/* <OakFlex $flexDirection={["column", "column", "row"]} $pb={["spacing-56", "spacing-80", "spacing-72"]} $pt={["spacing-56", "spacing-80", "spacing-56"]} $gap={["spacing-16", "spacing-40", "spacing-16"]}> */}
+        <OakFlex
+          $justifyContent={"center"}
+          $height={[undefined, undefined, "spacing-480"]}
+          $alignItems={"flex-start"}
+          $flexDirection={"column"}
+          $gap={"spacing-24"}
+        >
+          <OakFlex
+            $alignItems={"flex-start"}
+            $background={"bg-decorative2-main"}
+            $ph={"spacing-4"}
+            $gap={"spacing-8"}
+          >
+            <OakHeading tag={"h1"} $color={"text-primary"} $font={"heading-2"}>
+              {title}
+            </OakHeading>
+          </OakFlex>
+          <OakP $color={"text-primary"} $font={"heading-light-3"}>
+            {body}
+          </OakP>
+        </OakFlex>
+        <OakFlex $flexDirection={"column"} $gap={"spacing-16"}>
+          <VideoLayout>
+            <OakVideo
+              videoSlot={
+                video && (
+                  <VideoPlayer
+                    playbackPolicy="public"
+                    thumbnailTime={video.video.asset.thumbTime}
+                    playbackId={video.video.asset.playbackId}
+                    title={video.title}
+                    isLegacy={true}
+                    location="marketing"
+                    omitBorder={true}
+                  />
+                )
+              }
+              body={videoDescription}
+              transcript={["test", "test", "test"]}
+              showTranscript={true}
+            />
+          </VideoLayout>
+        </OakFlex>
+      </Layout>
+    </NewGutterMaxWidth>
+  );
+}
