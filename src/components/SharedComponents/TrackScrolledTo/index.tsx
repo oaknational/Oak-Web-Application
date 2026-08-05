b4a755636b3b21cd1a4f4c02ad3f29507bf321ae
Cat Miller <cat.miller@thenational.academy>
2026-08-05 11:59:00 +0100

    feat: extract scrolledTo component and call track event

---
+++ src/components/SharedComponents/TrackScrolledTo/index.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from "react";
+import { OakBox } from "@oaknational/oak-components";
+
+import useAnalytics from "@/context/Analytics/useAnalytics";
+
+
+export type TrackScrolledToProps = {
+  eventKey: string;
+};
+
+// to do - add tests, storybook?
+export default function TrackScrolledTo({ eventKey }: Readonly<TrackScrolledToProps>) {
+  const { track } = useAnalytics();
+  const ref = useRef<HTMLDivElement>(null);
+  
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry?.isIntersecting) {
+          track.scrolledTo({ key: eventKey });
+        }
+      },
+      { threshold: 0, rootMargin: "-1px 0px 0px" },
+    );
+
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, [ref, eventKey, track]);
+
+  return (
+    <OakBox 
+      ref={ref}
+    />
+  )
+}
