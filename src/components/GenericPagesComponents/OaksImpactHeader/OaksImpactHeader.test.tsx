efd29015a9e68135054370b170c7b3afffae22de
Jamie Blair <info@orangemug.co.uk>
2026-08-06 10:34:27 +0100

    test: fix broken impact tests

--- src/components/GenericPagesComponents/OaksImpactHeader/OaksImpactHeader.test.tsx
+++ src/components/GenericPagesComponents/OaksImpactHeader/OaksImpactHeader.test.tsx
@@ -1,5 +1,6 @@
 import { OaksImpactHeader } from ".";
 
+import MockedAnalyticsProvider from "@/__tests__/__helpers__/MockedAnalyticsProvider";
 import { renderWithProvidersByName } from "@/__tests__/__helpers__/renderWithProviders";
 
 const render = renderWithProvidersByName(["oakTheme"]);
@@ -7,12 +8,23 @@ const render = renderWithProvidersByName(["oakTheme"]);
 describe("OaksImpactHeader", () => {
   it("renders correctly", () => {
     const { baseElement, getByRole } = render(
-      <OaksImpactHeader
-        videoDescription={"TEST_VIDEO_DESCRIPTION"}
-        video={undefined}
-        title={"TEST_TITLE"}
-        body={"TEST_BODY"}
-      />,
+      <MockedAnalyticsProvider>
+        <OaksImpactHeader
+          videoDescription={"TEST_VIDEO_DESCRIPTION"}
+          video={{
+            video: {
+              asset: {
+                assetId: "Kx9emKZOPdDNfMB2q202Oksaf7wHHywaXlcG3YURSTEw",
+                playbackId: "VUW02Q7BTn3t11L027yUl9iDWwtOqdMgMyUxmo3O65p00k",
+                thumbTime: 82,
+              },
+            },
+            title: "Test Video",
+          }}
+          title={"TEST_TITLE"}
+          body={"TEST_BODY"}
+        />
+      </MockedAnalyticsProvider>,
     );
     expect(baseElement).toMatchSnapshot();
     expect(getByRole("heading")).toHaveTextContent("TEST_TITLE");
