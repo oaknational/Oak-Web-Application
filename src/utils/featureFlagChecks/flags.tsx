a6b610242fb82a2a63e463fcc6022c2aa4ff7f5a
Cat Miller <cat.miller@thenational.academy>
2026-08-24 14:06:38 +0100

    fix: remove oaks impact flag from FLAGS const in util

--- src/utils/featureFlagChecks/flags.tsx
+++ src/utils/featureFlagChecks/flags.tsx
@@ -1,7 +1,4 @@
 export const FLAGS = {
-  get "oaks-impact"() {
-    return process.env.NEXT_PUBLIC_FORCE_FEATURE_FLAG_OAKS_IMPACT ?? "true";
-  },
   get "implementation-guides"() {
     return (
       process.env.NEXT_PUBLIC_FORCE_FEATURE_FLAG_IMPLEMENTATION_GUIDES ??
