454fefb49eb07e4121e6ccc17b13994f9763262a
Cat Miller <cat.miller@thenational.academy>
2026-08-24 14:13:03 +0100

    fix: add example flag in flags const and use only this in tests to avoid future test changes

--- src/utils/featureFlagChecks/flags.tsx
+++ src/utils/featureFlagChecks/flags.tsx
@@ -1,4 +1,9 @@
 export const FLAGS = {
+  get "example-feature"() {
+    return (
+      process.env.NEXT_PUBLIC_FORCE_FEATURE_FLAG_EXAMPLE_FEATURE ?? "false"
+    );
+  },
   get "implementation-guides"() {
     return (
       process.env.NEXT_PUBLIC_FORCE_FEATURE_FLAG_IMPLEMENTATION_GUIDES ??
