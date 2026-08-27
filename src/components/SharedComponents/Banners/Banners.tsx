8393d8b07c27bf7d7d019ebdcccb44592b01cc06
Alex Cupit <alexcupit@outlook.com>
2026-08-27 15:04:28 +0100

    feat: replace myth busting with oaks impact banner

--- src/components/SharedComponents/Banners/Banners.tsx
+++ src/components/SharedComponents/Banners/Banners.tsx
@@ -1,27 +1,13 @@
-import { OakP, OakSpan } from "@oaknational/oak-components";
-
-import { PromoSpan } from "../PromoSpan/PromoSpan";
-
 import PromoBanner from "@/components/SharedComponents/PromoBanner";
 import { ResolveOakHrefProps } from "@/common-lib/urls";
 
 export default function Banners() {
-  const mythbustingContent = (
-    <OakP $font={"body-2"}>
-      <PromoSpan>
-        <OakSpan $font={"body-3-bold"}>Myths</OakSpan>
-      </PromoSpan>{" "}
-      about teaching <b>can hold you back</b>
-    </OakP>
-  );
-
-  const message = mythbustingContent;
-  const background = "bg-decorative5-very-subdued";
-  const ctaText = "Learn why";
+  const message = "Oak is now used in 72% of schools";
+  const background = "bg-decorative2-main";
+  const ctaText = "Explore our impact";
 
   const linkProps: ResolveOakHrefProps = {
-    page: "campaign-single",
-    campaignSlug: "mythbusting",
+    page: "about-oaks-impact",
   };
 
   return (
