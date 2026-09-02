d4984eb6e50f8115b84251c155f15a144427dcdc
Jamie Blair <info@orangemug.co.uk>
2026-09-02 17:27:26 +0100

    feat: added case study list page behind flag

--- src/pages/about-us/case-studies/index.tsx
+++ src/pages/about-us/case-studies/index.tsx
@@ -6,18 +6,32 @@ import Layout from "@/components/AppComponents/AppLayout";
 import { TopNavProps } from "@/components/AppComponents/TopNav/TopNav";
 import { TeacherBrowseAnalyticsStoreProvider } from "@/context/TeacherBrowseAnalytics/TeacherBrowseAnalyticsProvider";
 import { isFeatureFlagEnabledServer } from "@/utils/featureFlagChecks/server";
+import { OakBox, OakCard, OakFlex } from "@/styles/oakThemeApp";
+import { NewGutterMaxWidth } from "@/components/GenericPagesComponents/NewGutterMaxWidth";
+import CMSClient from "@/node-lib/cms";
+import { OaksImpactCaseStudyListPage } from "@/common-lib/cms-types/aboutPages";
+import getProxiedSanityAssetUrl from "@/common-lib/urls/getProxiedSanityAssetUrl";
+import { resolveOakHref } from "@/common-lib/urls";
 
 export type AboutUsOaksImpactCaseStudyListPageProps = {
-  // pageData: {};
+  pageData: {
+    caseStudies: OaksImpactCaseStudyListPage;
+  };
   topNav: TopNavProps;
 };
 
 export const AboutUsOaksImpactCaseStudyList: NextPage<
   AboutUsOaksImpactCaseStudyListPageProps
-> = ({
-  // pageData: {},
-  topNav,
-}) => {
+> = ({ pageData: { caseStudies }, topNav }) => {
+  const items = caseStudies.map((caseStudy) => ({
+    heading: caseStudy.video.title,
+    href: resolveOakHref({
+      page: "about-case-study",
+      slug: caseStudy.slug.current,
+    }),
+    imageSrc: getProxiedSanityAssetUrl(caseStudy.image?.asset?.url) ?? "",
+  }));
+
   return (
     <TeacherBrowseAnalyticsStoreProvider
       programmeState={null}
@@ -28,7 +42,34 @@ export const AboutUsOaksImpactCaseStudyList: NextPage<
         $background={"bg-primary"}
         topNavProps={topNav}
       >
-        TODO: Case Study list page
+        <OakBox $zIndex={"neutral"} $color={"text-primary"}>
+          <NewGutterMaxWidth>
+            <OakFlex
+              $pv={["spacing-56", "spacing-80", "spacing-80"]}
+              $alignItems="flex-start"
+            >
+              <OakFlex
+                $maxWidth="spacing-960"
+                $gap="spacing-16"
+                $flexDirection="column"
+                $pa={["spacing-16", "spacing-20", "spacing-20"]}
+                $borderRadius="border-radius-l"
+                $background="bg-decorative2-very-subdued"
+              >
+                {items.map((item) => {
+                  return (
+                    <OakCard
+                      key={item.href}
+                      aspectRatio="4/3"
+                      cardOrientation={["column", "row", "row"]}
+                      {...item}
+                    />
+                  );
+                })}
+              </OakFlex>
+            </OakFlex>
+          </NewGutterMaxWidth>
+        </OakBox>
       </Layout>
     </TeacherBrowseAnalyticsStoreProvider>
   );
@@ -45,22 +86,24 @@ export const getServerSideProps: GetServerSideProps<
     return { notFound: true };
   }
 
-  //   const isPreviewMode = context.preview === true;
-  //   const oaksImpactCaseStudyPage = await CMSClient.oaksImpactCaseStudyPage({
-  //     previewMode: isPreviewMode,
-  //   });
+  const isPreviewMode = context.preview === true;
+  const oaksImpactCaseStudyPage = await CMSClient.oaksImpactCaseStudyListPage({
+    previewMode: isPreviewMode,
+  });
 
   const topNav = await curriculumApi2023.topNav();
 
-  //   if (!oaksImpactCaseStudyPage) {
-  //     return {
-  //       notFound: true,
-  //     };
-  //   }
+  if (!oaksImpactCaseStudyPage) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: {
-      // pageData: {},
+      pageData: {
+        caseStudies: oaksImpactCaseStudyPage,
+      },
       topNav,
     },
   };
