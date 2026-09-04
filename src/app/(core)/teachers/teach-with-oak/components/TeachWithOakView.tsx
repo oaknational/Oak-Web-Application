20662fbd7cfbd540d92ff9ab81af97d754f72829
Natassja Blore <natassja.blore@thenational.academy>
2026-09-04 11:04:49 +0100

    feat: add explore link section

--- src/app/(core)/teachers/teach-with-oak/components/TeachWithOakView.tsx
+++ src/app/(core)/teachers/teach-with-oak/components/TeachWithOakView.tsx
@@ -1,5 +1,10 @@
 "use client";
 
+import { resolveOakHref } from "@/common-lib/urls";
+import {
+  ExploreItem,
+  WhoAreWeExplore,
+} from "@/components/GenericPagesComponents/WhoAreWeExplore";
 import { TeachWithOakDescription } from "@/components/TeacherComponents/TeachWithOakDescription/TeachWithOakDescription";
 import { TeachWithOakHeader } from "@/components/TeacherComponents/TeachWithOakHeader/TeachWithOakHeader";
 
@@ -12,6 +17,45 @@ export const TeachWithOakView = ({
     <>
       <TeachWithOakHeader href={backToLessonLink} />
       <TeachWithOakDescription />
+      <WhoAreWeExplore
+        title={"Explore more guidance from Oak"}
+        items={exploreItems}
+      />
     </>
   );
 };
+
+const exploreItems: ExploreItem[] = [
+  {
+    iconName: "homepage-teacher-map",
+    title: "Plan a lesson",
+    href: resolveOakHref({
+      page: "lesson-planning",
+    }),
+    componentType: "about_oak",
+  },
+  {
+    iconName: "data",
+    title: "Blogs",
+    href: resolveOakHref({
+      page: "blog-index",
+    }),
+    componentType: "about_oak",
+  },
+  {
+    iconName: "chatting",
+    title: "Webinars",
+    href: resolveOakHref({
+      page: "webinar-index",
+    }),
+    componentType: "about_oak",
+  },
+  {
+    iconName: "logo",
+    title: "Help",
+    href: resolveOakHref({
+      page: "help",
+    }),
+    componentType: "about_oak",
+  },
+];
