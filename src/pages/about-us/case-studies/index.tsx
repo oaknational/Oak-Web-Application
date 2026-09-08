5997dc95352b9935623795cc61dc2291fcdb9075
Jamie Blair <info@orangemug.co.uk>
2026-09-08 18:07:45 +0100

    feat: added header to case studies list page

--- src/pages/about-us/case-studies/index.tsx
+++ src/pages/about-us/case-studies/index.tsx
@@ -1,4 +1,10 @@
 import { NextPage, GetServerSideProps } from "next";
+import {
+  OakImage,
+  OakBox,
+  OakCard,
+  OakFlex,
+} from "@oaknational/oak-components";
 
 import { getSeoProps } from "@/browser-lib/seo/getSeoProps";
 import curriculumApi2023 from "@/node-lib/curriculum-api-2023";
@@ -6,12 +12,13 @@ import Layout from "@/components/AppComponents/AppLayout";
 import { TopNavProps } from "@/components/AppComponents/TopNav/TopNav";
 import { TeacherBrowseAnalyticsStoreProvider } from "@/context/TeacherBrowseAnalytics/TeacherBrowseAnalyticsProvider";
 import { isFeatureFlagEnabledServer } from "@/utils/featureFlagChecks/server";
-import { OakBox, OakCard, OakFlex } from "@/styles/oakThemeApp";
 import { NewGutterMaxWidth } from "@/components/GenericPagesComponents/NewGutterMaxWidth";
 import CMSClient from "@/node-lib/cms";
 import { OaksImpactCaseStudyListPage } from "@/common-lib/cms-types/aboutPages";
 import getProxiedSanityAssetUrl from "@/common-lib/urls/getProxiedSanityAssetUrl";
 import { resolveOakHref } from "@/common-lib/urls";
+import { AboutSharedHeader } from "@/components/GenericPagesComponents/AboutSharedHeader";
+import { getCloudinaryImageUrl } from "@/utils/getCloudinaryImageUrl";
 
 export type AboutUsOaksImpactCaseStudyListPageProps = {
   pageData: {
@@ -43,6 +50,23 @@ export const AboutUsOaksImpactCaseStudyList: NextPage<
         topNavProps={topNav}
       >
         <OakBox $zIndex={"neutral"} $color={"text-primary"}>
+          <AboutSharedHeader
+            $background="bg-decorative2-main"
+            titleHighlight={"bg-primary"}
+            title="Case studies"
+            content="Explore how schools and trusts are using Oak to tackle their biggest challenges"
+          >
+            <OakImage
+              src={getCloudinaryImageUrl(
+                "v1788884308/647625e5e86caf2c5f0c1e48b25ea1ab6c77c4c8_vzw6ax.png",
+              )}
+              alt=""
+              $height={"spacing-360"}
+              $width={"spacing-360"}
+              $pa={"spacing-0"}
+            />
+          </AboutSharedHeader>
+
           <NewGutterMaxWidth>
             <OakFlex
               $pv={["spacing-56", "spacing-80", "spacing-80"]}
