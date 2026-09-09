c5d3e06ccc4d0f27303fc4379ecf160784f958d1
Jamie Maxwell <Jamie.maxwell@gemstoneit.co.uk>
2026-09-09 19:55:40 +0100

    test: make component regression checks self-contained

--- jest.base.config.js
+++ jest.base.config.js
@@ -34,6 +34,9 @@ module.exports = {
   ],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
+    "^styled-jsx/style$": require.resolve("styled-jsx/style", {
+      paths: [require.resolve("next/package.json")],
+    }),
     // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
     "^uuid$": require.resolve("uuid"),
     "^@oaknational/oak-components$": require.resolve(
