6d0e26c8282829c14c6a297dc7ff5715baee5f75
Jamie Blair <info@orangemug.co.uk>
2026-09-15 13:47:37 +0100

    fix: label always required don't include fallback

--- src/pages/api/curriculum-downloads/index.ts
+++ src/pages/api/curriculum-downloads/index.ts
@@ -348,7 +348,7 @@ export async function getFile({
               examboardTitle: data.combinedCurriculumData?.examboardTitle,
               childSubjectSlug,
               tierSlug,
-              prefix: definition.filenameOverride ?? definition.label ?? type,
+              prefix: definition.filenameOverride ?? definition.label,
               isWithinArchive: data.isWithinArchive,
             });
           },
