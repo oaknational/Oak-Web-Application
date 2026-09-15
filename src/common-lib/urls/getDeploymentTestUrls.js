c3ee7f7bdefdd691d0cc9faec4e7a712d8ed9cc5
Jamie Blair <info@orangemug.co.uk>
2026-09-15 13:26:37 +0100

    fix: re-enable "/curriculum-change-explained/guidance" as it's live

--- src/common-lib/urls/getDeploymentTestUrls.js
+++ src/common-lib/urls/getDeploymentTestUrls.js
@@ -47,7 +47,7 @@ function getDeploymentTestUrls() {
 
     // National Curriculum Insights pages
     // "/teachers/national-curriculum-insights",
-    // "/curriculum-change-explained/guidance",
+    "/curriculum-change-explained/guidance",
     // "/teachers/national-curriculum-insights/science",
     // "/teachers/national-curriculum-insights/science/primary",
     // "/teachers/national-curriculum-insights/science/primary/key-stage-1",
