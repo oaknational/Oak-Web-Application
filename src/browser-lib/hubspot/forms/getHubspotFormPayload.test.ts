cfa805123a8173edd8f0305a32d5642e68664fd7
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2026-09-18 10:13:28 +0100

    test: rm duplicate hubspot form test, add a test for server env

--- src/browser-lib/hubspot/forms/getHubspotFormPayload.test.ts
+++ src/browser-lib/hubspot/forms/getHubspotFormPayload.test.ts
@@ -90,40 +90,6 @@ describe("getHubspotNewsletterFormPayload()", () => {
       },
     });
   });
-  test("page values use the current jsdom location", () => {
-    const result = getHubspotNewsletterPayload({
-      hutk: "hubspotutk value 123",
-      data: {
-        email: "email value",
-        name: "full_name value",
-        userRole: "Student",
-        oakUserId: "oak_user_id value",
-        utm_campaign: "a campaign",
-        utm_content: "some content",
-        utm_medium: "some medium",
-        utm_source: "a source",
-        utm_term: "term",
-      },
-    });
-    expect(result).toEqual({
-      fields: [
-        { name: "email", value: "email value" },
-        { name: "full_name", value: "full_name value" },
-        { name: "latest_utm_campaign", value: "a campaign" },
-        { name: "latest_utm_content", value: "some content" },
-        { name: "latest_utm_medium", value: "some medium" },
-        { name: "latest_utm_source", value: "a source" },
-        { name: "latest_utm_term", value: "term" },
-        { name: "oak_user_id", value: "oak_user_id value" },
-        { name: "user_type", value: "Student" },
-      ],
-      context: {
-        hutk: "hubspotutk value 123",
-        pageUri: "http://localhost/",
-        pageName: "",
-      },
-    });
-  });
 });
 describe("getHubspotDownloadFormPayload()", () => {
   test("primary form payload is correct", () => {
