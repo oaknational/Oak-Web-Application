72dc9805ace462086b5620408a7cf8e9b91200b4
Natassja Blore <natassja.blore@thenational.academy>
2026-09-18 11:45:40 +0100

    fix: add Teach With Oak accessed to send instantly events

--- src/browser-lib/avo/getAvoBridge.ts
+++ src/browser-lib/avo/getAvoBridge.ts
@@ -21,6 +21,7 @@ type AnalyticsServices = {
  */
 const SEND_INSTANTLY_EVENTS: ReadonlySet<string> = new Set<string>([
   "User Onboarding Completed",
+  "Teach With Oak Accessed",
 ]);
 /**
  * getAvoBridge returns the bridge between Avo and our analytics services.
